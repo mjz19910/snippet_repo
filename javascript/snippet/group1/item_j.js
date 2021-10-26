fac2b4d011b17a8fabf886f271a00e9f344f0a02
mjz19910 <matthias291999@gmail.com>
2021-10-26 14:48:57 -0700

    make brackets friendly to chrome bracket matcher

--- javascript/snippet/group1/item_j.js
+++ javascript/snippet/group1/item_j.js
@@ -1,8 +1,8 @@
 x = function() {
 	var fmstr = t.toString();
-	var fms2 = fmstr.slice(0, fstr.indexOf("{"));
-	var fmnb = fmstr.slice(fstr.indexOf("{"));
-	var fmar = fms2.slice(fms2.indexOf("(")).replace(/[()]/g, "").split(",");
+	var fms2 = fmstr.slice(0, fstr.indexOf("{}"[0]));
+	var fmnb = fmstr.slice(fstr.indexOf("{}"[0]));
+	var fmar = fms2.slice(fms2.indexOf("()"[0])).replace(/[()]/g, "").split(",");
 	var idm_mod = fmar[0];
 	var idm_imp = fmar[2];
 	var fnm = fnb.replace(/[{}]/g, ""), idx_load;
@@ -10,14 +10,14 @@ x = function() {
 		idx_load = fnm.slice(11).replace(/[()]/g, "");
 	}
 	var fstr = ars[idx_load].toString();
-	var fs2 = fstr.slice(0, fstr.indexOf("{"));
-	var fnb = fstr.slice(fstr.indexOf("{"));
-	var argsar = fs2.slice(fs2.indexOf("(")).replace(/[()]/g, "").split(",");
+	var fs2 = fstr.slice(0, fstr.indexOf("{}"[0]));
+	var fnb = fstr.slice(fstr.indexOf("{}"[0]));
+	var argsar = fs2.slice(fs2.indexOf("()"[0])).replace(/[()]/g, "").split(",");
 	var id_mod = argsar[0];
 	var id_imp = argsar[2];
 	var fnc = fnb.replace(/[{}]/g, "");
 	var outar = [];
-	fnc.split("(").map(e => e.split(")")).forEach(e => e.forEach(e => outar.push(e)));
+	fnc.split("()"[0]).map(e => e.split("()"[1])).forEach(e => e.forEach(e => outar.push(e)));
 	var outar2 = outar;
 	outar = [];
 	outar2.map(e => e.split(",")).forEach(e => e.forEach(e => outar.push(e)));
