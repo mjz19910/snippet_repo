f230401bdd0d016724f85598b7966216a06ba25c
mjz19910 <matthias291999@gmail.com>
2022-01-23 23:58:24 -0700

    JSSnipGroup3: Update xml_event.js

--- javascript/snippet/group3/xml_event.js
+++ javascript/snippet/group3/xml_event.js
@@ -1,26 +1,27 @@
 function split_tags(s) {
-	var t = "";
-	var res = [];
-	while(s.length > 0) {
-		if(s[0] == "<") {
-			if(t != "") {
-				res.push([1, t]);
-				t = "";
+	var t = ""
+	var res = []
+	while (s.length > 0) {
+		if (s[0] == "<") {
+			if (t != "") {
+				res.push([1, t])
+				t = ""
 			}
-			s = s.slice(1);
-			while(s[0] != ">") {
-				t += s[0];
-				s = s.slice(1);
+			s = s.slice(1)
+			while (s[0] != ">") {
+				t += s[0]
+				s = s.slice(1)
 			}
-			res.push([0, t]);
-			t = "";
+			res.push([0, t])
+			t = ""
 		} else {
-			t = t + s[0];
+			t = t + s[0]
 		}
-		s = s.slice(1);
+		s = s.slice(1)
 	}
-	return res;
+	return res
 }
-ar = split_tags(document.documentElement.innerHTML);
-ar2 = ar.slice(ar.map((e, n) => [e[1], e[0], n]).filter(e => e[0].match("class=\"text"))[0][2]).map((e, n) => [e[1], e[0], n]);
-target = ar2[ar2.find(e => e[0].match("greentext"))[2] + 2][0].split(";").pop();
+ar = split_tags(document.documentElement.innerHTML)
+ar2 = ar.slice(ar.map((e,n)=>[e[1], e[0], n]).filter(e=>e[0].match("class=\"text"))[0][2]).map((e,n)=>[e[1], e[0], n])
+//cspell:ignore greentext
+target = ar2[ar2.find(e=>e[0].match("greentext"))[2] + 2][0].split(";").pop()
