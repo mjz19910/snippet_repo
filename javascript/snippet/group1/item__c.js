52343f87287af95a57be006071339dd396e82607
mjz19910 <matthias291999@gmail.com>
2022-01-23 22:32:56 -0700

    JSSnipGroup1: Update item c

--- javascript/snippet/group1/item__c.js
+++ javascript/snippet/group1/item__c.js
@@ -6,7 +6,7 @@ var z = function(fn, cbfn, ex) {
 	fn.wait = function(dl) {
 		fn.dl = dl;
 		return new Promise(wt);
-	};
+	}
 	var rng = Math.random();
 	window.postMessage(rng);
 	var nc = (new class {
@@ -18,19 +18,19 @@ var z = function(fn, cbfn, ex) {
 		}
 		reset(ex) {
 			var t = this;
-			if(t.ru) {
+			if (t.ru) {
 				t.p = fn(fn);
-				if(t.promise_result) {
+				if (t.promise_result) {
 					t.promise_result();
 				}
-			} else if(ex) {
+			} else if (ex) {
 				t.ru = 1;
 				ex.p.then(function() {
-					if(t.precreate) {
+					if (t.precreate) {
 						t.precreate();
 					}
 					t.p = fn(fn);
-					if(t.promise_result) {
+					if (t.promise_result) {
 						t.promise_result();
 					}
 				});
@@ -38,11 +38,11 @@ var z = function(fn, cbfn, ex) {
 				t.ru = 1;
 				t.p = Promise.resolve(t);
 				t.p.then(function() {
-					if(t.precreate) {
+					if (t.precreate) {
 						t.precreate();
 					}
 					t.p = fn(fn);
-					if(t.promise_result) {
+					if (t.promise_result) {
 						t.promise_result();
 					}
 				});
@@ -52,7 +52,8 @@ var z = function(fn, cbfn, ex) {
 		ru = 0;
 		precreate = null;
 		promise_result = null;
-	});
+	}
+	);
 	Object.defineProperty(fn, "run", {
 		get: function() {
 			return nc.ru;
@@ -64,21 +65,26 @@ var z = function(fn, cbfn, ex) {
 		enumerable: true
 	});
 	fn.o = nc;
-	fn.timeout = function() {nc.ru = 0; nc.p.then(e => console.log("timeout done"));};
+	fn.timeout = function() {
+		nc.ru = 0;
+		nc.p.then(e=>console.log("timeout done"));
+	}
 	var mlis = function(e) {
-		if(e.data === rng) {return; }
+		if (e.data === rng) {
+			return;
+		}
 		window.removeEventListener("message", mlis);
 		nc.ru = 0;
 	};
-	nc.precreate = e => window.addEventListener("message", mlis);
+	nc.precreate = e=>window.addEventListener("message", mlis);
 	nc.promise_result = cbfn;
 	var pr;
-	if(ex) {
+	if (ex) {
 		nc.reset(ex);
 	} else {
 		nc.reset();
-	};
-	return nc;
+	}
+	;return nc;
 };
 var b = async function(fn) {
 	p1();
@@ -93,17 +99,18 @@ var b = async function(fn) {
 	await fn.wait(80);
 	p1();
 	await fn.wait(50);
-	for(var i = 0; i < 30; i++)buyRu3();
+	for (var i = 0; i < 30; i++)
+		buyRu3();
 	await fn.wait(50);
-	if(fn.run) {
+	if (fn.run) {
 		fn.o.reset();
 	}
 	return;
 };
 var promise_result = function() {
-	a.p.catch(e => delete a);
+	a.p.catch(e=>delete a);
 };
-if(typeof a == "undefined") {
+if (typeof a == "undefined") {
 	a = z(b, promise_result);
 } else {
 	a = z(b, promise_result, a);
