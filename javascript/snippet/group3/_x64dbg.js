44d1913f0f46690b28d3b9dee40438ebbaae313a
mjz19910 <matthias291999@gmail.com>
2022-01-23 23:06:16 -0700

    JSSnipGroup3: Update _x64dbg.js

--- javascript/snippet/group3/_x64dbg.js
+++ javascript/snippet/group3/_x64dbg.js
@@ -1,36 +1,39 @@
-done = 0;
-unkobj = {};
-console.t = {};
+done = 0
+let unk_obj = {}
+console.t = {}
 var res = 0;
-crashfn = function(e) {
-    class ListInfo {
-    }
-    //SWIG
-    class SwigPyIterator {
-    }
-    function ListInfo_swigregister() {}
-    function SwigPyIterator_swigregister() {}
-    class ThreadVector {
-    }
-    function ThreadVector_swigregister() {}
-    function GetThreadInfoList() {
-        _.getThreadInfoList(1);
-    }
-    function BridgeInit() {}
-    function BridgeStart() {}
-    function BridgeAlloc(size) {}
-    function BridgeFree(ptr) {}
-    function BridgeSettingGet(section, key, value) {}
-    function BridgeSettingGetUint(section, key, value) {}
-    function BridgeSettingSet(section, key, value) {}
-    function BridgeSettingSetUint(section, key, value) {}
-    function BridgeSettingFlush() {}
-    function BridgeSettingRead(errorLine) {}
-    function BridgeGetDbgVersion() {}
-    e`MAX_SETTING_SIZE``DBG_VERSION``Bridge[Init,Start,Alloc,Free,GetDbgVersion]``BridgeSetting[Get,GetUint,Set,SetUint,Flush,Read]``MAX_[LABEL,COMMENT,MODULE,IMPORT,BREAKPOINT,CONDITIONAL_[EXPR,TEXT],SCRIPT_LINE,THREAD_NAME,WATCH_NAME,STRING,ERROR,SECTION,COMMAND_LINE,MNEMONIC]._SIZE``PAGE_SIZE``[initialized,paused,running,stopped]``SEG_[DEFAULT,ES,DS,FS,GS,CS,SS]``flag[module,label,comment,bookmark,$function,loop,args,NoFuncOffset]``bp_[none,normal,hardware,memory,dll,exception]``FUNC_[NONE,BEGIN,MIDDLE,END,SINGLE]``LOOP_[NONE,BEGIN,MIDDLE,ENTRY,END,SINGLE]``XREF_[NONE,DATA,JMP,CALL]``ARG_[NONE,BEGIN,MIDDLE,END,SINGLE]`;
+function create_dbg_api(e) {
+	class ListInfo {
+	}
+	//SWIG
+	class SwigPyIterator {
+	}
+	//cspell:disable
+	function ListInfo_swigregister() {}
+	function SwigPyIterator_swigregister() {}
+	class ThreadVector {
+	}
+	function ThreadVector_swigregister() {}
+	function GetThreadInfoList() {
+		_.getThreadInfoList(1)
+	}
+	//cspell:enable
+	function BridgeInit() {}
+	function BridgeStart() {}
+	function BridgeAlloc(size) {}
+	function BridgeFree(ptr) {}
+	function BridgeSettingGet(section, key, value) {}
+	function BridgeSettingGetUint(section, key, value) {}
+	function BridgeSettingSet(section, key, value) {}
+	function BridgeSettingSetUint(section, key, value) {}
+	function BridgeSettingFlush() {}
+	function BridgeSettingRead(errorLine) {}
+	function BridgeGetDbgVersion() {}
+	e`MAX_SETTING_SIZE``DBG_VERSION``Bridge[Init,Start,Alloc,Free,GetDbgVersion]``BridgeSetting[Get,GetUint,Set,SetUint,Flush,Read]``MAX_[LABEL,COMMENT,MODULE,IMPORT,BREAKPOINT,CONDITIONAL_[EXPR,TEXT],SCRIPT_LINE,THREAD_NAME,WATCH_NAME,STRING,ERROR,SECTION,COMMAND_LINE,MNEMONIC]._SIZE``PAGE_SIZE``[initialized,paused,running,stopped]``SEG_[DEFAULT,ES,DS,FS,GS,CS,SS]``flag[module,label,comment,bookmark,$function,loop,args,NoFuncOffset]``bp_[none,normal,hardware,memory,dll,exception]``FUNC_[NONE,BEGIN,MIDDLE,END,SINGLE]``LOOP_[NONE,BEGIN,MIDDLE,ENTRY,END,SINGLE]``XREF_[NONE,DATA,JMP,CALL]``ARG_[NONE,BEGIN,MIDDLE,END,SINGLE]`
 
-    "DBG_ ARRAY START";
-    e`DBG_[SCRIPT_LOAD[UNLOAD,RUN,STEP,BPTOGGLE,BPGET,CMDEXEC,ABORT,GETLINETYPE,SETIP,GETBRANCHINFO],SYMBOL_ENUM,ASSEMBLE_AT,
+	"DBG_ ARRAY START";
+	//cspell:disable
+	e`DBG_[SCRIPT_LOAD[UNLOAD,RUN,STEP,BPTOGGLE,BPGET,CMDEXEC,ABORT,GETLINETYPE,SETIP,GETBRANCHINFO],SYMBOL_ENUM,ASSEMBLE_AT,
 MODBASE_FROM_NAME,DISASM_AT,STACK_COMMENT_GET,SETTINGS_UPDATED,DISASM_FAST_AT,MENU_ENTRY_CLICKED,
 FUNCTION_[GET,OVERLAPS,ADD,DEL],
 SET_[AUTO_COMMENT,AUTO_LABEL,AUTO_BOOKMARK,AUTO_FUNCTION]._AT,
@@ -42,8 +45,8 @@ ENCODE_[TYPE,SIZE]._GET,
 ARGUMENT_[GET,OVERLAPS,ADD,DEL],SELCHANGED,
 GET_[THREAD_LIST,TIME_WASTED_COUNTER,STRING_AT,FUNCTIONS,XREF_COUNT_AT,XREF_TYPE_AT,
 WATCH_LIST,PROCESS_HANDLE,THREAD_HANDLE,PROCESS_ID,THREAD_ID,[PEB,TEB]._ADDRESS],
-ANALYZE_FUNCTION,MENU_PREPARE]`;
-    e`
+ANALYZE_FUNCTION,MENU_PREPARE]`
+	e`
 linecommand
 linebranch
 linelabel
@@ -142,135 +145,137 @@ ex_secondchance
 ex_all``hw_byte
 hw_word
 hw_dword
-hw_qword`;
-    return function(e) {
-        return eval(e);
-    };
-};
-while(!done) {
-    try {
-        res = crashfn((function() {
-            var fn = function(e) {
-                if(e[0].indexOf("\n") > -1) {
-                    var ar = e[0].split("\n");
-                    var dist = 1;
-                    var results = [];
-                    ar.forEach(e => {
-                        if(results.indexOf(e.slice(0, dist)) == -1) {
-                            results.push(e);
-                        }
-                    }
-                    );
-                    console.log(results);
-                } else {
-                    console.log(e);
-                }
-                return fn;
-            };
-            return fn;
-        }
-        )());
-        done = 1;
-    } catch(e) {
-        addmessage = function(e) {
-            if(e.message.indexOf("' of undefined") > -1) {
-                var unqname = e.message.match(/.+'(.+)'.+/)[1];
-                if(typeof unkobj[unkobj] == "undefined")
-                    unkobj[unkobj] = {};
-                Object.defineProperty(unkobj[unkobj], unqname, {
-                    get: function() {
-                        console.t = +"." + unqname;
-                        console.log(unqname);
-                        return JSON.parse(`{"${unqname}":{}}`);
-                    },
-                    set: function() {}
-                });
-                done = 0;
-            }
-        };
-        adddef = function() {
-            if(e.message.indexOf(" is not defined") > -1) {
-                var unqobjt = e.message.slice(0, e.message.indexOf(" is not defined"));
-                Object.defineProperty(window, unqobjt, {
-                    get: function() {
-                        console.log(console.t);
-                        console.t = unqobjt;
-                        return unkobj;
-                    },
-                    set: function() {}
-                });
-            } else {
-                done = 1;
-            }
-        };
-        adddef(e);
-        addmessage(e);
-        console.log(e);
-        t = e;
-    }
+hw_qword`
+	//cspell:enable
+	return function(e) {
+		return eval(e)
+	}
+}
+while (!done) {
+	try {
+		res = create_dbg_api((function() {
+			var fn = function(e) {
+				if (e[0].indexOf("\n") > -1) {
+					var ar = e[0].split("\n")
+					var dist = 1
+					var results = []
+					ar.forEach(e=>{
+						if (results.indexOf(e.slice(0, dist)) == -1) {
+							results.push(e)
+						}
+					}
+					)
+					console.log(results)
+				} else {
+					console.log(e)
+				}
+				return fn
+			};
+			return fn
+		}
+		)())
+		done = 1
+	} catch (e) {
+		function add_message(e) {
+			if (e.message.indexOf("' of undefined") > -1) {
+				var unq_name = e.message.match(/.+'(.+)'.+/)[1]
+				if (typeof unk_obj[unk_obj] == "undefined")
+					unk_obj[unk_obj] = {}
+				Object.defineProperty(unk_obj[unk_obj], unq_name, {
+					get: function() {
+						console.t = +"." + unq_name;
+						console.log(unq_name);
+						return JSON.parse(`{"${unq_name}":{}}`)
+					},
+					set: function() {}
+				})
+				done = 0
+			}
+		}
+		function add_def() {
+			if (e.message.indexOf(" is not defined") > -1) {
+				var unq_obj_t = e.message.slice(0, e.message.indexOf(" is not defined"))
+				Object.defineProperty(window, unq_obj_t, {
+					get: function() {
+						console.log(console.t);
+						console.t = unq_obj_t;
+						return unk_obj
+					},
+					set: function() {}
+				})
+			} else {
+				done = 1
+			}
+		}
+		add_def(e)
+		add_message(e)
+		console.log(e);
+		t = e
+	}
 }
 (function() {
-    var logger = function() {
-        var ar = Array.from(Object.keys(Object.getOwnPropertyDescriptors(window)));
-        var ard = ar.indexOf("done");
-        console.log(ar.slice(ard));
-    };
-    var waitforapi = function(cnt) {
-        var ar = Object.getOwnPropertyNames(window);
-        var ard = ar.indexOf("done");
-        console.before = ar.slice(ard);
-        if(console.notify_get) {
-            var result = console.notify_get();
-            console.log("consoleapi in:" + cnt);
-            console.log(result);
-            return;
-        }
-        setTimeout(waitforapi, 50, cnt + 1);
-    };
-    var ar = Object.getOwnPropertyNames(window);
-    var ar2 = ar;
-    var ard = ar.indexOf("done");
-    var val = ar.slice(ard);
-    var capi;
-    var gcapi;
-    var scapi;
-    console.log(val);
-    if(val.indexOf("$_") > -1) {
-        capi = {};
-        gcapi = {};
-        scapi = {};
-        var capn = val.slice(3);
-        console.log(capn.length);
-        for(var i of capn) {
-            var dsc = Object.getOwnPropertyDescriptor(window, i)
-                , gs = 0;
-            if(dsc.get) {
-                gcapi[i] = dsc.get;
-                gs = 1;
-            }
-            if(dsc.set) {
-                scapi[i] = dsc.set;
-                gs = 1;
-            }
-            if(!gs) {
-                capi[i] = dsc.value;
-            }
-        }
-    }
-    if(console.before) {
-        console.notify_get = function() {
-            var ar = Object.getOwnPropertyNames(window);
-            var ard = ar.indexOf("done");
-            var ary = ar2.indexOf("done");
-            var val = ar.slice(ard);
-            return [val, {
-                n: capi,
-                g: gcapi,
-                s: scapi
-            }];
-        };
-    } else {
-        waitforapi(1);
-    }
+	var logger = function() {
+		var ar = Array.from(Object.keys(Object.getOwnPropertyDescriptors(window)))
+		var ard = ar.indexOf("done")
+		console.log(ar.slice(ard))
+	}
+	var wait_for_api = function(cnt) {
+		var ar = Object.getOwnPropertyNames(window)
+		var ard = ar.indexOf("done")
+		console.before = ar.slice(ard)
+		if (console.notify_get) {
+			var result = console.notify_get();
+			console.log("console_api in:" + cnt)
+			console.log(result)
+			return
+		}
+		setTimeout(wait_for_api, 50, cnt + 1)
+	}
+	var ar = Object.getOwnPropertyNames(window)
+	var ar2 = ar
+	var ard = ar.indexOf("done")
+	var val = ar.slice(ard)
+	var c_api
+	var gc_api
+	var sc_api
+	console.log(val)
+	if (val.indexOf("$_") > -1) {
+		c_api = {};
+		gc_api = {};
+		sc_api = {}
+		var cap_n = val.slice(3);
+		console.log(cap_n.length)
+		for (var i of cap_n) {
+			var dsc = Object.getOwnPropertyDescriptor(window, i)
+			  , gs = 0
+			if (dsc.get) {
+				gc_api[i] = dsc.get
+				gs = 1
+			}
+			if (dsc.set) {
+				sc_api[i] = dsc.set
+				gs = 1
+			}
+			if (!gs) {
+				c_api[i] = dsc.value
+			}
+		}
+	}
+	if (console.before) {
+		console.notify_get = function() {
+			var ar = Object.getOwnPropertyNames(window)
+			var ard = ar.indexOf("done")
+			var ary = ar2.indexOf("done");
+			void ary;
+			var val = ar.slice(ard)
+			return [val, {
+				n: c_api,
+				g: gc_api,
+				s: sc_api
+			}];
+		}
+	} else {
+		wait_for_api(1)
+	}
 }
-)();
+)()
