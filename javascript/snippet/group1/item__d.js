a3e22ee5cfcec07649adc781a4e7bcc84a9ee6c9
mjz19910 <matthias291999@gmail.com>
2022-01-23 22:33:51 -0700

    JSSnipGroup1: Update item d

--- javascript/snippet/group1/item__d.js
+++ javascript/snippet/group1/item__d.js
@@ -1,10 +1,14 @@
-if(window.cint) {clearTimeout(cint);}
-if(window.cint2) {clearInterval(cint2);}
+if (window.cint) {
+	clearTimeout(cint);
+}
+if (window.cint2) {
+	clearInterval(cint2);
+}
 cint = setTimeout(function() {
 	cint2 = setInterval(maxAll, 17 * 1000);
 	//setTimeout(pointReset,200);
 	setTimeout(maxAll, 400);
-	reset_func = (e, dl) => {
+	reset_func = (e,dl)=>{
 		var c = {
 			i: 0,
 			d: dl,
@@ -13,29 +17,29 @@ cint = setTimeout(function() {
 		};
 		e.nr = function() {
 			pointReset();
-			if(c.j < 8) {
+			if (c.j < 8) {
 				c.j++;
 				c.i = 0;
 				c.s = 0;
 				cint2 = setInterval(maxAll, 17 * 1000);
 				e(e, c);
 			}
-		};
+		}
 		e(e, c);
-	};
-	loop_func = (rep, c) => {
+	}
+	loop_func = (rep,c)=>{
 		maxAll();
 		var dsx = 300;
-		if(c.i < (3600 * 2 - dsx)) {
-			if(c.s == 0 && c.i > 60) {
+		if (c.i < (3600 * 2 - dsx)) {
+			if (c.s == 0 && c.i > 60) {
 				c.d = 200;
 				c.s = 1;
 			}
-			if(c.s == 1 && c.i > 700) {
+			if (c.s == 1 && c.i > 700) {
 				c.d = 2 * 1000;
 				c.s = 2;
 			}
-			if(c.s == 2 && c.i > 1800) {
+			if (c.s == 2 && c.i > 1800) {
 				c.s = 3;
 				c.d = 10 * 1000;
 				clearInterval(cint2);
@@ -46,14 +50,18 @@ cint = setTimeout(function() {
 			console.log("min:" + dsx / 60, Math.random());
 			cint = setTimeout(rep.nr, dsx * 1000);
 		}
-	};
-	cint = setTimeout(e => {
+	}
+	cint = setTimeout(e=>{
 		var ds = performance.now();
 		e(e, ds);
 	}
-		, 600, function() {
-			console.log(player.tier1.cost.logarithm / player.points.logarithm);
-			reset_func(loop_func, 100);
-		});
-}
-	, 2 * 1000);
+	, 600, function(rc, ds) {
+		var dr, dl;
+		void rc,
+		ds,
+		dr,
+		dl;
+		console.log(player.tier1.cost.logarithm / player.points.logarithm);
+		reset_func(loop_func, 100);
+	});
+}, 2 * 1000);
