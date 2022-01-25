224458766ba88f99240df17b6ec8bf884a6f1629
mjz19910 <matthias291999@gmail.com>
2022-01-25 11:36:24 -0700

    JSSnipGroup1+JSFinal: Add a version to final myhtml_tokenizer and item 1

--- javascript/final/myhtml_tokenizer.js
+++ javascript/final/myhtml_tokenizer.js
@@ -1,4 +1,7 @@
-// cspell:words myhtml rcdata rawtext
+/* cspell:words myhtml_tokenizer rcdata rawtext
+--- version_list item 1 ---
+v1 (loc-s): snippet_repo_v2/javascript/final/myhtml_tokenizer.js
+*/
 `
 // ***********
     // for ends
---
+++ javascript/snippet/group1/sub_a/item-_1.js
@@ -0,0 +1,5 @@
+/* spell:words myhtml_tokenizer
+--- version_list item 1 ---
+v1 (old-o): snippet_repo_v2/javascript/final/myhtml_tokenizer.js
+v2 (cur-c): snippet_repo_v2/javascript/snippet/group1/sub_a/item-_1.js
+*/
