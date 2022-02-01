58214978d2da8bc5dcb7a56c1a9be67324e81109
mjz19910 <matthias291999@gmail.com>
2022-02-01 10:04:48 -0700

    JSSnipGroup1: Update item 3

--- javascript/snippet/group1/sub_a/item-_3.js
+++ javascript/snippet/group1/sub_a/item-_3.js
@@ -5,9 +5,9 @@ v2 (cur-c): snippet_repo_v2/javascript/snippet/group1/sub_a/item-_3.js
 --- version_list end ---
 this snippet requires g_error_stack to be set
 */
-if(!window.src_str){
-	window.src_str_response=await fetch.__proxy_target__.call(window, g_error_stack.split("\n").slice(3)[0].split(/(?:\s+)(.+)/, 2)[1].split("at ")[1].split(/(.+?)\((.+)\)/)[2].split(/(.+):(.+?):(.+?)$/)[1]);
-	window.src_str=await src_str_response.text();
+if(!window.src_str_lines){
+	window.src_str_response=await fetch.__proxied_target__.call(window, g_error_stack.split("\n").slice(3)[0].split(/(?:\s+)(.+)/, 2)[1].split("at ")[1].split(/(.+?)\((.+)\)/)[2].split(/(.+):(.+?):(.+?)$/)[1]);
+	window.src_str_lines=(await src_str_response.text()).split("\n");
 }
 function code_after_line_location_from_lines(lines, url_str){
 	let [,line, col]=url_str.match(/(?:.+):(.+?):(.+?)$/);
@@ -26,3 +26,4 @@ let source_location;
 let bad_loc=get_location_from_error_stack_lines(g_error_stack_lines, 3+4);
 source_location=get_location_from_error_stack_lines(g_error_stack_lines, 3+3);
 let last_good_loc=code_after_line_location_from_lines(src_str_lines, source_location);
+last_good_loc;
