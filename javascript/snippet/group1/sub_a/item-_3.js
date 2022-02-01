aa7e0a986b184854d9481c293c6cbe9bc15cd84a
mjz19910 <matthias291999@gmail.com>
2022-02-01 09:46:56 -0700

    JSSnipGroup1: Update item 3 (devtools bundle and supernova)

--- javascript/snippet/group1/sub_a/item-_3.js
+++ javascript/snippet/group1/sub_a/item-_3.js
@@ -1,17 +1,22 @@
+/* spell:words
+--- version_list item 3 ---
+v1 (cur-c): snippet_repo_v2/javascript/snippet/group1/sub_a/item-_3.js
+*/
 (function() {
+	const open_or_space_match = RegExp('[\\s]' + "{}"[0], 'g');
+	void open_or_space_match;
+	let does_return = true;
 	undebug = undebug;
 	debug = debug;
 	async function xxx(fn, cb) {
 		debug.f_i = debug.f.size;
 		debug.f.set(debug.f_i, fn);
 		debug(debug.f, 'try{debug.g[' + debug.f_i + '](function(e){return eval(e)},[this,...arguments])}catch(e){console.log(e)};0;');
-		let x = {};
-		x.cid = debug.f_i;
-		x.Y = function() {}
-		let w = new Promise(e=>{
-			x.e = e;
-		}
-		);
+		let x = {
+			Y() {},
+			cid: debug.f_i
+		};
+		let w = new Promise(e=>x.e = e);
 		if (debug.g instanceof Function) {
 			delete debug.g;
 		}
@@ -20,24 +25,21 @@
 		cb.scope = x;
 		let cur = cb;
 		debug.g[debug.f_i] = cur;
-		let o = new Promise(e=>{
-			setTimeout(e, 12000, null);
-		}
-		);
+		let o = new Promise(e=>setTimeout(e, 12000, null));
 		console.log('pre_race');
 		let r = await Promise.race([w, o, debug.q]);
 		if (r == null) {
 			debug.g[x.cid](null);
 			return r;
 		}
-		if (this instanceof DedicatedWorkerGlobalScope) {
-			this.res = r;
+		if (globalThis instanceof DedicatedWorkerGlobalScope) {
+			globalThis.res = r;
 		} else {
 			debugger ;
 		}
 		return r;
 	}
-	let rf = async function(done_callback) {
+	async function rf(done_callback) {
 		if (debug.f instanceof Map && debug.f.size > 0) {
 			return debug.f;
 		} else {
@@ -61,7 +63,7 @@
 			debug.q = new Promise(e=>debug.c = e);
 			debug.q.then(()=>delete debug.q);
 		}
-		let cb = function(e, args) {
+		function cb(e, args) {
 			let x = cb.scope;
 			x.get_func = e;
 			if (e === null) {
@@ -70,20 +72,18 @@
 				return;
 			}
 			if (args[0].toString().indexOf('native') == -1) {
-				/*undebug(debug.f)*/
 				if (args[0].name == 'it') {
 					x.Y(args[0]);
 				}
 				x.e([e, args]);
 			}
 			console.log(args.length, args[0].name);
-		};
+		}
 		return done_callback(Function.prototype.call, cb);
-	};
-	/**@param {CallableFunction} func */
+	}
+	/**@arg {CallableFunction} func@arg {string} src_url_js */
 	function make_async_function(func, src_url_js) {
-		let rc = "(" + (func).toString() + ")";
-		/**@type {(typeof func)["prototype"]} */
+		let rc = "(" + func.toString() + ")";
 		let func_prototype = Object.getPrototypeOf(func);
 		/**@type {typeof func} */
 		let func_constructor = func_prototype.constructor;
@@ -93,107 +93,153 @@
 		let re_func_str_1 = re_func_str_0.split(/\s+/g);
 		let base_function_def = re_func_str_1.join(" ");
 		let is_async = base_function_def.match('async') !== null;
-		let generates = base_function_def.match(/\*/) !== null;
-		let g = (e,...x)=>{
+		let is_generator = base_function_def.match('\\*') !== null;
+		void is_generator;
+		function g(e, ...x) {
 			for (var g = x.length, w = e.raw, s = ''; g; ) {
 				s = x.pop() + w[g--] + s;
 			}
 			return w[0] + s;
 		}
-		///(?<=\(async function\()((?:\s*(\w+)\s*)?(?:,\s*(\w+)\s*)*)(?=\)\s*(?=\{))/
-		let match_args = new RegExp(g`(?<=\(${is_async ? 'async ' : g`\s?`}function${generates ? g`\s?\*\s?` : g`\s?`}\()((?:\s*\w+\s*)?(?:,\s*\w+\s*)*)(?=\)\s*(?=\{))`);
-		console.log(match_args);
-		let j = rc.match(match_args);
-		if (j)
-			j = j[0];
-		console.log(j);
-		var AsyncFunction = func_constructor;
-		let e, m, x = 0;
-		e = rc.matchAll(/[\s{]/g);
-		do {
-			m = e.next();
-			x++;
-		} while (!m.done && m.value == ' ');
-		let tx_regex = /.+\n/;
-		let q = 0;
-		e = j.matchAll(/[\s{]/g);
-		do {
-			m = e.next();
-			q++;
-		} while (!m.done && m.value == ' ');
-		x = x - q;
-		let func_out = rc.slice(x + is_async * 5 + generates * 1 + 12 + j.length, rc.lastIndexOf('}'));
-		let slice_me = func_out.slice(0, func_out.match(tx_regex)[0].length - func_out.trim().match(tx_regex)[0].length);
-		let vp = 0, r;
+		void g;
+		let i = 0;
+		let rev_i = -1;
+		if (rc[i] == '()'[0]) {
+			i++;
+		}
+		if (rc.at(rev_i) == '()'[1]) {
+			rev_i--;
+		}
+		let word_regex = /\w+/g;
+		let ws_regex = /\s+/g;
+		if (is_async && word_regex.exec(rc)[0] == "async") {
+			let ws_len = ws_regex.exec(rc);
+			console.assert(ws_len.index == word_regex.lastIndex);
+			i += "async".length + 1;
+		}
+		if (word_regex.exec(rc)[0] == "function") {
+			let ws_len = ws_regex.exec(rc);
+			console.assert(ws_len.index == word_regex.lastIndex);
+			i += "function".length + 1;
+		}
+		let fn_name = word_regex.exec(rc)[0];
+		i += fn_name.length;
+		console.assert(rc[word_regex.lastIndex] == '()'[0]);
+		word_regex.lastIndex++;
+		let fn_args = [];
+		let start;
+		while (rc[word_regex.lastIndex] != "()"[1]) {
+			if (rc[word_regex.lastIndex] == ",") {
+				word_regex.lastIndex++;
+			}
+			if (rc[word_regex.lastIndex] == " ") {
+				word_regex.lastIndex++;
+			}
+			start = word_regex.lastIndex;
+			let mat = word_regex.exec(rc)[0];
+			console.assert(rc.slice(start, word_regex.lastIndex) == mat);
+			start = word_regex.lastIndex;
+			fn_args.push(mat);
+			i += mat.length;
+		}
+		i = word_regex.lastIndex;
+		console.assert(rc[word_regex.lastIndex] == '()'[1]);
+		i++;
+		while (rc[i] == ' ' || rc[i] == '\t') {
+			i++;
+		}
+		if (rc[i] == "{}"[0]) {
+			i++;
+		}
+		if (rc[i] == "\n") {
+			i++;
+		}
+		if (rc.at(rev_i) == "{}"[1]) {
+			rev_i--;
+		}
+		if (rc.at(rev_i) == "\n") {
+			rev_i--;
+		}
+		if (rev_i != -1) {
+			rev_i++;
+		}
+		let j = rc.slice(i, rev_i);
+		let tx_regex = /^.+\n/;
+		function get_padding_len(str) {
+			return str.match(tx_regex)[0].length;
+		}
+		let func_out = j.trimEnd();
+		console.log(func_out);
+		let slice_me = func_out.slice(0, get_padding_len(func_out) - get_padding_len(func_out.trim()));
+		let vp = 0;
 		if (slice_me)
 			vp = slice_me.split('').map(e=>e == '\t' ? 4 : 1).reduce((a,b)=>a + b);
-		let no_pad = function(e, d) {
-			var x = e[0];
-			var a = x == '\t';
-			var b = x == ' ';
-			if (d == 0) {
+		function no_pad(e, d) {
+			if (d == 0)
 				return e;
-			}
-			if (x == ' ' || x == '\t') {
-				return no_pad(e.slice(1), d - (a * 4 + b));
-			} else {
-				return e;
-			}
-		};
-		func_out = func_out.split("\n").map(e=>{
-			if (vp == 0) {
-				return e;
-			}
-			r = no_pad(e, vp - 1);
-			return r;
+			let x, i = 0;
+			do {
+				x = e[i];
+				if (x == '\t')
+					d -= 4;
+				else if (x == ' ')
+					d -= 1;
+				else
+					break;
+				i++;
+			} while (d > 0);
+			return e.slice(i);
 		}
-		).join('\n');
-		return AsyncFunction(j, func_out + "\n//" + "# sourceURL=" + src_url_js);
+		function source_url_comment(url) {
+			return `//# sourceURL=${url}`;
+		}
+		let code_str = func_out.split("\n").map(e=>no_pad(e, vp)).join("\n") + "\n";
+		return func_constructor(fn_args.join(","), code_str + source_url_comment(src_url_js));
 	}
 	let src_url_func = make_async_function(rf, "devtools://devtools/bundled/acorn._debug.rf.js");
 	let res_func_1 = make_async_function(xxx, "devtools://devtools/bundled/acorn._debug.xxx.js");
+	if (does_return)
+		return;
 	let res_func_2 = src_url_func(res_func_1);
-	let does_return = true;
 	if (does_return)
 		return res_func_2;
 	if (typeof cint != "undefined") {
 		clearTimeout(cint);
 	}
-	var nxfn;
-	var presfn = function() {
+	function presfn(some_function) {
 		if (getPrestigePower(player.stars).div(player.prestigePower).toNumber() > 100) {
 			checkToReset(1);
 			if (player.stars.toNumber() == 10) {
 				console.log("r1", player.supernovaPlaytime.toFixed(3));
 			}
-			nxfn();
+			some_function(presfn);
 		} else if (showTooMuch) {
 			console.log("r3");
 			reset(3);
-			nxfn();
+			some_function(presfn);
 		} else {
 			maxAll();
-			nxfn();
+			some_function(presfn);
 		}
-	};
-	nxfn = function() {
+	}
+	function nxfn(other_function) {
 		if (!window.hasOwnProperty("player")) {
-			cint = setTimeout(nxfn, 70);
+			cint = setTimeout(nxfn, 70, other_function);
 			return;
 		}
-		//console.log("upg");
 		if (player.stars.log(10) < 90) {
-			cint = setTimeout(presfn, 70);
+			cint = setTimeout(other_function, 70, nxfn);
 		} else if (player.stars.log(10) < 250) {
-			cint = setTimeout(presfn, 70);
+			cint = setTimeout(other_function, 70, nxfn);
 		} else if (player.stars.log(10) < 280) {
-			cint = setTimeout(presfn, 70);
+			cint = setTimeout(other_function, 70, nxfn);
 		} else {
-			cint = setTimeout(presfn, 70);
+			cint = setTimeout(other_function, 70, nxfn);
 		}
-
 	}
-	nxfn();
+	void nxfn;
+	void presfn;
+	//nxfn(presfn)
 }
 )();
 //# sourceURL=UA_user_auto.js
