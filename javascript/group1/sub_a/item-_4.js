b6fc1e4f8d787fe810b395bc45730756adba5f5a
mjz19910 <matthias291999@gmail.com>
2022-02-02 12:01:46 -0700

    JSGroup1: Update item 4

--- javascript/group1/sub_a/item-_4.js
+++ javascript/group1/sub_a/item-_4.js
@@ -4,10 +4,48 @@ v1 (old): snippet_repo_v2/javascript/final/items/item4_v1.js
 v2 (old): snippet_repo_v2/javascript/final/items/item4_v2.js
 v3 (cur): snippet_repo_v2/javascript/snippet/group1/sub_a/item-_4.js
 */
+function avg(arr) {
+	let min = null;
+	let max = null;
+	let total = 0;
+	let cnt = 0;
+	for (let i = 0; i < arr.length; i++) {
+		let v = arr[i];
+		total += v;
+		cnt++;
+		if (!min || v < min) {
+			min = v;
+		}
+		if (!max || v > max) {
+			max = v;
+		}
+	}
+	return [total / cnt, min, max];
+}
+function avg_up(arr, v_avg) {
+	let min = null;
+	let max = null;
+	let total = 0;
+	let cnt = 0;
+	for (let i = 0; i < arr.length; i++) {
+		let v = arr[i];
+		if (v_avg > v) {
+			total += v;
+			cnt++;
+		}
+		if (!min || v < min) {
+			min = v;
+		}
+		if (!max || v > max) {
+			max = v;
+		}
+	}
+	return [total / cnt, min, max];
+}
 class AutoState {
 	constructor() {
 		this.cint_arr = [];
-		this.current_generator_autobuy=0;
+		this.current_generator_autobuy = 0;
 	}
 	destroy_cint_item(item) {
 		switch (item[0]) {
@@ -29,11 +67,55 @@ class AutoState {
 	}
 	start() {
 		if (!player.generators_autobuyer[0][0]) {
-			let t=this;
-			this.setInterval(()=>tierGenerator(t.current_generator_autobuy++ % 8), 50);
-			this.setInterval(()=>tierMult(), 300);
-			this.setInterval(()=>buyMeta(), 300);
+			let t = this;
+			this.setInterval(e=>tierGenerator(t.current_generator_autobuy++ % 8), 50);
+			this.setInterval(e=>tierMult(), 300);
+			this.setInterval(e=>buyMeta(), 300);
 		}
+		let money_log = player.money.log().toNumber();
+		let sd_arr = [];
+		function auto_sacrificeGen() {
+			let cur_log = player.money.log().toNumber();
+			let log_diff = cur_log - money_log;
+			let mode_run = 0;
+			money_log = cur_log;
+			if (log_diff > 0.1 && log_diff < 1) {
+				if (FORMULA.sacrifice().gt('1e4')) {
+					mode_run = 2;
+					sacrificeGen();
+				}
+			} else if (log_diff > 0 && log_diff < 0.1) {
+				if (FORMULA.sacrifice().gt(10)) {
+					mode_run = 3;
+					sacrificeGen();
+				}
+			}
+			if (log_diff !== 0) {
+				let[v_avg] = avg(sd_arr);
+				let ov = v_avg;
+				[v_avg] = avg_up(sd_arr, v_avg);
+				let avg_1 = ov;
+				let avg_2 = v_avg;
+				let cnt = 0;
+				while (v_avg < 100) {
+					v_avg *= 10;
+					ov *= 10;
+					cnt++;
+				}
+				let lost = sd_arr.filter(e=>e < avg_2);
+				while (sd_arr.length > 500) {
+					let rm = sd_arr.indexOf(lost[0]);
+					sd_arr.splice(rm, 1);
+					lost.shift();
+					if (lost.length === 0) {
+						break;
+					}
+				}
+				console.log('sac %o (%s %s)*1.0e-%o', mode_run, v_avg.toFixed(4), ov.toFixed(4), cnt, lost.length);
+				sd_arr.push(log_diff);
+			}
+		}
+		this.setInterval(auto_sacrificeGen, 500);
 	}
 	destroy_cint() {
 		for (let item of this.cint_arr) {
