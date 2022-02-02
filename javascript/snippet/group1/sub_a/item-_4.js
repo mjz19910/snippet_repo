ae37fc5cac9b58dd74c2b197fbf319470a756880
mjz19910 <matthias291999@gmail.com>
2022-02-01 21:53:42 -0700

    JSSnipGroup1: Create item 4 v3

---
+++ javascript/snippet/group1/sub_a/item-_4.js
@@ -0,0 +1,6 @@
+/* spell:words
+-- version_list item 4 --
+v1 (old): snippet_repo_v2/javascript/final/items/item4_v1.js
+v2 (old): snippet_repo_v2/javascript/final/items/item4_v2.js
+v3 (cur): snippet_repo_v2/javascript/snippet/group1/sub_a/item-_4.js
+*/
