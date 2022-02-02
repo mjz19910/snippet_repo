66ec86c841df982d0bf85805fe778ea5447b5858
mjz19910 <matthias291999@gmail.com>
2022-02-02 07:50:48 -0700

    JSFinal: Update simple_stack_vm

--- javascript/final/simple_stack_vm.mjs
+++ javascript/final/simple_stack_vm.mjs
@@ -24,25 +24,25 @@ export class SimpleStackVM {
 			let cur_instruction = this.instructions[this.instruction_pointer];
 			let [cur_opcode] = cur_instruction;
 			switch(cur_opcode) {
-				case 'push': {
+				case 'push'/*Stack*/: {
 					for(let i = 1; i < cur_instruction.length; i++) {
 						let item = cur_instruction[i];
 						this.push(item);
 					}
 					break;
 				}
-				case 'get': {
+				case 'drop'/*Stack*/: {
+					let drop = this.pop();
+					void drop;
+					break;
+				}
+				case 'get'/*Object*/: {
 					let name = this.pop();
 					let obj = this.pop();
 					this.push(obj[name]);
 					break;
 				}
-				case 'drop': {
-					let drop = this.pop();
-					void drop;
-					break;
-				}
-				case 'call': {
+				case 'call'/*Call*/: {
 					let number_of_arguments = cur_instruction[1];
 					let arg_arr = [];
 					for(let i = 0; i < number_of_arguments; i++) {
@@ -54,12 +54,12 @@ export class SimpleStackVM {
 					this.push(ret);
 					break;
 				}
-				case 'return': {
+				case 'return'/*Call*/: {
 					let ret = this.pop();
 					this.return_value = ret;
 					break;
 				}
-				case 'halt':{
+				case 'halt'/*Running*/:{
 					this.running=false;
 					break;
 				}
@@ -71,7 +71,7 @@ export class SimpleStackVM {
 					this.push(this);
 					break;
 				}
-				case 'push_window': {
+				case 'push_window'/*Special*/: {
 					this.push(window);
 					break;
 				}
@@ -79,9 +79,10 @@ export class SimpleStackVM {
 					debugger;
 					break;
 				}
-				default:
+				default/*Debug*/:{
 					console.log('unk opcode', cur_opcode);
 					throw new Error("halt");
+				}
 			}
 			this.instruction_pointer++;
 		}
