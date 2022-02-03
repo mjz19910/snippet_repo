f428176b65c00fb3761eb70cb0b45898cc1c2a9a
mjz19910 <matthias291999@gmail.com>
2022-02-03 01:26:00 -0700

    JSUserScript: Update

--- javascript/userscript_store/DebugAPI.user.d.ts
+++ javascript/userscript_store/DebugAPI.user.d.ts
@@ -1,3 +1,4 @@
+//spell:words deref
 interface RustSimpleTokenizer {
 	index: number;
 	source: string | null;
@@ -19,7 +20,7 @@ interface SimpleJavascriptParser {
 	ecma_262_section_script_parse_factory(): any;
 	ecma_262_section_module_parse_factory(): any;
 	get_sequence_of_input_elements(utf8_string: string): any;
-	ecma_262_psudo(): any;
+	ecma_262_pseudo(): any;
 	create_token_generator(): any;
 }
 type data_value_arr = {
