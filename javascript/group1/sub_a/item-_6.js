93aa2b6e9fc066db2838443d2b62f4752b8d5841
mjz19910 <matthias291999@gmail.com>
2022-02-09 16:57:26 -0700

    JSGroup1: Update item 6

--- javascript/group1/sub_a/item-_6.js
+++ javascript/group1/sub_a/item-_6.js
@@ -1,8 +1,9 @@
-/* spell:words
+/* spell:words mustafaq cupcc
 -- version_list item 6 --
 v1 (old): snippet_repo_v2/javascript/final/item6_v1.js
 v2 (cur): snippet_repo_v2/javascript/group1/sub_a/item-_6.js
 */
+// the code in the template block was written by @mustafaq#8791 in his programming language with file extension `.cup`
 let code=`struct Vec2D {
     x: int;
     y: int;
