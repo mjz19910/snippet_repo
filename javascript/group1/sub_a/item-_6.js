4352b41b2b5a4dccea3f23fda5a4bd7f6a60c69b
mjz19910 <matthias291999@gmail.com>
2022-02-13 23:04:07 -0700

    JSGroup1: Update item 6

--- javascript/group1/sub_a/item-_6.js
+++ javascript/group1/sub_a/item-_6.js
@@ -3,50 +3,16 @@
 v1 (old): snippet_repo_v2/javascript/final/item6_v1.js
 v2 (cur): snippet_repo_v2/javascript/group1/sub_a/item-_6.js
 */
-// the code in the template block was written by @mustafaq#8791 in his programming language with file extension `.cup`
-let code=`struct Vec2D {
-    x: int;
-    y: int;
-};
-
-method Vec2D::add(other: Vec2D*) {
-    self.x = self.x + other.x;
-    self.y = self.y + other.y;
-}
-
-method Vec2D::cmp_wise_max(): int {
-    return self.x > self.y ? self.x : self.y;
-}
-
-method Vec2D::cmp_wise_min(): int {
-    return self.x < self.y ? self.x : self.y;
-}
-
-fn main() {
-    let a: Vec2D;
-    let b: Vec2D;
-    let c = &a;
-
-
-    a.x = 1;
-    a.y = 2;
-    b.x = 3;
-    b.y = 4;
-
-    c::add(&b);
-    print(a::cmp_wise_max());
-    print(c::cmp_wise_min());
-}`;
 function encode_section(id, arr){
 	if(arr.length >= 128){
-		console.assert(false, "Unable to encode section length, need to support varint", arr.length);
+		console.assert(false, "Variable length ints unsupported, length=%o is too long", arr.length);
 		return null;
 	}
 	return [id, arr.length, ...arr];
 }
 function encode_string(arr) {
 	if(arr.length >= 128){
-		console.assert(false, "variable length ints unsupported, length=%o is too long", arr.length);
+		console.assert(false, "Variable length ints unsupported, length=%o is too long", arr.length);
 		return null;
 	}
 	return [arr.length, ...arr];
