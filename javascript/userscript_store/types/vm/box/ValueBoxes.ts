9c3843d7a4c1370b712d706de302dc41523d79b6
mjz19910 <matthias291999@gmail.com>
2022-02-27 03:09:13 -0700

    JSUser: Update rebuild the universe auto v2

--- javascript/userscript_store/types/vm/box/ValueBoxes.ts
+++ javascript/userscript_store/types/vm/box/ValueBoxes.ts
@@ -1,9 +1,30 @@
+import {ArgumentTypeBoxes} from "./ArgumentTypeBoxes";
 import {ArrayBoxes} from "./ArrayBoxes";
-import {BoxesWithValue} from "./BoxesWithValue";
+import {ConstructorBoxes} from "./ConstructorBoxes";
+import {FunctionBoxes} from "./FunctionBoxes";
+import {FunctionReturnBoxes} from "./FunctionReturnBoxes";
+import {GlobalThisBox} from "./GlobalThisBox";
 import {InstanceBoxes} from "./InstanceBoxes";
 import {ObjectBoxes} from "./ObjectBoxes";
+import {PromiseBox} from "./PromiseBox";
+import {PromiseTypeBoxes} from "../PromiseTypeBoxes";
 import {VoidBox} from "./VoidBox";
+import {WindowBox} from "./WindowBox";
 
-type ValueBoxes = ArrayBoxes | ObjectBoxes | InstanceBoxes | BoxesWithValue | VoidBox;
+type ValueBoxes =
+ArgumentTypeBoxes |
+ArrayBoxes |
+ArrayBoxes |
+ConstructorBoxes |
+FunctionBoxes |
+FunctionReturnBoxes |
+GlobalThisBox |
+InstanceBoxes |
+ObjectBoxes |
+PromiseBox |
+PromiseTypeBoxes |
+WindowBox |
+// No value
+VoidBox;
 
 export default ValueBoxes;
