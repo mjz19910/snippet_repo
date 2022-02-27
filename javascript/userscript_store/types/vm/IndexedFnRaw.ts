f9c02658daa0588bd4c65d78b8034e964637e410
mjz19910 <matthias291999@gmail.com>
2022-02-27 01:59:04 -0700

    JSUser: Update rebuild the universe auto v2

--- javascript/userscript_store/types/vm/IndexedFnRaw.ts
+++ javascript/userscript_store/types/vm/IndexedFnRaw.ts
@@ -2,4 +2,4 @@ import {Indexed} from "./vm";
 import {Function as VMFunction} from "./vm";
 
 
-export type VMIndexedCallableRaw = Indexed<VMFunction>;
+export type IndexedFnRaw = Indexed<VMFunction>;
