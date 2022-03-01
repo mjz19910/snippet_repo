e7c1273c741922fb07bdc49328e8208643e6af51
mjz19910 <matthias291999@gmail.com>
2022-02-28 18:37:21 -0700

    JSUser: Update JSParser ecma_12_8_6

--- javascript/userscript_store/types/vm/ecma_12_8_6.ts
+++ javascript/userscript_store/types/vm/ecma_12_8_6.ts
@@ -2,20 +2,6 @@ import {ecma_base} from "./ecma_base";
 import {ecma_return_type} from "./ecma_return_type";
 
 export class ecma_12_8_6 extends ecma_base {
-	// https://tc39.es/ecma262/#prod-CodePoint
-	CodePoint(str: string, index: number): ecma_return_type {
-		let res = this.m_dispatcher.HexDigits(str, index);
-		if(res[0]) {
-			if(res[1] > 0 && typeof res[0] === 'string') {
-				// but only if MV of HexDigits ≤ 0x10FFFF
-				let MV = parseInt(res[0], 16);
-				if(MV <= 0x10FFFF) {
-					return ['CodePoint', res[1]];
-				}
-			}
-		}
-		return [null, 0];
-	}
 	/*TemplateMiddle ::*/
 	/* | } TemplateCharacters opt ${*/
 	/* TemplateTail ::*/
@@ -34,19 +20,26 @@ export class ecma_12_8_6 extends ecma_base {
 		let [, , ...ct_ret] = ct1;
 		return ct_ret;
 	}
-	/* NotCodePoint ::*/
-	/* | HexDigits[~Sep] but only if MV of HexDigits > 0x10FFFF*/
-	/* CodePoint ::*/
-	/* | HexDigits[~Sep] but only if MV of HexDigits ≤ 0x10FFFF*/
-	/* TemplateHead ::*/
-	/* | ` TemplateCharacters opt ${*/
-	/* | TemplateSubstitutionTail ::*/
-	/* | TemplateMiddle*/
-	/* | TemplateTail*/
+	// https://tc39.es/ecma262/#prod-CodePoint
+	CodePoint(str: string, index: number): ecma_return_type {
+		// HexDigits[~Sep] but only if MV of HexDigits ≤ 0x10FFFF
+		let res = this.m_dispatcher.HexDigits(str, index);
+		if(res[0]) {
+			if(res[1] > 0 && typeof res[0] === 'string') {
+				// but only if MV of HexDigits ≤ 0x10FFFF
+				let MV = parseInt(res[0], 16);
+				if(MV <= 0x10FFFF) {
+					return ['CodePoint', res[1]];
+				}
+			}
+		}
+		return [null, 0];
+	}
+	// https://tc39.es/ecma262/#prod-TemplateHead
 	TemplateHead(str: string, index: number): ecma_return_type {
 		let cur_index = index;
-		// ` TemplateCharacters opt ${
-		cur: if(str[cur_index] === '`') {
+		// ` TemplateCharacters_opt ${
+		if(str[cur_index] === '`') {
 			cur_index++;
 			let res = this.TemplateCharacters(str, cur_index);
 			if(res[0] === false) throw res[1];
@@ -57,34 +50,61 @@ export class ecma_12_8_6 extends ecma_base {
 				return [true, cur_index + 2];
 			}
 		}
-		// TemplateSubstitutionTail
-		let cur = this.TemplateSubstitutionTail(str, index);
-		if(cur[0] && cur[1] > 0) {
-			return [true, cur[1]];
-		}
-		// TemplateMiddle
-		cur = this.TemplateMiddle(str, index);
-		if(cur[0] && cur[1] > 0) {
-			return [true, cur[1]];
-		}
-		// TemplateTail
-		cur = this.TemplateTail(str, index);
-		if(cur[0] && cur[1] > 0) {
-			return [true, cur[1]];
-		}
-
+		return [null, 0];
 	}
 	// https://tc39.es/ecma262/#prod-TemplateMiddle
 	TemplateMiddle(str: string, index: number): ecma_return_type {
-
+		let len=0;
+		// } TemplateCharacters_opt ${
+		if(str[index] === '{}'[1]){
+			len++;
+			if(str[index+len] === '$' && str[index+len + 1] === '{}'[0]){
+				return [true, len+2];
+			}
+			let res=this.TemplateCharacters(str, index);
+			if(res[0]){
+				len+=res[1];
+				if(str[index+len] === '$' && str[index+len + 1] === '{}'[0]){
+					return [true, len+2];
+				}	
+			}
+		}
+		return [null, 0];
 	}
 	// https://tc39.es/ecma262/#prod-TemplateTail
 	TemplateTail(str: string, index: number): ecma_return_type {
-
+		let len=0;
+		// } TemplateCharacters_opt `
+		if(str[index] === '{}'[0]){
+			len++;
+			if(str[index+len] === '`'){
+				len++;
+				return [true, len];
+			}
+			let res=this.TemplateCharacters(str, index);
+			if(res[0]){
+				len+=res[1];
+				if(str[index+len] === '`'){
+					len++;
+					return [true, len];
+				}	
+			}
+		}
+		return [null, 0];
 	}
 	// https://tc39.es/ecma262/#prod-TemplateSubstitutionTail
 	TemplateSubstitutionTail(str: string, index: number): ecma_return_type {
-
+		// TemplateMiddle
+		let res=this.TemplateMiddle(str, index);
+		if(res[0]){
+			return [true, res[1]];
+		}
+		// TemplateTail
+		res=this.TemplateTail(str, index);
+		if(res[0]){
+			return [true, res[1]];
+		}
+		return [null, 0];
 	}
 	/*Template ::*/
 	/* | NoSubstitutionTemplate*/
