ece6fa4bff465cbf705532055ff80463fa125926
mjz19910 <matthias291999@gmail.com>
2022-03-02 09:59:48 -0700

    JSUser: Update types/vm/mod.ts

--- javascript/userscript_store/types/vm/mod.ts
+++ javascript/userscript_store/types/vm/mod.ts
@@ -1,15 +1,11 @@
 import TemporaryBox from "./box/TemporaryBox";
-import {InstructionCall} from "./instruction/general/InstructionCall";
-import {InstructionConstruct} from "./instruction/general/InstructionConstruct";
 import Primitives from "./Primitives";
 type _T=TemporaryBox|0;
 // --- Misc ---
 type SkipItem0_t<T extends [f: string, ...v: any[]], X> = T extends [X, ...infer U] ? U : T[1];
 export type SkipItem0<T extends [f: any, ...v: any[]]> = SkipItem0_t<T, T[0]>;
 export {
-	InstructionCall,
 	Primitives,
-	InstructionConstruct,
 	TemporaryBox,
 	_T
 };
