e26bdf61b4690279a9a64299dd8b8537e1785193
mjz19910 <matthias291999@gmail.com>
2022-03-02 17:40:21 -0700

    JSUser: Update types/vm/

--- javascript/userscript_store/types/vm/AbstractVM.ts
+++ javascript/userscript_store/types/vm/AbstractVM.ts
@@ -1,6 +1,3 @@
-import {InstructionType} from "./vm/instruction/mod";
-import {Box} from "./vm/box/Box";
-
 export abstract class AbstractVM {
 	abstract execute_instruction(instruction: InstructionType): void;
 	abstract run(): Box;
