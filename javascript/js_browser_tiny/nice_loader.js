f101a7fcef476898dfb65b73c12517aacec48011
mjz19910 <matthias291999@gmail.com>
2022-03-20 09:44:20 -0700

    Update nice_loader.js

--- javascript/js_browser_tiny/nice_loader.js
+++ javascript/js_browser_tiny/nice_loader.js
@@ -1,4 +1,5 @@
-const debug=true;
+const debug = false;
+
 const system_modules = [
 	'repl',
 	'http',
@@ -6,41 +7,42 @@ const system_modules = [
 	'path',
 	'process',
 	'vm',
-]
+];
+
 class ContextType {
 	/**@type {string[]}*/
-	conditions=[];
-	importAssertions={};
-	parentURL="";
+	conditions = [];
+	importAssertions = {};
+	parentURL = "";
 }
+
 /**
  * @param {string} specifier
  * @param {ContextType} context
  * @param {import("./nice_loader_types.js").ResolveFn} defaultResolve
  */
 export async function resolve(specifier, context, defaultResolve) {
-	if(debug)console.log('spec', specifier);
-	if(debug)console.log('ctx', context);
+	if(debug) console.log('spec', specifier);
+	if(debug) console.log('ctx', context);
 	if(specifier.endsWith(".js")) {
-		try{
-			return defaultResolve(specifier, context, defaultResolve);
-		}catch(err){
-			console.log('module err', err);
-			return {};
+		try {
+			return await defaultResolve(specifier, context, defaultResolve);
+		} catch(err) {
+			if(debug) console.log('Failed to load import specifier: ', specifier);
+			throw err;
 		}
 	}
 	if(system_modules.includes(specifier)) {
 		return defaultResolve(specifier, context, defaultResolve);
 	}
-	try{
+	try {
 		return await defaultResolve(specifier + ".js", context, defaultResolve);
+	} catch {}
+	if(debug) console.log('Failed to load import specifier: ', specifier);
+	try {
+		return await defaultResolve(specifier, context, defaultResolve);
 	} catch(err) {
-		console.log('module err', err);
-		try{
-			return defaultResolve(specifier, context, defaultResolve);
-		}catch(err){
-			console.log('module err', err);
-			return {};
-		}
+		if(debug) console.log('Failed to load import specifier: ', specifier);
+		throw err;
 	}
 }
