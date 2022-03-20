c227f677bd453835c742543eaf323c772b8c527a
mjz19910 <matthias291999@gmail.com>
2022-03-20 05:28:00 -0700

    JSBrowserTiny+FakeDomBrowse: Update

--- javascript/js_browser_tiny/fake_dom/browse/do_create_window.js
+++ javascript/js_browser_tiny/fake_dom/browse/do_create_window.js
@@ -1,9 +1,9 @@
-import {fake, FakeWindow} from "fake-dom";
+import {DOMBadge, fake, FakeWindow} from "fake-dom";
 import {intercept_setTimeoutAPI} from "fake-dom-api";
 /**@type {FakeWindow|null} */
 export let fake_window = null;
 export function init() {
-	fake_window=new FakeWindow();
+	fake_window=new FakeWindow(new DOMBadge);
 	fake.window=fake_window;
 	fake_window.setTimeout = intercept_setTimeoutAPI(fake_window.setTimeout);
 }
