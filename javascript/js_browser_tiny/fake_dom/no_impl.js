142bcaa466d2789c10a18a2ecbda59d6e4edaf1b
mjz19910 <matthias291999@gmail.com>
2022-03-20 01:50:09 -0700

    JSBrowserTiny: Take any arguments to drop

--- javascript/js_browser_tiny/fake_dom/no_impl.js
+++ javascript/js_browser_tiny/fake_dom/no_impl.js
@@ -1,4 +1,8 @@
-/**@returns {Error} */
-export function no_impl() {
+/**
+ * @returns {Error}
+ * @param {(Function | number | any[] | undefined)[]} a
+ */
+export function no_impl(...a) {
+	a;
 	return new Error("NoImpl");
 }
