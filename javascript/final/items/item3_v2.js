c28908bd066f7c6489428b6b0c4f8ff73bb7974b
mjz19910 <matthias291999@gmail.com>
2022-03-21 10:41:54 -0700

    update item locations

--- javascript/final/items/item3_v2.js
+++ javascript/final/items/item3_v2.js
@@ -1,11 +1,10 @@
 /* spell:words
 --- version_list item 3 ---
-v1 (old-o): snippet_repo_v2/javascript/final/items/item3_v1.js
-v2 (cur-c): snippet_repo_v2/javascript/group1/sub_a/item-_3.js
-v3 (use-m): snippet_repo_v2/javascript/final/missing/item3_v3.js
-v4 (use-m): snippet_repo_v2/javascript/final/missing/item3_v4.js
---- version_list end ---
-this snippet requires g_error_stack to be set
+v1 (old): snippet_repo_v2/javascript/final/items/item3_v1.js
+v2 (cur): snippet_repo_v2/javascript/final/items/item3_v2.js
+v3 (new): snippet_repo_v2/javascript/final/items/item3_v3.js
+v4 (new): snippet_repo_v2/javascript/final/items/item3_v4.js
+v5 (new): snippet_repo_v2/javascript/group1/sub_a/item-_3.js
 */
 function get_location_from_error_stack_lines(error_lines, offset){
 	let cur_line=error_lines[offset];
