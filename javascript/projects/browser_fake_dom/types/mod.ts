079950050e288ce6493373a7a4600391bcbef426
mjz19910 <matthias291999@gmail.com>
2022-03-22 10:47:07 -0700

    fix PageLoadStateType and export FakeWindowType

--- javascript/projects/browser_fake_dom/types/mod.ts
+++ javascript/projects/browser_fake_dom/types/mod.ts
@@ -1,3 +1,4 @@
 export {HTMLParserCallback} from "./callback_types";
-export {page_load_state} from "./onPageLoadStarted";
+export {PageLoadStateType} from "./onPageLoadStarted";
 export {TagName} from "./TagName";
+export {FakeWindowType} from "./FakeWindowType";
