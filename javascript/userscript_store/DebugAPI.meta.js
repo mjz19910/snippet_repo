1157110d44f5d290b604f23a45fdde64095e3757
mjz19910 <matthias291999@gmail.com>
2022-03-29 19:46:05 -0700

    DebugAPI: Update version to 0.3.1

--- javascript/userscript_store/DebugAPI.meta.js
+++ javascript/userscript_store/DebugAPI.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         global DebugAPI userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.3.0
+// @version      0.3.1
 // @description  global DebugAPI userscript snippet from https://github.com/mjz19910/snippet_repo/blob/master/javascript/userscript_store/DebugAPI.user.js
 // @author       @mjz19910
 // @match        https://*/*
