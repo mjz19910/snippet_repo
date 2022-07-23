d2c9a550d354abc573602b54ec0388903d7964b0
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-23 16:41:26 -0700

    u

--- javascript/userscript_store/types/box/is_dom_instruction_type.ts
+++ javascript/userscript_store/types/box/is_dom_instruction_type.ts
@@ -12,11 +12,11 @@ export function is_dom_instruction_type(value: DomInstructionType): value is Dom
 	let [, ...instruction_base] = value;
 	if (is_instruction_type(instruction_base)) return true;
 	switch (instruction_base[0]) {
-		case 'marker':
-			assert_type<DomInstructionNullMarker>([value[0], ...instruction_base]);
-			return instruction_base.length === 2 && instruction_base[1] === null;
 		case 'dom_filter_6': return instruction_base.length == 5
 		case 'dom_filter_7': return instruction_base.length == 6
+		case 'marker':
+			assert_type<DomInstructionNullMarker>([value[0], ...instruction_base])
+			return instruction_base.length === 2 && instruction_base[1] === null
 		case 'push_global_object': return is_instruction_type(['push_window_object'])
 		case 'vm_call_at': return value.length === 3 && is_dom_instruction_tagged_pack(value[2])
 		default: assert_type<never>(instruction_base); return false
