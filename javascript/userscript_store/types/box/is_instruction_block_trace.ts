e31a7dc2d4579c38fba8898c3521fde7bacfe484
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-23 16:46:36 -0700

    u

--- javascript/userscript_store/types/box/is_instruction_block_trace.ts
+++ javascript/userscript_store/types/box/is_instruction_block_trace.ts
@@ -5,53 +5,50 @@ import { is_dom_instruction_type } from "./is_dom_instruction_type";
 import { is_null } from "./is_null";
 
 export function is_instruction_block_trace<T>(v: T | BlockTrace): v is BlockTrace {
-	if(!is_array(v)) throw new Error("Invalid type");
-	if (v instanceof Array) {
-		switch (v[0]) {
-			case 'vm_block_trace': {
-				switch (v[1]) {
-					case 'begin': {
-						let vv = v[2];
-						if (is_null(vv))
-							return true;
-						if (is_dom_instruction_type(vv)) {
-							return true;
-						}
-					} break;
-					case 'call': {
-						let vv = v[2];
-						if (is_null(vv))
-							return true;
-						if (is_dom_instruction_type(vv)) {
-							return true;
-						}
-					} break;
-					case 'tagged': {
-						let vv = v[2];
-						if (is_null<typeof vv>(vv))
-							return true;
-						if (is_DomTaggedPack(vv))
-							return true;
-					} break;
-					case 'tagged_begin': {
-						let vv = v[2];
-						if (is_null<typeof vv>(vv))
-							return true;
-						if (is_DomTaggedPack(vv)) {
-							return true;
-						}
-					} break;
-					case 'tagged_call': {
-						let vv = v[2];
-						if (is_null<typeof vv>(vv))
-							return true;
-						if (is_DomTaggedPack(vv)) {
-							return true;
-						}
-					} break;
-				}
+	if (!is_array(v)) throw new Error("Invalid type");
+	switch (v[0]) {
+		case 'vm_block_trace': {
+			switch (v[1]) {
+				case 'begin': {
+					let vv = v[2];
+					if (is_null(vv))
+						return true;
+					if (is_dom_instruction_type(vv)) {
+						return true;
+					}
+				} break;
+				case 'call': {
+					let vv = v[2];
+					if (is_null(vv))
+						return true;
+					if (is_dom_instruction_type(vv)) {
+						return true;
+					}
+				} break;
+				case 'tagged': {
+					let vv = v[2];
+					if (is_null<typeof vv>(vv))
+						return true;
+					if (is_DomTaggedPack(vv))
+						return true;
+				} break;
+				case 'tagged_begin': {
+					let vv = v[2];
+					if (is_null<typeof vv>(vv))
+						return true;
+					if (is_DomTaggedPack(vv)) {
+						return true;
+					}
+				} break;
+				case 'tagged_call': {
+					let vv = v[2];
+					if (is_null<typeof vv>(vv))
+						return true;
+					if (is_DomTaggedPack(vv)) {
+						return true;
+					}
+				} break;
 			}
 		}
 	}
-	return false;
 }
