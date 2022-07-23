d8a3e4a3d758816181ffabb070b480e032912856
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-23 11:38:54 -0700

    add youtube plugin

---
+++ javascript/userscript_store/raw/youtube_plugin.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         youtube plugin
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
