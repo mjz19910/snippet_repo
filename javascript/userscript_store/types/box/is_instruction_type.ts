533dcb5d109c05fe3b24e9636ebbc124c3ef9197
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-23 16:28:48 -0700

    u

--- javascript/userscript_store/types/box/is_instruction_type.ts
+++ javascript/userscript_store/types/box/is_instruction_type.ts
@@ -10,6 +10,14 @@ import { is_number } from "./is_number";
 
 export function is_instruction_type<T extends any[]>(value: T | InstructionType): value is InstructionType {
 	if (assume_exclude_type<T>(value)) throw new Error("Never");
+	switch (value[0]) {
+		case 'push': let [, ...rest] = value;
+			if (is_empty_arr(rest)) return true;
+			if (is_array_of(rest, is_box)) return true;
+			assert_type<never>(rest);
+			return false;
+		case 'vm_block_trace': return is_instruction_block_trace(value);
+	}
 	switch (value.length) {
 		case 1: switch (value[0]) {
 			case 'append': return true;
@@ -19,7 +27,6 @@ export function is_instruction_type<T extends any[]>(value: T | InstructionType)
 			case 'get': return true;
 			case 'halt': return true;
 			case 'nop': return true;
-			case 'push': return true;
 			case 'push_window_object': return true;
 			case 'return': return true;
 			case 'vm_push_args': return true;
@@ -29,7 +36,7 @@ export function is_instruction_type<T extends any[]>(value: T | InstructionType)
 			default: assert_type<never>(value); return false;
 		}
 		case 2: switch (value[0]) {
-			case 'construct':
+			case 'construct': break;
 			case 'je':
 			case 'jmp':
 			case 'peek':
@@ -40,22 +47,14 @@ export function is_instruction_type<T extends any[]>(value: T | InstructionType)
 				case 'object_index_to_function': return true;
 				case 'vm_function': return true;
 			}
-		}
+			default: assert_type<never>(value); return false;
+		} break;
 		case 3: switch (value[0]) {
-			case 'modify_operand':return is_instruction_modify_op(value);
-			case 'push': {
-				let [, ...rest] = value;
-				if (is_empty_arr(rest))
-					return true;
-				if (is_array_of(rest, is_box))
-					return true;
-				return false;
-			}
+			case 'modify_operand': return is_instruction_modify_op(value);
 			case 'vm_block_trace': return is_instruction_block_trace(value);
+			default: assert_type<never>(value); return false;
 		}
+		default: value; return false;
 	}
-	switch (value[0]) {
-		case 'append': return true;
-		default: return false
-	};
+	return false;
 }
