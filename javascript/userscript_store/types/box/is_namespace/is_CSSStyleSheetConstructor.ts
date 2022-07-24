1a60b892340909111b7944d505da6427b273e08a
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-23 20:32:38 -0700

    u

--- javascript/userscript_store/types/box/is_namespace/is_CSSStyleSheetConstructor.ts
+++ javascript/userscript_store/types/box/is_namespace/is_CSSStyleSheetConstructor.ts
@@ -1,3 +1,5 @@
 export function is_CSSStyleSheetConstructor<T extends Function>(value: T | typeof CSSStyleSheet): value is typeof CSSStyleSheet {
-	return value === CSSStyleSheet;
+	value = value as typeof CSSStyleSheet;
+	const dsc=Object.getOwnPropertyDescriptors(value.prototype);
+	return !!(dsc.replace && dsc.rules);
 }
