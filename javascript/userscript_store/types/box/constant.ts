dc1433a0f0c03ea3d492e60c8fcafd09479b465c
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-23 20:50:00 -0700

    u

--- javascript/userscript_store/types/box/constant.ts
+++
@@ -1,4 +0,0 @@
-import {PropertiesToIterate} from "./PropertiesToIterate"
-
-export const PropertiesToIterateArray: [PropertiesToIterate]=["type"]
-export const box_value_property_cache=new Set<string>()
