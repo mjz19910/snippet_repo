f67fa639ec4e67e13a3c1843bc80527cce180d2e
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-23 19:36:47 -0700

    u

--- javascript/userscript_store/types/box/can_property_return_a_box.ts
+++ javascript/userscript_store/types/box/can_property_return_a_box.ts
@@ -8,7 +8,6 @@ export function can_property_return_a_box<T extends string>(
 ): v is ObjectIndexToOptBox<T, ObjectIndexToBox<T>> {
 	let vv = prop;
 	if(force_type_upgrade<ObjectIndexToBox<T>, {}>(v)) {
-		let x=v[vv];
 		if(is_obj_index_to_box(v, vv)) {
 			return true;
 		}
