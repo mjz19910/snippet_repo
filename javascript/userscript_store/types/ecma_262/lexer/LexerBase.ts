5af7658264b15400ca1ec81de4cdfd59414f836b
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-23 22:14:04 -0700

    semicolon fixup

--- javascript/userscript_store/types/ecma_262/lexer/LexerBase.ts
+++ javascript/userscript_store/types/ecma_262/lexer/LexerBase.ts
@@ -1,11 +1,11 @@
-import {Dispatcher} from "./Dispatcher";
+import {Dispatcher} from "./Dispatcher"
 
-export const debug_flag_override=false;
+export const debug_flag_override=false
 
 export class LexerBase {
-	type:"ecma_base"="ecma_base";
-	m_dispatcher:Dispatcher;
+	readonly type="ecma_base"
+	m_dispatcher:Dispatcher
 	constructor(dispatcher:Dispatcher){
-		this.m_dispatcher=dispatcher;
+		this.m_dispatcher=dispatcher
 	}
 }
