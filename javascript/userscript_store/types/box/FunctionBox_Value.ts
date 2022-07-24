f0691ae0bf5fb6f8081d789eb3e000b33f002ddc
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-23 22:06:32 -0700

    Types: Update

--- javascript/userscript_store/types/box/FunctionBox_Value.ts
+++ javascript/userscript_store/types/box/FunctionBox_Value.ts
@@ -3,7 +3,7 @@ import {assert_type} from "./helper/assert_type"
 import {Primitives} from "./helper/Primitives"
 import {NewableInstancePack} from "./NewableInstancePack"
 
-export type FunctionBox_Value_never=Exclude<Extract<Exclude<Box,Primitives|null>['value'],Function>,FunctionBox_Value2>
+export type FunctionBox_Value_never=Exclude<Extract<Exclude<Box,Primitives|null>['value'],Function>,FunctionBox_Value>
 const ok=assert_type<FunctionBox_Value_never&never>(null as never);
 export type FunctionBox_Value=
 	typeof CSSStyleSheet|
