2e3e4f1e4580de07ac748fe54bbd90f5d71e0074
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-23 22:16:38 -0700

    u

--- javascript/userscript_store/types/ecma_262/parser/ParserBase.ts
+++ javascript/userscript_store/types/ecma_262/parser/ParserBase.ts
@@ -1,7 +1,4 @@
 import {ParserDispatcher} from "./ParserDispatcher"
-import {section_14_2} from "./section_14_2"
-
-export const debug_flag_override=false
 
 export class ParserBase {
 	readonly type="ecma_parser_base"
