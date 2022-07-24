31935bcc9ee930c2d24c919be46961330108e859
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-23 20:54:01 -0700

    u

--- javascript/userscript_store/types/box/is_namespace/convert_type_erase_convert.ts
+++ javascript/userscript_store/types/box/is_namespace/convert_type_erase_convert.ts
@@ -1,7 +1,7 @@
 import {TypeEraseClass} from "../TypeEraseClass"
 
 export function convert_type_erase_convert<T,U>(
-	value: TypeEraseClass<U>|TypeEraseClass<T>,
+	value: TypeEraseClass<T>|TypeEraseClass<U>,
 	can_convert: (v: T|U) => v is U): value is TypeEraseClass<U> {
 	if(can_convert(value.erase_value)) {
 		return true
