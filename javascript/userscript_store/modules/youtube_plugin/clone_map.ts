f85bc84dcbd51c2b3affe388df1dd7c4416589a2
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-25 05:06:30 -0700

    u

--- javascript/userscript_store/modules/youtube_plugin/clone_map.ts
+++ javascript/userscript_store/modules/youtube_plugin/clone_map.ts
@@ -1,6 +1,6 @@
 import {deep_clone} from "./deep_clone"
 
-export function clone_map<T, U extends undefined|string|boolean|bigint|number|object|Function|any[], X extends Map<T, U>>(map: X): X {
+export function clone_map<T, U, X extends Map<T, U>>(map: X): X {
 	let arr=Array.from(map)
 	let cloned_arr: [T,U][]=arr.map((map_entry): [T,U] => [map_entry[0],deep_clone<U>(map_entry[1])])
 	return new Map(cloned_arr) as Map<T,U> as X
