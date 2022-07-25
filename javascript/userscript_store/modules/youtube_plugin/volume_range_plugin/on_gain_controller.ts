896d90f0b488a98d29089a9735d781083117dc1f
Matthias Zimmerman <matthias291999@gmail.com>
2022-07-25 02:27:27 -0700

    u

--- javascript/userscript_store/modules/youtube_plugin/volume_range_plugin/on_gain_controller.ts
+++ javascript/userscript_store/modules/youtube_plugin/volume_range_plugin/on_gain_controller.ts
@@ -1,11 +1,12 @@
 import {HTMLMediaElementGainController} from "./HTMLMediaElementGainController"
 import {g_api} from "../youtube_plugin.user"
 
-export function on_gain_controller(create_gain_controller: () => HTMLMediaElementGainController) {
-	if(g_api.value && g_api.value.gain_controller) {
-		return g_api.value.gain_controller
+export function on_gain_controller(create_gain_controller: () => HTMLMediaElementGainController): HTMLMediaElementGainController {
+	if(g_api.value&&g_api.value.gain_controller) {
+		return g_api.value.gain_controller as HTMLMediaElementGainController
 	}
 	let controller=create_gain_controller()
-	if(g_api.value) g_api.value.gain_controller=controller
+	if(!g_api.value) g_api.value={}
+	g_api.value.gain_controller=controller
 	return controller
 }
