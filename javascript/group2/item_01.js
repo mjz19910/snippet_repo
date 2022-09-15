d67a60c5fec852f2b173df1d2c05907ff706d587
Matthias Zimmerman <matthias291999@gmail.com>
2022-09-15 01:18:23 -0700

    Javascript: add new item 1

---
+++ javascript/group2/item_01.js
@@ -0,0 +1,14 @@
+/* spell:words
+--- version_list item 1 ---
+v1 (old): snippet_repo_v2/javascript/final/items/item_01_v1.js
+v2 (cur): snippet_repo_v2/javascript/group2/item_01.js
+*/
+class Element {}
+class Transmutation {
+	constructor(arg1, arg1_count, arg2, arg2_count) {
+		this.data = [arg1, arg1_count, arg2, arg2_count];
+	}
+}
+const RECIPES=[
+	new Transmutation(new Element("Copper"), 1, new Element("Silver"), 1/2)
+];
