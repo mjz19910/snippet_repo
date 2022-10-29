14d570e831ede0555ff09cafd912d7bd4b4d3324
Matthias Zimmerman <matthias291999@gmail.com>
2022-10-29 00:36:02 -0700

    u

--- javascript/userscript_store/types/src/DebugInfoBox.ts
+++ javascript/userscript_store/types/src/DebugInfoBox.ts
@@ -1,4 +1,5 @@
 import {EvalLostBox as EvalLostBox} from "./EvalLostBox"
 import {NoVarBox as NoVarBox} from "./NoVarBox"
 import {VarBox as VarBox} from "./VarBox"
+
 export type DebugInfoBox=EvalLostBox|VarBox|NoVarBox
