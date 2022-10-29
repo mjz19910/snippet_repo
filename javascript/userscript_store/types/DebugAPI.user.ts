21a3da7714fb1bcb816a67eee63889adfe48a045
Matthias Zimmerman <matthias291999@gmail.com>
2022-10-29 01:49:19 -0700

    u

--- javascript/userscript_store/types/DebugAPI.user.ts
+++ javascript/userscript_store/types/DebugAPI.user.ts
@@ -1,6 +1,5 @@
 import {DebugAPI} from "./src/DebugAPI"
-import {Dumper} from "./vm/Dumper"
 import {GenericDataEvent} from "./vm/GenericDataEvent"
+
 window.GenericDataEvent=GenericDataEvent
-export const local_dumper=new Dumper()
 window.DebugAPI=DebugAPI.the()
