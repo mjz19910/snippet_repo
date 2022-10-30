682615ff3261234a2d319474305fcfe782916cd2
Matthias Zimmerman <matthias291999@gmail.com>
2022-10-29 21:36:12 -0700

    u

--- typescript/modules/rebuild_universe_automation/version_00_20/support.ts
+++ typescript/modules/rebuild_universe_automation/version_00_20/support.ts
@@ -1,58 +1,62 @@
-import Box from "types/box/Box";
-import CSSStyleSheetBox from "types/box/CSSStyleSheetBox.js";
-import CSSStyleSheetConstructorBox from "types/box/CSSStyleSheetConstructorBox";
+import {Box} from "../../../box/Box"
+import {NoArgsType} from "../../../box/NoArgsType"
+import {CSSStyleSheetBox,CSSStyleSheetConstructorBox} from "../version_00_30/support"
+
 class CSSStyleSheetConstructorBoxImplSupport {
 	type: "constructor_box"="constructor_box";
 	from: "javascript"="javascript";
-	instance_type: "CSSStyleSheet"= "CSSStyleSheet";
+	instance_type: "CSSStyleSheet"="CSSStyleSheet";
 	constructor_type: "CSSStyleSheet"="CSSStyleSheet";
-	value:typeof CSSStyleSheet;
-	constructor(value: typeof CSSStyleSheet){
-		this.value=value;
+	value: typeof CSSStyleSheet
+	constructor(value: typeof CSSStyleSheet) {
+		this.value=value
 	}
 	/**@arg {'function'} to_match */
 	as_type(to_match: 'function') {
-		if(typeof this.value === to_match){
-			return this;
+		if(typeof this.value===to_match) {
+			return this
 		}
-		return null;
+		return null
 	}
 	/**@arg {Box[]} arr */
-	factory(...arr: Box[]){
-		/**@type {CSSStyleSheetConstructorBox['args_type']} */
-		let args_state: CSSStyleSheetConstructorBox['args_type']={
-			id:0,
-			value:[]
+	factory(...arr: Box[]) {
+		let initial_state: NoArgsType={
+			state: 0,
+			value: []
 		}
-	for(let i=0;i<arr.length;i++){
-		let val=arr[i];
-		if(typeof val != 'object')continue;
-		if(val === null)continue;
-		if(val.value instanceof Document){
-			throw new Error("Unexpected document");
+		let args_state: CSSStyleSheetConstructorBox['args_type']=initial_state
+		for(let i=0;i<arr.length;i++) {
+			let val=arr[i]
+			if(typeof val!='object') continue
+			if(val===null) continue
+			if(val.value instanceof Document) {
+				throw new Error("Unexpected document")
+			}
+			if(val.type!='shape_box') continue
+			args_state={
+				value: [val.value],
+				state: 1
+			}
 		}
-		if(val.type != 'shape_box')continue;
-		args_state={
-			value:[val.value],
-			state:1
-		}
-	}
-	/**@type {CSSStyleSheet} */
-	let obj: CSSStyleSheet=new this.value(...args_state.value);
-	return new CSSStyleSheetBox(obj);
+		/**@type {CSSStyleSheet} */
+		let obj: CSSStyleSheet=new this.value(...args_state.value)
+		return new CSSStyleSheetBox(obj)
 	}
 }
 
-export type CreateDesc=[1|2, 'create', 'div', 'state_log', {id:'state_log'}] |
-[2, 'create', 'div', 'history', string]|
-[2, 'create', 'div', "timeout_element", string]|
-[2, 'create', 'div', "hours_played", string]|
-[2, 'create', 'div', "ratio", string]|
-[2, 'create', 'div', "ratio_change", string];
-export type NewDesc=[
-	0, 'new', CSSStyleSheetConstructorBoxImplSupport, [],
-	(obj:CSSStyleSheet, str:string)=>Promise<CSSStyleSheet>,
-	string[]
-];
+export type CreateDesc=
+	[1|2,'create','div','state_log',{id: 'state_log'}]|
+	[2,'create','div','history',string]|
+	[2,'create','div',"timeout_element",string]|
+	[2,'create','div',"hours_played",string]|
+	[2,'create','div',"ratio",string]|
+	[2,'create','div',"ratio_change",string]
 
-export {Primitives} from "types/vm/Primitives";
+export type NewDesc=[
+	v:0,
+	type:'new',
+	a:CSSStyleSheetConstructorBoxImplSupport,
+	b:[],
+	c:(obj: CSSStyleSheet,str: string) => Promise<CSSStyleSheet>,
+	d:string[]
+]
