267dc600e317353accce95e797719e8e206ef5cb
Matthias Zimmerman <matthias291999@gmail.com>
2022-10-29 22:58:03 -0700

    u

--- typescript/modules/rebuild_universe_automation/version_00_20/AsyncTimeoutNode.ts
+++ typescript/modules/rebuild_universe_automation/version_00_20/AsyncTimeoutNode.ts
@@ -1,30 +1,33 @@
-import {LOG_LEVEL_INFO} from "../../../constants.js";
-import {l_log_if} from "./l_log_if";
-import {TimeoutNode} from "./TimeoutNode";
+import {LOG_LEVEL_INFO} from "../../../constants.js"
+import {l_log_if} from "./l_log_if"
+import {TimeoutNode} from "./TimeoutNode"
+
+interface TimeoutNodeTarget {
+	wait(): Promise<any>
+	destroy(): void
+}
 
 export class AsyncTimeoutNode extends TimeoutNode {
-	/**@arg {{wait():Promise<any>;destroy():void}} target */
-	async start_async(target) {
-		if(!target)
-			throw new Error("unable to start_async without anything to wait for");
-		l_log_if(LOG_LEVEL_INFO, 'start_async');
-		this.m_target = target;
-		this.set();
-		let promise = this.m_target.wait();
-		l_log_if(LOG_LEVEL_INFO, 'p', promise);
-		await promise;
+	m_target?: TimeoutNodeTarget
+	async start_async(target: TimeoutNodeTarget) {
+		l_log_if(LOG_LEVEL_INFO,'start_async')
+		this.m_target=target
+		this.set()
+		let promise=this.m_target.wait()
+		l_log_if(LOG_LEVEL_INFO,'p',promise)
+		await promise
 	}
 	set() {
-		l_log_if(LOG_LEVEL_INFO, 'set', this);
-		super.set();
+		l_log_if(LOG_LEVEL_INFO,'set',this)
+		super.set()
 	}
 	run() {
-		l_log_if(LOG_LEVEL_INFO, 'run', this);
-		return super.run();
+		l_log_if(LOG_LEVEL_INFO,'run',this)
+		return super.run()
 	}
 	destroy() {
 		if(this.m_target)
-			this.m_target.destroy();
-		super.destroy();
+			this.m_target.destroy()
+		super.destroy()
 	}
 }
