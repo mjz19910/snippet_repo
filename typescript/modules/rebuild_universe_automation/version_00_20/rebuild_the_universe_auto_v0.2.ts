ea139e27293163185b2be9252decce08356b2640
Matthias Zimmerman <matthias291999@gmail.com>
2022-10-29 23:00:27 -0700

    u

--- typescript/modules/rebuild_universe_automation/version_00_20/rebuild_the_universe_auto_v0.2.ts
+++ typescript/modules/rebuild_universe_automation/version_00_20/rebuild_the_universe_auto_v0.2.ts
@@ -1,17 +1,17 @@
 import {AutoBuy} from "../../../vm/AutoBuy"
-import {BaseMutationObserver} from "./BaseMutationObserver";
+import {BaseMutationObserver} from "./BaseMutationObserver"
 import {module_entry_function} from "./module_entry_function"
-import {NamedIdGenerator} from "./NamedIdGenerator";
-import {UniqueIdGenerator} from "./UniqueIdGenerator";
+import {NamedIdGenerator} from "./NamedIdGenerator"
+import {UniqueIdGenerator} from "./UniqueIdGenerator"
 
-export const debug_id_gen = new UniqueIdGenerator;
-export const debug_id_syms: WeakRef<{sym: symbol }>[] = [];
-export const sym_id_gen = new UniqueIdGenerator;
-export const named_sym_gen = new NamedIdGenerator;
-export const sym_id_syms: (symbol|[string,number,symbol])[] = [];
-export const auto_buy_obj = new AutoBuy;
-export let seen_elements = new WeakSet;
-export let mut_observers: BaseMutationObserver[] = [];
+export const debug_id_gen=new UniqueIdGenerator
+export const debug_id_syms: WeakRef<{sym: symbol}>[]=[]
+export const sym_id_gen=new UniqueIdGenerator
+export const named_sym_gen=new NamedIdGenerator
+export const sym_id_syms: (symbol|[string,number,symbol])[]=[]
+export const auto_buy_obj=new AutoBuy
+export let seen_elements=new WeakSet
+export let mut_observers: BaseMutationObserver[]=[]
 
-window.g_mut_observers = mut_observers;
-module_entry_function();
+window.g_mut_observers=mut_observers
+module_entry_function()
