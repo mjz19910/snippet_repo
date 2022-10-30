6f99b45f4239098f1d376b8936c66f9eaec2386b
Matthias Zimmerman <matthias291999@gmail.com>
2022-10-29 23:24:36 -0700

    u

--- typescript/modules/rebuild_universe_automation/version_00_20/labeled_sym.ts
+++
@@ -1,8 +0,0 @@
-import {named_sym_gen,sym_id_syms} from "./rebuild_the_universe_auto_v0.2"
-
-export function labeled_sym(name: string): symbol {
-	const id=named_sym_gen.next_named(name)
-	const sym=Symbol(`${name}@${id}`)
-	sym_id_syms.push(['named_sym_gen',[name,id],sym])
-	return sym
-}
--- typescript/modules/rebuild_universe_automation/version_00_20/next_sym.ts
+++
@@ -1,8 +0,0 @@
-import {sym_id_gen,sym_id_syms} from "./rebuild_the_universe_auto_v0.2"
-
-export function next_sym() {
-	const id=sym_id_gen.next()
-	const sym=Symbol(id)
-	sym_id_syms.push(['sym_id_gen',id,sym])
-	return sym
-}
