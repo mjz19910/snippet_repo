fb454edc85afcfa645b23cb50044bd61f402a219
Matthias Zimmerman <matthias291999@gmail.com>
2022-10-29 23:23:01 -0700

    u

--- typescript/modules/rebuild_universe_automation/version_00_20/next_debug_id.ts
+++
@@ -1,8 +0,0 @@
-import {debug_id_gen,debug_id_syms} from "./rebuild_the_universe_auto_v0.2"
-
-export function next_debug_id() {
-	const id=debug_id_gen.next()
-	const sym=Symbol(id)
-	debug_id_syms.push(new WeakRef({sym}))
-	return sym
-}
