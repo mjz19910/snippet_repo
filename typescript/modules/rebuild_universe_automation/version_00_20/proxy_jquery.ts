1a1ab3d5368f6a28df299b779355728b918c0215
Matthias Zimmerman <matthias291999@gmail.com>
2022-10-29 23:01:52 -0700

    u

--- typescript/modules/rebuild_universe_automation/version_00_20/proxy_jquery.ts
+++ typescript/modules/rebuild_universe_automation/version_00_20/proxy_jquery.ts
@@ -1,10 +1,7 @@
 import {use_jquery} from "./use_jquery";
 import {set_jq_proxy} from "./set_jq_proxy";
 
-/**
- * @param {undefined} [_value]
- */
-export function proxy_jquery(_value) {
+export function proxy_jquery(_value: undefined) {
 	let val = use_jquery();
 	set_jq_proxy(val);
 }
