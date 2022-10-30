9fa4afaa3b80658ca9ebc4b0be9cfa108fa9fda9
Matthias Zimmerman <matthias291999@gmail.com>
2022-10-29 23:24:56 -0700

    u

--- typescript/modules/rebuild_universe_automation/version_00_20/rebuild_the_universe_auto_v0.2.ts
+++ typescript/modules/rebuild_universe_automation/version_00_20/rebuild_the_universe_auto_v0.2.ts
@@ -1,10 +1,7 @@
 import {AutoBuy} from "../../../vm/AutoBuy"
 import {BaseMutationObserver} from "./BaseMutationObserver"
 import {module_entry_function} from "./module_entry_function"
-import {named_sym_gen_item} from "./named_sym_gen_item"
-import {sym_id_gen_item} from "./sym_id_gen_item"
 
-export const sym_id_syms: (named_sym_gen_item|sym_id_gen_item)[]=[]
 export const auto_buy_obj=new AutoBuy
 export let seen_elements=new WeakSet
 export let mut_observers: BaseMutationObserver[]=[]
