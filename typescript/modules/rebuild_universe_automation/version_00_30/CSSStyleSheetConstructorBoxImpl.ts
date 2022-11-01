2663486efc8e6391728e58f90de6b579b9f683c4
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-01 04:00:21 -0700

    u

--- typescript/modules/rebuild_universe_automation/version_00_30/CSSStyleSheetConstructorBoxImpl.ts
+++ typescript/modules/rebuild_universe_automation/version_00_30/CSSStyleSheetConstructorBoxImpl.ts
@@ -1,20 +1,30 @@
 import {Box} from "typescript/box/Box.js";
 import {CSSStyleSheetBox} from "typescript/box/CSSStyleSheetBox.js";
+import {CSSStyleSheetConstructorArgsTypeState1} from "typescript/box/CSSStyleSheetConstructorArgsTypeState1.js";
 import {CSSStyleSheetConstructorBox} from "typescript/box/CSSStyleSheetConstructorBox.js";
 import {NoArgsType} from "typescript/box/NoArgsType.js";
 
-export class CSSStyleSheetConstructorBoxImpl {
+export class CSSStyleSheetConstructorBoxImpl implements CSSStyleSheetConstructorBox {
 	type: "constructor_box";
 	from: "javascript";
 	instance_type: "CSSStyleSheet";
 	constructor_type: "CSSStyleSheet";
+	arguments: "[options?: CSSStyleSheetInit | undefined]";
+	args_type: CSSStyleSheetConstructorArgsTypeState1|NoArgsType;
+	m_verify_name: "CSSStyleSheetConstructorBox";
 	value: typeof CSSStyleSheet;
 	constructor(value: typeof CSSStyleSheet) {
 		this.type='constructor_box';
 		this.from='javascript';
 		this.instance_type='CSSStyleSheet';
 		this.constructor_type='CSSStyleSheet';
+		this.arguments='[options?: CSSStyleSheetInit | undefined]';
+		this.m_verify_name='CSSStyleSheetConstructorBox';
+		this.args_type=new NoArgsType;
 		this.value=value;
+	}
+	verify_name(name: "CSSStyleSheetConstructorBox") {
+		return this.m_verify_name==='CSSStyleSheetConstructorBox'&&name==='CSSStyleSheetConstructorBox';
 	}
 	as_type(to_match: 'function') {
 		if(typeof this.value===to_match) {
@@ -23,11 +33,10 @@ export class CSSStyleSheetConstructorBoxImpl {
 		return null;
 	}
 	factory(...arr: Box[]) {
-		let initial_state: NoArgsType={
+		let args_state: CSSStyleSheetConstructorBox['args_type']={
 			state: 0,
 			value: []
 		};
-		let args_state: CSSStyleSheetConstructorBox['args_type']=initial_state;
 		for(let i=0;i<arr.length;i++) {
 			let val=arr[i];
 			if(typeof val!='object')
@@ -40,11 +49,11 @@ export class CSSStyleSheetConstructorBoxImpl {
 			if(val.type!='shape_box')
 				continue;
 			args_state={
+				state: 1,
 				value: [val.value],
-				state: 1
 			};
 		}
-		let obj: CSSStyleSheet=new this.value(...args_state.value);
+		let obj=new this.value(...args_state.value);
 		return new CSSStyleSheetBox(obj);
 	}
 }
