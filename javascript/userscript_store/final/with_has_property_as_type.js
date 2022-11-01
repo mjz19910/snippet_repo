372ac1df26ba0e5d6d7fc293453d8029e93252c9
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-01 02:51:21 -0700

    u

--- javascript/userscript_store/final/with_has_property_as_type.js
+++ javascript/userscript_store/final/with_has_property_as_type.js
@@ -1,9 +1,6 @@
 import {does_have_property_as_type} from "./does_have_property_as_type";
 
-/**
-* @type {<T, F>(v:T, k:(v:T)=>F)=>T|null}
-*/
-
+/** @type {<T, F>(v:T, k:(v:T)=>F)=>T|null} */
 export function with_has_property_as_type(v, k) {
 	if(does_have_property_as_type(v, k))
 		return v;
