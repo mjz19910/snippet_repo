58a43a384accc23d3d4f8582172f63cb60550686
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-01 01:47:09 -0700

    u

--- typescript/box/has_any_properties_to_box.ts
+++ typescript/box/has_any_properties_to_box.ts
@@ -1,20 +1,15 @@
-import {Box} from "./Box.js";
 import {is_obj_index_to_box} from "./is_namespace/is_obj_index_to_box.js";
 import {box_value_property_cache} from "./const.js";
 import {ObjectIndexToBox} from "./helper/ObjectIndexToBox.js";
-import {convert_to_type} from "./helper/convert_to_type.js";
 
 export function has_any_properties_to_box<
 	T extends string,
 	C extends ObjectIndexToBox<T>
 >(v: C,props: T[]): v is C {
-	let on: typeof v|null=v;
 	let has_some_to_return_box=false;
 	for(let i=0;i<props.length;i++) {
 		let vv=props[i];
-		let val1=convert_to_type<C,ObjectIndexToBox<T>>(on);
-		let val2=convert_to_type<{},ObjectIndexToBox<T>>(val1);
-		if(!is_obj_index_to_box(val2,vv)) continue;
+		if(!is_obj_index_to_box(v,vv)) continue;
 		box_value_property_cache.add(vv);
 		has_some_to_return_box=true;
 	}
