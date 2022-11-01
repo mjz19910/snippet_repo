729e019c0e1dcfcdc2ccbca15f6ed660f3db388e
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-01 04:29:39 -0700

    u

--- typescript/vm/mod.ts
+++ typescript/vm/mod.ts
@@ -1,9 +1,1 @@
-import {bool} from "typescript/c++_trans/mod"
 export const seen_elements: WeakSet<object>=new WeakSet
-type _T=bool|0
-// --- Misc ---
-type SkipItem0_t<T extends [f: string,...v: any[]],X>=T extends [X,...infer U]? U:T[1]
-export type SkipItem0<T extends [f: any,...v: any[]]>=SkipItem0_t<T,T[0]>
-export {
-	_T
-}
