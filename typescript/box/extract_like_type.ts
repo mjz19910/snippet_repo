9d11ccce606b0156ca33790661430936e6ad95b1
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-03 02:14:38 -0700

    u

--- typescript/box/extract_like_type.ts
+++
@@ -1,9 +0,0 @@
-import {Box} from "./Box.js"
-import {is_box} from "./is_namespace/is_box.js"
-
-export function extract_like_type<
-	T,U extends TypedPropertyDescriptor<T>,C extends TypedPropertyDescriptor<Box>
->(property_descriptor: U|C): property_descriptor is C {
-	if(!('value' in property_descriptor)) return false
-	return is_box(property_descriptor.value)
-}
