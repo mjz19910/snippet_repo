ebb7d0d2f822e5179abe31ec67a2fe00542f4272
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-12 11:50:32 -0700

    u

--- typescript/src/mod.ts
+++ typescript/src/mod.ts
@@ -1,5 +1,5 @@
-import {AutoBuy} from "./vm/AutoBuy.js"
-import {UniqueIdGenerator} from "./vm/UniqueIdGenerator.js"
+import {AutoBuy} from "./vm/AutoBuy.js";
+import {UniqueIdGenerator} from "./vm/UniqueIdGenerator.js";
 
-export const debug_id_gen=new UniqueIdGenerator
+export const debug_id_gen=new UniqueIdGenerator;
 export const auto_buy_obj=new AutoBuy;
