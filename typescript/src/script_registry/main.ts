699eab774fe402572219c4f242c6a9fdfff2ca92
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-12 11:17:47 -0700

    u

--- typescript/src/script_registry/main.ts
+++ typescript/src/script_registry/main.ts
@@ -1,13 +1,20 @@
-import {attach_event_target_add_event_listener_proxy} from "./attach_event_target_add_event_listener_proxy.js"
-import {attach_promise_then_proxy} from "./attach_promise_then_proxy.js"
-import {attach_request_animation_frame_proxy} from "./attach_request_animation_frame_proxy.js"
-import {attach_string_index_of_proxy} from "./attach_string_index_of_proxy.js"
-import {attached_proxy_arr} from "./mod.js"
+import {attach_event_target_add_event_listener_proxy} from "./attach_event_target_add_event_listener_proxy.js";
+import {attach_promise_then_proxy} from "./attach_promise_then_proxy.js";
+import {attach_request_animation_frame_proxy} from "./attach_request_animation_frame_proxy.js";
+import {attach_string_index_of_proxy} from "./attach_string_index_of_proxy.js";
+import {attached_proxy_arr} from "./mod.js";
+
+declare global {
+	// script_registry/main.ts
+	interface Window {
+		proxy_set: ((...args: any[]) => any)[];
+	}
+}
 
 export function main() {
-	window.proxy_set=attached_proxy_arr
-	attach_event_target_add_event_listener_proxy()
-	attach_request_animation_frame_proxy()
-	attach_promise_then_proxy()
-	attach_string_index_of_proxy()
+	window.proxy_set=attached_proxy_arr;
+	attach_event_target_add_event_listener_proxy();
+	attach_request_animation_frame_proxy();
+	attach_promise_then_proxy();
+	attach_string_index_of_proxy();
 }
