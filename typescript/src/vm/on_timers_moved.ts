3e752a2472d97a52aed186fb594099de7012f655
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-12 11:15:15 -0700

    u

--- typescript/src/vm/on_timers_moved.ts
+++ typescript/src/vm/on_timers_moved.ts
@@ -2,6 +2,13 @@ import {remove_bad_dom_script_element} from "./remove_bad_dom_script_element.js"
 import {on_game_data_set} from "./on_game_data_set.js"
 import {wait_for_game_data} from "./wait_for_game_data.js"
 
+declare global {
+	// on_timers_moved.ts
+	interface Window {
+		_SM_Data: unknown;
+	}
+}
+
 export function on_timers_moved() {
 	if(window._SM_Data) {
 		on_game_data_set()
