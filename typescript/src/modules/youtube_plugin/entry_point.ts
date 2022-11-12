5c9e896ec7f23050d9f2fa01cd66820c34e61c87
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-12 13:01:12 -0700

    u

--- typescript/src/modules/youtube_plugin/entry_point.ts
+++ typescript/src/modules/youtube_plugin/entry_point.ts
@@ -1,31 +1,31 @@
-import {attach_resize_handler} from "./player_plugin_activate/attach_resize_handler.js"
-import {init_blob_logger} from "./blob_logger/init_blob_logger.js"
-import {init_dom_observer} from "./dom_observer/init_dom_observer.js"
-import {init_fetch_plugin} from "./fetch_plugin/init_fetch_plugin.js"
-import {init_g_api} from "./g_api/init_g_api.js"
-import {init_Image_plugin} from "./loose_plugins/init_Image_plugin.js"
-import {init_JSON_parse_plugin} from "./json_parse_plugin/init_JSON_parse_plugin.js"
-import {init_navigator_sendBeacon_plugin} from "./loose_plugins/init_navigator_sendBeacon_plugin.js"
-import {init_override_getInitialData} from "./loose_plugins/init_override_getInitialData.js"
-import {init_playlist_arr} from "./player_plugin_activate/init_playlist_arr.js"
-import {init_title_on_save} from "./player_plugin_activate/init_title_on_save.js"
-import {init_window_watch} from "./yt_application_create_plugin/init_window_watch.js"
-import {init_yt_player_application_plugin} from "./yt_application_create_plugin/init_yt_player_application_plugin.js"
-import {start_message_channel_loop} from "./dom_observer/start_message_channel_loop.js"
+import {attach_resize_handler} from "./player_plugin_activate/attach_resize_handler.js";
+import {init_blob_logger} from "./blob_logger/init_blob_logger.js";
+import {init_dom_observer} from "./dom_observer/init_dom_observer.js";
+// import {init_fetch_plugin} from "./fetch_plugin/init_fetch_plugin.js"
+// import {init_g_api} from "./g_api/init_g_api.js"
+// import {init_Image_plugin} from "./loose_plugins/init_Image_plugin.js"
+// import {init_JSON_parse_plugin} from "./json_parse_plugin/init_JSON_parse_plugin.js"
+// import {init_navigator_sendBeacon_plugin} from "./loose_plugins/init_navigator_sendBeacon_plugin.js"
+// import {init_override_getInitialData} from "./loose_plugins/init_override_getInitialData.js"
+// import {init_playlist_arr} from "./player_plugin_activate/init_playlist_arr.js"
+// import {init_title_on_save} from "./player_plugin_activate/init_title_on_save.js"
+// import {init_window_watch} from "./yt_application_create_plugin/init_window_watch.js"
+// import {init_yt_player_application_plugin} from "./yt_application_create_plugin/init_yt_player_application_plugin.js"
+// import {start_message_channel_loop} from "./dom_observer/start_message_channel_loop.js"
 
 export function entry_point() {
-	attach_resize_handler()
-	init_blob_logger()
-	init_dom_observer()
-	init_fetch_plugin()
-	init_g_api()
-	init_Image_plugin()
-	init_JSON_parse_plugin()
-	init_navigator_sendBeacon_plugin()
-	init_override_getInitialData()
-	init_playlist_arr()
-	init_title_on_save()
-	init_window_watch()
-	init_yt_player_application_plugin()
-	start_message_channel_loop()
+	attach_resize_handler();
+	init_blob_logger();
+	init_dom_observer();
+	// init_fetch_plugin()
+	// init_g_api()
+	// init_Image_plugin()
+	// init_JSON_parse_plugin()
+	// init_navigator_sendBeacon_plugin()
+	// init_override_getInitialData()
+	// init_playlist_arr()
+	// init_title_on_save()
+	// init_window_watch()
+	// init_yt_player_application_plugin()
+	// start_message_channel_loop()
 }
