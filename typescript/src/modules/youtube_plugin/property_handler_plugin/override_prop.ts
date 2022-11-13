fa85acc83a2eb036635c4f7bb6eca7513f370afa
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-13 08:06:41 -0700

    u

--- typescript/src/modules/youtube_plugin/property_handler_plugin/override_prop.ts
+++ typescript/src/modules/youtube_plugin/property_handler_plugin/override_prop.ts
@@ -1,6 +1,5 @@
 import {PropertyHandler} from "./PropertyHandler.js";
 
 export function override_prop<X extends string,T extends {[U in X]: any}>(object: T,property: X,property_handler: PropertyHandler) {
-	object[property];
 	Object.defineProperty(object,property,property_handler);
 }
