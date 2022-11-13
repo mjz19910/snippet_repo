8b924f8cd32ae94bb8e6ac81b01b95301f1729d4
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-13 08:45:12 -0700

    u

--- typescript/src/modules/youtube_plugin/init_override_getInitialData.ts
+++ typescript/src/modules/youtube_plugin/init_override_getInitialData.ts
@@ -1,10 +1,9 @@
 import {do_proxy_call_getInitialData} from "./fetch_result_handler_plugin/do_proxy_call_getInitialData.js";
-import {InitialData} from "./fetch_result_handler_plugin/InitialData.js";
 import {PropertyHandler} from "./PropertyHandler.js";
 
 declare global {
 	interface Window {
-		getInitialData(): {}
+		getInitialData(): {};
 	}
 }
 
@@ -12,6 +11,6 @@ export function init_override_getInitialData() {
 	Object.defineProperty(
 		window,
 		"getInitialData",
-		new PropertyHandler(do_proxy_call_getInitialData, void 0)
+		new PropertyHandler(do_proxy_call_getInitialData,void 0)
 	);
 }
