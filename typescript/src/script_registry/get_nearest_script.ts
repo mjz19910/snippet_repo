194c9b2c2a8c3896800c8880b2753e3f8225a7f4
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-13 10:30:44 -0700

    u

--- typescript/src/script_registry/get_nearest_script.ts
+++ typescript/src/script_registry/get_nearest_script.ts
@@ -6,27 +6,27 @@ import {is_in_userscript_fn} from "./is_in_userscript_fn.js";
 
 export function get_nearest_script() {
 	if(document.currentScript!==null) {
-		return document.currentScript
+		return document.currentScript;
 	}
-	let cur_script
+	let cur_script;
 	while(cur_event_fns.at(-1)===void 0&&cur_event_fns.length>0) {
-		cur_event_fns.pop()
+		cur_event_fns.pop();
 	}
-	let script_ghost=cur_event_fns.at(-1)
+	let script_ghost=cur_event_fns.at(-1);
 	if(!script_ghost)
-		return null
+		return null;
 	if(has_reg_id(script_ghost)) {
 		if(cur_script===void 0&&!is_in_userscript.flag&&!is_in_userscript_fn.flag&&!is_in_ignored_from_src_fn.flag) {
-			debugger
+			debugger;
 		}
 	}
-	script_ghost=cur_event_fns.at(-1)
+	script_ghost=cur_event_fns.at(-1);
 	if(script_ghost)
 		if(has_reg_id(script_ghost)) {}
-	let doc_script=document.currentScript
+	let doc_script=document.currentScript;
 	if(doc_script===null) {
-		return null
+		return null;
 	} else {
-		return doc_script
+		return doc_script;
 	}
 }
