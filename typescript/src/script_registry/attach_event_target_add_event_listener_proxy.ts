0b13caecb07deae69b2c72913dae1ec1870646de
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-13 10:29:04 -0700

    u

--- typescript/src/script_registry/attach_event_target_add_event_listener_proxy.ts
+++ typescript/src/script_registry/attach_event_target_add_event_listener_proxy.ts
@@ -5,12 +5,9 @@ import {replace_cb_with_safe_proxy} from "./replace_cb_with_safe_proxy.js";
 export function attach_event_target_add_event_listener_proxy() {
 	EventTarget.prototype.addEventListener=new Proxy(EventTarget.prototype.addEventListener,{
 		apply(...a): any {
-			// this will always be EventTarget.prototype.addEventListener (the real one)
-			// let target_fn=a[0]
 			let target_obj=a[1];
 			let call_args=a[2];
 			replace_cb_with_safe_proxy(call_args,1);
-			// ignore any calls from this script
 			if(!is_in_userscript.flag) {
 				debugger;
 				console.log(target_obj,call_args);
