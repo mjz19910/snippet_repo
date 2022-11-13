b5096c676cabcf39094ae9fdf7cdd2f75cf3b784
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-13 08:00:30 -0700

    u

--- typescript/src/modules/youtube_plugin/init_override_getInitialData.ts
+++ typescript/src/modules/youtube_plugin/init_override_getInitialData.ts
@@ -1,7 +1,7 @@
-import {override_prop} from "./property_handler_plugin/override_prop.js"
-import {PropertyHandler} from "./property_handler_plugin/PropertyHandler.js"
-import {do_proxy_call_getInitialData} from "./fetch_result_handler_plugin/do_proxy_call_getInitialData.js"
+import {do_proxy_call_getInitialData} from "./fetch_result_handler_plugin/do_proxy_call_getInitialData.js";
+import {override_prop} from "./property_handler_plugin/override_prop.js";
+import {PropertyHandler} from "./property_handler_plugin/PropertyHandler.js";
 
 export function init_override_getInitialData() {
-	override_prop(window,"getInitialData",new PropertyHandler("getInitialData",do_proxy_call_getInitialData))
+	override_prop(window,"getInitialData",new PropertyHandler(do_proxy_call_getInitialData));
 }
--- typescript/src/modules/youtube_plugin/property_handler_plugin/PropertyHandler.ts
+++ typescript/src/modules/youtube_plugin/property_handler_plugin/PropertyHandler.ts
@@ -1,34 +1,35 @@
+import {Box} from "../../../box/Box.js";
+import {ObjectBox} from "../../../box/ObjectBox.js";
+import {VoidBox} from "../../../box/VoidBox.js";
 import {create_proxy} from "./create_proxy.js"
 
+type CallbackType = [target: any, thisArg: any, argArray: any[]];
+
 export class PropertyHandler {
-	static proxy_map: Map<{},{}>=new Map
-	static override_map: Map<string,{}>=new Map
-	key: string
-	on_target_apply_callback: (args: any) => any
-	constructor(key: string,on_target_apply_callback: (args: any) => any) {
-		this.key=key
-		this.on_target_apply_callback=on_target_apply_callback
+	static instances:PropertyHandler[]=[];
+	proxy_map: Map<Box,Box>=new Map;
+	override_value:{value: Box}={value:new VoidBox};
+	on_target_apply_callback: (args: CallbackType) => any
+	constructor(on_target_apply_callback: (args: CallbackType) => any) {
+		this.on_target_apply_callback=on_target_apply_callback;
 	}
-	get():{}|undefined {
-		return PropertyHandler.override_map.get(this.key)
+	get():Box {
+		return this.override_value.value;
 	}
-	set(value: any) {
-		if(value===void 0) {
-			PropertyHandler.override_map.delete(this.key)
-			return
-		} else if(value===null) {
-			PropertyHandler.override_map.set(this.key,value)
-			return
+	set(value: Box) {
+		if(value===void 0 || value===null) {
+			this.override_value.value=value;
+			return;
 		}
-		if(PropertyHandler.proxy_map.has(value)) {
-			let nv=PropertyHandler.proxy_map.get(value)
-			if(!nv)
-				return
-			PropertyHandler.override_map.set(this.key,nv)
-			return
+		let proxy_override: Box;
+		if(this.proxy_map.has(value)) {
+			let proxy_override=this.proxy_map.get(value);
+			if(!proxy_override) return;
+			this.override_value.value=proxy_override;
+		} else {
+			proxy_override=create_proxy(value,this.on_target_apply_callback);
+			this.proxy_map.set(value,proxy_override);
+			this.override_value.value=proxy_override;
 		}
-		let proxy_override=create_proxy(value,this.on_target_apply_callback)
-		PropertyHandler.proxy_map.set(value,proxy_override)
-		PropertyHandler.override_map.set(this.key,proxy_override)
 	}
 }
