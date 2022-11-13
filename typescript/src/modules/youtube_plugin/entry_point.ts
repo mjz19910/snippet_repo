f512a688fe37e5b02616456680e8efa96ded83aa
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-13 09:05:30 -0700

    u

--- typescript/src/modules/youtube_plugin/entry_point.ts
+++ typescript/src/modules/youtube_plugin/entry_point.ts
@@ -1,5 +1,5 @@
 import {init_blob_logger} from "./blob_logger/init_blob_logger.js";
-import {init_dom_observer} from "./dom_observer/init_dom_observer.js";
+import {dom_observer} from "./dom_observer/dom_observer.js";
 import {start_message_channel_loop} from "./dom_observer/start_message_channel_loop.js";
 import {init_fetch_plugin} from "./fetch_plugin/init_fetch_plugin.js";
 import {init_g_api} from "./init_g_api.js";
@@ -9,6 +9,7 @@ import {init_override_getInitialData} from "./init_override_getInitialData.js";
 import {attach_resize_handler} from "./player_plugin_activate/attach_resize_handler.js";
 import {init_playlist_arr} from "./player_plugin_activate/init_playlist_arr.js";
 import {init_title_on_save} from "./player_plugin_activate/init_title_on_save.js";
+import {ui_plugin_attach_to_dom_observer} from "./player_plugin_activate/ui_plugin_attach_to_dom_observer.js";
 import {init_window_watch} from "./yt_application_create_plugin/init_window_watch.js";
 import {init_yt_player_application_plugin} from "./yt_application_create_plugin/init_yt_player_application_plugin.js";
 
@@ -17,7 +18,7 @@ export function entry_point() {
 		attach_resize_handler();
 	}
 	init_blob_logger();
-	init_dom_observer();
+	ui_plugin_attach_to_dom_observer(dom_observer);
 	if(globalThis.window) {
 		init_fetch_plugin();
 		init_g_api();
