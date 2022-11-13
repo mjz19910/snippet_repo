6b1e47687ed95c18bbd57ba0dccecffa0279db6e
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-13 07:52:09 -0700

    u

--- typescript/src/modules/youtube_plugin/loose_plugins/init_override_getInitialData.ts
+++
@@ -1,7 +0,0 @@
-import {override_prop} from "../property_handler_plugin/override_prop.js"
-import {PropertyHandler} from "../property_handler_plugin/PropertyHandler.js"
-import {do_proxy_call_getInitialData} from "../fetch_result_handler_plugin/do_proxy_call_getInitialData.js"
-
-export function init_override_getInitialData() {
-	override_prop(window,"getInitialData",new PropertyHandler("getInitialData",do_proxy_call_getInitialData))
-}
