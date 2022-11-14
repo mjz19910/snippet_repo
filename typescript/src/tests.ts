2f4bb0c07692ef9e04deb04055be0a75efde2e93
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-14 10:31:24 -0700

    u

--- typescript/src/tests.ts
+++ typescript/src/tests.ts
@@ -3,15 +3,23 @@ import * as section_12_6 from "ecma_262/lexer/section_12_6.js";
 import * as section_12 from "ecma_262/lexer/section_12.js";
 import {test_mod_execute_tests} from "./tests_mod/main.js"
 async function do_import<T>(e: string): Promise<T> {
+	let errors=[];
 	try {
 		let module_ = await import(e);
 		module_load_success(module_);
 		return module_;
 	} catch(result_1) {
-		console.log(result_1);
-		module_load_failure();
-		throw result_1;
+		errors.push(result_1);
 	}
+	try {
+		let module_ = await import(e+".js");
+		module_load_success(module_);
+		return module_;
+	} catch(result_1) {
+		errors.push(result_1);
+	}
+	module_load_failure();
+	throw new AggregateError(errors);
 }
 function module_load_success(e: {run_tests: () => void}) {
 	e.run_tests()
