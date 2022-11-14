f32cd38d0a8dd73d530adbdb55a55057af329bd7
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-14 10:37:45 -0700

    u

--- typescript/src/tests.ts
+++ typescript/src/tests.ts
@@ -1,18 +1,19 @@
 import * as section_12_8_6 from "ecma_262/lexer/section_12_8_6.js";
 import * as section_12_6 from "ecma_262/lexer/section_12_6.js";
 import * as section_12 from "ecma_262/lexer/section_12.js";
-import {test_mod_execute_tests} from "./tests_mod/main.js"
+import {test_mod_execute_tests} from "./tests_mod/main.js";
 async function do_import<T>(e: string): Promise<T> {
 	let errors=[];
 	try {
-		let module_ = await import(e);
+		let module_=await import(e);
 		module_load_success(module_);
 		return module_;
 	} catch(result_1) {
 		errors.push(result_1);
 	}
 	try {
-		let module_ = await import(e+".js");
+		let module_string="build/"+e+".js";
+		let module_=await import(module_string);
 		module_load_success(module_);
 		return module_;
 	} catch(result_1) {
@@ -22,16 +23,16 @@ async function do_import<T>(e: string): Promise<T> {
 	console.log(errors);
 	throw new AggregateError(errors);
 }
-function module_load_success(e: {run_tests: () => void}) {
-	e.run_tests()
-	console.log("test completed")
+function module_load_success(e: {run_tests: () => void;}) {
+	e.run_tests();
+	console.log("test completed");
 }
 function module_load_failure() {
-	console.log("failed to load module")
+	console.log("failed to load module");
 }
 
 test_mod_execute_tests([
-	["section_12_8_6",function(): Promise<typeof section_12_8_6> {return do_import("./ecma_262/section_12_8_6")}],
-	["section_12_6",function(): Promise<typeof section_12_6> {return do_import("./ecma_262/section_12_6")}],
-	["section_12",function(): Promise<typeof section_12> {return do_import("./ecma_262/section_12")}],
-])
+	["section_12_8_6",function(): Promise<typeof section_12_8_6> {return do_import("./ecma_262/section_12_8_6");}],
+	["section_12_6",function(): Promise<typeof section_12_6> {return do_import("./ecma_262/section_12_6");}],
+	["section_12",function(): Promise<typeof section_12> {return do_import("./ecma_262/section_12");}],
+]);
