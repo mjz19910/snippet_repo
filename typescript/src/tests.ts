41d730000a9b33253be5d2b2c1c51ad57b395e88
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-14 10:25:18 -0700

    u

--- typescript/src/tests.ts
+++ typescript/src/tests.ts
@@ -1,9 +1,13 @@
+import {section_12_8_6} from "ecma_262/lexer/section_12_8_6.js";
 import {test_mod_execute_tests} from "./tests_mod/main.js"
-async function do_import(e: string): Promise<void> {
+async function do_import<T>(e: string): Promise<T> {
 	try {
-		return module_load_success(await import(e))
+		let module_ = await import(e);
+		module_load_success(module_);
+		return module_;
 	} catch(result_1) {
-		return module_load_failure()
+		module_load_failure();
+		throw result_1;
 	}
 }
 function module_load_success(e: {run_tests: () => void}) {
@@ -15,7 +19,7 @@ function module_load_failure() {
 }
 
 test_mod_execute_tests([
-	["section_12_8_6",() => do_import("./ecma_262/section_12_8_6")],
+	["section_12_8_6",function(): Promise<section_12_8_6> {return do_import("./ecma_262/section_12_8_6")}],
 	["section_12_6",() => do_import("./ecma_262/section_12_6")],
 	["section_12",() => do_import("./ecma_262/section_12")],
 ])
