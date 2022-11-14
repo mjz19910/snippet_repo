76cfe7b33b5142fd398f90f24ce48c216915ff1d
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-14 11:59:47 -0700

    u

--- typescript/src/ecma_262/lexer/test/run_test_2.ts
+++ typescript/src/ecma_262/lexer/test/run_test_2.ts
@@ -3,11 +3,9 @@ import {Dispatcher} from "../Dispatcher.js";
 import {test_2_critical} from "./test_2_critical.js";
 
 export function run_test_2() {
-	let dispatcher=new Dispatcher;
+	let input=`(class {#name=12;})`;
+	let dispatcher=new Dispatcher(input);
 	// Test 2 (test_2_code)
-	let test_2=new Test(
-		`(class {#name=12;})`,
-		"(class {#name=12;})[eof]"
-	);
+	let test_2=new Test(input,"(class {#name=12;})[eof]");
 	test_2_critical(dispatcher,test_2);
 }
