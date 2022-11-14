e87260ccfb2102f09196583e9f36bb9686245921
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-14 10:29:47 -0700

    u

--- typescript/src/tests.ts
+++ typescript/src/tests.ts
@@ -1,4 +1,6 @@
-import {section_12_8_6} from "ecma_262/lexer/section_12_8_6.js";
+import * as section_12_8_6 from "ecma_262/lexer/section_12_8_6.js";
+import * as section_12_6 from "ecma_262/lexer/section_12_6.js";
+import * as section_12 from "ecma_262/lexer/section_12.js";
 import {test_mod_execute_tests} from "./tests_mod/main.js"
 async function do_import<T>(e: string): Promise<T> {
 	try {
@@ -20,7 +22,7 @@ function module_load_failure() {
 }
 
 test_mod_execute_tests([
-	["section_12_8_6",function(): Promise<section_12_8_6> {return do_import("./ecma_262/section_12_8_6")}],
-	["section_12_6",() => do_import("./ecma_262/section_12_6")],
-	["section_12",() => do_import("./ecma_262/section_12")],
+	["section_12_8_6",function(): Promise<typeof section_12_8_6> {return do_import("./ecma_262/section_12_8_6")}],
+	["section_12_6",function(): Promise<typeof section_12_6> {return do_import("./ecma_262/section_12_6")}],
+	["section_12",function(): Promise<typeof section_12> {return do_import("./ecma_262/section_12")}],
 ])
