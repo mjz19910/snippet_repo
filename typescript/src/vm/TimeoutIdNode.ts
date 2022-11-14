47d507194db4890e4f042e746d1cb5578b4bade8
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-13 18:20:58 -0700

    u

--- typescript/src/vm/TimeoutIdNode.ts
+++ typescript/src/vm/TimeoutIdNode.ts
@@ -1,11 +1,8 @@
-import {BaseTimeoutNode} from "./BaseTimeoutNode.js"
-
-export class TimeoutIdNode extends BaseTimeoutNode {
-	id: ReturnType<typeof setInterval>|ReturnType<typeof setTimeout>|null
-	m_is_timeout: boolean
+export class TimeoutIdNode {
+	id: ReturnType<typeof setInterval>|ReturnType<typeof setTimeout>|null;
+	m_is_timeout: boolean;
 	constructor(id: ReturnType<typeof setInterval>|ReturnType<typeof setTimeout>|null=null,is_timeout_flag: boolean) {
-		super(void 0)
-		this.id=id
-		this.m_is_timeout=is_timeout_flag
+		this.id=id;
+		this.m_is_timeout=is_timeout_flag;
 	}
 }
