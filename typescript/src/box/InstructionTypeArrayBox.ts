c3d95514b3a9d3ba828c7e4fdfc6a58cc479f49c
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-13 17:49:15 -0700

    u

--- typescript/src/box/InstructionTypeArrayBox.ts
+++ typescript/src/box/InstructionTypeArrayBox.ts
@@ -1,4 +1,4 @@
-import {InstructionType} from "../vm/instruction/InstructionType.js";
+import {InstructionType} from "vm/instruction/InstructionType.js";
 import {BoxVerify} from "./BoxVerify.js";
 import {BoxTemplate} from "./template/BoxTemplate.js";
 
