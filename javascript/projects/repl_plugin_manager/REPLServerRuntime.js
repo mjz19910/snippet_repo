cf5e912b16c7e38989cc2c7ca6652b47a5f3a9ae
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-16 08:28:55 -0700

    u

--- javascript/projects/repl_plugin_manager/REPLServerRuntime.js
+++ javascript/projects/repl_plugin_manager/REPLServerRuntime.js
@@ -17,7 +17,7 @@ export class REPLServerRuntime extends Interface {
 	 * @param {NodeJS.ReadableStream} input
 	 * @param {repl.REPLServer} real_value
 	 */
-	constructor(input, real_value) {
+	constructor(input,real_value) {
 		debugger;
 		super(input);
 		this.X=real_value;
@@ -36,7 +36,7 @@ export class REPLServerRuntime extends Interface {
 	get useGlobal() {return this.X.useGlobal;}
 	get ignoreUndefined() {return this.X.ignoreUndefined;}
 	get writer() {return this.X.writer;}
-	get replMode(){return this.X.replMode;}
+	get replMode() {return this.X.replMode;}
 	get last() {return this.X.last;}
 	/**
 	 * @param {string} evalCmd
@@ -44,8 +44,8 @@ export class REPLServerRuntime extends Interface {
 	 * @param {string} file
 	 * @param {(err: Error | null, result: any) => void} cb
 	 */
-	eval(evalCmd, context, file, cb) {
-		return this.X.eval(evalCmd, context, file, cb);
+	eval(evalCmd,context,file,cb) {
+		return this.X.eval(evalCmd,context,file,cb);
 	}
 	get lastError() {return this.X.lastError;}
 	get completer() {return this.X.completer;}
@@ -66,10 +66,10 @@ export class REPLServerRuntime extends Interface {
 	/**
 	 * @type {this['X']['setupHistory']}
 	 * @param {string} path
-	 * @param {import('REPLServerRuntime.ty.js').CallbackType} callback
+	 * @param {import('./REPLServerRuntime.ty.js').CallbackType} callback
 	 */
-	setupHistory(path, callback) {
-		this.X.setupHistory(path, callback)
+	setupHistory(path,callback) {
+		this.X.setupHistory(path,callback);
 	}
 	historySize=0;
 	removeHistoryDuplicates=false;
