c2c120a13b6a39a802da8b3c3d03a24ccb3acec4
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-16 07:49:21 -0700

    u

--- javascript/projects/repl_plugin_manager/plugins/REPLPlugin.js
+++ javascript/projects/repl_plugin_manager/plugins/REPLPlugin.js
@@ -1,12 +1,9 @@
-import {repl_activator} from "../mod.js";
 export class REPLPlugin {
-	/** @param {repl_activator} repl @param {{}} state */
+	/** @param {{}} repl @param {{}} state */
 	constructor(repl,state) {
 		this.repl=repl;
 		this.state=state;
 	}
 	enable() {}
 }
-export function use_types() {
-	repl_activator;
-}
+
