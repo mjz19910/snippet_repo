0bff976e039d3039c9a428666e650466657dabed
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-16 07:48:42 -0700

    u

--- javascript/projects/repl_plugin_manager/mod.js
+++ javascript/projects/repl_plugin_manager/mod.js
@@ -1,5 +1,5 @@
 import {ReplPluginReplSupport} from "./ReplPluginReplSupport.js"
-import {repl_plugin_get_global_repl_activator} from "./get_repl_activator";
+import {repl_plugin_get_global_repl_activator} from "./repl_plugin_get_global_repl_activator";
 import {g_repl_activator} from "g_repl_activator.js";
 
 export function use_imports() {
