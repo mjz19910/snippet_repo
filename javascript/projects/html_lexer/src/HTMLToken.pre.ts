5a2c17d7d39ec42a2c9da98e7a1437b36ee67c87
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-17 10:46:29 -0700

    u

--- javascript/projects/html_lexer/src/HTMLToken.pre.ts
+++ javascript/projects/html_lexer/src/HTMLToken.pre.ts
@@ -1,3 +1,4 @@
+import {any} from "../../browser_fake_dom/src/any.js";
 import {HTMLToken_Type} from "./HTMLToken_Type";
 import {SourcePosition} from "./SourcePosition.js";
 
@@ -64,8 +65,26 @@ export function use_types() {
         CodePoint,
         __stringify,
         ak_verification_failed,
+        HTMLTokenBase,
     ] as const;
     return [
         ...ex,HTMLToken_Type,SourcePosition
     ] as const;
+}
+
+abstract class HTMLTokenBase {
+    static Position=SourcePosition;
+    static Type=HTMLToken_Type;
+    m_type: HTMLToken_Type|null;
+    m_data: Variant<[Empty, u32, OwnPtr<DoctypeData>, OwnPtr<Vector<Attribute>>]>;
+    m_code_point: CodePoint|null = null;
+    m_start_position: SourcePosition|null = null;
+    m_string_data: any;
+    abstract set_code_point(v:CodePoint):void;
+    constructor() {
+        this.m_type=HTMLToken_Type.Invalid;
+        this.m_data=new Variant(any([]));
+        this.m_data.set(Empty);
+        this.set_code_point(new CodePoint);
+    }
 }
