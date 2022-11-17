1868a0b817c33c8b54106dd01797aba130d1944c
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-17 00:03:57 -0700

    u

--- javascript/modules/DebugAPI/src/compress/CompressTemplateType.ts
+++ javascript/modules/DebugAPI/src/compress/CompressTemplateType.ts
@@ -4,8 +4,31 @@ import {AnyOrRepeat} from "../repeat/AnyOrRepeat.js";
 import {BaseCompression} from "./BaseCompression.js";
 import {CompressState} from "./CompressState.js";
 
-export class CompressTemplateType {
+export class CompressTemplateType<T extends InstanceType<U>,U extends ConstructorWithSymbolType> {
+	i: number;
+	arr: T[]=[];
+	ret: AnyOrRepeat<T>[]=[];
+	constructor_key: U;
+	constructor(arr: T[],constructor_key: U) {
+		this.i=0;
+		this.arr=arr;
+		this.constructor_key=constructor_key;
+		this.ret=[];
+	}
+	try_compress_T_this() {
+		let state=this;
+		for(;state.i<state.arr.length;state.i++) {
+			let item=state.arr[state.i];
+			let use_item=this.compress_rle_T_X(item);
+			if(use_item) continue;
+			state.ret.push(item);
+		}
+		return BaseCompression.compress_result_state(state);
+	}
 	try_compress_T<U extends ConstructorWithSymbolType,T extends InstanceType<U>>(arr: T[],constructor_key: U): [true,AnyOrRepeat<T>[]]|[false,T[]] {
+		let sq=new CompressTemplateType(arr,constructor_key);
+		sq.try_compress_T_this();
+		sq.arr;
 		let state: CompressState<T,AnyOrRepeat<T>>=new CompressState(arr);
 		for(;state.i<state.arr.length;state.i++) {
 			let item=state.arr[state.i];
@@ -15,12 +38,25 @@ export class CompressTemplateType {
 		}
 		return BaseCompression.compress_result_state(state);
 	}
-	compress_rle_T_X<U extends ConstructorWithSymbolType,T extends InstanceType<U>>(state: CompressState<T,AnyOrRepeat<T>>,item: T,constructor_key: U) {
+	compress_rle_T_X_this(item: T,constructor_key: U) {
+		let state=this;
 		if(state.i+1>=state.arr.length&&item!==state.arr[state.i+1]) return false;
 		let off=1;
 		while(item===state.arr[state.i+off]) off++;
 		if(off==1) return false;
 		let mp=Repeat.N.get_map_T(constructor_key,item);
+		Repeat.get_with(mp,item,off);
+		state.ret.push(new Repeat(item,off));
+		state.i+=off-1;
+		return true;
+	}
+	compress_rle_T_X(item: T) {
+		let state=this;
+		if(state.i+1>=state.arr.length&&item!==state.arr[state.i+1]) return false;
+		let off=1;
+		while(item===state.arr[state.i+off]) off++;
+		if(off==1) return false;
+		let mp=Repeat.N.get_map_T(this.constructor_key,item);
 		Repeat.get_with(mp,item,off);
 		state.ret.push(new Repeat(item,off));
 		state.i+=off-1;
