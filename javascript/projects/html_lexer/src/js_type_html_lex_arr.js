21ffe2fe687369efb1c2d11b36555a0a9d815eb4
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-17 06:30:49 -0700

    u

--- javascript/projects/html_lexer/src/js_type_html_lex_arr.js
+++ javascript/projects/html_lexer/src/js_type_html_lex_arr.js
@@ -1,11 +1,10 @@
-import {HTMLSpecialLexBox} from "../../box_plugin/index.js"
-import {HTMLDataLexBox} from "../../box_plugin/index.js"
-import {HTMLEntityLexBox} from "../../box_plugin/index.js"
+import {HTMLDataLexBox,HTMLEntityLexBox,HTMLSpecialLexBox} from "../../box_plugin/index.js";
+
 /**@arg {1|2|3} mode */
 export function js_type_html_lex_arr(mode) {
 	switch(mode) {
-		case 1: return new HTMLSpecialLexBox
-		case 2: return new HTMLDataLexBox
-		case 3: return new HTMLEntityLexBox
+		case 1: return new HTMLSpecialLexBox;
+		case 2: return new HTMLDataLexBox;
+		case 3: return new HTMLEntityLexBox;
 	}
 }
