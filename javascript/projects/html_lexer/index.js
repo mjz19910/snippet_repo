5ca8cb845887e548dee1e7803eb8468000dff86c
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-17 12:48:09 -0700

    u

--- javascript/projects/html_lexer/index.js
+++ javascript/projects/html_lexer/index.js
@@ -1,5 +1,5 @@
 export {HTMLLexerResult} from "./src/HTMLLexerResult.js";
-export {HTMLTokenizer} from "./src/HTMLTokenizer.jstxt/index.js";
+export {HTMLTokenizer} from "./src/HTMLTokenizer.js";
 export {html_parser_callback} from "./src/html_parser_callback.js";
 export {on_html_lex_result} from "./src/on_html_lex_result.js";
 export {lex_html} from "./src/lex_html.js";
