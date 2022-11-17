a5aac26b1cabd1a78ffc47d975f27eaec9397982
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-17 06:06:21 -0700

    u

--- javascript/projects/html_lexer/src/HTMLTokenizer.inc.js
+++ javascript/projects/html_lexer/src/HTMLTokenizer.inc.js
@@ -12,8 +12,8 @@ import {dbgln_if} from "./dbgln_if.js";
 import {Utf8CodePointIterator} from "./Utf8CodePointIterator.js";
 import {NullOptional,Optional} from "./Optional.js";
 import {Utf8View} from "./Utf8View.js";
-
-const TOKENIZER_TRACE_DEBUG=false;
+import {state_name} from "./state_name.js";
+import {TOKENIZER_TRACE_DEBUG} from "./defines.js";
 
 export class HTMLTokenizer extends HTMLTokenizerH {
     m_utf8_view=new Utf8View;
@@ -68,6 +68,17 @@ export class HTMLTokenizer extends HTMLTokenizerH {
             return new NullOptional();
         return new Optional(it.deref());
     }
+    /**
+     * @param {number} n
+     */
+    nth_last_position(n) {
+        if(n+1>this.m_source_positions.size()) {
+            dbgln_if(TOKENIZER_TRACE_DEBUG,"(Tokenizer::nth_last_position) Invalid position requested: {}th-last of {}. Returning (0-0).",n,this.m_source_positions.size());
+            return new HTMLToken.Position(0,0);
+        };
+        return this.m_source_positions.at(this.m_source_positions.size()-1-n);
+    }
+    next_token() {}
     dont_consume_next_input_character() {
         this.restore_to(this.m_prev_utf8_iterator);
     }
@@ -89,29 +100,31 @@ export class HTMLTokenizer extends HTMLTokenizerH {
         this.m_utf8_iterator=new_iterator;
     }
     emit_eof() {
-        if(this.m_has_emitted_eof)
-            return {};
+        if(this.m_has_emitted_eof) return new NullOptional();
         this.m_has_emitted_eof=true;
         this.create_new_token(HTMLToken.Type.EndOfFile);
         this.will_emit(this.m_current_token);
         this.m_queued_tokens.push(this.m_current_token);
-        return this.m_queued_tokens.shift();
+        let last_token=this.m_queued_tokens.shift();
+        if(last_token===void 0) {
+            return new NullOptional;
+        }
+        return new Optional(this.m_queued_tokens.shift());
     }
-    /**
-     * @param {HTMLToken | null} m_current_token
-     */
+    /** @param {HTMLToken | null} m_current_token */
     will_emit(m_current_token) {
         m_current_token;
         throw new Error("Method not implemented.");
     }
-    /**
-     * @param {string} code_point
-     * @param {State} new_state
-     */
+    /** @param {string} code_point @param {State} new_state */
     emit_character_and_reconsume_in(code_point,new_state) {
         this.m_queued_tokens.push(HTMLToken.make_character(code_point));
         this.will_reconsume_in(new_state);
         this.m_state=new_state;
+    }
+    /** @param {State} new_state */
+    will_reconsume_in(new_state) {
+        dbgln_if(TOKENIZER_TRACE_DEBUG,"[{}] Reconsume in {}",state_name(this.m_state),state_name(new_state));
     }
     /**@type {Extract<typeof State[keyof typeof State], number>}*/
     m_state=0;
@@ -151,16 +164,6 @@ export class HTMLTokenizer extends HTMLTokenizerH {
         }
 
         this.m_current_token.set_start_position("Badge_HTMLTokenizer",this.nth_last_position(offset));
-    }
-    /**
-     * @param {number} n
-     */
-    nth_last_position(n) {
-        if(n+1>this.m_source_positions.size()) {
-            dbgln_if(TOKENIZER_TRACE_DEBUG,"(Tokenizer::nth_last_position) Invalid position requested: {}th-last of {}. Returning (0-0).",n,this.m_source_positions.size());
-            return new HTMLToken.Position(0,0);
-        };
-        return this.m_source_positions.at(this.m_source_positions.size()-1-n);
     }
     /**@arg {State} next_state */
     reconsume_in(next_state) {
