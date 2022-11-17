aac8dd64afc16d8819989c7882b2cba9b85b9c7e
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-16 17:01:37 -0700

    u

--- javascript/projects/browser_fake_dom/index.js
+++ javascript/projects/browser_fake_dom/index.js
@@ -3,5 +3,6 @@ export {do_create_window} from "./src/browse/do_create_window.js";
 export {fake} from "./src/browse/mod.js";
 export {handle_onPageLoadStarted} from "./src/event/handle_onPageLoadStarted.js";
 export {FakeDocument} from "./src/FakeDocument.js";
+export {FakeWindow} from "./src/FakeWindow.js";
 export {PageLoadStateType} from "./src/types/PageLoadStateType.js";
 
