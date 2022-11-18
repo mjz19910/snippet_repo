2f11bbd9fc3f225f26ec6c1ac35d03e9706ad661
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-18 01:25:29 -0700

    u

--- javascript/projects/js_browser/src/mod.js
+++ javascript/projects/js_browser/src/mod.js
@@ -3,11 +3,6 @@ import {on_page_data_loaded,PageLoaderState} from "../../page_loader/index.js";
 import {repl_plugin_get_global_repl_activator} from "../../repl_plugin/src/repl_plugin_get_global_repl_activator.js";
 
 /**
- * @param {PageLoaderState} state
- */
-export function init_wget(state) {
-}
-/**
  * @arg {Uint8Array} page_content
  * @arg {string} page_url
  * @param {PageLoaderState} state
@@ -30,15 +25,4 @@ export function wget_on_static_page_load(state,page_content,page_url) {
 	if(repl) {
 		repl.on_finished();
 	}
-}
-export const use_mod={
-	get imports() {
-		/**@type {{}[]}*/
-		let t=[];
-		return t;
-	}
-};
-
-export function get_use_data() {
-	return use_mod;
 }
