9adb124b799b5b08a0a5f3a091bd765c5218a458
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-17 21:41:38 -0700

    u

--- javascript/projects/browser_fake_dom/src/Badge.js
+++ javascript/projects/browser_fake_dom/src/Badge.js
@@ -7,12 +7,11 @@ export class Badge {
 		if(!badge) throw create_missing_badge_err();
 		if(badge instanceof Badge) {
 			if(!badge.is_valid) throw create_validation_err();
-			if(!badge.is_valid()) throw create_validation_err();
 		}
 		throw create_wrong_prototype_err();
 	}
-	/**@type {undefined|(()=>true)} */
-	is_valid=() => true;
+	/**@type {true|undefined} */
+	is_valid=true;
 }
 function create_validation_err() {
 	return new Error("Badge is invalid");
