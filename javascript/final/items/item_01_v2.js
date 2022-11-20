b0f320cd93f0a19c87029948ceb1cdbe25feeab8
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-20 06:42:39 -0700

    u

--- javascript/final/items/item_01_v2.js
+++ javascript/final/items/item_01_v2.js
@@ -4,13 +4,19 @@ v1 (old): snippet_repo_v2/javascript/final/items/item_01_v1.js
 v2 (old): snippet_repo_v2/javascript/final/items/item_01_v2.js
 v3 (unk)
 */
-class Element {}
+class RElement {
+	/** @param {any} v */
+	constructor(v) {
+		this.v=v;
+	}
+}
 class Transmutation {
+	/** @param {RElement} arg1 @param {number} arg1_count @param {RElement} arg2 @param {number} arg2_count */
 	constructor(arg1, arg1_count, arg2, arg2_count) {
 		this.data = [arg1, arg1_count, arg2, arg2_count];
 	}
 }
 const RECIPES=[
-	new Transmutation(new Element("Copper"), 2, new Element("Silver"), 1),
-	new Transmutation(new Element("Silver"), 5, new Element("Gold"), 2)
+	new Transmutation(new RElement("Copper"), 2, new RElement("Silver"), 1),
+	new Transmutation(new RElement("Silver"), 5, new RElement("Gold"), 2)
 ];
