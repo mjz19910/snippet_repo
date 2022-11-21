a29970d557dbccd83c8107f6a16b5b062dc63c53
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-21 10:35:25 -0700

    u

--- javascript/projects/js_browser/src/parse_dom_preload_2.js
+++ javascript/projects/js_browser/src/parse_dom_preload_2.js
@@ -1,9 +1,61 @@
 import * as path from "path";
 import * as process from "process";
 
+class UndefinedParseResult {
+	/**
+	 * @param {boolean} fallback
+	 */
+	bool(fallback) {
+		return fallback;
+	}
+}
+
+class JSONParseResult {
+	/**
+	 * @param {any} value
+	 */
+	constructor(value){
+		this.value = value;
+	}
+	/**
+	 * @param {boolean} fallback
+	 */
+	bool(fallback) {
+		if(typeof this.value==='boolean'){
+			return this.value;
+		}
+		return fallback;
+	}
+}
+
+/**
+ * @param {string | undefined} env_value
+ */
+function try_parse_env(env_value) {
+	if(env_value === undefined) {
+		return new UndefinedParseResult;
+	}
+	return new JSONParseResult(JSON.parse(env_value));
+}
+
+const debug=try_parse_env(process.env.DEBUG).bool(false);
+
 process.on('unhandledRejection',(/** @type {unknown} */ error) => {
 	console.log('unhandled promise rejection',error);
 });
+
+/**
+ * @param {{}} state
+ */
+async function do_browse(state) {
+	let res=await new_FetchRequestState(url);
+	let lexer=await import_ipc_plugin(ipc_loader_state,"html_lexer");
+	if(!lexer) throw new Error("Can't import lexer plugin");
+	let repl_plugin=await import_ipc_plugin(ipc_loader_state,"repl_plugin_manager/mod.js");
+	if(debug) console.log('repl plug',repl_plugin);
+	await Promise.resolve();
+	await fetch_url(res);
+}
 
 async function async_main() {
 	let node_path=process.argv[0];
@@ -36,6 +88,8 @@ Options:
 `);
 		return;
 	}
+	let state={}
+	await do_browse(state);
 }
 
 try {
@@ -45,3 +99,4 @@ try {
 	console.log(err);
 	throw err;
 }
+
