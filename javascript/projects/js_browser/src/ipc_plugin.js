0230f974bb3cafb6d8437c8d3ab49e6c3c761fc7
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-21 11:23:06 -0700

    u

--- javascript/projects/js_browser/src/ipc_plugin.js
+++ javascript/projects/js_browser/src/ipc_plugin.js
@@ -24,7 +24,8 @@ export async function import_ipc_plugin(state,plugin_key) {
 			try{
 				return await ReplPluginManagerModule.import_ipc_plugin(state,plugin_key,module_page_loader_str);
 			} catch(/**@type {any}*/e) {
-				console.error("import error", e);
+				console.error("import error");
+				console.error(e);
 				// recapture the stack trace so it includes async frames
 				Error.captureStackTrace(e);
 				throw e;
