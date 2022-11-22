f5d3d1bac9e4e5166f87b8238e433190e81ed99f
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-22 08:37:09 -0700

    u

--- javascript/final/named/Impl.ts
+++ javascript/final/named/Impl.ts
@@ -1,4 +1,16 @@
 export namespace Impl {
+	export class CustomInputMatcher {
+		test_string: string|RegExp;
+		test_needle: string|RegExp;
+		ts_get: unknown;
+		str?: string;
+		constructor(test_string: string|RegExp,string_getter: unknown) {
+			this.test_string=test_string;
+			this.test_needle="";
+			this.ts_get=string_getter;
+		}
+	}
+
 	export class DebugAPI {
 		asyncExecuteEval(gameiframe: any,function_: () => number): void {
 			gameiframe;
--- javascript/final/named/__global.ts
+++ javascript/final/named/__global.ts
@@ -10,45 +10,9 @@ declare global {
 	}
 
 	var debugApi: Impl.DebugAPI;
-	var CustomInputMatcher: typeof X.CustomInputMatcher;
+	var CustomInputMatcher: typeof Impl.CustomInputMatcher;
 
 	var __ret: any;
-	var __w: {I_listener: any; game_scope: any; obj_field?: any;};
-	var __m: {click: (arg0: number,arg1: number) => void; opened: {field: {[x: string]: undefined;}; get: (arg0: number,arg1: number) => boolean;}; mines: {field: {[x: string]: boolean;};};}|null;
-	var __r_ret: any;
-	var __res: any[];
-	var __instance: {constructor: any;};
-	var __nx_names: never[];
-
-	var Upgrade: {apply: (arg0: any) => {(): any; new(): any; toNumber: {(): any; new(): any;};};};
-	var game: {upgrades: {maxObjects: any;}; mergeObjects: string|any[]; spawnTime: {cd: any;}; matter: any;};
-	var gameFunctions: {decreaseSpawnCooldown: (arg0: number) => void; maxUpgrades: (arg0: any,arg1: any) => void;};
-
-	interface Document {
-		gameiframe: any;
-	}
-}
-namespace X {
-	export class CustomInputMatcher {
-		test_string: string|RegExp;
-		test_needle: string|RegExp;
-		ts_get: unknown;
-		str?: string;
-		constructor(test_string: string|RegExp,string_getter: unknown) {
-			this.test_string=test_string;
-			this.test_needle="";
-			this.ts_get=string_getter;
-		}
-	}
-}
-
-declare global {
-	interface Window {
-		cint?: number;
-		citv?: number;
-	}
-	var cint: ReturnType<typeof setTimeout>|undefined;
-	var citv: ReturnType<typeof setInterval>|undefined;
 }
 
 declare global {
@@ -57,6 +21,14 @@ declare global {
 
 // lunar-atoms-tycoon.js
 declare global {
+	var Upgrade: {apply: (arg0: any) => {(): any; new(): any; toNumber: {(): any; new(): any;};};};
+	var game: {upgrades: {maxObjects: any;}; mergeObjects: string|any[]; spawnTime: {cd: any;}; matter: any;};
+	var gameFunctions: {decreaseSpawnCooldown: (arg0: number) => void; maxUpgrades: (arg0: any,arg1: any) => void;};
+
+	interface Document {
+		gameiframe: any;
+	}
+
 	interface Window {
 		event_info;
 		x2;
