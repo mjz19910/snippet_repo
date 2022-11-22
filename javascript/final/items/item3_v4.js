231b6c04f84d2e4b9374dce0035c61ed2a3b06f9
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-22 08:52:46 -0700

    u

--- javascript/final/items/item3_v4.js
+++ javascript/final/items/item3_v4.js
@@ -1,6 +1,5 @@
 /* spell:words
 --- version_list item 3 ---
-v2 (old): snippet_repo/javascript/final/items/item3_v2.js
 v3 (old): snippet_repo/javascript/final/items/item3_v3.js
 v4 (cur): snippet_repo/javascript/final/items/item3_v4.js
 v5 (new): snippet_repo/javascript/group1/sub_a/item-_3.js
