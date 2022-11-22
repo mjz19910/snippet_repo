e3d8589870139fb27748851ead8d91901948ef0b
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-22 10:37:52 -0700

    u

--- javascript/final/named/makiki99_prestige-frame.js
+++ javascript/final/named/makiki99_prestige-frame.js
@@ -3,80 +3,84 @@
 v1 (spl-f): snippet_repo/javascript/final/makiki99_prestige-frame.js
 */
 function main() {
-	var fnlist=[]
-	var fnname=[]
+	var fnlist=[];
+	var fnname=[];
 	{
+		/**
+		 * @param {any} name
+		 * @param {{ user_run_name: any; }} func
+		 */
 		function add_func(name,func) {
-			var y=fnlist.push(func)
+			var y=fnlist.push(func);
 			if(fnname.indexOf(name)>-1) {
-				throw SyntaxError("Name conflict")
+				throw SyntaxError("Name conflict");
 			}
-			var x=fnname.push(name)
-			func.user_run_name=name
+			var x=fnname.push(name);
+			func.user_run_name=name;
 			if(x!=y) {
-				throw SyntaxError("unbalanced function or name number")
+				throw SyntaxError("unbalanced function or name number");
 			}
-			return x
+			return x;
 		}
-		var execute=function(t,pre_exec,post_exec) {
-			var r_fnname=fnname[t]
-			var func=fnlist[t]
+		var execute=function(/** @type {number} */ t,/** @type {{ (fn: any): void; (arg0: any): void; }} */ pre_exec,/** @type {((arg0: any) => void) | undefined} */ post_exec) {
+			var r_fnname=fnname[t];
+			var func=fnlist[t];
 			try {
-				var sf=func.toString()
+				var sf=func.toString();
 				if(sf.indexOf("/*arg_start*/")>-1) {
-					let eval_func
+					let eval_func;
 					{
-						var func_split=sf.split(/(\/\*arg_start\*\/|\/\*arg_end\*\/)/)
-						var no_head=func_split[4].trim().slice(1).trim().slice(1)
-						var body=no_head.slice(0,no_head.length-2)
-						var is_strict
-						var is_strict_p1=body.split('"use strict"')
-						is_strict=is_strict_p1.length>1
+						var func_split=sf.split(/(\/\*arg_start\*\/|\/\*arg_end\*\/)/);
+						var no_head=func_split[4].trim().slice(1).trim().slice(1);
+						var body=no_head.slice(0,no_head.length-2);
+						var is_strict;
+						var is_strict_p1=body.split('"use strict"');
+						is_strict=is_strict_p1.length>1;
 						if(is_strict) {
-							body=is_strict_p1[1].trim()
+							body=is_strict_p1[1].trim();
 						}
-						var args="/*arg_start*/"+func_split[2].trim()+"/*arg_end*/"
-						let src_url='//'+'# sourceURL='+r_fnname
-						let func_str
+						var args="/*arg_start*/"+func_split[2].trim()+"/*arg_end*/";
+						let src_url='//'+'# sourceURL='+r_fnname;
+						let func_str;
 						if(is_strict) {
-							func_str=`"use strict";\nconsole.log("run ${r_fnname}")\n${body}\n${src_url}`
-							eval_func=new Function(args,func_str)
+							func_str=`"use strict";\nconsole.log("run ${r_fnname}")\n${body}\n${src_url}`;
+							eval_func=new Function(args,func_str);
 						} else {
-							func_str=`console.log("run ${r_fnname}")\n${body}\n${src_url}`
-							eval_func=new Function(args,func_str)
+							func_str=`console.log("run ${r_fnname}")\n${body}\n${src_url}`;
+							eval_func=new Function(args,func_str);
 						}
 						if('mc' in window&&window.mc instanceof MessageChannel) {
 							let mc=window.mc;
-							mc.port2.onmessage=function() {}
-							mc.port2.close()
-							mc.port1.onmessage=function() {}
-							mc.port1.close()
-							delete window.mc
+							mc.port2.onmessage=function() {};
+							mc.port2.close();
+							mc.port1.onmessage=function() {};
+							mc.port1.close();
+							delete window.mc;
 							if(typeof mc!='undefined') {
-								window.mc=undefined
+								window.mc=undefined;
 							}
 						}
-						console.log("fi:",eval_func.name=="anonymous","len:",eval_func.length)
+						console.log("fi:",eval_func.name=="anonymous","len:",eval_func.length);
 					}
 					if(eval_func) {
-						eval_func(func)
+						eval_func(func);
 					}
-					let ret=eval_func()
+					let ret=eval_func();
 					if(post_exec)
-						post_exec(ret)
-					return ret
+						post_exec(ret);
+					return ret;
 				} else {
 					if(pre_exec) {
-						pre_exec(func)
+						pre_exec(func);
 					}
-					let ret=func()
+					let ret=func();
 					if(post_exec)
-						post_exec(ret)
-					return ret
+						post_exec(ret);
+					return ret;
 				}
 			} finally {}
-			return
-		}
+			return;
+		};
 		let stt=eval(`(class {
 			static #unused = this.#init()
 			static #init(){
@@ -85,160 +89,155 @@ function main() {
 			static _n = "<empty>"
 			static n_on = true
 			static f_on = true
-		})`)
+		})`);
 		window.CustomInputMatcher=class {
+			/**
+			 * @param {any} t_needle
+			 * @param {any} t_string_getter
+			 */
 			constructor(t_needle,t_string_getter) {
-				this.ts_get=t_string_getter
-				this.tr=t_needle
+				this.ts_get=t_string_getter;
+				this.tr=t_needle;
 			}
 			get test_string() {
-				return this.ts_get()
+				return this.ts_get();
 			}
 			get test_needle() {
-				return this.tr
+				return this.tr;
 			}
-		}
+		};
 		var cur=class extends stt {
 			static get f() {
-				return this._f
+				return this._f;
 			}
 			static set f(f) {
-				let cur=this._ln
-				this._lf=f
+				let cur=this._ln;
+				this._lf=f;
 				if(fnlist.indexOf(this._lf)==-1) {
-					add_func(this._ln,this._lf)
+					add_func(this._ln,this._lf);
 				}
 				if(cur instanceof CustomInputMatcher&&typeof cur.test_string=='string') {
-					let custom_str=cur.test_string
-					let needle=cur.test_needle
+					let custom_str=cur.test_string;
+					let needle=cur.test_needle;
 					if(custom_str.match(needle)==null) {
-						this._f=f
-						return
+						this._f=f;
+						return;
 					}
 				}
 				if(this.f_on) {
-					this.f_on=false
-					this._f=f
+					this.f_on=false;
+					this._f=f;
 				}
 			}
 			static get n() {
-				return this._n
+				return this._n;
 			}
 			static set n(n) {
-				let cur=n
+				let cur=n;
 				if(cur instanceof CustomInputMatcher) {
-					let custom_str=cur.test_string
-					let m_needle=cur.test_needle
+					let custom_str=cur.test_string;
+					let m_needle=cur.test_needle;
 					if(m_needle instanceof RegExp&&typeof custom_str=='string') {
-						let m_match=custom_str.match(m_needle)
+						let m_match=custom_str.match(m_needle);
 						if(m_match==null) {
-							this._ln=n
-							return
+							this._ln=n;
+							return;
 						} else if(this.rx_off===undefined) {
-							this.rx_off=true
-							this.rx_lx=n
+							this.rx_off=true;
+							this.rx_lx=n;
 						}
 					}
 					if(typeof m_needle=='string'&&custom_str!=m_needle) {
-						this._ln=n
-						return
+						this._ln=n;
+						return;
 					}
 				}
-				this._ln=n
+				this._ln=n;
 				if(this.n_on) {
-					this.n_on=false
-					this._n=n
+					this.n_on=false;
+					this._n=n;
 				}
 			}
-		}
-		let sym=Symbol()
-		var cur__class={[sym]: cur}
-		cur.self_sym=sym
-		cur.funcs=fnlist
-		cur.names=fnname
+		};
+		let sym=Symbol();
+		var cur__class={[sym]: cur};
+		cur.self_sym=sym;
+		cur.funcs=fnlist;
+		cur.names=fnname;
 	}
-	cur.n=new CustomInputMatcher("https://makiki99.github.io/prestige-frame/",() => location.href)
+	cur.n=new CustomInputMatcher("https://makiki99.github.io/prestige-frame/",() => location.href);
 	cur.f=function() {
 		class IframeExt extends HTMLIFrameElement {
 			constructor() {
-				super()
+				super();
 			}
 			get src() {
-				return super.src
+				return super.src;
 			}
 			set src(e) {
 				switch(e) {
 					case 'https://makiki99.github.io/prestige':
-						this.width=800
-						this.height=424
-						break
+						this.width=""+800;
+						this.height=""+424;
+						break;
 					case 'https://makiki99.github.io/metaprestige/':
-						this.width=800
-						this.height=472
-						break
+						this.width=""+800;
+						this.height=""+472;
+						break;
 				}
-				super.src=e
-			}
-			getClientCode() {
-				return 0,
-					`x:{
-if(activatePrestige.length==3){
-nx_l=function(x,y,l){return nx_df(x,y,l,x,y,l+1)};nx_x=function(x,y,l){return nx_df(x,y,l,x+1,y,l)};nx_y=function(x,y,l){return nx_df(x,y,l,x,y+1,l)};nx_df=function(x1,y1,l1,x2,y2,l2){let ret,nx=getRequirement(x2,y2,l2);for(let i=0;i<=nx;i++){if (canActivatePrestige(x1,y1,l1)){activatePrestige(x1,y1,l1)}else{ret=[i,nx];break}};activatePrestige(x2,y2,l2);if(!ret)ret=[-1,nx];return ret}
-}else{
-nx_x=function(x,y){return nx_df(x,y,x+1,y)};nx_y=function(x,y){return nx_df(x,y,x,y+1)};nx_df=function(x1,y1,x2,y2){let ret,nx=getRequirement(x2,y2);for(let i=0;i<=nx;i++){if (canActivatePrestige(x1,y1)){activatePrestige(x1,y1)}else{ret=[i,nx];break}};activatePrestige(x2,y2);if(!ret)ret=[-1,nx];return ret}
-}
-}
-if(activatePrestige.length==3){
-al:for(let i=0;i<7;i++){let r=[...nx_x(0,0,0),...nx_y(0,0,0),...nx_l(0,0,0)];let zc=0,om=[];for(let y=0;y<r.length;y+=2){if(r[y]==0){zc++};om.push(r[y+1])};console.log(om,zc);if(zc===3){break}}
-}else{
-al:for(let i=0;i<6;i++){let r=[...nx_x(0,0),...nx_y(0,0)];let zc=0,om=[];for(let y=0;y<r.length;y+=2){if(r[y]==0){zc++};om.push(r[y+1])};console.log(om,zc);if(zc===2){break}}
-}`
+				super.src=e;
 			}
 		}
 		Object.defineProperty(IframeExt.prototype,Symbol.toStringTag,{
 			value: 'HTMLIFrameExtElement',
 			configurable: true
-		})
-		window.IframeExt=IframeExt
-		fr=document.getElementById('frames')
+		});
+		window.IframeExt=IframeExt;
+		let fr=document.getElementById('frames');
 		if(fr.rows.length==0) {
-			fr.insertRow()
+			fr.insertRow();
 		}
-		frame_row=fr.rows[0]
-		let make_cell=function(c_row) {
+		frame_row=fr.rows[0];
+		let make_cell=function(/** @type {{ cells: string | any[]; insertCell: () => any; }} */ c_row) {
 			if(c_row.cells.length==0) {
-				return c_row.insertCell()
+				return c_row.insertCell();
 			}
-			return c_row.cells[c_row.cells.length-1]
-		}
-		cd=make_cell(frame_row)
+			return c_row.cells[c_row.cells.length-1];
+		};
+		cd=make_cell(frame_row);
+		/**
+		 * @param {{ children: any; }} cd
+		 */
 		function run_for_cell(cd) {
-			[...cd.children].map(e => e.remove())
+			[...cd.children].map(e => e.remove());
 		}
-		run_for_cell(cd)
+		run_for_cell(cd);
 		if(customElements.get('iframe-ext')) {
-			location.reload()
-			return
+			location.reload();
+			return;
 		}
 		customElements.define('iframe-ext',IframeExt,{
 			extends: "iframe"
-		})
-		let create_iframe_cell_for_url=function(url) {
-			let frame_row=fr.insertRow()
-			let cd=frame_row.insertCell()
+		});
+		let create_iframe_cell_for_url=function(/** @type {string} */ url) {
+			let frame_row=fr.insertRow();
+			let cd=frame_row.insertCell();
 			rr=document.createElement('iframe',{
 				is: 'iframe-ext'
-			})
-			rr.src=url
-			cd.append(rr)
-		}
+			});
+			rr.src=url;
+			cd.append(rr);
+		};
 		rr=document.createElement('iframe',{
 			is: 'iframe-ext'
-		})
-		rr.src='https://makiki99.github.io/prestige'
-		cd.append(rr)
-		create_iframe_cell_for_url('https://makiki99.github.io/metaprestige/')
-	}
+		});
+		rr.src='https://makiki99.github.io/prestige';
+		cd.append(rr);
+		create_iframe_cell_for_url('https://makiki99.github.io/metaprestige/');
+	};
+	/**
+	 * @param {undefined[]} e
+	 */
 	function do_cur(...e) {
 		var i;
 		if(cur.rx_lx) {
@@ -246,27 +245,27 @@ al:for(let i=0;i<6;i++){let r=[...nx_x(0,0),...nx_y(0,0)];let zc=0,om=[];for(let
 		} else {
 			i=fnname.indexOf(cur.n);
 		}
-		let px_fn=function(fn) {
+		let px_fn=function(/** @type {{ argv: any[]; }} */ fn) {
 			fn.argv=e;
 		};
 		var _result=execute(i,px_fn);
 		return _result;
 	}
-	let ret
+	let ret;
 	if(top!==window) {
 		if(window.debugApi==undefined) {
-			debugApi=new DebugAPI
+			debugApi=new DebugAPI;
 		}
 		//console.log('restart on top frame')
-		ret=debugApi.asyncExecuteFunction(top,main)
+		ret=debugApi.asyncExecuteFunction(top,main);
 	} else {
-		ret=do_cur()
+		ret=do_cur();
 	}
 	if(ret instanceof Promise) {
-		ret.then(() => void 0).catch(e => console.error(e))
+		ret.then(() => void 0).catch(e => console.error(e));
 	}
-	cur.value=ret
-	return {...cur,_class: cur__class}
+	cur.value=ret;
+	return {...cur,_class: cur__class};
 	//# sourceURL=snippet:///%24_2
 }
-window.__ret=main()
+window.__ret=main();
---
+++ javascript/final/named/makiki99_prestige-frame_client_code.js
@@ -0,0 +1,106 @@
+
+if(activatePrestige.length==3) {
+	/**
+	 * @param {number} x
+	 * @param {number} y
+	 * @param {number} l
+	 */
+	function nx_l(x,y,l) {
+		return nx_df(x,y,l,x,y,l+1);
+	}
+	/**
+	 * @param {number} x
+	 * @param {number} y
+	 * @param {number} l
+	 */
+	function nx_x(x,y,l) {
+		return nx_df(x,y,l,x+1,y,l);
+	}
+	/**
+	 * @param {number} x
+	 * @param {number} y
+	 * @param {number} l
+	 */
+	function nx_y(x,y,l) {
+		return nx_df(x,y,l,x,y+1,l);
+	}
+	function nx_df(prestige_a,prestige_b,prestige_count,requirement_a,requirement_b,requirement_count) {
+		let ret,nx=getRequirement(requirement_a,requirement_b,requirement_count);
+		for(let i=0;i<=nx;i++) {
+			if(canActivatePrestige(prestige_a,prestige_b,prestige_count)) {
+				activatePrestige(prestige_a,prestige_b,prestige_count);
+			} else {
+				ret=[i,nx];
+				break;
+			}
+		}
+		activatePrestige(requirement_a,requirement_b,requirement_count);
+		if(!ret)
+			ret=[-1,nx];
+		return ret;
+	}
+	for(let i=0;i<7;i++) {
+		let r=[...nx_x(0,0,0),...nx_y(0,0,0),...nx_l(0,0,0)];
+		let zc=0
+		let om=[];
+		for(let y=0;y<r.length;y+=2) {
+			if(r[y]==0) {
+				zc++;
+			}
+			om.push(r[y+1]);
+		}
+		console.log(om,zc);
+		if(zc===3) {
+			break;
+		}
+	}
+} else {
+	/**
+	 * @param {number} x
+	 * @param {number} y
+	 */
+	function nx_x(x,y) {
+		return nx_df(x,y,x+1,y);
+	}
+	/**
+	 * @param {number} x
+	 * @param {number} y
+	 */
+	function nx_y(x,y) {
+		return nx_df(x,y,x,y+1);
+	}
+	/**
+	 * @param {number} prestige_1_a
+	 * @param {number} prestige_1_b
+	 * @param {number} prestige_2_a
+	 * @param {number} prestige_2_b
+	 */
+	function nx_df(prestige_1_a,prestige_1_b,prestige_2_a,prestige_2_b) {
+		let prestige_2_requirement=getRequirement(prestige_2_a,prestige_2_b);
+		for(let i=0;i<=prestige_2_requirement;i++) {
+			if(canActivatePrestige(prestige_1_a,prestige_1_b)) {
+				activatePrestige(prestige_1_a,prestige_1_b);
+			} else {
+				activatePrestige(prestige_2_a,prestige_2_b);
+				return [i,prestige_2_requirement];
+			}
+		}
+		activatePrestige(prestige_2_a,prestige_2_b);
+		return [-1,prestige_2_requirement];
+	}
+	for(let i=0;i<6;i++) {
+		let r=[...nx_x(0,0),...nx_y(0,0)];
+		let zc=0
+			,om=[];
+		for(let y=0;y<r.length;y+=2) {
+			if(r[y]==0) {
+				zc++;
+			}
+			om.push(r[y+1]);
+		}
+		console.log(om,zc);
+		if(zc===2) {
+			break;
+		}
+	}
+}
