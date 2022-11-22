9a98c0045d22af203d4f2c776cc34e0786319897
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-22 08:53:10 -0700

    u

--- javascript/final/items/item3_v2.js
+++ javascript/final/items/item3_v2.js
@@ -3,7 +3,6 @@
 v1 (old): snippet_repo/javascript/final/items/item3_v1.js
 v2 (cur): snippet_repo/javascript/final/items/item3_v2.js
 v3 (new): snippet_repo/javascript/final/items/item3_v3.js
-v4 (new): snippet_repo/javascript/final/items/item3_v4.js
 */
 function get_location_from_error_stack_lines(error_lines,offset) {
 	let cur_line=error_lines[offset]
