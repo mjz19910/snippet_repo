18e4c52d85186ed0b2841a281ed9ac1e96920c01
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-22 11:52:11 -0700

    u

--- javascript/final/named/kongregate_yet-another-merge-game.js
+++ javascript/final/named/kongregate_yet-another-merge-game.js
@@ -1,6 +1,6 @@
 /* spell:words
 --- version_list item 2 ---
-v1 (spl-f): snippet_repo/javascript/final/kongregate_yet-another-merge-game.js
+v1 (cur): snippet_repo/javascript/final/kongregate_yet-another-merge-game.js
 */
 function main() {
 	/** @type {import("./__global.js").Holder} */
