cc075854ce0928ddf1a0ef913d15e1a1ec51e3b3
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-23 10:10:14 -0700

    u

--- javascript/javascript_parser/ecma_parse_main.js
+++ javascript/javascript_parser/ecma_parse_main.js
@@ -2,9 +2,11 @@ export function ecma_parse_main() {
 	'use strict';
 	{
 		class ecma_12_2 {
-			static _attach(root) {
-				root.export(this,'12.2',['WhiteSpace']);
+			/** @arg {ecma_root} parent */
+			constructor(parent) {
+				this.parent=parent;
 			}
+			/** @returns {[string,number]|null} */
 			WhiteSpace(str,index) {
 				if(str[index]===' ') {
 					return ['WhiteSpace',1];
@@ -12,18 +14,15 @@ export function ecma_parse_main() {
 				if(str[index]==='\t') {
 					return ['WhiteSpace',1];
 				}
-				return [null,0];
+				return null;
 			}
 		}
 		class ecma_12_3 {
-			static the() {
-				if(this._the)
-					return this._the;
-				this._the=new this;
+			/** @arg {ecma_root} parent */
+			constructor(parent) {
+				this.parent=parent;
 			}
-			static _attach(root) {
-				root.export(this,'12.3',['LineTerminator','LineTerminatorSequence']);
-			}
+			/** @returns {[string,number]|null} */
 			LineTerminator(str,index) {
 				let len=0;
 				if(str[index]==='\r')
@@ -39,17 +38,21 @@ export function ecma_parse_main() {
 				if(len>0) {
 					return ['LineTerminator',1];
 				}
-				return [null,0];
+				return null;
 			}
-			LineTerminatorSequence() {
+			/**
+			 * @param {string} a
+			 * @param {number} b
+			 * @returns {number}
+			 */
+			LineTerminatorSequence(a,b) {
 				console.info('LineTerminatorSequence not implemented');
+				return 0;
 			}
 		}
 		class ecma_12_4 {
-			static _attach(root) {
-				let exports=[];
-				exports=Object.getOwnPropertyNames(this.prototype);
-				root.export(this,'12.4',exports);
+			constructor(parent) {
+				this.parent=parent;
 			}
 			Comment(str,index) {
 				let ml_len=this.MultiLineComment(str,index);
@@ -184,6 +187,10 @@ export function ecma_parse_main() {
 			}
 		}
 		class ecma_12_5 {
+			/** @arg {ecma_root} parent */
+			constructor(parent) {
+				this.parent=parent;
+			}
 			/*
 			CommonToken ::
 				IdentifierName
@@ -193,39 +200,45 @@ export function ecma_parse_main() {
 				StringLiteral
 				Template
 			*/
+			get ecma_12_3() {
+				if(!this.m_ecma_12_3) {
+					this.m_ecma_12_3=new ecma_12_3(this);
+				}
+				return this.parent.ecma_12_3;
+			}
 			get ecma_12_6() {
 				if(!this.m_ecma_12_6) {
-					this.m_ecma_12_6=new ecma_12_6;
+					this.m_ecma_12_6=new ecma_12_6(this);
 				}
 				return this.m_ecma_12_6;
 			}
 			get ecma_12_7() {
 				if(!this.m_ecma_12_7) {
-					this.m_ecma_12_7=new ecma_12_7;
+					this.m_ecma_12_7=new ecma_12_7(this);
 				}
 				return this.m_ecma_12_7;
 			}
 			get ecma_12_8_3() {
 				if(!this.m_ecma_12_8_3) {
-					this.m_ecma_12_8_3=new ecma_12_8_3;
+					this.m_ecma_12_8_3=new ecma_12_8_3(this);
 				}
 				return this.m_ecma_12_8_3;
 			}
 			get ecma_12_8_4() {
 				if(!this.m_ecma_12_8_4) {
-					this.m_ecma_12_8_4=new ecma_12_8_4;
+					this.m_ecma_12_8_4=new ecma_12_8_4(this);
 				}
 				return this.m_ecma_12_8_4;
 			}
 			get ecma_12_8_6() {
 				if(!this.m_ecma_12_8_6) {
-					this.m_ecma_12_8_6=new ecma_12_8_6;
+					this.m_ecma_12_8_6=new ecma_12_8_6(this);
 				}
 				return this.m_ecma_12_8_6;
 			}
 			CommonToken(str,index) {
 				let cur=null,item=null,len=0;
-				cur=this.ecma_12_6.IdentifierName(str,index);
+				cur=this.parent.ecma_12_6.IdentifierName(str,index);
 				if(cur[1]>len) {
 					len=cur[1];
 					item=cur;
@@ -259,9 +272,9 @@ export function ecma_parse_main() {
 			}
 		}
 		class ecma_12_6 {
-			static _attach(root) {
-				let exports=Object.getOwnPropertyNames(this.prototype);
-				root.export(this,'12.6',exports);
+			/** @param {ecma_root} parent */
+			constructor(parent) {
+				this.parent=parent;
 			}
 			static source=`
 			PrivateIdentifier ::
@@ -293,7 +306,7 @@ export function ecma_parse_main() {
 			PrivateIdentifier(str,index) {
 				if(str[0]!=='#')
 					return [null,0];
-				let cur=this.IdentifierName(src,index+1);
+				let cur=this.IdentifierName(str,index+1);
 				return cur[1]+1;
 			}
 			/** @returns {[string, number]|[null,number]} */
@@ -325,15 +338,9 @@ export function ecma_parse_main() {
 			}
 		}
 		class ecma_12_7 {
-			static _attach(root) {
-				let exports=Object.getOwnPropertyNames(this.prototype);
-				let test_class=new ecma_12_7();
-				let static_names_arr=Object.getOwnPropertyNames(test_class);
-				exports=exports.concat(static_names_arr);
-				for(let x of static_names_arr) {
-					this.prototype[x]=test_class[x];
-				}
-				root.export(this,'12.7',exports);
+			/** @param {ecma_root} parent */
+			constructor(parent) {
+				this.parent=parent;
 			}
 			Punctuator(str,index) {
 				let len=this.OptionalChainingPunctuator(str,index);
@@ -344,7 +351,7 @@ export function ecma_parse_main() {
 			}
 			OptionalChainingPunctuator(str,index) {
 				if(str.slice(index,index+2)==='?.') {
-					let num_len=this.DecimalDigit(str,index+2);
+					let num_len=this.parent.ecma_12_8_3.DecimalDigit(str,index+2);
 					if(num_len>0) {
 						return 0;
 					}
@@ -390,28 +397,23 @@ export function ecma_parse_main() {
 			}
 		}
 		class ecma_12_8 {
-			static the() {
-				if(ecma_12_8._the)
-					return ecma_12_8._the;
-				ecma_12_8._the=new ecma_12_8;
+			/** @arg {ecma_root} parent */
+			constructor(parent) {
+				this.parent=parent;
 			}
-			constructor() {}
-			static _forward_imports(root) {
-				let fn=root._pull_import('LineTerminator');
-				this.LineTerminator=fn;
-			}
+			/** @param {string} str @returns {[number,null,null]|[number,["regexpNonTerm"],null]} */
 			RegularExpressionNonTerminator(str) {
-				let _val=this.LineTerminator(str);
-				if(_val[0]===0) {
+				let _val=this.parent.ecma_12_3.LineTerminator(str);
+				if(!_val) {
 					return [1,['regexpNonTerm'],null];
 				}
 				return [0,null,null];
 			}
 		}
 		class ecma_12_8_3 {
-			static _attach(root) {
-				let exports=Object.getOwnPropertyNames(this.prototype);
-				root.export(this,'12.8.3',exports);
+			/** @param {ecma_root} parent */
+			constructor(parent) {
+				this.parent=parent;
 			}
 			DecimalDigit(str,index) {
 				if(str.charCodeAt(index)>=48&&str.charCodeAt(index)<=57) {
@@ -479,17 +481,10 @@ export function ecma_parse_main() {
 			DecimalIntegerLiteral(str,index) {
 			}
 		}
-		class ecma_base {
-			static _attach(root) {
-				let exports=Object.getOwnPropertyNames(this.prototype);
-				let ecma_section_name=this.name.slice(5).replaceAll('_','.');
-				root.export(this,ecma_section_name,exports);
-			}
-		}
 		class ecma_12_8_4 {
-			static _attach(root) {
-				let exports=Object.getOwnPropertyNames(this.prototype);
-				root.export(this,'12.8.4',exports);
+			/** @param {ecma_root} parent */
+			constructor(parent) {
+				this.parent=parent;
 			}
 			/*
 			EscapeCharacter ::
@@ -527,6 +522,7 @@ export function ecma_parse_main() {
 			Hex4Digits ::
 			HexDigit HexDigit HexDigit HexDigit
 			*/
+			/** @returns {[string,number]|[null,number]} */
 			StringLiteral(str,index) {
 				let cur=str[index];
 				if(cur==='"') {
@@ -571,8 +567,8 @@ export function ecma_parse_main() {
 					if(str[index]==='\\') {
 						break x;
 					}
-					let len=this.LineTerminator(str,index);
-					if(len>0) {
+					let len=this.parent.ecma_12_3.LineTerminator(str,index);
+					if(len!==null) {
 						break x;
 					}
 					return 1;
@@ -613,8 +609,8 @@ export function ecma_parse_main() {
 					if(str[index]==='\\') {
 						break x;
 					}
-					let len=this.LineTerminator(str,index);
-					if(len>0) {
+					let len=this.parent.ecma_12_3.LineTerminator(str,index);
+					if(len!==null) {
 						break x;
 					}
 					return 1;
@@ -637,12 +633,13 @@ export function ecma_parse_main() {
 			}
 			LineContinuation(str,index) {
 				if(str[index]==='\\') {
-					let lt_len=this.LineTerminatorSequence(str,index+1);
+					let lt_len=this.parent.ecma_12_3.LineTerminatorSequence(str,index+1);
 					if(lt_len>0) {
 						return lt_len+1;
 					}
 					return 0;
 				}
+				return 0;
 			}
 			/*
 			EscapeSequence ::
@@ -663,7 +660,7 @@ export function ecma_parse_main() {
 				}
 				x: {
 					if(str[index]==='0') {
-						let peek=this.DecimalDigit(str,index);
+						let peek=this.parent.ecma_12_8_3.DecimalDigit(str,index);
 						if(peek>0) {
 							break x;
 						}
@@ -698,6 +695,7 @@ export function ecma_parse_main() {
 				if(len>0) {
 					return len;
 				}
+				return 0;
 			}
 			SingleEscapeCharacter(str,index) {
 				let val=["'","\"","\\","b","f","n","r","t","v"];
@@ -705,19 +703,20 @@ export function ecma_parse_main() {
 				if(val.includes(cur)) {
 					return 1;
 				}
+				return 0;
 			}
 			NonEscapeCharacter(str,index) {
 				if(this.EscapeCharacter(str,index)) {
 					return 0;
 				}
-				if(this.LineTerminator(str,index)) {
+				if(this.parent.ecma_12_3.LineTerminator(str,index)) {
 					return 0;
 				}
 				return 1;
 			}
 			EscapeCharacter(str,index) {
 				let len0=this.SingleEscapeCharacter(str,index);
-				let len1=this.DecimalDigit(str,index);
+				let len1=this.parent.ecma_12_8_3.DecimalDigit(str,index);
 				let act=0;
 				if(len0>len1) {
 					act=1;
@@ -730,6 +729,9 @@ export function ecma_parse_main() {
 				}
 				if(len1>0&&len1>len0) {
 					return len1;
+				}
+				if(act===1) {
+					throw new Error("TODO");
 				}
 			}
 			/*LegacyOctalEscapeSequence ::
@@ -795,7 +797,11 @@ export function ecma_parse_main() {
 					}
 					return 3;
 				}
-
+				return 0;
+			}
+			/** @returns {number} */
+			OctalDigit(str,arg1) {
+				throw new Error("Method not implemented.");
 			}
 			NonZeroOctalDigit(str,index) {
 				if(str[index]==='0') {
@@ -813,6 +819,7 @@ export function ecma_parse_main() {
 				if(chk.includes(cur)) {
 					return 1;
 				}
+				return 0;
 			}
 			FourToSeven(str,index) {
 				let cur=str[index];
@@ -820,11 +827,13 @@ export function ecma_parse_main() {
 				if(chk.includes(cur)) {
 					return 1;
 				}
+				return 0;
 			}
 			NonOctalDecimalEscapeSequence(str,index) {
 				if(str[index]==='8'||str[index]==='9') {
 					return 1;
 				}
+				return 0;
 			}
 			HexEscapeSequence(str,index) {
 				if(str[index]==='x') {
@@ -838,6 +847,7 @@ export function ecma_parse_main() {
 					}
 					return 3;
 				}
+				return 0;
 			}
 			UnicodeEscapeSequence(str,index) {
 				let off=0;
@@ -861,13 +871,17 @@ export function ecma_parse_main() {
 				}
 				return 0;
 			}
+			/** @returns {number} */
+			CodePoint(str,arg1) {
+				throw new Error("Method not implemented.");
+			}
 			Hex4Digits(str,index) {
 				let len=this.HexDigit(str,index);
 				if(!len) {
 					return 0;
 				}
 				len=this.HexDigit(str,index);
-				if(!len0) {
+				if(!len) {
 					return 0;
 				}
 				len=this.HexDigit(str,index);
@@ -880,11 +894,20 @@ export function ecma_parse_main() {
 				}
 				return 4;
 			}
+			/** @returns {number} */
+			HexDigit(str,index) {
+				throw new Error("Method not implemented.");
+			}
 		}
-		class ecma_12_8_6 extends ecma_base {
+		class ecma_12_8_6 {
+			/** @param {ecma_root} parent */
+			constructor(parent) {
+				this.parent=parent;
+			}
 			/*Template ::
 			NoSubstitutionTemplate
 			TemplateHead*/
+			/** @returns {[string,number]|[null,number]} */
 			Template(str,_index) {
 				// TODO:implement template parsing without Substitution
 				if(str[0]==='`') {
@@ -895,96 +918,33 @@ export function ecma_parse_main() {
 				return [null,0];
 			}
 		}
-		class js_root {
-			static init() {
-				this._init=true;
-				this.export_list=[];
-				let ecma_sections=[];
-				ecma_sections.push(ecma_12_2);
-				ecma_sections.push(ecma_12_3);
-				ecma_sections.push(ecma_12_4);
-				ecma_sections.push(ecma_12_5);
-				ecma_sections.push(ecma_12_6);
-				ecma_sections.push(ecma_12_7);
-				ecma_sections.push(ecma_12_8_3);
-				ecma_sections.push(ecma_12_8_4);
-				ecma_sections.push(ecma_12_8_6);
-				for(let i of ecma_sections)
-					i._attach(js_root);
-				let pull_imp=[ecma_12_8];
-				for(let i of pull_imp)
-					i._forward_imports(js_root);
-			}
-			static _pull_import(fn_name) {
-				let mat=this.export_list.find(e => e[2].includes(fn_name));
-				let cls=mat[0];
-				if(!cls.the) {
-					console.log('missing',[cls],'the',fn_name);
-				}
-				let c_t=cls.the();
-				return cls.prototype[fn_name].bind(c_t);
-			}
-			static import(name) {
-				if(this._init===void 0)
-					this.init();
-				let item=name.split(':');
-				let toc=item[0];
-				let fn=item[1];
-				let mat=this.export_list.find(e => e[1]===toc&&e[2].includes(fn));
-				let cls=mat[0];
-				return cls.prototype[fn];
-			}
-			static export(_class,toc_loc,name_vec) {
-				if(this._init===void 0)
-					this.init();
-				this.export_list.push([_class,toc_loc,name_vec]);
-			}
-			static import_all_items(trg_class) {
-				if(this._init===void 0)
-					this.init();
-				let arr=this.export_list;
-				rt: for(let i=0;i<arr.length;i++) {
-					let cur=arr[i];
-					let n_arr=cur[2];
-					let src_class=cur[0];
-					for(let x of n_arr) {
-						if(x==='constructor') {
-							continue;
-						}
-						if(trg_class.prototype[x]===src_class.prototype[x]) {
-							continue rt;
-						}
-						trg_class.prototype[x]=src_class.prototype[x];
-					}
-				}
-			}
-			static import_all(toc_loc,trg_class) {
-				if(this._init===void 0)
-					this.init();
-				let arr=this.export_list.filter(e => e[1]===toc_loc);
-				for(let i=0;i<arr.length;i++) {
-					let cur=arr[i];
-					for(let x of cur[2]) {
-						if(x==='constructor') {
-							continue;
-						}
-						trg_class.prototype[x]=this.import(toc_loc+':'+x);
-					}
-				}
+		class ecma_root {
+			constructor() {
+				this.ecma_12_2=new ecma_12_2(this);
+				this.ecma_12_3=new ecma_12_3(this);
+				this.ecma_12_4=new ecma_12_4(this);
+				this.ecma_12_5=new ecma_12_5(this);
+				this.ecma_12_6=new ecma_12_6(this);
+				this.ecma_12_7=new ecma_12_7(this);
+				this.ecma_12_8=new ecma_12_8(this);
+				this.ecma_12_8_3=new ecma_12_8_3(this);
+				this.ecma_12_8_4=new ecma_12_8_4(this);
+				this.ecma_12_8_6=new ecma_12_8_6(this);
 			}
 		}
 		class js_token_generator {
 			static EOF_TOKEN=Symbol();
+			/** @param {string} str */
 			constructor(str) {
 				this.str=str;
 				this.index=0;
+				this.root=new ecma_root;
 			}
 			describe_token(token_value) {
 				let tok_str=this.str.slice(token_value[2],token_value[2]+token_value[1]);
 				return [token_value[0],tok_str];
 			}
 			next_token() {
-				let tok_arr=[];
 				let cur;
 				let ret;
 				cur=this.InputElementDiv(this.str,this.index);
@@ -1167,7 +1127,7 @@ export function ecma_parse_main() {
 				return [max_item,max_val];
 			}
 			static add_proto() {
-				js_root.import_all_items(this);
+				ecma_root.import_all_items(this);
 			}
 		}
 		js_token_generator.add_proto();
