635569ae9b91112b229b3da8a930b3e022e8bcf8
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-23 07:05:03 -0700

    u

--- javascript/final/named/read_library-ms_xml.js
+++ javascript/final/named/read_library-ms_xml.js
@@ -24,25 +24,29 @@ async function x() {
 	let xml_document=dom_parser.parseFromString(text,"application/xml");
 	/**
 	 * @param {{ [x: string]: any; }} obj
-	 * @param {{ (obj: any, key: any, p: any): any; (arg0: any, arg1: string | undefined, arg2: any[] | undefined): void; }} cb
+	 * @param {(obj: {}, key: string, p: string[])=>{}} cb
+	 * @returns {{}[][]}
 	 */
 	function do_iter(obj,cb,c=0,/** @type {string[]} */kp=[]) {
+		let out=[];
 		if(typeof obj==='string') {
-			return;
+			return [];
 		}
 		let kl=Object.keys(obj);
 		if(!c)
-			cb(obj,"",{});
+			out.push([kp.join("."),cb(obj,"",[])]);
 		if(c>12) {
 			console.log('deep',obj);
-			return;
+			return [];
 		}
 		for(let i of kl) {
 			obj[i]=cb(obj,i,kp.slice());
 			kp.push(i);
-			do_iter(obj[i],cb,c+1,kp);
+			let ret=do_iter(obj[i],cb,c+1,kp);
+			out.push([kp.join("."),ret]);
 			kp.pop();
 		}
+		return out;
 	}
 	let root_element=xml_document.firstElementChild;
 	if(!root_element) {
@@ -52,7 +56,7 @@ async function x() {
 	let obj={
 		[root_element.tagName]: root_element.children
 	};
-	do_iter(obj,function(/** @type {any} */ obj,/** @type {any} */ key,/** @type {any} */ p) {
+	let res=do_iter(obj,function(/** @type {any} */ obj,/** @type {any} */ key,/** @type {any} */ p) {
 		let e=obj[key];
 		if(!p) {
 			return e;
@@ -129,46 +133,48 @@ async function x() {
 		obj[e.tagName]=e.children;
 		return obj;
 	});
-	const con_list=obj.libraryDescription.searchConnectorDescriptionList;
-	let binary_data,overflow,b32_data;
-	/**
-	 * @param {{ searchConnectorDescription: any; }} list_item
-	 */
-	function get_data_slice(list_item) {
-		let con_desc=list_item.searchConnectorDescription;
-		let simple_loc=con_desc.simpleLocation;
-		let ser=simple_loc.serialized;
-		let un_ser=atob(ser);
-		let un_ser_vec=[...un_ser];
-		let bin_ser_data=un_ser_vec.map(e => e.charCodeAt(0));
-		binary_data=new Uint8Array(bin_ser_data);
-		overflow=binary_data.length%4;
-		b32_data=new Uint32Array(binary_data.buffer,0,(binary_data.length-overflow)/4);
-		let offset=0;
-		//typeof b32_data.slice(offset,offset+6) == 'static'
-		offset+=6;
-		//typeof b32_data.slice(offset,offset+8) == 'dynamic'
-		offset+=8;
-		//typeof b32_data.slice(offset,offset+16) == 'static'
-		offset+=16;
-		//b32_data.slice(offset,offset+8); // disk description
-		//[1560281088, 2246618090, 609064146, 1217224307, 297022384, 50240, 0, 0] === C:
-		//[318767104,  1418456472, 539076172, 2638738344, 561747559, 50240, 0, 0] === D:
-		offset+=4;
-		// length is different
-		return b32_data.slice(offset,offset+32);
-	}
-	for(let list_item of con_list) {
-		let res=get_data_slice(list_item);
-		let max_idx=res.indexOf(3092);
-		/*
-		297022384, 50240, 0, 0, 3654636608, 4039168456, 66052,    3232939776, ---MISSING---  0,          2147680256, 128, 0, 0, 0, 0, 0, 0, 3426763009, 2164267284,                       1100992, 1077936129, 3092, 0,
-		561747559, 50240, 0, 0, 2648511296, 2241535419, 16882717, 16786432,    11520771,     0,          185073664,  0,   0, 0, 0, 0, 0, 0, 3238203648, 454614144,  3162505475, 16848916, 1100928, 1480589314, 3092, 0,
-		*/
-		console.log([...res].slice(0,max_idx+4));
-		// after 1073741824 is probably the library name
-		//1100992, 1346371585, 3092
-		// ^ probably url type? === [val]::{[GUID]}
-	}
+	console.log("res",res);
+	// const lib_desc=obj.libraryDescription;
+	// const con_list=lib_desc.searchConnectorDescriptionList;
+	// let binary_data,overflow,b32_data;
+	// /**
+	//  * @param {{ searchConnectorDescription: any; }} list_item
+	//  */
+	// function get_data_slice(list_item) {
+	// 	let con_desc=list_item.searchConnectorDescription;
+	// 	let simple_loc=con_desc.simpleLocation;
+	// 	let ser=simple_loc.serialized;
+	// 	let un_ser=atob(ser);
+	// 	let un_ser_vec=[...un_ser];
+	// 	let bin_ser_data=un_ser_vec.map(e => e.charCodeAt(0));
+	// 	binary_data=new Uint8Array(bin_ser_data);
+	// 	overflow=binary_data.length%4;
+	// 	b32_data=new Uint32Array(binary_data.buffer,0,(binary_data.length-overflow)/4);
+	// 	let offset=0;
+	// 	//typeof b32_data.slice(offset,offset+6) == 'static'
+	// 	offset+=6;
+	// 	//typeof b32_data.slice(offset,offset+8) == 'dynamic'
+	// 	offset+=8;
+	// 	//typeof b32_data.slice(offset,offset+16) == 'static'
+	// 	offset+=16;
+	// 	//b32_data.slice(offset,offset+8); // disk description
+	// 	//[1560281088, 2246618090, 609064146, 1217224307, 297022384, 50240, 0, 0] === C:
+	// 	//[318767104,  1418456472, 539076172, 2638738344, 561747559, 50240, 0, 0] === D:
+	// 	offset+=4;
+	// 	// length is different
+	// 	return b32_data.slice(offset,offset+32);
+	// }
+	// for(let list_item of con_list) {
+	// 	let res=get_data_slice(list_item);
+	// 	let max_idx=res.indexOf(3092);
+	// 	/*
+	// 	297022384, 50240, 0, 0, 3654636608, 4039168456, 66052,    3232939776, ---MISSING---  0,          2147680256, 128, 0, 0, 0, 0, 0, 0, 3426763009, 2164267284,                       1100992, 1077936129, 3092, 0,
+	// 	561747559, 50240, 0, 0, 2648511296, 2241535419, 16882717, 16786432,    11520771,     0,          185073664,  0,   0, 0, 0, 0, 0, 0, 3238203648, 454614144,  3162505475, 16848916, 1100928, 1480589314, 3092, 0,
+	// 	*/
+	// 	console.log([...res].slice(0,max_idx+4));
+	// 	// after 1073741824 is probably the library name
+	// 	//1100992, 1346371585, 3092
+	// 	// ^ probably url type? === [val]::{[GUID]}
+	// }
 }
 x();
