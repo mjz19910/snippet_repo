348ff81e5cae94571ad6524f9e29b52130474af7
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-23 07:43:39 -0700

    u

--- javascript/modules/DebugAPI_raw/DebugAPI.user.js
+++ javascript/modules/DebugAPI_raw/DebugAPI.user.js
@@ -2,14 +2,14 @@
 // @name         DebugAPI and g_api
 // @namespace    http://tampermonkey.net/
 // @version      0.4.1
-// @description  DebugAPI.js from https://github.com/mjz19910/snippet_repo/blob/master/javascript/modules/DebugAPI_raw/DebugAPI.js
+// @description  DebugAPI.js from https://github.com/mjz19910/snippet_repo/blob/master/javascript/modules/DebugAPI_raw/DebugAPI.user.js
 // @author       @mjz19910
 // @match        https://*/*
 // @match        http://*/*
 // @run-at       document-start
 // @grant        none
 // @updateURL    https://github.com/mjz19910/snippet_repo/blob/master/javascript/modules/DebugAPI_raw/meta/DebugAPI.meta.js
-// @downloadURL  https://github.com/mjz19910/snippet_repo/blob/master/javascript/modules/DebugAPI_raw/DebugAPI.js
+// @downloadURL  https://github.com/mjz19910/snippet_repo/blob/master/javascript/modules/DebugAPI_raw/DebugAPI.user.js
 // ==/UserScript==
 /* Copyright 2019-2022 @mjz19910 */
 /* eslint-disable no-undef */
