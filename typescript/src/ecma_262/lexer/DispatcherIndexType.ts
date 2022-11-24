91464ae72c5362f9a6ecd110aa3fe1b2b76b1ead
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-24 03:05:49 -0700

    u

--- typescript/src/ecma_262/lexer/DispatcherIndexType.ts
+++
@@ -1,5 +0,0 @@
-import {LexerBase} from "./LexerBase.js"
-import {LexReturnType} from "./LexReturnType.js"
-import {EnvSettingsType} from "../EnvSettingsType.js"
-
-export type DispatcherIndexType=((str: string,index: number) => LexReturnType)|LexerBase|EnvSettingsType
--- typescript/src/ecma_262/lexer/Tokens.ts
+++ typescript/src/ecma_262/lexer/Tokens.ts
@@ -1,4 +1,3 @@
-import {Dispatcher} from "./Dispatcher.js";
 import {LexerBase} from "./LexerBase.js";
 import {LexReturnType} from "./LexReturnType.js";
 
@@ -7,10 +6,9 @@ export class Tokens extends LexerBase {
 	// https://tc39.es/ecma262/#prod-CommonToken
 	CommonToken(str: any,index: any): LexReturnType {
 		let common_token: ['IdentifierName','PrivateIdentifier','Punctuator','NumericLiteral','StringLiteral','Template']=['IdentifierName','PrivateIdentifier','Punctuator','NumericLiteral','StringLiteral','Template'];
-		let acc=new Dispatcher("");
 		let type=null,len=0;
 		for(let m_name of common_token) {
-			const cur=acc[m_name](str,index);
+			const cur=this.m_dispatcher[m_name](str,index);
 			if(cur[0]&&cur[1]>len) {
 				type=cur[0];
 				len=cur[1];
