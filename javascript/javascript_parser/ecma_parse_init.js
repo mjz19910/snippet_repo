d9cf773de1a601f30bae80ce65c5b83b6cbdf293
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-24 02:43:56 -0700

    u

--- javascript/javascript_parser/ecma_parse_init.js
+++ javascript/javascript_parser/ecma_parse_init.js
@@ -1,62 +1,16 @@
+import {get_module_data} from "./get_module_data";
+import {is_valid_utf8} from "./is_valid_utf8";
 import window from "./window_def.js";
-
-/*!
-* The buffer module from node.js, for the browser.
-*
-* @author   Feross Aboukhadijeh <http://feross.org>
-* @license  MIT
-*/
-function is_valid_utf8(/** @type {string} */ e, /** @type {number} */ t) {
-	var r;
-	t=t||1/0;
-	for(var n=e.length,i=null,o=[],s=0;s<n;++s) {
-		if((r=e.charCodeAt(s))>55295&&r<57344) {
-			if(!i) {
-				if(r>56319) {
-					(t-=3)>-1&&o.push(239,191,189);
-					continue;
-				}
-				if(s+1===n) {
-					(t-=3)>-1&&o.push(239,191,189);
-					continue;
-				}
-				i=r;
-				continue;
-			}
-			if(r<56320) {
-				(t-=3)>-1&&o.push(239,191,189),
-					i=r;
-				continue;
-			}
-			r=65536+(i-55296<<10|r-56320);
-		} else i&&(t-=3)>-1&&o.push(239,191,189);
-		if(i=null,
-			r<128) {
-			if((t-=1)<0)
-				break;
-			o.push(r);
-		} else if(r<2048) {
-			if((t-=2)<0)
-				break;
-			o.push(r>>6|192,63&r|128);
-		} else if(r<65536) {
-			if((t-=3)<0)
-				break;
-			o.push(r>>12|224,r>>6&63|128,63&r|128);
-		} else {
-			if(!(r<1114112))
-				throw new Error("Invalid code point");
-			if((t-=4)<0)
-				break;
-			o.push(r>>18|240,r>>12&63|128,r>>6&63|128,63&r|128);
-		}
-	}
-	return o;
-}
 
 export {Holder} from "./__global.js";
 
 export function ecma_parse_init() {
 	if('code' in window&&window.code) return;
-	window.code=is_valid_utf8.toString();
+	const module_data=[get_module_data()];
+	let use_valid_utf8_function=false;
+	if(use_valid_utf8_function) {
+		window.code=is_valid_utf8.toString();
+		return;
+	}
+	window.code=module_data.toString();
 }
