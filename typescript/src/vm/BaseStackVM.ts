758b489a32003e06d9a50aa0e8bfd3d7872f837e
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-28 21:03:34 -0700

    u

--- typescript/src/vm/BaseStackVM.ts
+++ typescript/src/vm/BaseStackVM.ts
@@ -242,22 +242,4 @@ export class BaseStackVM implements AbstractVM {
 		console.assert(this.stack.length===0,"stack length is not zero, unhandled data on stack");
 		return this.return_value;
 	}
-	fell_off_instructions(instruction) {
-		if(this.exec_stack.length>0) {
-			let exec_top=this.exec_stack.pop();
-			if(!exec_top) {
-				throw new Error("Invalid");
-			}
-			[this.stack,this.instructions]=exec_top;
-			let base_ptr=this.pop();
-			let next_ip=this.pop();
-			if(base_ptr.type !== 'number') throw new Error("Invalid");
-			if(next_ip.type !== 'number') throw new Error("Invalid");
-			this.base_pointer=base_ptr.value;
-			this.instruction_pointer=next_ip.value;
-			l_log_if(LOG_LEVEL_VERBOSE,'returned to',this.instruction_pointer,this.exec_stack.length);
-			return;
-		}
-		l_log_if(LOG_LEVEL_VERBOSE,'reached end of instruction stream, nothing to return too',instruction,this.instructions,this.instruction_pointer);
-	}
 }
