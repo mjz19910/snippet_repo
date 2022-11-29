a50719dad72ac3dc2141f69dabbf2ac90edc3842
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-28 21:04:54 -0700

    u

--- typescript/src/vm/BaseStackVM.ts
+++ typescript/src/vm/BaseStackVM.ts
@@ -212,8 +212,9 @@ export class BaseStackVM implements AbstractVM {
 		let ret=target_fn.value.apply(target_this,arg_arr);
 		this.push(new PromiseBox(ret));
 	}
-	run(): Box {this.running=true;
-		while(this.instruction_pointer<this.instructions.length&&this.running) {
+	run(): Box {
+		this.running=true;
+		while(this.running) {
 			let instruction=this.instructions[this.instruction_pointer];
 			this.execute_instruction(instruction);
 			if(this.jump_instruction_pointer!=null) {
@@ -241,5 +242,24 @@ export class BaseStackVM implements AbstractVM {
 		}
 		console.assert(this.stack.length===0,"stack length is not zero, unhandled data on stack");
 		return this.return_value;
+	}
+	fell_off_instructions(instruction) {
+		if(this.exec_stack.length>0) {
+			let exec_top=this.exec_stack.pop();
+			if(!exec_top) {
+				throw new Error("Invalid");
+			}
+			[this.stack,this.instructions]=exec_top;
+			let base_ptr=this.pop();
+			let next_ip=this.pop();
+			if(base_ptr.type !== 'number') throw new Error("Invalid");
+			if(next_ip.type !== 'number') throw new Error("Invalid");
+			this.base_pointer=base_ptr.value;
+			this.instruction_pointer=next_ip.value;
+			l_log_if(LOG_LEVEL_VERBOSE,'returned to',this.instruction_pointer,this.exec_stack.length);
+			return;
+		}
+		l_log_if(LOG_LEVEL_VERBOSE,'reached end of instruction stream, nothing to return too',instruction,this.instructions,this.instruction_pointer);
+		this.running=false;
 	}
 }
