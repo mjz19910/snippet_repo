b93569079ad2285aa3a34096672985e5931e3c47
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-28 17:05:03 -0700

    u

--- javascript/userscript_store/youtube_plugin/youtube_plugin.meta.js
+++ javascript/userscript_store/youtube_plugin/youtube_plugin.meta.js
@@ -7,4 +7,6 @@
 // @match        https://www.youtube.com/*
 // @grant        none
 // @run-at       document-start
+// @updateURL    https://github.com/mjz19910/snippet_repo/raw/master/javascript/userscript_store/youtube_plugin/youtube_plugin.meta.js
+// @downloadURL  https://github.com/mjz19910/snippet_repo/raw/master/javascript/userscript_store/youtube_plugin/youtube_plugin.user.js
 // ==/UserScript==
