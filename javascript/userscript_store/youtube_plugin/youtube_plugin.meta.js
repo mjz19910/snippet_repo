cb247298edd6a5f0d74d9dc30526411533cf5513
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-28 17:04:51 -0700

    u

--- javascript/userscript_store/youtube_plugin/youtube_plugin.meta.js
+++ javascript/userscript_store/youtube_plugin/youtube_plugin.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         youtube plugin
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      4.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.youtube.com/*
--- javascript/userscript_store/youtube_plugin/youtube_plugin.user.js
+++ javascript/userscript_store/youtube_plugin/youtube_plugin.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         youtube plugin
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      4.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.youtube.com/*
 // @grant        none
 // @run-at       document-start
+// @updateURL    https://github.com/mjz19910/snippet_repo/raw/master/javascript/userscript_store/youtube_plugin/youtube_plugin.meta.js
+// @downloadURL  https://github.com/mjz19910/snippet_repo/raw/master/javascript/userscript_store/youtube_plugin/youtube_plugin.user.js
 // ==/UserScript==
 /* eslint-disable no-native-reassign,no-implicit-globals,no-undef,no-lone-blocks,no-sequences */
 
