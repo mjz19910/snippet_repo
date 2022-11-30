01aaabe77b0ada40a44be960056b87be1c67e649
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-30 11:13:08 -0700

    u

---
+++ javascript/modules/remote_worker_timers/remote_worker_timers.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         setTimeout on remote worker
+// @namespace    http://tampermonkey.net/
+// @version      3.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://rebuildtheuniverse.com/test_remote_setTimeout
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+/* eslint-disable no-undef,no-lone-blocks,no-eval */
