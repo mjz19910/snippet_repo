58a77de71cfcad2834131768f21b99a87ab923ec
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-30 07:57:38 -0700

    u

--- typescript/src/box/FunctionBox.ts
+++ typescript/src/box/FunctionBox.ts
@@ -1,7 +1,7 @@
-import {Box} from "./Box.js"
-import {BoxTemplate} from "./template/BoxTemplate.js"
+import {Box} from "./Box.js";
+import {BoxTemplate} from "./template/BoxTemplate.js";
 
 export class FunctionBox extends BoxTemplate<"function_box",(...a: Box[]) => Box> {
-	readonly type="function_box"
-	readonly return_type="null"
+	readonly type="function_box";
+	readonly return_type="null";
 }
