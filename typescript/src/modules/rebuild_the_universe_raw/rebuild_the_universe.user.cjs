95d4664880287484912972f70fe5cf4a562d10bb
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-30 09:59:17 -0700

    u

--- typescript/src/modules/rebuild_the_universe_raw/rebuild_the_universe.user.cjs
+++ typescript/src/modules/rebuild_the_universe_raw/rebuild_the_universe.user.cjs
@@ -1,21 +1,23 @@
 // ==UserScript==
-// @name			rebuild the universe
-// @namespace		http://tampermonkey.net/
-// @version			4.1
-// @description		rebuildtheuniverse.com automation
-// @author			You
-// @match			http://rebuildtheuniverse.com/?type=real
-// @match			http://rebuildtheuniverse.com/?type=mjz_version
-// @match			http://rebuildtheuniverse.com
-// @match			https://rebuildtheuniverse.com/?type=real
-// @match			https://rebuildtheuniverse.com/?type=mjz_version
-// @match			https://rebuildtheuniverse.com
-// @match			https://ssh.login.local:9342/mirror/rebuildtheuniverse.com/
-// @match			https://ssh.login.local:9342/mirror/rebuildtheuniverse.com/?type=inject
-// @match			https://ssh.login.local:9342/mirror/rebuildtheuniverse.com/?type=mjz_version
-// @match			https://ssh.login.local:9342/mirror/rebuildtheuniverse.com/?type=real
-// @run-at			document-start
-// @grant			none
+// @name		rebuild the universe
+// @namespace	http://tampermonkey.net/
+// @version		4.1
+// @description	rebuildtheuniverse.com automation
+// @author		You
+// @match		http://rebuildtheuniverse.com/?type=real
+// @match		http://rebuildtheuniverse.com/?type=mjz_version
+// @match		http://rebuildtheuniverse.com
+// @match		https://rebuildtheuniverse.com/?type=real
+// @match		https://rebuildtheuniverse.com/?type=mjz_version
+// @match		https://rebuildtheuniverse.com
+// @match		https://ssh.login.local:9342/mirror/rebuildtheuniverse.com/
+// @match		https://ssh.login.local:9342/mirror/rebuildtheuniverse.com/?type=inject
+// @match		https://ssh.login.local:9342/mirror/rebuildtheuniverse.com/?type=mjz_version
+// @match		https://ssh.login.local:9342/mirror/rebuildtheuniverse.com/?type=real
+// @run-at		document-start
+// @grant		none
+// @updateURL	https://github.com/mjz19910/snippet_repo/raw/master/javascript/modules/DebugAPI_raw/DebugAPI.user.js
+// @downloadURL	https://github.com/mjz19910/snippet_repo/raw/master/javascript/modules/DebugAPI_raw/DebugAPI.user.js
 // ==/UserScript==
 /* eslint-disable no-undef,no-lone-blocks,no-eval */
 
