297b4e7ad7bf479a81d344fc38ee5b7f3f4e2c41
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-30 10:13:01 -0700

    u

--- typescript/src/vm/TimerMessageTypes.ts
+++ typescript/src/vm/TimerMessageTypes.ts
@@ -1,14 +1,16 @@
-import {WorkerAsyncMessageTy} from "./WorkerAsyncMessageTy.js";
-import {TimeoutFireInfoTy} from "./TimeoutFireInfoTy.js";
-import {TimeoutWorkerTypesTy} from "./TimeoutWorkerTypesTy.js";
-import {TimerMessageTypesTy} from "./TimerMessageTypesTy.js";
-import {ReplyTypes} from "./ReplyTypes.js";
-import {TimeoutWorkerTypes} from "./TimeoutWorkerTypes.js";
-import {TimeoutFireInfo} from "./TimeoutFireInfo.js";
-import {ReplyTypesTy} from "./ReplyTypesTy.js";
 import {WorkerAsyncMessage} from "../constants.js";
+import {ReplyTypes} from "./ReplyTypes.js";
+import {ReplyTypesTy} from "./ReplyTypesTy.js";
+import {TimeoutFireInfo} from "./TimeoutFireInfo.js";
+import {TimeoutFireInfoTy} from "./TimeoutFireInfoTy.js";
+import {TimeoutWorkerTypes} from "./TimeoutWorkerTypes.js";
+import {TimeoutWorkerTypesTy} from "./TimeoutWorkerTypesTy.js";
+import {type Type} from "./TimerMessageTypesTy.js";
+import {WorkerAsyncMessageTy} from "./WorkerAsyncMessageTy.js";
 
-export class TimerMessageTypes implements TimerMessageTypesTy {
+type TimerMessageTypes_=Type.TimerMessageTypes;
+
+export class TimerMessageTypes implements TimerMessageTypes_ {
 	async: WorkerAsyncMessageTy=WorkerAsyncMessage;
 	reply: ReplyTypesTy=new ReplyTypes;
 	fire: TimeoutFireInfoTy=new TimeoutFireInfo;
--- typescript/src/vm/TimerMessageTypesTy.ts
+++ typescript/src/vm/TimerMessageTypesTy.ts
@@ -1,11 +1,13 @@
-import {WorkerAsyncMessageTy} from "./WorkerAsyncMessageTy.js"
-import {TimeoutFireInfoTy} from "./TimeoutFireInfoTy.js"
-import {TimeoutWorkerTypesTy} from "./TimeoutWorkerTypesTy.js"
-import {ReplyTypes} from "./ReplyTypes.js"
+import {WorkerAsyncMessageTy} from "./WorkerAsyncMessageTy.js";
+import {TimeoutFireInfoTy} from "./TimeoutFireInfoTy.js";
+import {TimeoutWorkerTypesTy} from "./TimeoutWorkerTypesTy.js";
+import {ReplyTypes} from "./ReplyTypes.js";
 
-export type TimerMessageTypesTy={
-	async: WorkerAsyncMessageTy
-	reply: ReplyTypes
-	fire: TimeoutFireInfoTy
-	worker: TimeoutWorkerTypesTy
+export namespace Type {
+	export type TimerMessageTypes={
+		async: WorkerAsyncMessageTy;
+		reply: ReplyTypes;
+		fire: TimeoutFireInfoTy;
+		worker: TimeoutWorkerTypesTy;
+	};
 }
