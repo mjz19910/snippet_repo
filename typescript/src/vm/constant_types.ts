1c09a89c3a9628cc378eaf52382d2d966dc50bf5
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-30 20:30:11 -0700

    u

--- typescript/src/vm/constant_types.ts
+++ typescript/src/vm/constant_types.ts
@@ -5,11 +5,16 @@ import {ReplyMessage1Ty} from "./ReplyMessage1Ty.js";
 import {ReplyMessage2Ty} from "./ReplyMessage2Ty.js";
 import {ReplySetMessages} from "./ReplySetMessages.js";
 import {ReplyToWorkerTy} from "./ReplyToWorkerTy.js";
-import {TimeoutWorkerTypesTy} from "./TimeoutWorkerTypesTy.js";
+import {TimeoutClearInfoTy} from "./TimeoutClearInfoTy.js";
+import {TimeoutMessageReplyTy} from "./TimeoutMessageReplyTy.js";
+import {TimeoutSetInfoTy} from "./TimeoutSetInfoTy.js";
+import {TimeoutSetTypesTy} from "./TimeoutSetTypesTy.js";
 import {WorkerAsyncMessageTy} from "./WorkerAsyncMessageTy.js";
 import {WorkerDestroyTypeT} from "./WorkerDestroyTypeT.js";
 import {WorkerReadyReplyTy} from "./WorkerReadyReplyTy.js";
+import {WorkerReplyTypes} from "./WorkerReplyTypes.js";
 import {WorkerUpdateMessageHandlerReplyTy} from "./WorkerUpdateMessageHandlerReplyTy.js";
+import {WorkerUpdateMessageHandlerTy} from "./WorkerUpdateMessageHandlerTy.js";
 
 export type TimeoutFireSingleT=typeof TimeoutFireSingle;
 export type TimeoutFireRT=typeof TimeoutFireRepeating;
@@ -28,6 +33,14 @@ export type ReplyTypesT={
 	set: ReplySetMessages;
 	clear: ReplyClearMessages;
 };
+export type TimeoutWorkerTypesTy={
+	reply: WorkerReplyTypes
+	update_message_handler: WorkerUpdateMessageHandlerTy
+	ready: TimeoutMessageReplyTy
+	set: TimeoutSetInfoTy
+	clear: TimeoutClearInfoTy
+	set_types: TimeoutSetTypesTy
+}
 export type TimerMessageTypesT={
 	async: WorkerAsyncMessageTy;
 	reply: ReplyTypesT;
