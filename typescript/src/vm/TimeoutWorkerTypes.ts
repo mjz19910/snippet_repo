5eabd0e22b047ad02d184e7a706aa91d940fa5e7
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-30 21:23:34 -0700

    u

--- typescript/src/vm/TimeoutWorkerTypes.ts
+++ typescript/src/vm/TimeoutWorkerTypes.ts
@@ -1,14 +1,14 @@
-import {WorkerReplyTypes} from "./WorkerReplyTypes.js";
-import {TimeoutSetInfo} from "./TimeoutSetInfo.js";
-import {TimeoutClearInfo} from "./TimeoutClearInfo.js";
 import {TimeoutMessageReady,TimeoutSetTypes,WorkerUpdateMessageHandler} from "../constants.js";
-import {TimeoutClearInfoT,TimeoutMessageReplyT,TimeoutSetInfoT,TimeoutSetTypesT,TimeoutWorkerTypesT,WorkerReplyTypesT,WorkerUpdateMessageHandlerT} from "./constant_types.js";
+import {TimeoutMessageReplyT,TimeoutSetTypesT,TimeoutWorkerTypesT,WorkerUpdateMessageHandlerT} from "./constant_types.js";
+import {TimeoutClearInfo} from "./TimeoutClearInfo.js";
+import {TimeoutSetInfo} from "./TimeoutSetInfo.js";
+import {WorkerReplyTypes} from "./WorkerReplyTypes.js";
 
 export class TimeoutWorkerTypes implements TimeoutWorkerTypesT {
-	reply: WorkerReplyTypesT=new WorkerReplyTypes;
+	reply=new WorkerReplyTypes;
 	update_message_handler: WorkerUpdateMessageHandlerT=WorkerUpdateMessageHandler;
 	ready: TimeoutMessageReplyT=TimeoutMessageReady;
-	set: TimeoutSetInfoT=new TimeoutSetInfo;
-	clear: TimeoutClearInfoT=new TimeoutClearInfo;
+	set=new TimeoutSetInfo;
+	clear=new TimeoutClearInfo;
 	set_types: TimeoutSetTypesT=TimeoutSetTypes;
 }
