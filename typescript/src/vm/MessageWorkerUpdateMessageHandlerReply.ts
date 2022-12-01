40ff3519ea7947ee05d8d9e7e2bed7dddb034d6a
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-30 21:27:11 -0700

    u

--- typescript/src/vm/MessageWorkerUpdateMessageHandlerReply.ts
+++
@@ -1,6 +0,0 @@
-import {WorkerUpdateMessageHandlerReplyT,WorkerUpdateMessageHandlerT} from "./constant_types.js";
-
-export type MessageWorkerUpdateMessageHandlerReply_OLD={
-	t: WorkerUpdateMessageHandlerReplyT;
-	v: WorkerUpdateMessageHandlerT;
-};
--- typescript/src/vm/ReplySetTypesT.ts
+++
@@ -1,7 +0,0 @@
-import {ReplySetSingleTy_OLD} from "./ReplySetSingleTy.js"
-import {ReplySetRepeatingTy_OLD} from "./ReplySetRepeatingTy.js"
-
-export type ReplySetTypesT_OLD={
-	single: ReplySetSingleTy_OLD
-	repeating: ReplySetRepeatingTy_OLD
-}
