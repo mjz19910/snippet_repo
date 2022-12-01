8a17aadcfe68678c14756135dccd314d53aa0eea
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 16:05:06 -0700

    u

--- typescript/src/script_registry/register_obj_with_registry.ts
+++ typescript/src/script_registry/register_obj_with_registry.ts
@@ -13,17 +13,17 @@ export let weak_target_object_arr: (WeakFinalInfo|null)[]=[];
 export let unregister_arr: {symbol: symbol;}[]=[];
 
 export function register_obj_with_registry<T extends object>(target: T) {
-	let obj_id;
+	let target_id;
 	if(!(target instanceof HTMLScriptElement)&&!(target instanceof SVGScriptElement)) {
 		let target_ref=weak_target_object_arr.find((e: {ref: {deref: () => any;};}|null) => e&&e.ref.deref()===target);
 		if(target_ref) {
 			return target_ref.id;
 		}
-		obj_id=script_id.next();
-		let obj_symbol=Symbol(obj_id);
+		target_id=script_id.next();
+		let obj_symbol=Symbol(target_id);
 		let held_value: HeldType={
 			type: 'held',
-			id: obj_id,
+			id: target_id,
 			key: obj_symbol
 		};
 		let unregister_token={
@@ -43,26 +43,26 @@ export function register_obj_with_registry<T extends object>(target: T) {
 	}
 	let obj_ref=weak_scripts_arr.find((e: null|{ref: {deref: () => any;};}) => e&&e.ref.deref()===target);
 	if(obj_ref) {
-		obj_id=obj_ref.id;
-		return obj_id;
+		target_id=obj_ref.id;
+		return target_id;
 	}
-	obj_id=script_id.id;
+	target_id=script_id.id;
 	script_id.inc();
 
 	let held_obj: HeldType={
 		type: 'held',
-		id: obj_id,
-		key: Symbol(obj_id)
+		id: target_id,
+		key: Symbol(target_id)
 	};
-	let token_sym={symbol: Symbol(obj_id)};
+	let token_sym={symbol: Symbol(target_id)};
 	scripts_holders.push(held_obj);
 	let token_val: WeakRefWithKey={key: held_obj.key,weak_ref: new WeakRef(token_sym)};
 	scripts_tokens.push(token_val);
 	weak_scripts_arr.push({
 		key: held_obj.key,
-		id: obj_id,
+		id: target_id,
 		ref: new WeakRef(target)
 	});
 	script_registry.register(target,held_obj,token_sym);
-	return obj_id;
+	return target_id;
 }
