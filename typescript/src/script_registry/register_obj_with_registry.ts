0afefff5188c5f4795c1a6ae5cdbdd147ae24d26
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 16:40:05 -0700

    u

--- typescript/src/script_registry/register_obj_with_registry.ts
+++ typescript/src/script_registry/register_obj_with_registry.ts
@@ -8,13 +8,50 @@ import {WeakRefTo} from "./WeakRefTo";
 
 export let unregister_target_script_arr: {symbol: symbol;}[]=[];
 
+class GCStorage<T extends {}> {
+	store_gc_object(target: T) {
+		let target_ref=this.target_arr.find(e => e!==null&&e.ref.deref()===target);
+		if(target_ref) {
+			return target_ref.id;
+		}
+		let target_id=script_id.next();
+		let obj_symbol=Symbol(target_id);
+		let held_value: HeldType={
+			type: 'held',
+			scope: "object",
+			id: target_id,
+			key: obj_symbol
+		};
+		let unregister_token={
+			symbol: obj_symbol
+		};
+		this.target_arr.push({
+			key: obj_symbol,
+			id: target_id,
+			ref: new WeakRef(target),
+		});
+		unregister_target_object_arr.push(unregister_token);
+		script_registry.register(target,held_value,unregister_token);
+		console.log("Called register_obj with non-script",target);
+		return target_id;
+	}
+	counter=new Counter;
+	target_arr: (WeakRefTo<T>|null)[]=[];
+	unregister_token_arr: {symbol: symbol;}[]=[];
+}
+
 export let target_object_id_counter=new Counter;
 export let weak_target_object_arr: (WeakRefTo<{}>|null)[]=[];
 export let unregister_target_object_arr: {symbol: symbol;}[]=[];
 
+export let target_object_storage=new GCStorage<{}>();
+
 export function register_obj_with_registry<T extends {}>(target: T) {
+	let cur_storage;
 	if(!(target instanceof HTMLScriptElement)&&!(target instanceof SVGScriptElement)) {
-		let target_ref=weak_target_object_arr.find((e: {ref: {deref: () => any;};}|null) => e&&e.ref.deref()===target);
+		cur_storage=target_object_storage;
+		cur_storage.store_gc_object(target);
+		let target_ref=cur_storage.target_arr.find((e: {ref: {deref: () => any;};}|null) => e&&e.ref.deref()===target);
 		if(target_ref) {
 			return target_ref.id;
 		}
