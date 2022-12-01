a1f9da52e668c2f84dda77a64a1ac9d4599ef62d
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 15:40:25 -0700

    u

--- typescript/src/timer_node/AsyncTimeoutFireNode.ts
+++ typescript/src/timer_node/AsyncTimeoutFireNode.ts
@@ -4,25 +4,17 @@ export class AsyncTimeoutFireNode {
 	constructor(description: string) {
 		this.description=description;
 		this.promise_accept=null;
-		this.callback=null;
 		this.m_promise=new Promise(this.promise_executor.bind(this));
 	}
 	promise_accept: (() => void)|null;
-	callback: (() => void)|null;
 	promise_executor(accept: () => void,reject: (reason?: any) => void) {
 		void reject;
 		this.promise_accept=accept;
-		this.callback=this.on_result.bind(this);
 	}
-	on_result() {
+	fire() {
 		this.m_promise=null;
 		if(this.promise_accept)
 			this.promise_accept();
-	}
-	fire() {
-		let callback=this.callback;
-		if(callback)
-			callback();
 	}
 	wait() {
 		if(this.m_promise===null) throw new Error("No promise to get");
