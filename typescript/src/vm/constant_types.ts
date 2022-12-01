52bb94e8219bfcfb5fd142cbaa841f5ab1119db7
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-30 20:33:12 -0700

    u

--- typescript/src/vm/constant_types.ts
+++ typescript/src/vm/constant_types.ts
@@ -1,15 +1,10 @@
-import {TimeoutFireRepeating,TimeoutFireSingle, WorkerAsyncMessage} from "../constants.js";
+import {ReplyFromWorker,ReplyMessage1,ReplyMessage2,ReplyToWorker,TimeoutFireRepeating,TimeoutFireSingle,WorkerAsyncMessage,WorkerDestroyType} from "../constants.js";
 import {ReplyClearMessages} from "./ReplyClearMessages.js";
-import {ReplyFromWorkerTy} from "./ReplyFromWorkerTy.js";
-import {ReplyMessage1Ty} from "./ReplyMessage1Ty.js";
-import {ReplyMessage2Ty} from "./ReplyMessage2Ty.js";
 import {ReplySetMessages} from "./ReplySetMessages.js";
-import {ReplyToWorkerTy} from "./ReplyToWorkerTy.js";
 import {TimeoutClearInfoTy} from "./TimeoutClearInfoTy.js";
 import {TimeoutMessageReplyTy} from "./TimeoutMessageReplyTy.js";
 import {TimeoutSetInfoTy} from "./TimeoutSetInfoTy.js";
 import {TimeoutSetTypesTy} from "./TimeoutSetTypesTy.js";
-import {WorkerDestroyTypeT} from "./WorkerDestroyTypeT.js";
 import {WorkerReadyReplyTy} from "./WorkerReadyReplyTy.js";
 import {WorkerReplyTypes} from "./WorkerReplyTypes.js";
 import {WorkerUpdateMessageHandlerReplyTy} from "./WorkerUpdateMessageHandlerReplyTy.js";
@@ -21,6 +16,11 @@ export type TimeoutFireInfoT={
 	single: TimeoutFireSingleT;
 	repeating: TimeoutFireRepeatingT;
 };
+export type ReplyMessage1Ty=typeof ReplyMessage1;
+export type ReplyMessage2Ty=typeof ReplyMessage2;
+export type ReplyFromWorkerTy=typeof ReplyFromWorker;
+export type ReplyToWorkerTy=typeof ReplyToWorker;
+export type WorkerDestroyTypeT=typeof WorkerDestroyType;
 export type ReplyTypesT={
 	msg1: ReplyMessage1Ty;
 	msg2: ReplyMessage2Ty;
@@ -33,13 +33,13 @@ export type ReplyTypesT={
 	clear: ReplyClearMessages;
 };
 export type TimeoutWorkerTypesT={
-	reply: WorkerReplyTypes
-	update_message_handler: WorkerUpdateMessageHandlerTy
-	ready: TimeoutMessageReplyTy
-	set: TimeoutSetInfoTy
-	clear: TimeoutClearInfoTy
-	set_types: TimeoutSetTypesTy
-}
+	reply: WorkerReplyTypes;
+	update_message_handler: WorkerUpdateMessageHandlerTy;
+	ready: TimeoutMessageReplyTy;
+	set: TimeoutSetInfoTy;
+	clear: TimeoutClearInfoTy;
+	set_types: TimeoutSetTypesTy;
+};
 export type WorkerAsyncMessageT=typeof WorkerAsyncMessage;
 export type TimerMessageTypesT={
 	async: WorkerAsyncMessageT;
