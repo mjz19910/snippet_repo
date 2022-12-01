2d034e03f4477062e1133115cda403ee78f1660c
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 15:39:25 -0700

    u

--- typescript/src/timer_node/AsyncTimeoutFireNode.ts
+++ typescript/src/timer_node/AsyncTimeoutFireNode.ts
@@ -7,17 +7,17 @@ export class AsyncTimeoutFireNode {
 		this.callback=null;
 		this.m_promise=new Promise(this.promise_executor.bind(this));
 	}
-	promise_accept: ((value: void|PromiseLike<void>) => void)|null;
-	callback: ((value: void|PromiseLike<void>) => void)|null;
-	promise_executor(accept: (value: void|PromiseLike<void>) => void,reject: (reason?: any) => void) {
+	promise_accept: (() => void)|null;
+	callback: (() => void)|null;
+	promise_executor(accept: () => void,reject: (reason?: any) => void) {
 		void reject;
 		this.promise_accept=accept;
 		this.callback=this.on_result.bind(this);
 	}
-	on_result(value: void|PromiseLike<void>) {
+	on_result() {
 		this.m_promise=null;
 		if(this.promise_accept)
-			this.promise_accept(value);
+			this.promise_accept();
 	}
 	fire() {
 		let callback=this.callback;
