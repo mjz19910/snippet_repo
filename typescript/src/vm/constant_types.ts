aeef66d5d0fd102297acc17c8a8f6644256b2540
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-30 20:30:51 -0700

    u

--- typescript/src/vm/constant_types.ts
+++ typescript/src/vm/constant_types.ts
@@ -1,4 +1,4 @@
-import {TimeoutFireRepeating,TimeoutFireSingle} from "../constants.js";
+import {TimeoutFireRepeating,TimeoutFireSingle, WorkerAsyncMessage} from "../constants.js";
 import {ReplyClearMessages} from "./ReplyClearMessages.js";
 import {ReplyFromWorkerTy} from "./ReplyFromWorkerTy.js";
 import {ReplyMessage1Ty} from "./ReplyMessage1Ty.js";
@@ -9,7 +9,6 @@ import {TimeoutClearInfoTy} from "./TimeoutClearInfoTy.js";
 import {TimeoutMessageReplyTy} from "./TimeoutMessageReplyTy.js";
 import {TimeoutSetInfoTy} from "./TimeoutSetInfoTy.js";
 import {TimeoutSetTypesTy} from "./TimeoutSetTypesTy.js";
-import {WorkerAsyncMessageTy} from "./WorkerAsyncMessageTy.js";
 import {WorkerDestroyTypeT} from "./WorkerDestroyTypeT.js";
 import {WorkerReadyReplyTy} from "./WorkerReadyReplyTy.js";
 import {WorkerReplyTypes} from "./WorkerReplyTypes.js";
@@ -41,8 +40,9 @@ export type TimeoutWorkerTypesT={
 	clear: TimeoutClearInfoTy
 	set_types: TimeoutSetTypesTy
 }
+export type WorkerAsyncMessageT=typeof WorkerAsyncMessage;
 export type TimerMessageTypesT={
-	async: WorkerAsyncMessageTy;
+	async: WorkerAsyncMessageT;
 	reply: ReplyTypesT;
 	fire: TimeoutFireInfoT;
 	worker: TimeoutWorkerTypesT;
