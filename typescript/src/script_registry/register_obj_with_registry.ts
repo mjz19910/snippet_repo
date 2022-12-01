9297b68c8f684ac09055a3bc0b83e22d01e41db3
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 16:47:15 -0700

    u

--- typescript/src/script_registry/register_obj_with_registry.ts
+++ typescript/src/script_registry/register_obj_with_registry.ts
@@ -7,6 +7,10 @@ export let unregister_target_script_arr: {symbol: symbol;storage_id: number;}[]=
 
 export let gc_storage_id_counter=new Counter;
 
+class UnregisterToken {
+	constructor(public key: symbol, public storage_id: number) {}
+}
+
 class GCStorage<T extends {}> {
 	storage_id=gc_storage_id_counter.next();
 	store_gc_object(target: T) {
@@ -14,33 +18,29 @@ class GCStorage<T extends {}> {
 		if(target_ref) {
 			return target_ref.id;
 		}
-		let target_id=this.counter.next();
-		let obj_symbol=Symbol(target_id);
+		let id=this.counter.next();
+		let key=Symbol(id);
 		let held_value: HeldType={
 			type: 'held',
 			scope: "object",
 			storage_id:this.storage_id,
-			id: target_id,
-			key: obj_symbol
+			id,
+			key
 		};
-		let unregister_token={
-			symbol: obj_symbol,
-			storage_id:this.storage_id,
-		};
+		let unregister_token=new UnregisterToken(key,this.storage_id);
 		this.target_arr.push({
-			key: obj_symbol,
+			key,
 			storage_id:this.storage_id,
-			id: target_id,
+			id,
 			ref: new WeakRef(target),
 		});
 		this.unregister_token_arr.push(unregister_token);
 		script_registry.register(target,held_value,unregister_token);
-		console.log("Called register_obj with non-script",target);
-		return target_id;
+		return id;
 	}
 	counter=new Counter;
 	target_arr: (WeakRefTo<T>|null)[]=[];
-	unregister_token_arr: {symbol: symbol;}[]=[];
+	unregister_token_arr: {key: symbol;}[]=[];
 }
 
 export let target_object_storage=new GCStorage<{}>();
