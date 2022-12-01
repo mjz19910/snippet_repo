f55f45689a74caa22990b45ee05c12e74ee5ec04
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 14:50:09 -0700

    u

--- typescript/src/vm/TimerApi.ts
+++ typescript/src/vm/TimerApi.ts
@@ -13,42 +13,40 @@ import {
 import {
 	TimeoutClearStrings,
 	TimeoutSetStrings,
+	TimerMessageTypes,
 } from "./interfaces.js";
 import {MakeReplyData} from "./MakeReplyData.js";
 import {NoDataMsg} from "./NoDataMsg.js";
-import {NumInfoMsg} from "./NumInfoMsg.js";
-import {RefVarMsg} from "./RefVarMsg.js";
-import {TimerMessageTypes} from "./TimerMessageTypes.js";
 
 export class TimerApi {
 	msg_types=new TimerMessageTypes;
 	set_names=new TimeoutSetStrings;
 	clear_names=new TimeoutClearStrings;
 	handled: number[]=[];
-	to_handle: (NoDataMsg|NumInfoMsg|RefVarMsg)[];
+	to_handle: (NoDataMsg|MakeReplyData)[];
 	constructor() {
 		this.to_handle=[
 			{t: TimeoutMessageReady},
 			{t: TimeoutSetSingle},
 			{t: TimeoutSetRepeating},
 			{t: TimeoutClearSingle},
-			new MakeReplyData(ReplyFromWorker,WorkerReadyReply,TimeoutMessageReady,{}),
+			new MakeReplyData(ReplyFromWorker,WorkerReadyReply,TimeoutMessageReady),
 			// TimeoutSetTypeS
 			new MakeReplyData(ReplyFromWorker,ReplySetSingle,{
 				var: 'local_id'
-			},{}),
+			}),
 			// TimeoutSetTypeR
 			new MakeReplyData(ReplyFromWorker,ReplySetRepeating,{
 				var: 'local_id'
-			},{}),
+			}),
 			// TimeoutClearS
 			new MakeReplyData(ReplyFromWorker,ReplyClearSingle,{
 				var: 'remote_id'
-			},{}),
+			}),
 			// TimeoutClearR
 			new MakeReplyData(ReplyFromWorker,ReplyClearRepeating,{
 				var: 'remote_id'
-			},{})
+			})
 		];
 	}
 }
