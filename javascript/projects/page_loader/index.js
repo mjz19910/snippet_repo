13d48103ad8c2b16675e8a31ce6f4709b2a71d6f
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 23:50:03 -0700

    u

---
+++ javascript/projects/page_loader/index.js
@@ -0,0 +1,1 @@
+export {page_loader_dom_state} from "./src/page_loader_dom_state.js";
--- javascript/projects/repl_plugin/extern.js
+++ javascript/projects/repl_plugin/extern.js
@@ -1,2 +1,8 @@
-export {ReplPluginManager} from "../repl_plugin/index.js";
-export {g_repl_plugin_value} from "../repl_plugin/index.js";
+import {page_loader_dom_state} from "../page_loader/index.js";
+import {g_repl_plugin_value,ReplPluginManager} from "../repl_plugin/index.js";
+
+export {
+	page_loader_dom_state,
+	g_repl_plugin_value,
+	ReplPluginManager,
+};
