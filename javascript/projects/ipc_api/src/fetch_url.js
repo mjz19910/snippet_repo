4b3efeef2b0cb858c5c9aeb5ef7062bf3c2b955e
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 23:33:19 -0700

    u

--- javascript/projects/ipc_api/src/fetch_url.js
+++ javascript/projects/ipc_api/src/fetch_url.js
@@ -1,5 +1,5 @@
-import {fetch_url as _real_fetch_url} from "../../page_loader/src/fetch_url.js"
-/**@arg {Parameters<typeof _real_fetch_url>[0]} state */
+import {fetch_url as ipc_base_fetch_url} from "../../page_loader/src/fetch_url.js"
+/**@arg {Parameters<typeof ipc_base_fetch_url>[0]} state */
 export async function fetch_url(state) {
-	await _real_fetch_url(state)
+	await ipc_base_fetch_url(state)
 }
