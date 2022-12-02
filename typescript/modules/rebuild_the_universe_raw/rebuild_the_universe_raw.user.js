a4d0cd10494fa0bec22bc0eec99fe6586887d8a2
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-02 14:59:48 -0700

    u

--- typescript/modules/rebuild_the_universe_raw/rebuild_the_universe_raw.user.js
+++ typescript/modules/rebuild_the_universe_raw/rebuild_the_universe_raw.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		rebuild the universe
 // @namespace	http://tampermonkey.net/
-// @version		4.2.2
+// @version		4.2.3
 // @description	rebuildtheuniverse.com automation
 // @author		You
 // @match		http://rebuildtheuniverse.com/?type=real
@@ -16,8 +16,8 @@
 // @match		https://ssh.login.local:9342/mirror/rebuildtheuniverse.com/?type=real
 // @run-at		document-start
 // @grant		none
-// @updateURL	https://github.com/mjz19910/snippet_repo/raw/master/typescript/src/modules/rebuild_the_universe_raw/rebuild_the_universe_raw.meta.js
-// @downloadURL	https://github.com/mjz19910/snippet_repo/raw/master/typescript/src/modules/rebuild_the_universe_raw/rebuild_the_universe_raw.user.js
+// @updateURL	https://github.com/mjz19910/snippet_repo/raw/master/typescript/modules/rebuild_the_universe_raw/rebuild_the_universe_raw.meta.js
+// @downloadURL	https://github.com/mjz19910/snippet_repo/raw/master/typescript/modules/rebuild_the_universe_raw/rebuild_the_universe_raw.user.js
 // ==/UserScript==
 /* eslint-disable no-undef,no-lone-blocks,no-eval */
 
