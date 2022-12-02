5ae853753507e9d2c5f4c08bc98cd468185fa37e
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 17:14:26 -0700

    u

--- typescript/src/script_registry/replace_cb_with_safe_proxy.ts
+++ typescript/src/script_registry/replace_cb_with_safe_proxy.ts
@@ -3,11 +3,22 @@ import {is_in_ignored_from_src_fn} from "./is_in_ignored_from_src_fn.js";
 import {is_in_userscript_fn} from "./is_in_userscript_fn.js";
 import {register_obj_with_registry} from "./register_obj_with_registry.js";
 
+function add_own_property<T>(obj: T, key: PropertyKey) {
+	Object.defineProperty(obj, key, {
+		value: undefined,
+		configurable: true,
+		enumerable: false,
+		writable: true,
+	});
+}
+
 export function replace_cb_with_safe_proxy(args: any[],index: number) {
 	let value=args[index];
 	if(index&&args&&value instanceof Function) {
-		if(document.currentScript) {
-			value.reg_id=register_obj_with_registry(document.currentScript);
+		let val_fn: Function=value;
+		add_own_property(val_fn,"reg_id");
+		if(document.currentScript && 'reg_id' in val_fn) {
+			val_fn.reg_id=register_obj_with_registry(document.currentScript);
 		}
 		args[index]=new Proxy(value,{
 			apply(...a) {
