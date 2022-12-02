b7e6941968ff6899de63cd0f0f25f5b89ad35008
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-02 15:04:50 -0700

    u

--- typescript/examples/express_1.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-const app=express();
-
-app.get("/:id",(req,res) => {
-	res.send(`id: ${req.params.id}`);
-});
--- typescript/examples/type_chal.ts
+++
@@ -1,6 +0,0 @@
-type TupleToObject<TTuple extends readonly PropertyKey[]> = {
-	[TIndex in TTuple[number]]: TIndex;
-};
-
-// @ts-expect-error
-type err=TupleToObject<[[1,2], {}]>;
