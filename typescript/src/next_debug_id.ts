98db75f226a7d4e24271e9fab226a7783ea21b20
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 22:37:48 -0700

    u

--- typescript/src/next_debug_id.ts
+++ typescript/src/next_debug_id.ts
@@ -1,5 +1,5 @@
-import {debug_id_syms} from ".././debug_id_syms.js";
-import {debug_id_gen} from "../debug_id_gen.js";
+import {debug_id_syms} from "./debug_id_syms.js";
+import {debug_id_gen} from "./debug_id_gen.js";
 
 export function next_debug_id() {
 	const id=debug_id_gen.next()
