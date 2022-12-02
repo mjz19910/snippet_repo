cd5350205dd4fe1d2d649bbc4a030d8a16bb6be8
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-02 00:23:43 -0700

    u

--- typescript/src/modules/rebuild_the_universe_raw/support/Box.ts
+++ typescript/src/modules/rebuild_the_universe_raw/support/Box.ts
@@ -1,11 +1,14 @@
 import {AsyncFunctionBox} from "./AsyncFunctionBox.js";
 import {FunctionBox} from "./FunctionBox.js";
+import {NodeBox} from "./NodeBox.js";
 import {NumberBox} from "./NumberBox.js";
 import {ObjectBox} from "./ObjectBox.js";
 import {PromiseBox} from "./PromiseBox.js";
 import {StringBox} from "./StringBox.js";
 import {VoidBox} from "./VoidBox.js";
 import {WindowBox} from "./WindowBox.js";
+import {StackVMBox} from "./StackVMBox.js";
+import {FunctionConstructorBox} from "./FunctionConstructorBox.js";
 
 export type Box=
 	NumberBox|
@@ -16,4 +19,7 @@ export type Box=
 	FunctionBox|
 	PromiseBox|
 	StringBox|
+	NodeBox|
+	StackVMBox|
+	FunctionConstructorBox|
 	never;
