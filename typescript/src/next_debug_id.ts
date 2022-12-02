792e1314098596d84d6054cdbec534b36d30dcb6
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 22:56:33 -0700

    u

--- typescript/src/next_debug_id.ts
+++
@@ -1,9 +0,0 @@
-import {debug_id_syms} from "./debug_id_syms.js";
-import {debug_id_gen} from "./debug_id_gen.js";
-
-export function next_debug_id() {
-	const id=debug_id_gen.next();
-	const sym=Symbol(id);
-	debug_id_syms.push(new WeakRef({sym}));
-	return sym;
-}
