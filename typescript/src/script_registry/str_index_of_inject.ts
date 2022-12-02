07ce75d38f11a5d642a393c4fca1a7a9efa62569
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 17:21:07 -0700

    u

--- typescript/src/script_registry/str_index_of_inject.ts
+++ typescript/src/script_registry/str_index_of_inject.ts
@@ -1,44 +1,25 @@
 import {get_nearest_script} from "./get_nearest_script.js";
-import {is_in_ignored_from_src_fn} from "./is_in_ignored_from_src_fn.js";
-import {is_in_userscript} from "./is_in_userscript.js";
 import {register_obj_with_registry} from "./register_obj_with_registry.js";
 import {scripts} from "./scripts.js";
 
 export function str_index_of_inject() {
 	let cur_script=get_nearest_script();
-	if(cur_script===void 0||cur_script===null) {
-		if(is_in_ignored_from_src_fn.flag)
-			return;
-		if(!is_in_userscript.flag)
-			throw new Error("No");
-		return;
-	}
-	let had_script=false;
-	if(cur_script) {
-		had_script=scripts.has(cur_script);
-	}
+	if(cur_script===null) return;
+	let had_script=scripts.has(cur_script);
 	if(!had_script) {
 		try {
-			if(cur_script)
-				scripts.add(cur_script);
+			scripts.add(cur_script);
 		} catch(e) {
 			debugger;
 		}
 		let id=register_obj_with_registry(cur_script);
 		console.log('new registry id',id);
-	}
-	if(!had_script) {
-		if(!cur_script)
-			return;
 		if(!(cur_script instanceof HTMLScriptElement))
 			return;
 		if(cur_script.src.includes("opentracker")) {
-			if(cur_script)
-				cur_script.remove();
-			cur_script=null;
+			cur_script.remove();
 			throw new Error("No tracking");
 		}
 		console.log(cur_script);
 	}
-	cur_script=null;
 }
