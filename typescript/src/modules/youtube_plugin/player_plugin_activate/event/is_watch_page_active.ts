d2eecf527125d95ca948bbdd0ba847a626301a8f
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-02 04:45:49 -0700

    u

--- typescript/src/modules/youtube_plugin/player_plugin_activate/event/is_watch_page_active.ts
+++ typescript/src/modules/youtube_plugin/player_plugin_activate/event/is_watch_page_active.ts
@@ -1,5 +1,7 @@
 import {ytd_page_manager} from "../elements/ytd_page_manager.js"
 
 export function is_watch_page_active() {
-	return ytd_page_manager.value&&ytd_page_manager.value.getCurrentPage()&&ytd_page_manager.value.getCurrentPage().nodeName=="YTD-WATCH-FLEXY"
+	if(!ytd_page_manager.value) return false;
+	if(!ytd_page_manager.value.getCurrentPage()) return false;
+	return ytd_page_manager.value.getCurrentPage().nodeName=="YTD-WATCH-FLEXY"
 }
