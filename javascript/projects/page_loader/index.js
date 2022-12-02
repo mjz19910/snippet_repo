9fbc2164aca7367b748ae3df01b7f32c96936a43
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 23:55:23 -0700

    u

--- javascript/projects/page_loader/index.js
+++ javascript/projects/page_loader/index.js
@@ -1,3 +1,9 @@
-export {PageLoaderState} from "./src/PageLoaderState.js";
-export {page_loader_dom_state} from "./src/page_loader_dom_state.js";
+import {get_cached_repl_plugin} from "./src/get_cached_repl_plugin.js";
+import {PageLoaderState} from "./src/PageLoaderState.js";
+import {page_loader_dom_state} from "./src/page_loader_dom_state.js";
 
+export {
+	get_cached_repl_plugin,
+	page_loader_dom_state,
+	PageLoaderState,
+};
