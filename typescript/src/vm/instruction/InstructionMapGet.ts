b9a96335d2be32795155878a20d74ae880f847d7
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 20:54:27 -0700

    u

--- typescript/src/vm/instruction/InstructionMapGet.ts
+++
@@ -1,4 +0,0 @@
-import {InstructionImplMap} from "./InstructionImplMap.js"
-import {InstructionMap} from "./InstructionMap.js"
-
-export type InstructionMapGet<T extends keyof InstructionImplMap&keyof InstructionMap>=[T,InstructionImplMap[T],InstructionMap[T]]
