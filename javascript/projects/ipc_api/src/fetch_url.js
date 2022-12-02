c6090019901faf36a6e7fe8fd0079bd4faa38c65
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-01 23:32:56 -0700

    u

--- javascript/projects/ipc_api/src/fetch_url.js
+++ javascript/projects/ipc_api/src/fetch_url.js
@@ -1,9 +1,5 @@
-import {fetch_url as _real_fetch_url} from "../../page_loader/index.js"
+import {fetch_url as _real_fetch_url} from "../../page_loader/src/fetch_url.js"
 /**@arg {Parameters<typeof _real_fetch_url>[0]} state */
 export async function fetch_url(state) {
-	if(!state)
-		throw new Error("No state")
-	if(!state.on_incoming_message)
-		throw new Error("No Handler for server response")
 	await _real_fetch_url(state)
 }
