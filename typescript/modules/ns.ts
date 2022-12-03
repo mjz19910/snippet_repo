e6e62c3f86532d64222b79a4a4d209c53f19a856
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-03 05:38:30 -0700

    u

---
+++ typescript/modules/ns.ts
@@ -0,0 +1,1 @@
+export * from "../ns_import.js";
--- typescript/modules/youtube_plugin/ns_import.ts
+++ typescript/modules/youtube_plugin/ns_import.ts
@@ -1,4 +1,4 @@
-export * from "../ns_import.js";
+export * from "../ns.js";
 
 import {CustomEventTarget} from "./dom_observer/CustomEventTarget.js";
 import {CustomEventType} from "./dom_observer/CustomEventType.js";
