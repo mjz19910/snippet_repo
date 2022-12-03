0cdf8d762cd8668b6777b22f4fb01c65289b8a5b
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-03 05:24:31 -0700

    u

--- typescript/src/rebuild_the_universe/ns.ts
+++ typescript/src/rebuild_the_universe/ns.ts
@@ -1,10 +1,1 @@
-import {log_if} from "../log_if.js";
-import {LOG_LEVEL_WARN} from "../log_level_enum.js";
-import {UniqueIdGenerator} from "../UniqueIdGenerator.js";
-
 export * from "../ns.js";
-export {
-	UniqueIdGenerator,
-	log_if,
-	LOG_LEVEL_WARN,
-};
