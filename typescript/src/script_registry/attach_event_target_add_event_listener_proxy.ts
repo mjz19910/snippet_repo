e08a51da7dc474c1d9c759e28004a534fd5a410a
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-03 18:21:54 -0700

    u

--- typescript/src/script_registry/attach_event_target_add_event_listener_proxy.ts
+++ typescript/src/script_registry/attach_event_target_add_event_listener_proxy.ts
@@ -1,5 +1,4 @@
 import {attached_proxy_arr} from "./attached_proxy_arr.js";
-import {is_in_userscript} from "./is_in_userscript.js";
 import {replace_cb_with_safe_proxy} from "./replace_cb_with_safe_proxy.js";
 
 export function attach_event_target_add_event_listener_proxy() {
@@ -8,10 +7,8 @@ export function attach_event_target_add_event_listener_proxy() {
 			let target_obj=a[1];
 			let call_args=a[2];
 			replace_cb_with_safe_proxy(call_args,1);
-			if(!is_in_userscript.flag) {
-				debugger;
-				console.log(target_obj,call_args);
-			}
+			debugger;
+			console.log(target_obj,call_args);
 			let ret=Reflect.apply(...a);
 			return ret;
 		}
