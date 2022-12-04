fd17827d34d6aac7e18fc5c4e9efc783f1df7ee2
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-03 18:21:13 -0700

    u

--- typescript/src/script_registry/replace_cb_with_safe_proxy.ts
+++ typescript/src/script_registry/replace_cb_with_safe_proxy.ts
@@ -1,17 +1,6 @@
+import {add_own_property} from "./add_own_property";
 import {cur_event_fns} from "./cur_event_fns.js";
-import {is_in_ignored_from_src_fn} from "./is_in_ignored_from_src_fn.js";
-import {is_in_userscript_fn} from "./is_in_userscript_fn.js";
 import {register_obj_with_registry} from "./register_obj_with_registry.js";
-
-function add_own_property<T>(obj: T,key: PropertyKey) {
-	Object.defineProperty(obj,key,{
-		value: void 0,
-		configurable: true,
-		enumerable: false,
-		writable: true,
-	});
-	return true;
-}
 
 export function replace_cb_with_safe_proxy(args: any[],index: number) {
 	let value=args[index];
@@ -23,27 +12,13 @@ export function replace_cb_with_safe_proxy(args: any[],index: number) {
 		args[index]=new Proxy(value,{
 			apply(...a) {
 				let ret;
-				let should_reset=false;
 				cur_event_fns.push(a[0]);
 				let idx=cur_event_fns.indexOf(a[0]);
-				if(a[0].is_userscript_fn) {
-					is_in_ignored_from_src_fn.flag=true;
-					if(is_in_userscript_fn.flag===false) {
-						is_in_userscript_fn.flag=true;
-						should_reset=true;
-					}
-				}
 				try {
 					ret=Reflect.apply(...a);
 				} finally {
-					if(should_reset) {
-						is_in_userscript_fn.flag=false;
-						should_reset=false;
-					}
-					is_in_ignored_from_src_fn.flag=false;
 					delete cur_event_fns[idx];
 				}
-				delete cur_event_fns[idx];
 				return ret;
 			}
 		});
