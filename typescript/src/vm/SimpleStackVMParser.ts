f93153c8013e8975385cb62569b2a771ac3dfa04
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-03 23:59:29 -0700

    u

--- typescript/src/vm/SimpleStackVMParser.ts
+++ typescript/src/vm/SimpleStackVMParser.ts
@@ -29,7 +29,7 @@ export class SimpleStackVMParser {
 		throw new Error("TODO");
 	}
 	static format_instruction(cur: string[],format_list: Box[]) {
-		let target_instruction: [string,...Box[]]=[cur[0]];
+		let target_instruction: (string|Box)[]=[cur[0]];
 		for(let i=1;i<cur.length;i++) {
 			let arg=cur[i];
 			if(arg.startsWith("int"+"()"[0])) {
@@ -43,7 +43,7 @@ export class SimpleStackVMParser {
 				target_instruction[i]=res;
 				continue;
 			}
-			target_instruction[i]=new StringBox(arg);
+			target_instruction[i]=arg;
 		}
 		return target_instruction;
 	}
@@ -82,12 +82,15 @@ export class SimpleStackVMParser {
 				)
 		);
 	}
-	static verify_instruction(instruction: [string,...Box[]],left: [number]): InstructionType {
+	static verify_instruction(instruction: (string | Box)[],left: [number]): InstructionType {
 		const [m_opcode,...m_operands]=instruction;
 		switch(m_opcode) {
 			// variable argument count
 			case 'push':
 				left[0]-=2;
+				if(typeof m_operands[0]==='string') {
+					return [m_opcode,new StringBox(m_operands[0])];
+				}
 				return [m_opcode,m_operands[0]];
 			case 'call':
 				left[0]-=2;
@@ -111,7 +114,7 @@ export class SimpleStackVMParser {
 				throw new Error("Unexpected opcode");
 		}
 	}
-	static typecheck_instruction_stream(raw_instructions: [string,...Box[]][]): InstructionType[] {
+	static typecheck_instruction_stream(raw_instructions: (string|Box)[][]): InstructionType[] {
 		const instructions: InstructionType[]=[];
 		for(let i=0;i<raw_instructions.length;i++) {
 			const instruction=raw_instructions[i];
