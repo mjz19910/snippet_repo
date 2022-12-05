7d4aaf19c58f4aa10f40f15e90f61a05e7c64028
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-05 13:33:12 -0700

    u

--- javascript/decrypt_js/dropgalaxy.com/next_bracket_pair.js
+++ javascript/decrypt_js/dropgalaxy.com/next_bracket_pair.js
@@ -8,7 +8,11 @@ import {is_term} from "./is_term.js";
  */
 export function next_bracket_pair(arr,start_index,callback) {
 	let index=arr.indexOf("{",start_index);
+	let next_beg_index=arr.indexOf("{",index);
 	index=arr.indexOf("}",index);
+	if(index < next_beg_index) {
+		console.log('index < next_beg_index', index);
+	}
 	if(is_term(arr[index+1])) {index++;}
 	callback(arr,[start_index,index]);
 	return index+1;
