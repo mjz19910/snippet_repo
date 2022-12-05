f8b096cfa78e120cdd5dbdf8fb83bb6a579dd374
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-05 12:56:19 -0700

    u

--- javascript/decrypt_js/decode_shuf.mjs
+++ javascript/decrypt_js/decode_shuf.mjs
@@ -19,7 +19,7 @@ function get_enc(a) {
  * @type {(...x:number[])=>string}
  * @set _0x4aec3f get_enc_4
  */
-function get_enc_4(_b,_c,_d,_e,a) {
+function get_enc_4_a(_b,_c,_d,_e,a) {
 	let expected=a- -0x298;
 	let my_ver=a+0x298;
 	console.assert(expected===my_ver,"simplify correct");
@@ -30,7 +30,7 @@ function get_enc_4(_b,_c,_d,_e,a) {
  * @type {(...x:number[])=>string}
  * #define _0x3d969b get_enc_5
  */
-function get_enc_5(...[,,,,a]) {
+function get_enc_5_a(...[,,,,a]) {
 	return get_enc(a+0x298);
 }
 
@@ -38,7 +38,7 @@ function get_enc_5(...[,,,,a]) {
  * @type {(...x:number[])=>string}
  * @enc _0x314769 -> get_enc_4_x_1
  */
-function get_enc_4_a(_b,_c,_d,_e,a) {
+function get_enc_4_b(_b,_c,_d,_e,a) {
 	return get_enc(a+0x298);
 }
 
@@ -50,14 +50,14 @@ export function process_00(arr) {
 	while(!![]) {
 		try {
 			var cur=
-				-parseInt(get_enc_4(0x15a,-0x1a,0x11b,-0x5,0xa6))+
-				-parseInt(get_enc_4(-0xf5,-0x36,-0x82,0x4f,-0x67))+
-				-parseInt(get_enc_5(0x18f,0x133,0xa5,0xfc,0xf6))+
-				-parseInt(get_enc_4_a(0x24,-0x83,0xa0,-0x3d,-0xb))+
-				-parseInt(get_enc_4(-0xb3,-0xf2,0x6,-0x36,-0x1d))+
-				-parseInt(get_enc_4_a(0x87,0x10e,0x16a,0x78,0xb0))+
-				parseInt(get_enc_4(0x17c,0x8c,0xb0,0x3e,0xb8))*
-				parseInt(get_enc_4(0x37,-0x68,-0xc2,-0x67,-0x45));
+				-parseInt(get_enc_4_a(0x15a,-0x1a,0x11b,-0x5,0xa6))+
+				-parseInt(get_enc_4_a(-0xf5,-0x36,-0x82,0x4f,-0x67))+
+				-parseInt(get_enc_5_a(0x18f,0x133,0xa5,0xfc,0xf6))+
+				-parseInt(get_enc_4_b(0x24,-0x83,0xa0,-0x3d,-0xb))+
+				-parseInt(get_enc_4_a(-0xb3,-0xf2,0x6,-0x36,-0x1d))+
+				-parseInt(get_enc_4_b(0x87,0x10e,0x16a,0x78,0xb0))+
+				parseInt(get_enc_4_a(0x17c,0x8c,0xb0,0x3e,0xb8))*
+				parseInt(get_enc_4_a(0x37,-0x68,-0xc2,-0x67,-0x45));
 			if(cur===target) break;
 			else arr.push(arr.shift());
 		} catch(_) {
