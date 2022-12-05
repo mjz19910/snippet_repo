c079ff0a36b947d4266d2a9d3700739e20ff0aa1
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-05 14:30:27 -0700

    u

--- javascript/decrypt_js/dropgalaxy.com/use_encrypt_code.js
+++ javascript/decrypt_js/dropgalaxy.com/use_encrypt_code.js
@@ -1,5 +1,4 @@
 import {to_token_arr} from "../js/to_token_arr.js";
-import {next_bracket_pair as x} from "./next_bracket_pair.js";
 import js_str_2 from "./src_template_code.js";
 import js_str_1 from "./src_template_decrypt_arr_str.js";
 
@@ -7,31 +6,9 @@ export function use_encrypt_code() {
 	let eq_idx=js_str_1.indexOf("=");
 	let var_def=js_str_1.slice(0,eq_idx);
 	let var_code=js_str_1.slice(eq_idx+1,js_str_1.lastIndexOf(";"));
-	let code_lvl=js_str_2.split(/(\{|\})/).filter(e => e!=="");
+	let code_lvl=js_str_2.split(/(\{|\})/);
 	let arr=to_token_arr(code_lvl,0);
-	let index=0;
-	function cx() {
-		return x(arr,index,function(arr,[start_index,index]) {
-			if(start_index>3) {
-				start_index+=2;
-			}
-			if(index-start_index>60) {
-				console.log(JSON.stringify([
-					arr.slice(start_index,start_index+30).join("").trim(),
-					arr.slice(index-30,index+1).join("").trim(),
-				]));
-			} else {
-				console.log(JSON.stringify(
-					arr.slice(start_index,index+1).join("").trim(),
-				));
-			}
-		});
-	}
-	for(let i=0;i<7;i++) {
-		let prev_index=index;
-		index=cx();
-		eval(arr.slice(prev_index+1,index+1).join(""));
-	}
+	console.log(arr);
 	let do_eval=false;
 	if(do_eval) {
 		eval(var_def+"="+var_code+";console.log("+var_def.split(" ")[1]+");"+js_str_2);
