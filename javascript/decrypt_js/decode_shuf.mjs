d9361f458f2540db3b6b62db8770547876084ff0
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-05 12:55:54 -0700

    u

--- javascript/decrypt_js/decode_shuf.mjs
+++ javascript/decrypt_js/decode_shuf.mjs
@@ -16,11 +16,7 @@ function get_enc(a) {
 }
 
 /**
- * @param {number} _b
- * @param {number} _c
- * @param {number} _d
- * @param {number} _e
- * @param {number} a
+ * @type {(...x:number[])=>string}
  * @set _0x4aec3f get_enc_4
  */
 function get_enc_4(_b,_c,_d,_e,a) {
@@ -31,42 +27,42 @@ function get_enc_4(_b,_c,_d,_e,a) {
 }
 
 /**
- * @param {number[]} arg0
+ * @type {(...x:number[])=>string}
  * #define _0x3d969b get_enc_5
  */
-function get_enc_5(...[,,,,v]) {
-	return get_enc(v+0x298);
+function get_enc_5(...[,,,,a]) {
+	return get_enc(a+0x298);
 }
 
 /**
- * @param {number[]} arg0
+ * @type {(...x:number[])=>string}
  * @enc _0x314769 -> get_enc_4_x_1
  */
-function get_enc_4_x_1(...[,,,,v]) {
-	return get_enc(v+0x298);
+function get_enc_4_a(_b,_c,_d,_e,a) {
+	return get_enc(a+0x298);
 }
 
 /**
- * @param {(string|undefined)[]} _0x36ea8
+ * @param {(string|undefined)[]} arr
  */
-export function process_00(_0x36ea8) {
-	let _0x206ef7=0xd357d;
+export function process_00(arr) {
+	let target=0xd357d;
 	while(!![]) {
 		try {
 			//cspell:words _0x3bbefc
-			var _0x3bbefc=
+			var cur=
 				-parseInt(get_enc_4(0x15a,-0x1a,0x11b,-0x5,0xa6))+
 				-parseInt(get_enc_4(-0xf5,-0x36,-0x82,0x4f,-0x67))+
 				-parseInt(get_enc_5(0x18f,0x133,0xa5,0xfc,0xf6))+
-				-parseInt(get_enc_4_x_1(0x24,-0x83,0xa0,-0x3d,-0xb))+
+				-parseInt(get_enc_4_a(0x24,-0x83,0xa0,-0x3d,-0xb))+
 				-parseInt(get_enc_4(-0xb3,-0xf2,0x6,-0x36,-0x1d))+
-				-parseInt(get_enc_4_x_1(0x87,0x10e,0x16a,0x78,0xb0))+
+				-parseInt(get_enc_4_a(0x87,0x10e,0x16a,0x78,0xb0))+
 				parseInt(get_enc_4(0x17c,0x8c,0xb0,0x3e,0xb8))*
 				parseInt(get_enc_4(0x37,-0x68,-0xc2,-0x67,-0x45));
-			if(_0x3bbefc===_0x206ef7) break;
-			else _0x36ea8['push'](_0x36ea8['shift']());
-		} catch(_0x75b472) {
-			_0x36ea8['push'](_0x36ea8['shift']());
+			if(cur===target) break;
+			else arr.push(arr.shift());
+		} catch(_) {
+			arr.push(arr.shift());
 		}
 	}
 }
