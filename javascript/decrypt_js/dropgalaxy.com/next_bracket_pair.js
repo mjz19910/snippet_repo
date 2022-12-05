e01a3a5601d162aa95f6231ec8ea3338232f630d
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-05 13:25:28 -0700

    u

--- javascript/decrypt_js/dropgalaxy.com/next_bracket_pair.js
+++ javascript/decrypt_js/dropgalaxy.com/next_bracket_pair.js
@@ -4,12 +4,12 @@ import {is_term} from "./is_term.js";
 /**
  * @param {number} start_index
  * @param {string[]} arr
- * @param {(start_index: number, index: number)=> void} callback
+ * @param {(arr: string[], result: [start:number,end:number])=> void} callback
  */
 export function next_bracket_pair(arr,start_index,callback) {
 	let index=arr.indexOf("{",start_index);
 	index=arr.indexOf("}",index);
 	if(is_term(arr[index+1])) {index++;}
-	callback(start_index,index);
+	callback(arr,[start_index,index]);
 	return index+1;
 }
