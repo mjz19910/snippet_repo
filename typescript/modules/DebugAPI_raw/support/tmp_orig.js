dd55affa00618d2436d18bf7f1ec381985e289af
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-05 08:40:28 -0700

    u

--- typescript/modules/DebugAPI_raw/support/tmp_orig.js
+++ typescript/modules/DebugAPI_raw/support/tmp_orig.js
@@ -103,7 +103,7 @@ let code_lvl=decrypt_code.split(/(\{|\})/).filter(e=>e!=="");
 /** @arg {string[]} arr */
 function to_level(arr) {
 	let level=0;
-	/** @type {[number,{}][]} */
+	/** @type {[number,[number,string]|['other',(number|string)[]]][]} */
 	let ret=[];
 	/** @param {string} str */
 	function is_open(str) {
@@ -123,7 +123,7 @@ function to_level(arr) {
 			debugger;
 			ret.push([level,to_level(item)]);
 		} else {
-			ret.push([level,item]);
+			ret.push([level,[level,item[0]]]);
 		}
 		if(arr[i]&&is_open(cur)) {
 			level++;
