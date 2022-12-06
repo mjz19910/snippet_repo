78935c108daab6dd17730aa34760e43891638b3a
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-06 15:23:12 -0700

    u

--- typescript/modules/DebugAPI_raw/DebugAPI.meta.js
+++ typescript/modules/DebugAPI_raw/DebugAPI.meta.js
@@ -11,6 +11,7 @@
 // @updateURL    https://github.com/mjz19910/snippet_repo/raw/master/typescript/modules/DebugAPI_raw/DebugAPI.meta.js
 // @downloadURL  https://github.com/mjz19910/snippet_repo/raw/master/typescript/modules/DebugAPI_raw/DebugAPI.user.js
 // ==/UserScript==
-// Cache bust(4) =====
+// Cache bust(2)
+//
 /* Copyright 2019-2022 @mjz19910 */
 /* eslint-disable no-undef */
