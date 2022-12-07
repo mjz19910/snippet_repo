42824ca74d879ead8e8a6aa96652e496151f6d2e
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-06 23:30:34 -0700

    u

--- javascript/projects/repl_plugin/extern.js
+++ javascript/projects/repl_plugin/extern.js
@@ -1,8 +1,6 @@
-import {FakeWindow} from "../browser_fake_dom/index.js";
 import {get_cached_repl_plugin,make_reload_page_request_handler,PageLoaderState,page_loader_dom_state} from "../page_loader/index.js";
 import {g_repl_plugin_value,ReplPluginManager} from "../repl_plugin/index.js";
 export {
-	FakeWindow,
 	g_repl_plugin_value,
 	get_cached_repl_plugin,
 	make_reload_page_request_handler,
