1a9a4de7a7d6bb0377c99385684aa52545e68a43
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-07 03:34:51 -0700

    u

--- javascript/final/items/item9_v1.js
+++ javascript/final/items/item9_v1.js
@@ -3,23 +3,36 @@
 v1 (cur): snippet_repo/javascript/final/items/item9_v1.js
 v2 (new): snippet_repo/javascript/group1/sub_a/item-_9.js
 */
+/**
+ * @param {any} a
+ * @param {any} c
+ * @param {any} m_require
+ */
 function found_modules(a,c,m_require) {
 	void a,c,m_require;
 }
+/**
+ * @param {(this: Function, thisArg: any, ...argArray: any[]) => any} oc
+ * @param {{ (a: any, c: any, m_require: any): void; (a: any, c: any, m_require: any): void; (arg0: any, arg1: any, arg2: any): void; }} cb
+ */
 function rv(oc,cb) {
 	void oc;
-	if(Function.prototype.call.rep) {
+	let fpc_x=Function.prototype.call;
+	if('rep' in fpc_x&&fpc_x.rep) {
 		location.reload();
 		return;
 	}
 	var fr=document.createElement("iframe");
 	document.head.append(fr);
-	var fpc=fr.contentWindow.Function.prototype.call;
-	var fa=fr.contentWindow.Function.prototype.apply.bind(fpc);
-	var fb=fr.contentWindow.Function.prototype.apply.bind(fr.contentWindow.Function.prototype.apply);
-	var npc,sfunc=[];
-	var nac;
-	npc=Function.prototype.call=function(...r) {
+	if(!fr.contentWindow) throw 1;
+	let frame_window=fr.contentWindow.window;
+	var fpc=frame_window.Function.prototype.call;
+	var fa=frame_window.Function.prototype.apply.bind(fpc);
+	var fb=frame_window.Function.prototype.apply.bind(frame_window.Function.prototype.apply);
+	var npc;
+	/** @type {string[]} */
+	var s_func=[];
+	npc=Function.prototype.call=function(/** @type {any[]} */ ...r) {
 		var c;
 		switch(r.length) {
 			case 3: {
@@ -28,7 +41,7 @@ function rv(oc,cb) {
 					var ars_i=ars[0][1].indexOf(this);
 					if(ars[0][1].indexOf(this)>-1) {
 						console.log("found module array:","require."+ars[0][0]);
-						var mods=Object.entries(r[3]).filter(([a,b]) => b.hasOwnProperty(ars_i)&&b[ars_i]===r[1]);
+						var mods=Object.entries(r[3]).filter(([,b]) => b.hasOwnProperty(ars_i)&&b[ars_i]===r[1]);
 						if(mods.length>0) {
 							console.log("found module cache:","require."+mods[0][0]);
 							cb(ars[0][1],mods[0][1],r[3]);
@@ -39,23 +52,26 @@ function rv(oc,cb) {
 			default:
 				c=fa(this,r);
 		}
-		if(sfunc.indexOf(this.toString())==-1) {
-			sfunc.push(this.toString());
+		if(s_func.indexOf(this.toString())==-1) {
+			s_func.push(this.toString());
 		}
 		return c;
 	};
-	nac=function(tv,r) {
+	/** @this {{}} */
+	let nac=function(/** @type {any} */ tv,/** @type {any} */ r) {
 		var c;
 		c=fb(this,[tv,r]);
-		if(sfunc.indexOf(this.toString())==-1) {
-			sfunc.push(this.toString());
+		if(s_func.indexOf(this.toString())==-1) {
+			s_func.push(this.toString());
 		}
 		return c;
 	};
 	Function.prototype.apply=nac;
-	npc.rep=1;
-	window.sfunc=sfunc;
-	return sfunc;
+	/** @arg {any} v */
+	function any(v) {return v;}
+	any(npc).rep=1;
+	any(window).s_func=s_func;
+	return s_func;
 }
 rv(Function.prototype.call,found_modules)
 [rv,found_modules];
