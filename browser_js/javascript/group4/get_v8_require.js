3bb0b30ffa06fd72eeb00f4994afa9764df4e087
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-07 08:49:30 -0700

    u

--- browser_js/javascript/group4/get_v8_require.js
+++ browser_js/javascript/group4/get_v8_require.js
@@ -1,11 +1,10 @@
 function run_d() {
-	let old_ver=null;
 	let old_state=null;
 	if(window.get_v8_require_run&&window.v8_require_state) {
-		old_ver=window.get_v8_require_run;
 		old_state=window.v8_require_state;
 	}
-	async function get_v8_require_run(a) {
+	/** @arg {ReqSt} state */
+	async function get_v8_require_run(state) {
 		if(!debug) {
 			console.log("no debug");
 			return null;
@@ -15,51 +14,56 @@ function run_d() {
 			return null;
 		}
 		let w=debug;
-		this.sco=a;
 		w.u=undebug;
 		window.native_module_scope??={
 			v8: window.nodeRequire("v8"),
 		};
 		let q=window.native_module_scope;
-		a.q=q;
-		let l=[];
-		a.l=l;
+		state.native_module_scope=q;
+		let import_arr=[];
+		if(state.back_ptr?.import_arr) {
+			import_arr=state.back_ptr.import_arr;
+		}
+		state.import_arr=import_arr;
 		let x;
-		w.m=async function(f,b,s) {
-			let w=this;
-			if(w.aborted) {
+		state.m=async function(f,b,s) {
+			if(this.aborted) {
+				console.log("aborted");
+				return null;
+			}
+			if(!w.u) {
+				console.log("lost undebug function");
 				return null;
 			}
 			w.u(f);
 			w(f,`debug.f=${s};debug.s();0`);
 			console.log(s);
-			w.s=(e) => {
-				s=w.f;
-			};
 			try {
 				w.b=b(f);
 			} catch(e) {
-				w.aborted=true;
+				this.aborted=true;
 				w.e=e;
 			}
 			return s;
 		};
-		l[0]=x=await w.m(q.v8.Serializer,f => new f,'assert');
+		import_arr[0]=x=await w.m(q.v8.Serializer,f => new f,'assert');
 		//v8.Serializer
-		l[1]=x=await w.m(x,f => f(true),'lazyError');
+		import_arr[1]=x=await w.m(x,f => f(true),'lazyError');
 		//assert
-		l[2]=x=q.require=await w.m(x,f => f(),'require');
+		import_arr[2]=x=q.require=await w.m(x,f => f(),'require');
 		//lazyError
-		l[3]=q.NativeModule=await w.m(x,f => f('v8'),'NativeModule');
+		import_arr[3]=q.NativeModule=await w.m(x,f => f('v8'),'NativeModule');
 		//require
-		l[4]=q.primordials=await w.m(q.require,f => f('v8'),'primordials');
+		import_arr[4]=q.primordials=await w.m(q.require,f => f('v8'),'primordials');
 		//require
-		return l;
+		return import_arr;
 	}
 	window.get_v8_require_run=get_v8_require_run;
 	class ReqSt {
 		/** @type {V8RequireState|null} */
 		back_ptr=null;
+		/** @type {{}|null} */
+		native_module_scope=null;
 	}
 	let state=new ReqSt;
 	window.v8_require_state=state;
