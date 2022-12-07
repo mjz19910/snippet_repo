79b2c5fe2d65d140309f2096497cc852e4c42ea6
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-07 02:55:03 -0700

    u

--- typescript/src/ns_import.ts
+++ typescript/src/ns_import.ts
@@ -1,6 +1,7 @@
 import {log_if} from "./log_if.js";
 import {LOG_LEVEL_WARN} from "./log_level_enum.js";
 import {UniqueIdGenerator} from "./UniqueIdGenerator.js";
+export {StringBox} from "./box/StringBox.js";
 
 // export
 export {
