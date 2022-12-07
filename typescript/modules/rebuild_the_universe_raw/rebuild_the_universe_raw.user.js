50e405acc43d26ad1584621646e8a1322a3b2f40
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-06 23:29:05 -0700

    u

--- typescript/modules/rebuild_the_universe_raw/rebuild_the_universe_raw.user.js
+++ typescript/modules/rebuild_the_universe_raw/rebuild_the_universe_raw.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		rebuild the universe
 // @namespace	http://tampermonkey.net/
-// @version		4.2.4
+// @version		4.2.5
 // @description	rebuildtheuniverse.com automation
 // @author		You
 // @match		http://rebuildtheuniverse.com/?type=real
