4c98cbd2d9e96a4c741daa8a5b6c3a95f02dc947
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-07 07:09:18 -0700

    u

--- cmd_js/projects/repl_plugin/src/plugins/REPLPlugin.js
+++
@@ -1,9 +0,0 @@
-import {ReplPluginManager} from "../ReplPluginManager.js";
-
-export class REPLPlugin {
-	/** @param {ReplPluginManager} repl*/
-	constructor(repl) {
-		this.repl=repl;
-	}
-	enable() {}
-}
