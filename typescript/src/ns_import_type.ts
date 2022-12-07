1a5b92d6e51692175032ebc38ca83bdfd51c994e
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-06 23:59:00 -0700

    u

--- typescript/src/ns_import_type.ts
+++ typescript/src/ns_import_type.ts
@@ -1,3 +1,5 @@
+export type {InstructionType} from "./instruction/InstructionType.js";
+export type {CastOperandTarget} from "./instruction/CastOperandTarget.js";
 export type {InstructionMap} from "./instruction/InstructionMap.js";
 export type {Box} from "./box/Box.js";
 export type {StackVM,StackVMBase} from "./vm/StackVM.js";
