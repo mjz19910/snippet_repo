ca39974ee1c8bdea44feedf172bbc0e89ff0ba37
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-07 06:23:07 -0700

    u

--- javascript/group3/xml_event.js
+++ javascript/group3/xml_event.js
@@ -1,5 +1,9 @@
+/**
+ * @param {string} s
+ */
 function split_tags(s) {
 	var t=""
+	/** @type {([1,string]|[0,string])[]} */
 	var res=[]
 	while(s.length>0) {
 		if(s[0]=="<") {
@@ -21,7 +25,22 @@ function split_tags(s) {
 	}
 	return res
 }
-ar=split_tags(document.documentElement.innerHTML)
-ar2=ar.slice(ar.map((e,n) => [e[1],e[0],n]).filter(e => e[0].match("class=\"text"))[0][2]).map((e,n) => [e[1],e[0],n])
-//cspell:ignore greentext
-target=ar2[ar2.find(e => e[0].match("greentext"))[2]+2][0].split(";").pop()
+function main() {
+	let ar=split_tags(document.documentElement.innerHTML);
+	/** @type {([string,0|1,number])[]} */
+	let ar_1=ar.map(
+		(e,n) => [e[1],e[0],n]
+	);
+	/** @type {([string,0|1,number])[]} */
+	let ar2=ar.slice(
+		ar_1.filter(
+			e => e[0].match("class=\"text"))[0][2]
+		).map(
+			(e,n) => [e[1],e[0],n]
+	)
+	let find_r=ar2.find(e => e[0].match("greentext"));
+	if(!find_r) return null;
+	//cspell:ignore greentext
+	let target=ar2[find_r[2]+2][0].split(";").pop()
+	return target;
+}
--- javascript/group3/yt-dbg.js
+++
@@ -1,5 +0,0 @@
-_yt_player={
-	bp: {
-		data: "??"
-	}
-}
