885ebf28e4c0f6cb281abc43bb5b61b688731812
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-07 07:15:14 -0700

    u

--- cmd_js/projects/page_loader/src/fetch_local_file.js
+++ cmd_js/projects/page_loader/src/fetch_local_file.js
@@ -4,6 +4,6 @@ import {PageLoaderState} from "./PageLoaderState.js";
  * @param {string} _filename
  */
 export function fetch_local_file(_filename) {
-	let fetch_state=new PageLoaderState(null);
+	let fetch_state=new PageLoaderState("");
 	fetch_state;
 }
