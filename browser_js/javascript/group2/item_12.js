dc9081125c277e80307bb366c4ae2f0f4fcd8148
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-07 09:51:15 -0700

    u

--- browser_js/javascript/group2/item_12.js
+++ browser_js/javascript/group2/item_12.js
@@ -1,20 +1,34 @@
 // spell:words lbry_chase
 function lbry_chase() {
 	if(location.href.match(/.+\/address\//)) {
-		let tmp=[...document.querySelector('tbody').children]
+		let tbody=document.querySelector('tbody');
+		if(!tbody) return;
+		/** @type {Element[]} */
+		let tmp=[];
+		for(let i=0;i<tbody.children.length;i++) {
+			tmp.push(tbody.children[i]);
+		}
 		console.log(tmp.map(e => e.innerHTML.replaceAll("\n","\\n")).join("\n"))
 		for(let i=0;i<tmp.length;i++) {
-			let str=tmp[i].children[6].innerText.split(" LBC",1)
-			if(parseFloat(str)<=0) {
+			let cur_child=tmp[i].children[6];
+			if(!(cur_child instanceof HTMLElement)) continue;
+			let str=cur_child.innerText.split(" LBC",1)
+			if(parseFloat(str[0])<=0) {
 				continue
 			}
-			tmp[i].children[1].children[0].children[0].click()
+			let element=tmp[i].children[1].children[0].children[0];
+			if(element instanceof HTMLInputElement) {
+				element.click();
+			}
 			break
 		}
 	}
 	let document_query_result=document.querySelectorAll(".inputs .input")
 	if(document_query_result.length===1) {
-		document_query_result[0].children[2].children[0].click()
+		let element=document_query_result[0].children[2].children[0];
+		if(element instanceof HTMLInputElement) {
+			element.click();
+		}
 	}
 
 }
