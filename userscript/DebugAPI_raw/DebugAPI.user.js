acfca65afe7a785848d330a5af8dccf831cc580d
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-08 16:08:30 -0700

    u

--- userscript/DebugAPI_raw/DebugAPI.user.js
+++ userscript/DebugAPI_raw/DebugAPI.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DebugAPI userscript
 // @namespace    https://github.com/mjz19910/
-// @version      1.1.9.2
+// @version      1.1.9.3
 // @description  DebugAPI.js from https://github.com/mjz19910/snippet_repo/blob/master/userscript/DebugAPI_raw/DebugAPI.user.js
 // @author       @mjz19910
 // @match        https://*/*
