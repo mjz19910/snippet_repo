09fafdb0eadc1c846b7542ce644e89e5e06f2924
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-08 07:26:20 -0700

    u

--- browser_js/typescript/modules/DebugAPI/types/BaseCompression.ts
+++ browser_js/typescript/modules/DebugAPI/types/BaseCompression.ts
@@ -2,16 +2,23 @@ import {CompressDual} from "./CompressDual.js";
 import {CompressStateBase} from "./CompressStateBase.js";
 
 export class BaseCompression {
-	static did_compress<T, 	U>(src: T[],dst: U[]) {
+	compress_result_state_dual(arg0: CompressDual): DualR_m {
+		return this.compress_result_dual(arg0.arr,arg0.ret);
+	}
+	compress_result_dual(src: AltPair<string,number>[],dst: AnyOrRepeat2_1<string,number>[]): DualR {
+		if(BaseCompression.did_compress(src,dst)) return [true,dst];
+		return [false,src];
+	}
+	static did_compress<T,U>(src: T[],dst: U[]) {
 		return dst.length<src.length;
 	}
 	did_decompress<T>(src: T[],dst: T[]) {
 		return dst.length>src.length;
 	}
-	static compress_result_state<T, 	U>(state: CompressStateBase<T,U>) {
-		return this.compress_result(state.arr, state.ret);
+	static compress_result_state<T,U>(state: CompressStateBase<T,U>) {
+		return this.compress_result(state.arr,state.ret);
 	}
-	static compress_result<T, 	U>(src: T[],dst: U[]): [true,U[]]|[false,T[]] {
+	static compress_result<T,U>(src: T[],dst: U[]): [true,U[]]|[false,T[]] {
 		if(this.did_compress(src,dst))
 			return [true,dst];
 		return [false,src];
@@ -22,9 +29,5 @@ export class BaseCompression {
 		if(this.did_decompress(src,dst))
 			return [true,dst];
 		return [false,dst];
-	}
-	compress_result_state_dual(v:CompressDual): DualR_m {
-		v;
-		throw new Error("Method not implemented.");
 	}
 }
