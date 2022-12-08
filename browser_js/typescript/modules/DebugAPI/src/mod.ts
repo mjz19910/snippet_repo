acdbe7c9cedf7a785009c959c097268f23e7633b
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-08 09:17:49 -0700

    u

--- browser_js/typescript/modules/DebugAPI/src/mod.ts
+++ browser_js/typescript/modules/DebugAPI/src/mod.ts
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         global DebugAPI userscript
+// @name         DebugAPI/src/mod.ts userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.3.2
-// @description  global DebugAPI userscript snippet from https://github.com/mjz19910/snippet_repo/blob/master/javascript/userscript_store/DebugAPI.user.js
+// @version      0.1.0
+// @description  DebugAPI/src/mod.ts userscript snippet from https://github.com/mjz19910/snippet_repo/raw/master/browser_js/typescript/modules/DebugAPI/src/mod.ts
 // @author       @mjz19910
 // @match        https://*/*
 // @match        http://*/*
 // @run-at       document-start
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/mjz19910/snippet_repo/master/javascript/userscript_store/DebugAPI.meta.js
-// @downloadURL  https://raw.githubusercontent.com/mjz19910/snippet_repo/master/javascript/userscript_store/DebugAPI.user.js
+// @updateURL    https://github.com/mjz19910/snippet_repo/raw/master/browser_js/typescript/modules/DebugAPI/src/mod.meta.ts
+// @downloadURL  https://github.com/mjz19910/snippet_repo/raw/master/browser_js/typescript/modules/DebugAPI/src/mod.ts
 // ==/UserScript==
 /* Copyright 2019-2022 @mjz19910 */
 /* eslint-disable no-undef */
