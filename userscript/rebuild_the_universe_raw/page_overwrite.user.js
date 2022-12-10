a4f11402517604d0c35a5ef96546a053e55f3be0
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-09 19:01:40 -0700

    u

--- userscript/rebuild_the_universe_raw/page_overwrite.user.js
+++ userscript/rebuild_the_universe_raw/page_overwrite.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         rebuild the universe overwrite with my version
+// @name         rebuild the universe page_overwrite.user.js
 // @namespace    https://github.com/mjz19910/
 // @version      0.1.0.0
 // @description  try to take over the world!
@@ -13,6 +13,8 @@
 // @match        https://ssh.login.local:9342/mirror/rebuildtheuniverse.com/
 // @grant        none
 // @run-at       document-start
+// @updateURL	https://github.com/mjz19910/snippet_repo/raw/master/userscript/rebuild_the_universe_raw/page_overwrite.meta.js
+// @downloadURL	https://github.com/mjz19910/snippet_repo/raw/master/userscript/rebuild_the_universe_raw/page_overwrite.user.js
 // ==/UserScript==
 /*eslint-disable no-undef*/
 
