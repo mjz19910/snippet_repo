6e1c8f5d560832ee600b66ad365eb56aa235342d
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-09 18:55:12 -0700

    u

--- userscript/DebugAPI_raw/__global.ts
+++ userscript/DebugAPI_raw/__global.ts
@@ -28,12 +28,18 @@ declare global {
 // inject_api global
 declare global {
 	interface InjectAPI extends IInjectAPI {
-		saved_instances: [string,{name:string},{}][];
+		saved_instances?: [string,{name:string},{}][];
 }
 	interface Window {
 		[InjectAPIStr]?: InjectAPI;
 	}
 }
+
+function test_inject_api() {
+	let inject_api:InjectAPI={};
+	inject_api;
+}
+test_inject_api();
 
 // SafeFunctionPrototype
 declare global {
