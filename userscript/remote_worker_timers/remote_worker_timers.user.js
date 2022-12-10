f3085b03161c28242281f3edf7a1d9c4bf3bf07d
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-10 02:26:42 -0700

    u

--- userscript/remote_worker_timers/remote_worker_timers.user.js
+++ userscript/remote_worker_timers/remote_worker_timers.user.js
@@ -7,8 +7,8 @@
 // @match        https://rebuildtheuniverse.com/test_remote_setTimeout
 // @grant        none
 // @run-at       document-start
-// @updateURL    https://github.com/mjz19910/snippet_repo/raw/master/javascript/modules/remote_worker_timers/remote_worker_timers.meta.js
-// @downloadURL  https://github.com/mjz19910/snippet_repo/raw/master/javascript/modules/remote_worker_timers/remote_worker_timers.user.js
+// @updateURL    https://github.com/mjz19910/snippet_repo/raw/master/userscript/remote_worker_timers/remote_worker_timers.meta.js
+// @downloadURL  https://github.com/mjz19910/snippet_repo/raw/master/userscript/remote_worker_timers/remote_worker_timers.user.js
 // ==/UserScript==
 /* eslint-disable no-undef,no-lone-blocks,no-eval */
 
