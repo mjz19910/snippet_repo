01c938049d0f7a0f90f7e49cd58f1a8a2c096630
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-12 18:34:27 -0700

    u

--- userscript/DebugAPI_raw/support/IInjectAPI.ts
+++ userscript/DebugAPI_raw/support/IInjectAPI.ts
@@ -1,22 +1,33 @@
-import {AddEventListenerExtension, CompressionStatsCalculator, CrossOriginConnection, DebugAPI} from "../DebugAPI.user";
+import {
+	CompressionStatsCalculator,
+	CrossOriginConnection,
+	DebugAPI,
+} from "../DebugAPI.user";
 
 export interface IInjectAPI_ extends IInjectAPI {}
 
+// saved_object_arrays
 declare global {
 	interface IInjectAPI {
 		saved_object_arrays?: {}[][];
 	}
 }
+
+// ModuleLoadDbg
 declare global {
 	interface IInjectAPI {
 		ModuleLoadDbg?: {};
 	}
 }
+
+// DisabledMulCompression
 declare global {
 	interface IInjectAPI {
 		DisabledMulCompression?: {};
 	}
 }
+
+// CrossOriginConnection
 declare global {
 	interface IInjectAPI {
 		CrossOriginConnection?: {};
@@ -37,159 +48,230 @@ declare global {
 		elevate_event_handlers?: {}[];
 	}
 }
-// proxyTargetMap
+
+// ProxyTargetMap
 declare global {
 	interface IInjectAPI {
 		ProxyTargetMap?: {};
 		proxyTargetMap?: {};
 	}
 }
+
 // saved_objects
 declare global {
 	interface IInjectAPI {
 		saved_objects?: [string,{name: string;}][];
 	}
 }
+
 // parse_javascript_str
 declare global {
 	interface IInjectAPI {
 		parse_javascript_str?: ((str: string) => void);
 	}
 }
+
+// DoCalc
 declare global {
 	interface IInjectAPI {
 		DoCalc?: {};
 	}
 }
+
+// ReversePrototypeChain
 declare global {
 	interface IInjectAPI {
-		reversePrototypeChain?: {};
 		ReversePrototypeChain?: {};
+		reversePrototypeChain?: {};
 	}
 }
+
+// any_api_logger
 declare global {
 	interface IInjectAPI {
 		any_api_logger?: {};
 	}
 }
+
+// parse_html_to_binary_arr
 declare global {
 	interface IInjectAPI {
 		parse_html_to_binary_arr?: (html: string) => unknown[];
 	}
 }
+
+// run_wasm_plugin
 declare global {
 	interface IInjectAPI {
 		run_modules_plugin?: VoidCallbackWith<() => void>;
 		run_wasm_plugin?: VoidCallbackWith<() => void>;
 	}
 }
+
+// compress_main
 declare global {
 	interface IInjectAPI {
 		compress_main?: VoidCallback<[CompressionStatsCalculator],void>;
 	}
 }
+
+// IterExtensions
 declare global {
 	interface IInjectAPI {
 		IterExtensions?: {};
 	}
 }
+
+// getPlaybackRateMap
 declare global {
 	interface IInjectAPI {
 		getPlaybackRateMap?: {};
 	}
 }
+
+// CreateObjURLCache
 declare global {
 	interface IInjectAPI {
 		CreateObjURLCache?: {};
 	}
 }
+
+// Repeat
 declare global {
 	interface IInjectAPI {
 		Repeat?: {};
 	}
 }
+
+// CompressRepeated
 declare global {
 	interface IInjectAPI {
 		CompressRepeated?: {};
 	}
 }
+
+// to_tuple_arr
 declare global {
 	interface IInjectAPI {
 		to_tuple_arr?: {};
 	}
 }
+
+// range_matches
 declare global {
 	interface IInjectAPI {
 		range_matches?: {};
 	}
 }
+
+// function_as_string_vec
 declare global {
 	interface IInjectAPI {
 		function_as_string_vec?: string[];
 	}
 }
+
+// CompressionStatsCalculator
 declare global {
 	interface IInjectAPI {
 		CompressionStatsCalculator?: {};
 	}
 }
+
+// HexRandomDataGenerator
 declare global {
 	interface IInjectAPI {
 		HexRandomDataGenerator?: {};
 	}
 }
+
+// EventListenerValue
 declare global {
 	interface IInjectAPI {
 		EventListenerValue?: {};
 	}
 }
+
+// GenericEvent
 declare global {
 	interface IInjectAPI {
 		GenericEvent?: {};
+	}
+}
+
+// GenericDataEvent
+declare global {
+	interface IInjectAPI {
 		GenericDataEvent?: {};
+	}
+}
+
+// GenericEventTarget
+declare global {
+	interface IInjectAPI {
 		GenericEventTarget?: {};
 	}
 }
+
+// Dumper
 declare global {
 	interface IInjectAPI {
 		Dumper?: {};
 	}
 }
+
+// RustSimpleTokenizer
 declare global {
 	interface IInjectAPI {
 		RustSimpleTokenizer?: {};
 		RustSimpleParser?: {};
 	}
 }
+
+// WeakValueRef
 declare global {
 	interface IInjectAPI {
 		WeakValueRef?: {};
 	}
 }
+
+// CSSCascade
 declare global {
 	interface IInjectAPI {
 		CSSCascade?: {};
 	}
 }
+
+// OriginState
 declare global {
 	interface IInjectAPI {
 		OriginState?: {};
 	}
 }
+
+// APIProxyManager
 declare global {
 	interface IInjectAPI {
 		APIProxyManager?: {};
 	}
 }
+
+// LoggingEventTarget
 declare global {
 	interface IInjectAPI {
 		LoggingEventTarget?: {};
 	}
 }
+
+// AddEventListenerExtension
+interface IAddEventListenerExtension {
+	elevate_handler(x: any): void;
+}
+
 declare global {
 	interface IInjectAPI {
 		AddEventListenerExtension?: {};
-		addEventListenerExtension?: AddEventListenerExtension;
+		addEventListenerExtension?: IAddEventListenerExtension;
 	}
 }
 
@@ -214,4 +296,3 @@ class VoidCallback<U extends any[],C> {
 }
 
 export {};
-
