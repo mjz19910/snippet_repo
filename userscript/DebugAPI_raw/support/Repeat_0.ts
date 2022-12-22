c12ea040805cb1be8c02a5f8900c5fad490d0e0a
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-22 00:27:23 -0700

    u

--- userscript/DebugAPI_raw/support/Repeat_0.ts
+++ userscript/DebugAPI_raw/support/Repeat_0.ts
@@ -1,4 +1,6 @@
-export {RepeatHolder} from "../DebugAPI.user.js";
+import {_DebugApiHolders} from "../DebugAPI.user.js";
+
+export const RepeatHolder=_DebugApiHolders['Repeat_0'];
 /** @template T */
 export class RepeatL_0<T> {
 	value: T;
