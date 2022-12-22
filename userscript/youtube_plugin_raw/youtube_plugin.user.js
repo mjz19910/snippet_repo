40250fb31ac30ea7b9251383b726c48ef97e7065
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-21 17:01:50 -0700

    u

--- userscript/youtube_plugin_raw/youtube_plugin.user.js
+++ userscript/youtube_plugin_raw/youtube_plugin.user.js
@@ -778,7 +778,6 @@ class HandleRendererContentItemArray {
 			} else if("commentsHeaderRenderer" in content_item) {
 				return true;
 			} else if("richItemRenderer" in content_item) {
-				if(!content_item.richItemRenderer) return true;
 				check_item_keys(path,".contents[].richItemRenderer",Object.keys(content_item.richItemRenderer));
 				console.assert(content_item.richItemRenderer.content!=void 0,"richItemRenderer has content");
 				let {content}=content_item.richItemRenderer;
