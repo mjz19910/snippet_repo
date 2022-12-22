575c777cd3bb512fcf8293eff059c9caed3e38d2
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-21 23:57:10 -0700

    u

--- userscript/DebugAPI_raw/support/MetaTagForConstructor.ts
+++ userscript/DebugAPI_raw/support/MetaTagForConstructor.ts
@@ -1,10 +1,5 @@
 import {SavedInstanceObject} from "./SavedInstanceObject";
 
-export type MetaTagForConstructor={
-	_tag: "for_constructor";
-	name: string;
-	constructor_meta: new () => SavedInstanceObject;
-};
 declare global {
 	type MetaTagForConstructor={
 		_tag: "for_constructor";
@@ -12,3 +7,4 @@ declare global {
 		constructor_meta: new () => SavedInstanceObject;
 	};
 }
+export {type MetaTagForConstructor}
