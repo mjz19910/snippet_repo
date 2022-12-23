9660eb45898c58a1b234fd31b4e6049d4bc964ef
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-23 01:42:57 -0700

    u

--- userscript/youtube_plugin_raw/__global.ts
+++ userscript/youtube_plugin_raw/__global.ts
@@ -24,11 +24,7 @@ declare global {
 
 // ReloadContinuationItemsCommand
 declare global {
-	type SectionItem={
-		richItemRenderer: RichItemRenderer;
-	}|{
-		richSectionRenderer: RichSectionRenderer;
-	};
+	type SectionItem=RichItemRendererItem|RichSectionRendererItem;
 	type ReloadContinuationItemsCommand={
 		slot: "RELOAD_CONTINUATION_SLOT_BODY";
 		targetId: "browse-feedFEwhat_to_watch";
