c0bd44f46ed2c9459bdc267f85a7e56f806fe852
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-23 07:47:21 -0700

    u

--- cmd_js/projects/yt_proto_buf/token_dec.js
+++ cmd_js/projects/yt_proto_buf/token_dec.js
@@ -1,16 +1,17 @@
 let token_enc=`
 4qmFsgKvAxIPRkV3aGF0X3RvX3dhdGNoGv4CQ0JoNmx3SkhVRWRpZVUxSU9HcGZkME5OWjI5SmNUUTJPSEZLZVRSNVptTnNWMjA0UzJKUmIxcGxXRkptWTBkR2JscFdPWHBpYlVaM1l6Sm9kbVJHT1hsYVYyUndZakkxYUdKQ1NXWldWRVkwWWpCb2RXTnFiRXhOVld4cVl6QjRUMVZJYjNwak1qVXhVbGRTU0ZWdVZubE5iRlkwV25odmRrRkJRbXhpYVRGSVVXZEJRbEV3UlVGQlZVNUNRVUZGUVZKclZqTmhSMFl3V0ROU2RsZ3paR2hrUjA1dlFVRkZRa0ZSUVVGQlVVRkNRVUZCUWtGUlFtbFJVV2RCUldoT2QxbFhaR3hZTTA1MVdWaENlbUZIT1RCWU0xSjJZVEpXZFVkb1RVbDVjbWxLZDJaNVVGOUJTVll4Y1VSRlEyZ3dNR2huWDB4SmFFMUplWEpwU25kbWVWQmZRVWxXTVhGRVJVTm9NREJvWjE5TUxYQjZTSFpSYTBORFFtcyUzRJoCGmJyb3dzZS1mZWVkRkV3aGF0X3RvX3dhdGNo
 `;
+token_enc="6FqcWoHZvrZixaGi1S3Re3Z90SCS3iq2_36hQSnSHQPtQVVkywH8WKka53MiBYBSP6DmqM-g9w";
 let base64_enc=decodeURIComponent(token_enc).replaceAll("_","/").replaceAll("-","+");
 const text=atob(base64_enc);
 const token_binary=new Uint8Array([...text].map(e => e.charCodeAt(0)));
 async function run() {
 	var protobuf=require('protobufjs');
 	let root=await protobuf.load("continue_cmd.proto");
-	var Type=root.lookupType("CommandToken");
+	var Type=root.lookupType("Int32");
 	console.log(new Uint32Array(token_binary.slice(0,4).buffer));
 	/** @type {protobuf.Message<YtTokenProtoMessage>} */
-	let message=Type.decode(token_binary.subarray(4));
+	let message=Type.decode(token_binary);
 	let obj=Type.toObject(message);
 	console.log(obj);
 }
