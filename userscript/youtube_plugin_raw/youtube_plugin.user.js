48614292cf32ff8fbd50408f6bfbe9aeba61f5cb
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-23 02:05:09 -0700

    u

--- userscript/youtube_plugin_raw/youtube_plugin.user.js
+++ userscript/youtube_plugin_raw/youtube_plugin.user.js
@@ -741,6 +741,7 @@ function check_item_keys(real_path,path,keys) {
 			case "backstagePostThreadRenderer": break;
 			case "channelAboutFullMetadataRenderer": break;
 			case "channelFeaturedContentRenderer": break;
+			case "channelRenderer": break;
 			case "commentsEntryPointHeaderRenderer": break;
 			case "compactPlaylistRenderer": break;
 			case "compactRadioRenderer": break;
