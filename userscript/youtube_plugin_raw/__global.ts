13ae179ef0509fd399f5e6859cf608d786cf57d6
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-23 04:42:00 -0700

    u

--- userscript/youtube_plugin_raw/__global.ts
+++ userscript/youtube_plugin_raw/__global.ts
@@ -81,7 +81,11 @@ declare global {
 }
 
 type AdSlotRenderer={
-	adSlotMetadata: {};
+	adSlotMetadata: {
+		"slotId": `${number}:${number}:${number}:${number}`,
+		"slotType": "SLOT_TYPE_IN_FEED",
+		"slotPhysicalPosition": number
+	};
 	fulfillmentContent: {};
 	enablePacfLoggingWeb: boolean;
 };
