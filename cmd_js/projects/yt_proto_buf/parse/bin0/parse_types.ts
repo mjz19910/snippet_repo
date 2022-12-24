f9fa0143d0e3f2ef4e7526536fba157c44c54833
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 15:53:17 -0700

    u

--- cmd_js/projects/yt_proto_buf/parse/bin0/parse_types.ts
+++ cmd_js/projects/yt_proto_buf/parse/bin0/parse_types.ts
@@ -1,7 +1,12 @@
 import {Bin0Imports} from './Bin0Imports.js';
+import {r} from './r.js';
 import {useTypeA} from './useTypeA.js';
-import {useTypeD} from './useTypeD.js';
 
 export async function parse_types(imp:Bin0Imports): Promise<void> {
-	await useTypeA(imp,useTypeD);
+	const {
+		protobuf,
+	}=imp;
+	let root=await protobuf.load(r("protobuf/bin0.proto"));
+	let proto_A_type=root.lookupType("A");
+	await useTypeA(imp,proto_A_type);
 }
