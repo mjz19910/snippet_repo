699a6a0cf7f7697d8209d9c386213be99bcbe3cd
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 15:20:46 -0700

    u

--- cmd_js/projects/yt_proto_buf/parse/bin0/useTypeD.ts
+++ cmd_js/projects/yt_proto_buf/parse/bin0/useTypeD.ts
@@ -1,4 +1,5 @@
 import {Root} from 'protobufjs';
+import {as_base64_typeid} from './as_base64_typeid.js';
 import {decode_as_type} from './decode_as_type.js';
 import {DesType} from './DesType.js';
 import {extract_items} from './extract_items.js';
@@ -10,9 +11,9 @@ import {r} from './r.js';
 export function useTypeD(root: Root,obj: ProtoBufTypeA) {
 	let token_binary=get_token_data(obj.token1,r);
 	let id_arr=new Uint8Array(token_binary.slice(0,4).buffer);
-	console.log('typeid=%o for base64(A.token1)',btoa(String.fromCharCode.apply("",Array.from(id_arr))).replaceAll("=",""));
+	console.log('typeid=%o for base64(A.token1)',as_base64_typeid(id_arr));
 	id_arr=new Uint8Array(token_binary.slice(4,7).buffer);
-	console.log('base64(A.token1).extra',id_arr);
+	console.log("extra=%o for base64(A.token1)",as_base64_typeid(id_arr));
 	let tmp_data=decode_as_type<ProtoBufTypeD>(root,"D",token_binary.slice(4));
 	let description: DesType=tmp_data.data.description;
 	let items=extract_items(description);
