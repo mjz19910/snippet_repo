b1037589d86fca779bfc35e02d970c64c85bab3d
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 15:06:28 -0700

    u

--- cmd_js/projects/yt_proto_buf/parse/bin0/useTypeA.ts
+++ cmd_js/projects/yt_proto_buf/parse/bin0/useTypeA.ts
@@ -3,10 +3,13 @@ import {as_base64_typeid} from './as_base64_typeid.js';
 import {into_type} from './into_type.js';
 import {ProtoBufTypeA} from './ProtoBufTypeA.js';
 
-export function useTypeA(root: Root,token_binary: Uint8Array) {
+export function useTypeA(root: Root,token_enc: string) {
+	let base64_enc=decodeURIComponent(token_enc).replaceAll("_","/").replaceAll("-","+");
+	const text=atob(base64_enc);
+	const token_binary=new Uint8Array([...text].map(e => e.charCodeAt(0)));
 	var Type=root.lookupType("A");
 	let id_arr=new Uint8Array(token_binary.slice(0,4).buffer);
-	console.log('A.typeid=%o',as_base64_typeid(id_arr));
+	console.log('typeid=%o for A',as_base64_typeid(id_arr));
 	let message=Type.decode(token_binary.subarray(4));
 	let untyped_obj=Type.toObject(message,{
 		longs: Number,
