bd9853de3be1c81937ae9380e73d81faf5e8936c
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 15:00:42 -0700

    u

--- cmd_js/projects/yt_proto_buf/parse/bin0/decode_as_type.ts
+++ cmd_js/projects/yt_proto_buf/parse/bin0/decode_as_type.ts
@@ -1,6 +1,6 @@
 import {Root} from 'protobufjs';
+import {decode_as} from './decode_as.js';
 import {into_type} from './into_type.js';
-import {decode_as} from './index';
 
 export function decode_as_type<T>(root: Root,type: string,data: Uint8Array) {
 	let obj=decode_as(root,type,data);
