ccb8caae0724c4c4e019ef73e7478a6c7191b053
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 14:39:28 -0700

    u

---
+++ cmd_js/projects/yt_proto_buf/parse/ProtoBufTypeA.ts
@@ -0,0 +1,17 @@
+import {ProtoBufTypeC} from './ProtoBufTypeC';
+
+export type ProtoBufTypeA={
+	videoId: string;
+	playlistId: string;
+	location: string;
+	token1: string;
+	token2: string;
+	type_C: ProtoBufTypeC;
+	a3: number;
+	a7: number;
+	a14: number;
+	a24: string;
+	a25: number;
+	a28: number;
+	a47: number;
+};
