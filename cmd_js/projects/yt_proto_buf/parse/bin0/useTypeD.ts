5fac97714378c015f7619d61f754f22c9e612709
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 15:50:41 -0700

    u

--- cmd_js/projects/yt_proto_buf/parse/bin0/useTypeD.ts
+++ cmd_js/projects/yt_proto_buf/parse/bin0/useTypeD.ts
@@ -12,12 +12,6 @@ export function useTypeD(root: Root,obj: ProtoBufTypeA) {
 	let id_arr=new Uint8Array(token_binary.slice(0,4).buffer);
 	let typeid=as_base64_typeid(id_arr,0);
 	console.log('typeid=%o for base64(A.token1)',typeid);
-	id_arr=new Uint8Array(token_binary.slice(4,7).buffer);
-	let id_arr_js=Array.from(id_arr);
-	let ext_id_arr=[0].concat(id_arr_js);
-	let id_arr_2=new Uint8Array(ext_id_arr);
-	typeid=as_base64_typeid(id_arr_2,1);
-	console.log("extra=%o for base64(A.token1)",typeid);
 	let tmp_data=decode_as_type<ProtoBufTypeD>(root,"D",token_binary.slice(4));
 	let description: DesType=tmp_data.data.description;
 	let items=extract_items(description);
