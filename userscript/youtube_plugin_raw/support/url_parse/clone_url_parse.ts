0650854e92ff522ed0db6d5fd05da521df2574e7
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-25 06:24:03 -0700

    u

--- userscript/youtube_plugin_raw/support/url_parse/clone_url_parse.ts
+++ userscript/youtube_plugin_raw/support/url_parse/clone_url_parse.ts
@@ -1,7 +1,8 @@
 import {UrlParse} from "./UrlParse";
+import {UrlParseErr} from "./UrlParseErr.js";
 
 
-export function clone_url_parse<T extends string>(v: UrlParse<T>) {
+export function clone_url_parse<T extends string extends (infer U extends string) ? UrlParse<U> extends UrlParseErr<any>?never:T:never>(v: UrlParse<T>|UrlParseErr<T>) {
 	if('_tag' in v) {
 		if(v._tag==="ERROR")
 			throw new Error("Failed");
