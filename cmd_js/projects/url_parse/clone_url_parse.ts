93acb2d826e97f9f04a69e084773766f912d600c
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-25 06:54:31 -0700

    u

--- cmd_js/projects/url_parse/clone_url_parse.ts
+++ cmd_js/projects/url_parse/clone_url_parse.ts
@@ -2,15 +2,15 @@ import {UrlParseOpt} from "./UrlParse.js";
 import {UrlParseErr} from "./UrlParseErr.js";
 
 
-export function clone_url_parse<T extends string extends (infer U extends string) ? UrlParseOpt<U> extends {} ? U:never:never>(v: UrlParseOpt<T>) {
-	let c:UrlParseOpt<T>|UrlParseErr<T>=v;
+export function clone_url_parse<T extends string extends (infer U extends string) ? UrlParseOpt<U> extends {} ? U : never : never>(v: UrlParseOpt<T>) {
+	let c: UrlParseOpt<T> | UrlParseErr<T> = v;
 	if('_tag' in c) {
-		let {_tag,err_path,parse}=c;
+		let {_tag,err_path,parse} = c;
 		return {_tag,err_path,parse};
 	}
 	let {
 		hash,host,hostname,href,origin,password,path,pathname,port,protocol,search,searchParams,
-	}=c;
+	} = c;
 	return {
 		hash,host,hostname,href,origin,password,path,pathname,port,protocol,search,searchParams,
 	};
--- cmd_js/projects/url_parse/do_test_url_parse_test_0.ts
+++ cmd_js/projects/url_parse/do_test_url_parse_test_0.ts
@@ -4,15 +4,15 @@ import {ParseUrlParams} from "../search_params/ParseUrlParams.js";
 import {get_url_params} from "./get_url_params.js";
 
 export function do_test_url_parse_test_0() {
-	let pagead=new class {
-		adview=new class {
-			ai: string|null=null;
+	let pagead = new class {
+		adview = new class {
+			ai: string | null = null;
 		};
 	};
-	pagead.adview={ai: new URLSearchParams(new URL("https://www.youtube.com/pagead/adview?ai=CR9DJiI6lY62vDMm7n88Pnre6kAngxfL9bfCOlrycEWQQASAAYP2Q9oDMA4IBF2NhLXB1Yi02MjE5ODExNzQ3MDQ5MzcxoAHLt6f4A6gDBMgDCqoEyAFP0EC2FSEYx70zuAUppUfEkYi_mOxY9ayL2TsEqmuwqQ2uR52X5F8mREDx148616YrlzJGcdfwasaotiQufJZQh_1P-F5EUxNcTrvVkGAoIHM-E6HcARQOzmMpiHURkEuWhENDfN2plBxHA5lui-nCj1tpA07TYASPOdBjxm-rT6PJyNg5mzjomgxxcY4JpG3rnZbC1TQ_an4StPQ5YOx-FCxGyY58dQA2TCk1vQwtBzqiGbOgOwWxy0tPKdPvQ51LcGX2i_nxJ8AEyNr865sEiAWjkqTnR5IFCQgTaAF47fX5BqAGboAHncjYB4gHAZAHAqgHgqqxAqgHhAioB6jSG6gHtgeoB-DPG6gH6dQbqAeMzRuoB7HcG6gHpJqxAqgHkZ-xAqgHsJuxAqgH36GxAqgHpqqxAqgHgcYbqAerxRuoB-PZG6gHt6mxAqgH6auxApIIC0FBQUFBQUFBQUFB0ggYCIDAgEAQAhgAMgSBgoAOOgeAgICAgIEEyAkAugs-CAIQBRgWIAgoATADQAFIAFABWCVgAGgAcAGIAQCYAQGiARIKAggBKAH4AQGQAgKoAgXAAgLYAQGAAgGIAgW4E____________wGwFALAFYGAgECKFwoIAxgBKAEwATgBoBcBqReJBEg1yjlletIXDhIK7E7vTqtctFyTaRhu&sigh=f6Ah-ilTPVs&cid=CAESD-D2saJYt_ikQ_sicNcKDQ").search).get("ai")};
-	const xx="https://i.ytimg.com/vi/OAIqCpqszVw/hqdefault.jpg?sqp=-oaymwE1CKgBEF5IVfKriqkDKAgBFQAAiEIYAXABwAEG8AEB-AH-CYAC0AWKAgwIABABGB8gZShWMA8=&rs=AOn4CLCpqrflce1_k2te4K_5kNbCpjCm6Q";
-	let vv=create_from_parse(xx);
-	type rt={
+	pagead.adview = {ai: new URLSearchParams(new URL("https://www.youtube.com/pagead/adview?ai=CR9DJiI6lY62vDMm7n88Pnre6kAngxfL9bfCOlrycEWQQASAAYP2Q9oDMA4IBF2NhLXB1Yi02MjE5ODExNzQ3MDQ5MzcxoAHLt6f4A6gDBMgDCqoEyAFP0EC2FSEYx70zuAUppUfEkYi_mOxY9ayL2TsEqmuwqQ2uR52X5F8mREDx148616YrlzJGcdfwasaotiQufJZQh_1P-F5EUxNcTrvVkGAoIHM-E6HcARQOzmMpiHURkEuWhENDfN2plBxHA5lui-nCj1tpA07TYASPOdBjxm-rT6PJyNg5mzjomgxxcY4JpG3rnZbC1TQ_an4StPQ5YOx-FCxGyY58dQA2TCk1vQwtBzqiGbOgOwWxy0tPKdPvQ51LcGX2i_nxJ8AEyNr865sEiAWjkqTnR5IFCQgTaAF47fX5BqAGboAHncjYB4gHAZAHAqgHgqqxAqgHhAioB6jSG6gHtgeoB-DPG6gH6dQbqAeMzRuoB7HcG6gHpJqxAqgHkZ-xAqgHsJuxAqgH36GxAqgHpqqxAqgHgcYbqAerxRuoB-PZG6gHt6mxAqgH6auxApIIC0FBQUFBQUFBQUFB0ggYCIDAgEAQAhgAMgSBgoAOOgeAgICAgIEEyAkAugs-CAIQBRgWIAgoATADQAFIAFABWCVgAGgAcAGIAQCYAQGiARIKAggBKAH4AQGQAgKoAgXAAgLYAQGAAgGIAgW4E____________wGwFALAFYGAgECKFwoIAxgBKAEwATgBoBcBqReJBEg1yjlletIXDhIK7E7vTqtctFyTaRhu&sigh=f6Ah-ilTPVs&cid=CAESD-D2saJYt_ikQ_sicNcKDQ").search).get("ai")};
+	const xx = "https://i.ytimg.com/vi/OAIqCpqszVw/hqdefault.jpg?sqp=-oaymwE1CKgBEF5IVfKriqkDKAgBFQAAiEIYAXABwAEG8AEB-AH-CYAC0AWKAgwIABABGB8gZShWMA8=&rs=AOn4CLCpqrflce1_k2te4K_5kNbCpjCm6Q";
+	let vv = create_from_parse(xx);
+	type rt = {
 		"hash": "",
 		"host": "i.ytimg.com",
 		"hostname": "i.ytimg.com",
@@ -25,13 +25,13 @@ export function do_test_url_parse_test_0() {
 		"search": "?sqp=-oaymwE1CKgBEF5IVfKriqkDKAgBFQAAiEIYAXABwAEG8AEB-AH-CYAC0AWKAgwIABABGB8gZShWMA8=&rs=AOn4CLCpqrflce1_k2te4K_5kNbCpjCm6Q",
 		"searchParams": URLSearchParams;
 	};
-	let rt_u: rt=vv;
+	let rt_u: rt = vv;
 	rt_u;
-	type ru=Decay<ParseUrlParams<rt['search']>>;
-	let ru_v: ru={};
+	type ru = Decay<ParseUrlParams<rt['search']>>;
+	let ru_v: ru = {};
 	ru_v;
 	console.log("rs",get_url_params(vv.search,"rs"));
-	console.log("sqp",get_url_params(vv.search,"sqp"))
+	console.log("sqp",get_url_params(vv.search,"sqp"));
 }
 
 do_test_url_parse_test_0();
