a7a8e20974572303dbc1590d24acafe3ec087ed5
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-25 06:46:04 -0700

    u

--- cmd_js/projects/url_parse/get_url_params.ts
+++ cmd_js/projects/url_parse/get_url_params.ts
@@ -1,6 +1,6 @@
 import {ParseUrlParams} from "../search_params/ParseUrlParams.js";
 
-export function get_url_params<T extends string,K extends Extract<keyof ParseUrlParams<T>,string>>(v: T,k: K): ParseUrlParams<T>[K]|null {
-	let res=new URLSearchParams(v).get(k) as ParseUrlParams<T>[K]|null;
+export function get_url_params<T extends string,U extends Extract<keyof ParseUrlParams<T>,string>>(t: T,u: U): ParseUrlParams<T>[U]|null {
+	let res=new URLSearchParams(t).get(u) as ParseUrlParams<T>[U]|null;
 	return res;
 }
