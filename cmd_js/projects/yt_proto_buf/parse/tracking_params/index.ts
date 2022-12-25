375285396dae9dc13151f7d3c338530b3b4ba502
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 23:31:00 -0700

    u

--- cmd_js/projects/yt_proto_buf/parse/tracking_params/index.ts
+++ cmd_js/projects/yt_proto_buf/parse/tracking_params/index.ts
@@ -138,15 +138,6 @@ export namespace codegen_ex {
 	const Enum=protobufjs.Enum;
 	const Type=protobufjs.Type; Type;
 	const types=protobufjs.types;
-	/**
- * Generates a partial message type encoder.
- */
-	function genTypePartial(gen: Codegen,field: Field,fieldIndex: number,ref: string): Codegen {
-		if(!field.resolvedType) throw new Error("Failed to resolve type");
-		return ((field.resolvedType as {} as {group: {}|null;}).group
-			? gen("types[%i].encode(%s,w.uint32(%i)).uint32(%i)",fieldIndex,ref,(field.id<<3|3)>>>0,(field.id<<3|4)>>>0)
-			:gen("types[%i].encode(%s,w.uint32(%i).fork()).ldelim()",fieldIndex,ref,(field.id<<3|2)>>>0)) as Codegen;
-	}
 
 	function missing(field: {name: string;}) {
 		return "missing required '"+field.name+"'";
@@ -270,86 +261,6 @@ export namespace codegen_ex {
 		return gen
 			("return m");
 		/* eslint-enable no-unexpected-multiline */
-	}
-	/**
-	 * Generates an encoder specific to the specified message type.
-	 */
-	export function encoder(mtype: MyUnkType): Codegen {
-		/* eslint-disable no-unexpected-multiline, block-scoped-var, no-redeclare */
-		var gen=util.codegen(["m","w"],mtype.name+"$encode")
-			("if(!w)")
-			("w=Writer.create()");
-
-		var i: number,ref;
-
-		// "when a message is serialized its known fields should be written sequentially by field number"
-		var fields= /* initializes */ mtype.fieldsArray.slice().sort(util.compareFieldsById);
-
-		for(var i=0;i<fields.length;++i) {
-			var field=fields[i].resolve();
-			var index=mtype._fieldsArray.indexOf(field);
-			var type=field.resolvedType instanceof Enum? "int32":field.type;
-			if(type!=="int32") continue;
-			var wireType=types.basic[type];
-			ref="m"+util.safeProp(field.name);
-
-			// Map fields
-			if(field.map) {
-				gen
-					("if(%s!=null&&Object.hasOwnProperty.call(m,%j)){",ref,field.name) // !== undefined && !== null
-					("for(var ks=Object.keys(%s),i=0;i<ks.length;++i){",ref)
-					("w.uint32(%i).fork().uint32(%i).%s(ks[i])",(field.id<<3|2)>>>0,8|(types.mapKey as any)[(field as any).keyType],(field as any).keyType);
-				if(wireType===undefined) gen
-					("types[%i].encode(%s[ks[i]],w.uint32(18).fork()).ldelim().ldelim()",index,ref); // can't be groups
-				else gen
-					(".uint32(%i).%s(%s[ks[i]]).ldelim()",16|wireType,type,ref);
-				gen
-					("}")
-					("}");
-
-				// Repeated fields
-			} else if(field.repeated) {
-				gen
-					("if(%s!=null&&%s.length){",ref,ref); // !== undefined && !== null
-
-				// Packed repeated
-				if(field.packed&&types.packed[type]!==undefined) {
-					gen
-
-						("w.uint32(%i).fork()",(field.id<<3|2)>>>0)
-						("for(var i=0;i<%s.length;++i)",ref)
-						("w.%s(%s[i])",type,ref)
-						("w.ldelim()");
-
-					// Non-packed
-				} else {
-					gen
-
-						("for(var i=0;i<%s.length;++i)",ref);
-					if(wireType===undefined)
-						genTypePartial(gen,field,index,ref+"[i]");
-					else gen
-						("w.uint32(%i).%s(%s[i])",(field.id<<3|wireType)>>>0,type,ref);
-
-				} gen
-					("}");
-
-				// Non-repeated
-			} else {
-				if(field.optional) gen
-					("if(%s!=null&&Object.hasOwnProperty.call(m,%j))",ref,field.name); // !== undefined && !== null
-
-				if(wireType===undefined)
-					genTypePartial(gen,field,index,ref);
-				else gen
-					("w.uint32(%i).%s(%s)",(field.id<<3|wireType)>>>0,type,ref);
-
-			}
-		}
-
-		return gen
-			("return w");
-		/* eslint-enable no-unexpected-multiline, block-scoped-var, no-redeclare */
 	}
 }
 class MyUnkType extends protobufjs.Type {
