54c65f8e4ff12996e441c17a49dc1eda688399b8
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 17:28:00 -0700

    u

--- cmd_js/projects/yt_proto_buf/parse/bin0/get_token_data.ts
+++ cmd_js/projects/yt_proto_buf/parse/bin0/get_token_data.ts
@@ -1,5 +1,4 @@
 export function get_token_data(token: string) {
 	let base64_enc_2=token.replaceAll("_","/").replaceAll("-","+");
-	let text_2=atob(base64_enc_2);
-	return new Uint8Array([...text_2].map(e => e.charCodeAt(0)));
+	return Buffer.from(base64_enc_2, 'base64');
 }
