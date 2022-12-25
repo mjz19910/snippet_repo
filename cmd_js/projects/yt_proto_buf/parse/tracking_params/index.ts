34393bf946cb6930bb89fe16fb4c1b1d9acb4072
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 23:12:31 -0700

    u

--- cmd_js/projects/yt_proto_buf/parse/tracking_params/index.ts
+++ cmd_js/projects/yt_proto_buf/parse/tracking_params/index.ts
@@ -3,9 +3,7 @@ import {
 	Codegen,
 	default as protobufjs,
 	Field,
-	type Reader,
 	type Root,
-	type Type,
 } from "protobufjs";
 import {into_type} from "../support/into_type.js";
 import {r} from "./r.js";
@@ -79,14 +77,14 @@ type MyState={
 	my_console: MyConsole;
 };
 
-function debug_l_delim_message(state: MyState,reader: Reader,unk_type: Type,field_id: number,size: number) {
+function debug_l_delim_message(state: MyState,reader: InstanceType<typeof MyReader>,unk_type: MyUnkType,field_id: number,size: number) {
 	let console=state.my_console;
-	let o=reader;
+	let o=reader.reader;
 	if(size>0) {
 		let has_error=false;
 		console.disabled=true;
 		try {
-			unk_type.decode(o.buf.subarray(o.pos,o.pos+size));
+			unk_type.decodeEx(reader,size);
 		} catch {
 			has_error=true;
 		} finally {
@@ -98,7 +96,7 @@ function debug_l_delim_message(state: MyState,reader: Reader,unk_type: Type,fiel
 			console.pad_log("\"field %o: L-delim message(length=%o)\": {",field_id,size);
 			let prev_pad=pad;
 			pad+=pad_with;
-			unk_type.decode(o.buf.subarray(o.pos,o.pos+size));
+			unk_type.decodeEx(reader,size);
 			pad=prev_pad;
 			console.pad_log("}");
 		}
@@ -107,9 +105,22 @@ function debug_l_delim_message(state: MyState,reader: Reader,unk_type: Type,fiel
 	}
 }
 
-export class MyReader extends protobufjs.Reader {
-	static override create(buffer: Uint8Array) {
-		return new MyReader(buffer);
+export class MyReader {
+	reader: protobufjs.Reader;
+	unk_type: MyUnkType;
+	constructor(buffer: Uint8Array,unk_type:MyUnkType) {
+		this.reader=new protobufjs.Reader(buffer);
+		this.unk_type=unk_type;
+	}
+	static create(buffer: Uint8Array,unk_type:MyUnkType) {
+		return new MyReader(buffer,unk_type);
+	}
+	skip(length?: number) {
+		this.reader.skip(length);
+		return this;
+	}
+	uint32() {
+		return this.reader.uint32();
 	}
 	skipTypeEx(state: MyState,fieldId: number,wireType: number) {
 		let console=state.my_console;
@@ -125,7 +136,7 @@ export class MyReader extends protobufjs.Reader {
 				break;
 			case 2:
 				let size=this.uint32();
-				debug_l_delim_message(state,this,unk_type,fieldId,size);
+				debug_l_delim_message(state,this,this.unk_type,fieldId,size);
 				this.skip(size);
 				break;
 			case 3:
@@ -145,156 +156,158 @@ export class MyReader extends protobufjs.Reader {
 
 			/* istanbul ignore next */
 			default:
-				throw Error("invalid wire type "+wireType+" at offset "+this.pos);
+				throw Error("invalid wire type "+wireType+" at offset "+this.reader.pos);
 		}
 	}
 }
-export namespace codegen_ex {	
+export namespace codegen_ex {
 	const util=protobufjs.util;
 	const Enum=protobufjs.Enum;
-	const Type=protobufjs.Type;Type;
+	const Type=protobufjs.Type; Type;
 	const types=protobufjs.types;
 	/**
  * Generates a partial message type encoder.
  */
-function genTypePartial(gen: Codegen, field: Field, fieldIndex: number, ref: string): Codegen {
-	if(!field.resolvedType) throw new Error("Failed to resolve type");
-	return ((field.resolvedType as {} as {group: {}|null}).group
-		? gen("types[%i].encode(%s,w.uint32(%i)).uint32(%i)", fieldIndex, ref, (field.id << 3 | 3) >>> 0, (field.id << 3 | 4) >>> 0)
-		: gen("types[%i].encode(%s,w.uint32(%i).fork()).ldelim()", fieldIndex, ref, (field.id << 3 | 2) >>> 0)) as Codegen;
-}
-
-function missing(field: {name: string;}) {
-	return "missing required '" + field.name + "'";
-}
-/**
- * Generates a decoder specific to the specified message type.
- * @param {Type} mtype Message type
- * @returns {Codegen} Codegen instance
- */
-export function decoder(mtype: MyUnkType): Codegen {
-	/* eslint-disable no-unexpected-multiline */
-	var gen = util.codegen(["r", "l"], mtype.name + "$decode")
-	("if(!(r instanceof Reader))")
-			("r=Reader.create(r)")
-	("var c=l===undefined?r.len:r.pos+l,m=new this.ctor" + (mtype.fieldsArray.filter(function(field) { return field.map; }).length ? ",k,value" : ""))
-	("while(r.pos<c){")
-			("var t=r.uint32()");
-	if ((mtype as {} as {group: {}|null}).group) gen
-			("if((t&7)===4)")
-					("break");
-	gen
-			("switch(t>>>3){");
-
-	var i = 0;
-	for (; i < /* initializes */ mtype.fieldsArray.length; ++i) {
-			var field = mtype._fieldsArray[i].resolve() as Field&{keyType:"int64"},
-					type  = (field.resolvedType instanceof Enum ? "int32" : field.type) as "int32",
-					ref   = "m" + util.safeProp(field.name); gen
-					("case %i: {", field.id);
-
-			// Map fields
-			if (field.map) { gen
-							("if(%s===util.emptyObject)", ref)
-									("%s={}", ref)
-							("var c2 = r.uint32()+r.pos");
-
-					if (types.defaults[field.keyType] !== undefined) gen
-							("k=%j", types.defaults[field.keyType]);
-					else gen
-							("k=null");
-
-					if (types.defaults[type] !== undefined) gen
-							("value=%j", types.defaults[type]);
-					else gen
-							("value=null");
-
-					gen
-							("while(r.pos<c2){")
-									("var tag2=r.uint32()")
-									("switch(tag2>>>3){")
-											("case 1: k=r.%s(); break", field.keyType)
-											("case 2:");
-
-					if (types.basic[type] === undefined) gen
-													("value=types[%i].decode(r,r.uint32())", i); // can't be groups
-					else gen
-													("value=r.%s()", type);
-
-					gen
-													("break")
-											("default:")
-													("r.skipType(tag2&7)")
-													("break")
-									("}")
-							("}");
-
-					if (types.long[field.keyType] !== undefined) gen
-							("%s[typeof k===\"object\"?util.longToHash(k):k]=value", ref);
-					else gen
-							("%s[k]=value", ref);
-
-			// Repeated fields
-			} else if (field.repeated) { gen
-
-							("if(!(%s&&%s.length))", ref, ref)
-									("%s=[]", ref);
-
-					// Packable (always check for forward and backward compatibility)
-					if (types.packed[type] !== undefined) gen
-							("if((t&7)===2){")
-									("var c2=r.uint32()+r.pos")
-									("while(r.pos<c2)")
-											("%s.push(r.%s())", ref, type)
-							("}else");
-
-					// Non-packed
-					if (types.basic[type] === undefined) gen((field.resolvedType! as {} as {group: {}|null}).group
-									? "%s.push(types[%i].decode(r))"
-									: "%s.push(types[%i].decode(r,r.uint32()))", ref, i);
-					else gen
-									("%s.push(r.%s())", ref, type);
-
-			// Non-repeated
-			} else if (types.basic[type] === undefined) gen((field.resolvedType! as {} as {group: {}|null}).group
-							? "%s=types[%i].decode(r)"
-							: "%s=types[%i].decode(r,r.uint32())", ref, i);
-			else gen
-							("%s=r.%s()", ref, type);
-			gen
-							("break")
-					("}");
-			// Unknown fields
-	} gen
-					("default:")
-							("r.skipType(t&7)")
-							("break")
-
-			("}")
-	("}");
-
-	// Field presence
-	for (i = 0; i < mtype._fieldsArray.length; ++i) {
-			var rfield = mtype._fieldsArray[i];
-			if (rfield.required) gen
-	("if(!m.hasOwnProperty(%j))", rfield.name)
-			("throw util.ProtocolError(%j,{instance:m})", missing(rfield));
+	function genTypePartial(gen: Codegen,field: Field,fieldIndex: number,ref: string): Codegen {
+		if(!field.resolvedType) throw new Error("Failed to resolve type");
+		return ((field.resolvedType as {} as {group: {}|null;}).group
+			? gen("types[%i].encode(%s,w.uint32(%i)).uint32(%i)",fieldIndex,ref,(field.id<<3|3)>>>0,(field.id<<3|4)>>>0)
+			:gen("types[%i].encode(%s,w.uint32(%i).fork()).ldelim()",fieldIndex,ref,(field.id<<3|2)>>>0)) as Codegen;
 	}
 
-	return gen
-	("return m");
-	/* eslint-enable no-unexpected-multiline */
-}
+	function missing(field: {name: string;}) {
+		return "missing required '"+field.name+"'";
+	}
+	/**
+	 * Generates a decoder specific to the specified message type.
+	 * @param {Type} mtype Message type
+	 * @returns {Codegen} Codegen instance
+	 */
+	export function decoder(mtype: MyUnkType): Codegen {
+		/* eslint-disable no-unexpected-multiline */
+		var gen=util.codegen(["r","l"],mtype.name+"$decode")
+			("if(!(r instanceof Reader))")
+			("r=Reader.create(r)")
+			("var c=l===undefined?r.len:r.pos+l,m=new this.ctor"+(mtype.fieldsArray.filter(function(field) {return field.map;}).length? ",k,value":""))
+			("while(r.pos<c){")
+			("var t=r.uint32()");
+		if((mtype as {} as {group: {}|null;}).group) gen
+			("if((t&7)===4)")
+			("break");
+		gen
+			("switch(t>>>3){");
+
+		var i=0;
+		for(;i< /* initializes */ mtype.fieldsArray.length;++i) {
+			var field=mtype._fieldsArray[i].resolve() as Field&{keyType: "int64";},
+				type=(field.resolvedType instanceof Enum? "int32":field.type) as "int32",
+				ref="m"+util.safeProp(field.name); gen
+					("case %i: {",field.id);
+
+			// Map fields
+			if(field.map) {
+				gen
+					("if(%s===util.emptyObject)",ref)
+				("%s={}",ref)
+				("var c2 = r.uint32()+r.pos");
+
+				if(types.defaults[field.keyType]!==undefined) gen
+					("k=%j",types.defaults[field.keyType]);
+				else gen
+					("k=null");
+
+				if(types.defaults[type]!==undefined) gen
+					("value=%j",types.defaults[type]);
+				else gen
+					("value=null");
+
+				gen
+					("while(r.pos<c2){")
+					("var tag2=r.uint32()")
+					("switch(tag2>>>3){")
+					("case 1: k=r.%s(); break",field.keyType)
+					("case 2:");
+
+				if(types.basic[type]===undefined) gen
+					("value=types[%i].decode(r,r.uint32())",i); // can't be groups
+				else gen
+					("value=r.%s()",type);
+
+				gen
+					("break")
+					("default:")
+					("r.skipTypeEx(tag2&7)")
+					("break")
+					("}")
+					("}");
+
+				if(types.long[field.keyType]!==undefined) gen
+					("%s[typeof k===\"object\"?util.longToHash(k):k]=value",ref);
+				else gen
+					("%s[k]=value",ref);
+
+				// Repeated fields
+			} else if(field.repeated) {
+				gen
+
+					("if(!(%s&&%s.length))",ref,ref)
+				("%s=[]",ref);
+
+				// Packable (always check for forward and backward compatibility)
+				if(types.packed[type]!==undefined) gen
+					("if((t&7)===2){")
+					("var c2=r.uint32()+r.pos")
+					("while(r.pos<c2)")
+					("%s.push(r.%s())",ref,type)
+					("}else");
+
+				// Non-packed
+				if(types.basic[type]===undefined) gen((field.resolvedType! as {} as {group: {}|null;}).group
+					? "%s.push(types[%i].decode(r))"
+					:"%s.push(types[%i].decode(r,r.uint32()))",ref,i);
+				else gen
+					("%s.push(r.%s())",ref,type);
+
+				// Non-repeated
+			} else if(types.basic[type]===undefined) gen((field.resolvedType! as {} as {group: {}|null;}).group
+				? "%s=types[%i].decode(r)"
+				:"%s=types[%i].decode(r,r.uint32())",ref,i);
+			else gen
+				("%s=r.%s()",ref,type);
+			gen
+				("break")
+				("}");
+			// Unknown fields
+		} gen
+			("default:")
+			("r.skipTypeEx(t&7)")
+			("break")
+
+			("}")
+			("}");
+
+		// Field presence
+		for(i=0;i<mtype._fieldsArray.length;++i) {
+			var rfield=mtype._fieldsArray[i];
+			if(rfield.required) gen
+				("if(!m.hasOwnProperty(%j))",rfield.name)
+				("throw util.ProtocolError(%j,{instance:m})",missing(rfield));
+		}
+
+		return gen
+			("return m");
+		/* eslint-enable no-unexpected-multiline */
+	}
 	/**
 	 * Generates an encoder specific to the specified message type.
 	 */
-	export function encoder(mtype:MyUnkType): Codegen {
+	export function encoder(mtype: MyUnkType): Codegen {
 		/* eslint-disable no-unexpected-multiline, block-scoped-var, no-redeclare */
 		var gen=util.codegen(["m","w"],mtype.name+"$encode")
 			("if(!w)")
 			("w=Writer.create()");
 
-		var i:number,ref;
+		var i: number,ref;
 
 		// "when a message is serialized its known fields should be written sequentially by field number"
 		var fields= /* initializes */ mtype.fieldsArray.slice().sort(util.compareFieldsById);
@@ -331,9 +344,9 @@ export function decoder(mtype: MyUnkType): Codegen {
 					gen
 
 						("w.uint32(%i).fork()",(field.id<<3|2)>>>0)
-					("for(var i=0;i<%s.length;++i)",ref)
-					("w.%s(%s[i])",type,ref)
-					("w.ldelim()");
+						("for(var i=0;i<%s.length;++i)",ref)
+						("w.%s(%s[i])",type,ref)
+						("w.ldelim()");
 
 					// Non-packed
 				} else {
@@ -367,35 +380,47 @@ export function decoder(mtype: MyUnkType): Codegen {
 	}
 }
 class MyUnkType extends protobufjs.Type {
+	state: any;
+	constructor(v: string,opt: {[k: string]: any;}|undefined,state: MyState) {
+		super(v,opt);
+		this.state=state;
+	}
 	_fieldsArray!: protobufjs.Field[];
 	override setup() {
-		let types=[];
+		let types: (protobufjs.Type|protobufjs.Enum|null)[]=[];
 		for(var i=0;i< /* initializes */ this.fieldsArray.length;++i)
 			types.push(this._fieldsArray[i].resolve().resolvedType);
-		this.decode=protobufjs.decoder(this)({
-			Reader: MyReader,
-			types: types,
-			util: protobufjs.util
-		}) as (reader: Reader|Uint8Array,length?: number|undefined) => protobufjs.Message<{}>;
 		return this;
+	}
+	decodeEx(r: MyReader,l?: number) {
+		var c=l===undefined? r.reader.len:r.reader.pos+l,m=(new this.ctor) as protobufjs.Message<{}>;
+		while(r.reader.pos<c) {
+			var t=r.uint32();
+			switch(t>>>3) {
+				default:
+					r.skipTypeEx(this.state,t<<3,t&7);
+					break;
+			}
+		}
+		return m;
 	}
 }
 
 
-const unk_type=new MyUnkType("SkipUnknown");
-
 export async function parse_types(): Promise<void> {
 	const my_console=new MyConsole;
 	const myArgs=process.argv.slice(2);
+	const my_state={my_console};
+	let unk_type=new MyUnkType("SkipUnknown",void 0,my_state);
 	if(myArgs[0]==="--input") {
 		my_console.pad_log("message Type.U {");
 		let reset=increase_padding();
 		const token_buffer=get_token_data(myArgs[1]);
-		let reader=MyReader.create(new Uint8Array(token_buffer));
+		let reader=MyReader.create(new Uint8Array(token_buffer),unk_type);
 		let ss=reader.uint32();
 		my_console.pad_log("%o",ss);
-		reader.pos=0;
-		unk_type.decode(reader);
+		reader.reader.pos=0;
+		unk_type.decodeEx(reader);
 		reset();
 		my_console.pad_log("}");
 		return;
