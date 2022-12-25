236cbd8662fd8bba14b2e38d9c9a446b88577576
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-25 01:05:57 -0700

    u

--- userscript/youtube_plugin_raw/youtube_plugin.user.js
+++ userscript/youtube_plugin_raw/youtube_plugin.user.js
@@ -2726,7 +2726,7 @@ inject_api.iterate_tracking_params=iterate_tracking_params;
 
 function main() {
 	if(!window.inject_api) {
-		throw new Error("Loaded before DebugApi");
+		window.inject_api=inject_api;
 	}
 	start_message_channel_loop();
 }
