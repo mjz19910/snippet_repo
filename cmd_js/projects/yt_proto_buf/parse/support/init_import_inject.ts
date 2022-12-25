3c21f7edc6336c2dc30555cbfb09ccb8a3a5a084
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 20:12:31 -0700

    u

--- cmd_js/projects/yt_proto_buf/parse/support/init_import_inject.ts
+++ cmd_js/projects/yt_proto_buf/parse/support/init_import_inject.ts
@@ -1,11 +1,33 @@
 import {default as protobufjs} from "protobufjs";
 
 export function init_import_inject(arg0: {protobufjs: typeof protobufjs;}) {
-	let Reader=arg0.protobufjs.Reader;
-	const real_skip_type=Reader.prototype.skipType;
-	Reader.prototype.skipType=function(wireNumber) {
-		console.log("wire skip", wireNumber);
-		return real_skip_type.call(this,wireNumber);
+	const {
+		Reader,
+	}=arg0.protobufjs;
+	Reader.prototype.skipType=function(wireType) {
+		console.log("wire skip", wireType);switch (wireType) {
+			case 0:
+					this.skip();
+					break;
+			case 1:
+					this.skip(8);
+					break;
+			case 2:
+					this.skip(this.uint32());
+					break;
+			case 3:
+					while ((wireType = this.uint32() & 7) !== 4) {
+							this.skipType(wireType);
+					}
+					break;
+			case 5:
+					this.skip(4);
+					break;
+
+			/* istanbul ignore next */
+			default:
+					throw Error("invalid wire type " + wireType + " at offset " + this.pos);
 	}
-	throw new Error("Function not implemented.");
+	return this;
+	}
 }
