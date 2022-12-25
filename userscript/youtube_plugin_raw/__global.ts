498afcd2315ff65f8ec89b30d38677871d08e65b
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-25 04:11:56 -0700

    u

--- userscript/youtube_plugin_raw/__global.ts
+++ userscript/youtube_plugin_raw/__global.ts
@@ -1,12 +1,10 @@
 import {RendererContentItemDef} from "./support/yt_api/RendererContentItemDef";
-import {MenuServiceItem} from "./support/yt_api/MenuServiceItem.js";
 import {SavedData} from "./youtube_plugin.user.js";
 import {SectionItemDef} from "./support/yt_api/SectionItemDef";
-import {MenuRendererH} from "./support/yt_api/MenuRendererH.js";
-import {DialogPopupTag} from "./support/yt_api/DialogPopupTag";
 import {RichItemRendererContentDef} from "./support/yt_api/RichItemRendererContentDef";
-import {NavigationEndpoint} from "./support/yt_api/NavigationEndpoint.js";
 import {ContinuationItemRendererHolder} from "./support/yt_api/ContinuationItemRendererHolder.js";
+import {CompactVideoRendererHolder} from "./support/yt_api/CompactVideoRendererHolder.js";
+import {RichSectionRenderer} from "./support/yt_api/RichSectionRenderer.js";
 
 // YtdPageManagerElement
 declare global {
@@ -103,258 +101,6 @@ declare global {
 	};
 }
 
-
-interface RichShelfRenderer {
-	icon: {
-		iconType: string;
-	}|null;
-	title: {
-		runs?: {
-			text: string;
-		}[];
-	};
-}
-interface RichSectionRenderer {
-	content: {
-		richShelfRenderer: RichShelfRenderer;
-	};
-}
-
-type CommentsHeaderRenderer={};
-type CommentThreadRenderer={};
-export type CommentsHeaderRendererHolder={
-	commentsHeaderRenderer: CommentsHeaderRenderer;
-};
-export type CommentThreadRendererHolder={
-	commentThreadRenderer: CommentThreadRenderer;
-};
-type CommandToken={
-	token: string;
-};
-type ContinuationCommand=CommandToken&{
-	request: string;
-};
-type WebCommandMetadata={
-	sendPost?: boolean;
-	apiUrl?: string;
-	rootVe?: number;
-	url?: string;
-	webPageType?: string;
-};
-export type CommandMetadata={
-	webCommandMetadata: WebCommandMetadata;
-};
-type ContinuationEndpoint={
-	clickTrackingParams: string;
-	commandMetadata: CommandMetadata;
-	continuationCommand: ContinuationCommand;
-};
-type GhostGridRenderer={
-	rows: number;
-};
-type GhostCards={
-	ghostGridRenderer: GhostGridRenderer;
-};
-export type MenuRenderer={
-	accessibility: Accessibility;
-	items: MenuServiceItem[];
-	targetId?: string;
-	trackingParams: string;
-};
-export type Icon={
-	iconType: string;
-};
-type CreatePlaylistServiceEndpoint={
-	params: string;
-	videoIds: string[];
-};
-type OnCreateListCommand={
-	clickTrackingParams: string;
-	commandMetadata: CommandMetadata;
-	createPlaylistServiceEndpoint: CreatePlaylistServiceEndpoint;
-};
-type AddToPlaylistCommand={
-	listType: string;
-	onCreateListCommand: OnCreateListCommand;
-	openListPanel: boolean;
-	openMiniplayer: boolean;
-	videoId: string;
-	videoIds: string[];
-};
-type NotificationActionRenderer={
-	responseText: SimpleText;
-	trackingParams: string;
-};
-type ToastPopup={
-	notificationActionRenderer: NotificationActionRenderer;
-};
-type ToastPopupTag={
-	popup: ToastPopup;
-	popupType: "TOAST";
-};
-export type OpenPopupAction=ToastPopupTag|DialogPopupTag;
-type OpenPopupActionHolder={
-	clickTrackingParams: string;
-	openPopupAction: OpenPopupAction;
-};
-type AddToPlaylistCommandHolder={
-	clickTrackingParams: string;
-	addToPlaylistCommand: AddToPlaylistCommand;
-};
-type ServiceEndpointAction=AddToPlaylistCommandHolder|OpenPopupActionHolder;
-type SignalServiceEndpoint={
-	signal: "CLIENT_SIGNAL";
-	actions: ServiceEndpointAction[];
-};
-type ServiceEndpointGeneral={
-	clickTrackingParams: string;
-	commandMetadata: CommandMetadata;
-};
-export type TrackedServiceEndpointGeneral={
-	clickTrackingParams: string;
-	commandMetadata: CommandMetadata;
-	trackingParams: string;
-};
-
-export interface SignalServiceEndpoints extends ServiceEndpointGeneral {
-	signalServiceEndpoint: SignalServiceEndpoint;
-};
-export interface PlaylistEditEndpointH {
-	playlistEditEndpoint: {};
-};
-
-export interface AddToPlaylistServiceEndpointH {
-	addToPlaylistServiceEndpoint: {};
-};
-type AccessibilityData={
-	label: string;
-};
-type Accessibility={
-	accessibilityData: AccessibilityData;
-};
-type MetadataBadgeRenderer={
-	icon?: Icon;
-	label?: string;
-	style: string;
-	tooltip?: string;
-	trackingParams: string;
-	accessibilityData?: AccessibilityData;
-};
-type MetadataBadgeRenderers={
-	metadataBadgeRenderer: MetadataBadgeRenderer;
-};
-type ThumbnailsHolder={};
-type SimpleText={
-	accessibility?: Accessibility;
-	simpleText: string;
-};
-type Thumbnail={
-	url: string;
-	width: number;
-	height: number;
-};
-interface ThumbnailHolder {
-	thumbnails: Thumbnail[];
-};
-type ACTION_ADD_VIDEO={
-	addedVideoId: string;
-	action: "ACTION_ADD_VIDEO";
-};
-type ACTION_REMOVE_VIDEO_BY_VIDEO_ID={
-	action: "ACTION_REMOVE_VIDEO_BY_VIDEO_ID";
-	removedVideoId: string;
-};
-type _ACTIONS=ACTION_ADD_VIDEO|ACTION_REMOVE_VIDEO_BY_VIDEO_ID;
-
-type PlaylistEditEndpoint={
-	playlistId: string;
-	actions: _ACTIONS[];
-};
-
-type ThumbnailOverlayToggleButtonRenderer={
-	isToggled?: boolean;
-	untoggledIcon: Icon;
-	toggledIcon: Icon;
-	untoggledTooltip: string;
-	toggledTooltip: string;
-	untoggledServiceEndpoint: {
-		clickTrackingParams: string;
-		commandMetadata: CommandMetadata;
-		playlistEditEndpoint?: PlaylistEditEndpoint;
-		signalServiceEndpoint?: SignalServiceEndpoint;
-	};
-	toggledServiceEndpoint?: {
-		clickTrackingParams: string;
-		commandMetadata: CommandMetadata;
-		playlistEditEndpoint: PlaylistEditEndpoint;
-	};
-	untoggledAccessibility: Accessibility;
-	toggledAccessibility: Accessibility;
-	trackingParams: string;
-};
-
-type ThumbnailOverlayTimeStatusRenderer={
-	text: SimpleText;
-	style: "DEFAULT";
-};
-
-type ThumbnailOverlayResumePlaybackRenderer={
-	percentDurationWatched: number;
-};
-
-type ThumbnailOverlayNowPlayingRenderer={
-	text: TextRuns;
-};
-
-type ThumbnailOverlay={
-	thumbnailOverlayResumePlaybackRenderer: ThumbnailOverlayResumePlaybackRenderer;
-}|{
-	thumbnailOverlayTimeStatusRenderer: ThumbnailOverlayTimeStatusRenderer;
-}|{
-	thumbnailOverlayToggleButtonRenderer: ThumbnailOverlayToggleButtonRenderer;
-}|{
-	thumbnailOverlayNowPlayingRenderer: ThumbnailOverlayNowPlayingRenderer;
-};
-interface MovingThumbnailDetails extends ThumbnailHolder {
-	logAsMovingThumbnail: boolean;
-};
-type MovingThumbnailRenderer={
-	movingThumbnailDetails?: MovingThumbnailDetails;
-	enableHoveredLogging: true,
-	enableOverlay: true;
-};
-export type RichThumbnail={
-	movingThumbnailRenderer: MovingThumbnailRenderer;
-};
-type CompactVideoRenderer={
-	accessibility: Accessibility;
-	badges?: MetadataBadgeRenderers[];
-	channelThumbnail: ThumbnailsHolder;
-	lengthText: SimpleText;
-	longBylineText: TextRuns;
-	menu: MenuRendererH;
-	navigationEndpoint: NavigationEndpoint;
-	publishedTimeText: SimpleText;
-	richThumbnail?: RichThumbnail;
-	shortBylineText: TextRuns;
-	shortViewCountText: SimpleText;
-	thumbnail: ThumbnailHolder;
-	thumbnailOverlays: ThumbnailOverlay[];
-	title: SimpleText;
-	ownerBadges?: MetadataBadgeRenderers[];
-	trackingParams: string;
-	videoId: string;
-	viewCountText: SimpleText;
-};
-export type CompactVideoRendererHolder={
-	compactVideoRenderer: CompactVideoRenderer;
-};
-
-type CompactPlaylistRenderer={};
-export type CompactPlaylistRendererHolder={
-	compactPlaylistRenderer: CompactPlaylistRenderer;
-};
-
 // RendererContentItem
 declare global {
 	type RendererContentItem=RendererContentItemDef;
@@ -378,8 +124,6 @@ declare global {
 		resolver: () => void;
 	};
 }
-
-export {};
 
 // Seen
 declare global {
@@ -479,7 +223,7 @@ declare global {
 	}
 }
 
-// export
+// PropertyHandler
 declare global {
 	interface InjectApi {
 		PropertyHandler?: {};
