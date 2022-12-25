f8982057904997811e8c5f1923a6cb6c1619beca
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 19:10:16 -0700

    u

--- cmd_js/projects/yt_proto_buf/parse/tracking_params/index.ts
+++ cmd_js/projects/yt_proto_buf/parse/tracking_params/index.ts
@@ -20,13 +20,13 @@ export async function parse_types(proto_name: string): Promise<void> {
 async function useTypeA(root: Root,proto_name: string) {
 	const token_buffer=await get_token_data_from_file(r(`binary/${proto_name}.bin`));
 	let buf_type=root.lookupType("A");
-	let message=buf_type.decode(token_buffer);
+	let message=buf_type.decode(token_buffer.subarray(2));
 	let u_obj=buf_type.toObject(message,{
 		longs: Number,
 		arrays: true,
 	});
 	let obj=into_type<typeof u_obj,{}>(u_obj);
-	console.log(buf_type,token_buffer);
+	console.log(obj);
 }
 export function get_token_data(token: string) {
 	let base64_enc_2=token.replaceAll("_","/").replaceAll("-","+");
