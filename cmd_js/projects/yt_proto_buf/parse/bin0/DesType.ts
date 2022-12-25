33b52e23afa52a7391af69d3c93443df89e2156f
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 17:37:01 -0700

    u

--- cmd_js/projects/yt_proto_buf/parse/bin0/DesType.ts
+++ cmd_js/projects/yt_proto_buf/parse/bin0/DesType.ts
@@ -1,8 +1,13 @@
 import {ItemType} from "./ItemType.js";
 
+type OccupiedValue={
+	key: number;
+	value: number;
+};
+
 export type DesType={
 	items: (ItemType)[];
 	keys: number[];
 	keysAlt: number[];
-	valueMap: {key: number; value: number;}[];
+	valueMap: OccupiedValue[];
 };
