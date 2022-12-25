7852c6feab8f8d79329c45d242c7cb5a68ccd76e
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-25 06:43:26 -0700

    u

--- cmd_js/projects/search_params/ParseUrlItems.ts
+++ cmd_js/projects/search_params/ParseUrlItems.ts
@@ -1,4 +1,4 @@
-import {ParseUrlValue} from "./ParseUrlValue";
+import {ParseUrlValue} from "./ParseUrlValue.js";
 
 export type ParseUrlItems<T extends string>=T extends `${infer U}&${infer Z}`?
 	ParseUrlValue<U>&ParseUrlItems<Z>:T extends `${infer U}`? ParseUrlValue<U>:never;
