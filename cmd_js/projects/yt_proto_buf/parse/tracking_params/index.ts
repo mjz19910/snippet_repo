21540ced05afa5ce7988d1d6dadd977162b533df
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-24 21:09:29 -0700

    u

--- cmd_js/projects/yt_proto_buf/parse/tracking_params/index.ts
+++ cmd_js/projects/yt_proto_buf/parse/tracking_params/index.ts
@@ -4,7 +4,7 @@ import {
 	Root,
 } from "protobufjs";
 import {into_type} from "../support/into_type.js";
-import {init_import_inject} from "../support/init_import_inject.js";
+import {increase_padding, init_import_inject, my_console} from "../support/init_import_inject.js";
 import {r} from "./r.js";
 
 function run() {
@@ -16,14 +16,27 @@ function run() {
 run();
 
 export async function parse_types(): Promise<void> {
+	const myArgs=process.argv.slice(2);
+	if(myArgs[0]==="--input") {
+		my_console.pad_log("message Type.U {");
+		let reset=increase_padding();
+		let type=new protobufjs.Type("U");
+		const token_buffer=get_token_data(myArgs[1]);
+		type.decode(token_buffer);
+		reset();
+		my_console.pad_log("}");
+		return;
+	}
+	const loaded_types=await load_types();
+	await useTypeA(loaded_types);
+}
+async function load_types() {
 	let root=await protobufjs.load(r(`protobuf/tracking_params.proto`));
 	const token_buffer=await get_token_data_from_file(r(`binary/tracking_params.bin`));
-	await useTypeA(root,token_buffer);
+	return {root,token_buffer};
 }
-async function useTypeA(root: Root,token_buffer: Buffer) {
+async function useTypeA({root,token_buffer}: {root: Root,token_buffer: Buffer;}) {
 	let buf_type=root.lookupType("A");
-	let type=new protobufjs.Type("U");
-	type.decode(token_buffer);
 	let message=buf_type.decode(token_buffer);
 	let u_obj=buf_type.toObject(message,{
 		longs: Number,
