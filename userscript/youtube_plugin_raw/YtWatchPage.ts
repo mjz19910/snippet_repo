57f53b6b091786114022ef84773573861b599db8
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-25 17:57:58 -0700

    u

---
+++ userscript/youtube_plugin_raw/YtWatchPage.ts
@@ -0,0 +1,9 @@
+import {ClickTrackedAndCommandMetadataWatchEndpointH} from "./support/yt_api/WatchEndpointH.js";
+
+
+
+export type YtWatchPage={
+	/** @readonly */
+	page: "watch";
+	endpoint: ClickTrackedAndCommandMetadataWatchEndpointH;
+};
