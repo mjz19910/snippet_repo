953905338dccdb2d9ea2a6acee59655eb753decb
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-25 18:45:44 -0700

    u

--- userscript/youtube_plugin_raw/YtdAppElementBase_.ts
+++ userscript/youtube_plugin_raw/YtdAppElementBase_.ts
@@ -3,8 +3,12 @@ import {HistoryManager} from "./HistoryManager";
 
 
 export abstract class YtdAppElementBase_ extends HTMLElement {
-	abstract $: {
+	abstract $?: {
 		historyManager: HistoryManager;
 		["page-manager"]: YtdPageManagerElementInterface;
 	};
+	abstract app_is_visible?: boolean;
+	abstract init_inject?:()=> void;
+	abstract ui_plugin_style_element?: HTMLStyleElement;
+	abstract ytp_click_cint?: number
 }
