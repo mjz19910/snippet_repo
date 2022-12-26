980105a0adc48da57fefba6d00e6eb54399098e4
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-25 21:51:17 -0700

    u

--- userscript/youtube_plugin_raw/CommentsSectionContinuationAction.ts
+++
@@ -1,6 +0,0 @@
-import {CommentsSectionItem} from "./CommentsSectionItem";
-
-type CommentsSectionContinuationAction={
-	targetId: "comments-section";
-	continuationItems: CommentsSectionItem[];
-};
