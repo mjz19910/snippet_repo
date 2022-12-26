7629c91b949b5b0f0a4877163e654e264a68d76b
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-25 21:52:48 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/WatchEndpointH.ts
+++ userscript/youtube_plugin_raw/support/yt_api/WatchEndpointH.ts
@@ -2,6 +2,8 @@ import {CommandMetadata} from "./CommandMetadata.js";
 import {WatchEndpoint} from "./WatchEndpoint";
 
 export type WatchEndpointH={
+	clickTrackingParams: string;
+	commandMetadata: CommandMetadata;
 	watchEndpoint: WatchEndpoint;
 };
 export type ClickTrackedWatchEndpointH={
