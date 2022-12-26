1fbdd806df059bc5db7c4af8084fb0820f891cc1
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 12:14:09 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_abc/w/WatchEndpointData.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_abc/w/WatchEndpointData.ts
@@ -1,9 +1,12 @@
 import {WatchEndpointConfig} from "./WatchEndpointConfig";
 
 export type WatchEndpointData={
-	nofollow: boolean;
 	videoId: string;
-	params?: string;
-	startTimeSeconds?: number;
-	watchEndpointSupportedOnesieConfig: WatchEndpointConfig;
 };
+
+export interface WatchEndpointDataCx extends WatchEndpointData {
+	nofollow: boolean;
+	params: string;
+	startTimeSeconds: number;
+	watchEndpointSupportedOnesieConfig: WatchEndpointConfig;
+}
