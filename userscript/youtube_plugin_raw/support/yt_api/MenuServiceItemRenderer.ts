b951798bead77c5a0095ca8509727107ff54ec5e
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 08:49:51 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/MenuServiceItemRenderer.ts
+++ userscript/youtube_plugin_raw/support/yt_api/MenuServiceItemRenderer.ts
@@ -1,11 +1,13 @@
 import {ServiceEndpoint} from "./ServiceEndpoint";
 import {TextRuns} from "./TextRuns";
 import {Icon} from "./Icon";
+import {TrackingParams} from "./TrackingParams.js";
+import {Separator} from "./Separator";
 
 export type MenuServiceItemRenderer={
 	icon: Icon;
 	serviceEndpoint: ServiceEndpoint;
 	text: TextRuns;
-	trackingParams?: string;
-	hasSeparator?: boolean;
 };
+export interface MenuServiceItemRendererTP extends TrackingParams,MenuServiceItemRenderer {}
+export interface MenuServiceItemRendererSP extends Separator,MenuServiceItemRenderer {} 
