dbabb5a9e91e73f4ccf7387a69ca5a640a859dce
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 12:08:14 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_exact_data/use_page_type_browse_detail.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_exact_data/use_page_type_browse_detail.ts
@@ -1,3 +1,4 @@
+import {BrowseEndpointH} from "../_abc/b/BrowseEndpointH.js";
 import * as json from "./page_type_browse_detail.json";
 export function use_page_type_browse_detail() {
 	type Json=typeof json;
@@ -7,9 +8,11 @@ export function use_page_type_browse_detail() {
 	type json_d_2=keyof JR;
 	const je_c: Je=json.endpoint;
 	const je_c_2: Je=json.response.endpoint;
+	const browse_ep: BrowseEndpointH=je_c;
 	type XX={a:{[U in json_d]: Json[U]}};
 	type JR_x={a:{[U in json_d_2]: JR[U]}};
 	return [
+		browse_ep,
 		je_c,
 		je_c_2,
 		class implements XX {a=json},
