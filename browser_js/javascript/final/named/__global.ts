8d9083e6e6fbefaba8cdd1cf175b7d3e146a0777
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-25 19:04:26 -0700

    u

--- browser_js/javascript/final/named/__global.ts
+++ browser_js/javascript/final/named/__global.ts
@@ -1,41 +1,33 @@
-import {SubARet} from "../../group1/sub_a/types/item_2_support.js";
-import {Runner} from "../support/Runner.js";
-
-// __m
-declare global {
-	var __m: {}|null;
-}
+import {__RetType} from "./__RetType";
 
 // __ret
 declare global {
 	interface Window {
+		__m: {}|null;
+		// __ret
 		__ret: __RetType;
+		// lunar-atoms-tycoon.js
+		event_info: any;
+		x2: any;
+		events: any;
+		syms: any;
+		state: any;
+		// kongregate_yet-another-merge-game.js
+		cint?: ReturnType<typeof window.setTimeout>|number;
+		citv?: ReturnType<typeof window.setInterval>|number;
+		// makiki99 prestige-frame
+		HTMLIFrameExt: typeof HTMLIFrameElement;
+		// youtube.com_lazyPrepareCriticalPages
+		debug?: debugI|undefined;
+		undebug?: undebugI|undefined;
+		// reddit_continueThread.js
+		react_ii: string;
+		root_new: never;
+		inner_dom: never;
+		refs: never[];
+		s_refs?: SRefBase[][];
 	}
 }
-
-type __RetType={
-	type: "site",
-	from: "1000mines.com",
-	ret: symbol|[any,any],
-}|{
-	type: "runner",
-	value: Runner,
-}|{
-	type: "sub_a",
-	ret: SubARet;
-};
-
-
-// captureStackTrace
-interface ErrorStackTrace {
-	stack?: string;
-}
-declare global {
-	interface ErrorConstructor {
-		captureStackTrace<T>(obj: ErrorStackTrace,constructorOpt?: T): void;
-	}
-}
-
 
 // lunar-atoms-tycoon.js
 declare global {
@@ -47,46 +39,21 @@ declare global {
 		gameiframe: any;
 	}
 
-	interface Window {
-		event_info: any;
-		x2: any;
-		events: any;
-		syms: any;
-		state: any;
-	}
-
 	interface FunctionConstructor {
 		events: {[x: string]: any;};
 	}
 }
 
-// kongregate_yet-another-merge-game.js
-declare global {
-	interface Window {
-		cint?: ReturnType<typeof window.setTimeout>|number;
-		citv?: ReturnType<typeof window.setInterval>|number;
-	}
-}
-
 // makiki99 prestige-frame*
 declare global {
-	// client_code
+	// makiki99 client_code
 	function activatePrestige(a: number,b: number,c: number): void;
 	function canActivatePrestige(a: number,b: number,c: number): boolean;
 	function getRequirement(a: number,b: number,c: number): number;
-
-	// prestige-frame
-	interface Window {
-		HTMLIFrameExt: typeof HTMLIFrameElement;
-	}
 }
 
 // youtube.com_lazyPrepareCriticalPages
 declare global {
-	interface Window {
-		debug?: debugI|undefined;
-		undebug?: undebugI|undefined;
-	}
 	interface debugI {
 		(fn: (...x: any[]) => void,code: string): void;
 		get_from: never;
@@ -106,7 +73,7 @@ declare global {
 		set_arr: never[];
 		ne: never[];
 		rx: RxType;
-		o: symbol|{[x:string]:never};
+		o: symbol|{[x: string]: never;};
 	}
 	interface IGame {}
 	var game: IGame;
@@ -117,13 +84,6 @@ declare global {
 
 // reddit_continueThread.js
 declare global {
-	interface Window {
-		react_ii: string;
-		root_new: never;
-		inner_dom: never;
-		refs: never[];
-		s_refs?: SRefBase[][];
-	}
 	type getSet=["get_set",PropertyDescriptor];
 	type Refs=["refs",string,number];
 	type S_or=["or",string,never];
