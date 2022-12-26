dbe1e87d9354c0f576bf57bc1a07dc89f5f3245f
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 12:38:47 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_abc/w/WatchEndpoint.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_abc/w/WatchEndpoint.ts
@@ -1,18 +1,19 @@
 import {ClickTrackingParams} from "../c/ClickTrackingParams.js";
 import {CommandMetadata} from "../c/CommandMetadata.js";
+import {TrackingParams} from "../t/TrackingParams.js";
 import {WatchEndpointData} from "./WatchEndpointData";
+
+export type ResolveUrlCommandMetadata=TrackingParams<"parentTrackingParams">;
 
 export interface WatchEndpoint extends ClickTrackingParams {
 	commandMetadata: CommandMetadata;
 	watchEndpoint: WatchEndpointData;
 }
 /*{
-    clickTrackingParams: 'IhMItJqj-eyX_AIV2UVMCB0lZAG8MghleHRlcm5hbA==',
+    clickTrackingParams: string,
     commandMetadata: {
-      webCommandMetadata: {url: '/watch?v=g2U2-i13970', webPageType: 'WEB_PAGE_TYPE_WATCH', rootVe: 3832},
-      resolveUrlCommandMetadata: {
-        parentTrackingParams: 'CPICEKQwGAEiEwiKtKP14Zf8AhUk4MQKHWZWCXgyB3JlbGF0ZWRIubGszNak3cvFAZoBBQgBEPgd'
-      }
+      webCommandMetadata: {url: `/watch?v=${YtVideoIdStr}`, webPageType: 'WEB_PAGE_TYPE_WATCH', rootVe: 3832},
+      resolveUrlCommandMetadata: TrackingParams<"parentTrackingParams">;
     },
     watchEndpoint: {videoId: 'g2U2-i13970'}
   }
