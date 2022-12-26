401b426ae66f37d1e1323d858f41b3d92744d366
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 12:14:09 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_abc/w/WatchEndpoint.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_abc/w/WatchEndpoint.ts
@@ -1,7 +1,19 @@
-import {CommandMetadata} from "./CommandMetadata.js";
+import {ClickTrackingParams} from "../c/ClickTrackingParams.js";
+import {CommandMetadata} from "../c/CommandMetadata.js";
 import {WatchEndpointData} from "./WatchEndpointData";
 
-export interface WatchEndpoint {
+export interface WatchEndpoint extends ClickTrackingParams {
 	commandMetadata: CommandMetadata;
 	watchEndpoint: WatchEndpointData;
-};
+}
+/*{
+    clickTrackingParams: 'IhMItJqj-eyX_AIV2UVMCB0lZAG8MghleHRlcm5hbA==',
+    commandMetadata: {
+      webCommandMetadata: {url: '/watch?v=g2U2-i13970', webPageType: 'WEB_PAGE_TYPE_WATCH', rootVe: 3832},
+      resolveUrlCommandMetadata: {
+        parentTrackingParams: 'CPICEKQwGAEiEwiKtKP14Zf8AhUk4MQKHWZWCXgyB3JlbGF0ZWRIubGszNak3cvFAZoBBQgBEPgd'
+      }
+    },
+    watchEndpoint: {videoId: 'g2U2-i13970'}
+  }
+ */
--- userscript/youtube_plugin_raw/support/yt_api/_exact_data/page_type_watch_detail_use.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_exact_data/page_type_watch_detail_use.ts
@@ -1,3 +1,4 @@
+import {WatchEndpoint} from "../_abc/w/WatchEndpoint.js";
 import * as json from "./page_type_watch_detail.json";
 export function page_type_watch_detail_use() {
 	type Json=typeof json;
@@ -7,10 +8,11 @@ export function page_type_watch_detail_use() {
 	type json_d_2=keyof JResponse;
 	const j_endpoint: JEndpoint=json.endpoint;
 	const j_res_endpoint: JEndpoint=json.response.endpoint;
+	const w_endpoint:WatchEndpoint=j_endpoint;
 	type XX={a:{[U in json_d]: Json[U]}};
 	type JR_x={a:{[U in json_d_2]: JResponse[U]}};
 	return [
-		j_endpoint,
+		w_endpoint,
 		j_res_endpoint,
 		class implements XX {a=json},
 		class implements JR_x {a=json.response},
