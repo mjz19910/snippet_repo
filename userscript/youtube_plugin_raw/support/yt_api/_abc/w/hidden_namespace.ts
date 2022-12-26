a6bde91bf5f262d73975f16deec335e1be449d20
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 12:38:47 -0700

    u

---
+++ userscript/youtube_plugin_raw/support/yt_api/_abc/w/hidden_namespace.ts
@@ -0,0 +1,26 @@
+import {Split} from "../../../Split.js";
+import {YtVideoIdStr} from "./WatchEndpointData";
+
+namespace hidden_namespace {
+	type AbcChar=Split<"abcdefghijklmnopqrstuvwxyz","">[number];
+	type NumChar=Split<"0123456789","">[number];
+	type Char=Uppercase<AbcChar>|AbcChar|NumChar;
+	type Delay2<T>=T;
+	type Delay1<T>=Delay2<T>;
+	type Delay<T>=T extends string? Delay1<T>:never;
+	export type TJoin<T extends string>=`${T}`;
+	export type Cj2<T extends string>=T extends infer U? U extends `${Delay<Char>}${Delay<Char>}`? U:never:never;
+	export type Cj4<T extends string>=T extends infer U? U extends Cj4<`${T}`>? U:never:never;
+	type cc=Split<'g2U2-i13970',"">;
+	export type c2=Split<'AAAAAAAAAAA',"">;
+	export type UU=cc['length'];
+	export const vv: {
+		videoId: YtVideoIdStr<'g2U2-i13970'>;
+	}={
+		videoId: 'g2U2-i13970'
+	};
+}
+
+hidden_namespace;
+
+export {};
