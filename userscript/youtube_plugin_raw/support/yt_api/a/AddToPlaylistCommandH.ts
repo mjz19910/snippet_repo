6f45855d911a0af5f6a32dde2162fab54fd63ab5
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 11:47:46 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/a/AddToPlaylistCommandH.ts
+++ userscript/youtube_plugin_raw/support/yt_api/a/AddToPlaylistCommandH.ts
@@ -1,5 +1,5 @@
 import {AddToPlaylistCommand} from "./AddToPlaylistCommand";
-import {ClickTrackingParams} from "./ClickTrackingParams";
+import {ClickTrackingParams} from "../c/ClickTrackingParams";
 
 export interface AddToPlaylistCommandH extends ClickTrackingParams {
 	addToPlaylistCommand: AddToPlaylistCommand;
