080c76e50ae2cbb86e3d9d29ba398108b983d087
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 11:57:24 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_abc/m/MenuRenderer.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_abc/m/MenuRenderer.ts
@@ -1,6 +1,6 @@
 import {Accessibility} from "../a/Accessibility";
 import {MenuServiceItem} from "./MenuServiceItem";
-import {TrackingParams} from "../../TrackingParams.js";
+import {TrackingParams} from "../t/TrackingParams.js";
 
 
 export interface MenuRenderer extends TrackingParams {
