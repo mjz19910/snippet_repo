49e752d76f554a594eb8993607443408367d4f34
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-25 22:29:13 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/TrackedServiceEndpointGeneral.ts
+++
@@ -1,8 +0,0 @@
-import {CT} from "./ClickTrackingParams.js";
-import {CommandMetadata} from "./CommandMetadata";
-
-
-export interface TrackedServiceEndpointGeneral extends CT {
-	trackingParams: string;
-	commandMetadata: CommandMetadata;
-};
