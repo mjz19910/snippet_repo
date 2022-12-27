fbfc6225ecf5eeaac31950a67143b4aa90d79ba2
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 17:27:47 -0700

    u

---
+++ userscript/youtube_plugin_raw/support/yt_api/_exact_data/use_guided_help.ts
@@ -0,0 +1,6 @@
+import {ServiceParams} from "./ServiceParams";
+
+export function use_guided_help(service_param: typeof serviceTrackingParams[2]) {
+	const service_param_t: ServiceParams<"GUIDED_HELP">=service_param;
+	service_param_t;
+}
