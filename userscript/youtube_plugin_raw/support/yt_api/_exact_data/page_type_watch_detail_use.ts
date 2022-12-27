837d4a20a22fc934f1e10053afeb17b4545cf71b
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 21:14:08 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_exact_data/page_type_watch_detail_use.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_exact_data/page_type_watch_detail_use.ts
@@ -1,16 +1,5 @@
 import * as json from "./page_type_watch_detail.json";
-import {use_page_load_response} from "./use_page_load_response";
-
-type Json=typeof json;
-type json_d=keyof Json;
-
-function use_watch_page_message_detail(json: Json) {
-	use_page_load_response(json.response);
-	type XX={a: {[U in json_d]: Json[U]};};
-	return [
-		class implements XX {a=json;},
-	] as {}[];
-}
+import {use_watch_page_message_detail} from "./use_watch_page_message_detail";
 
 export function page_type_watch_detail_use() {
 	use_watch_page_message_detail(json);
---
+++ userscript/youtube_plugin_raw/support/yt_api/_exact_data/use_watch_page_message_detail.ts
@@ -0,0 +1,16 @@
+import * as json from "./page_type_watch_detail.json";
+import {use_page_load_response} from "./use_page_load_response";
+
+export function use_watch_page_message_detail(json_arg: typeof json) {
+	type Json=typeof json;
+	type json_d=keyof Json;
+	use_page_load_response(json_arg.response);
+	type XX={
+		a: {
+			[U in json_d]: Json[U];
+		};
+	};
+	return [
+		class implements XX {a=json_arg;},
+	] as {}[];
+}
