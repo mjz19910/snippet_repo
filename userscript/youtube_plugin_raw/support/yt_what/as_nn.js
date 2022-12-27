6e3e86b6b605c4bb2cc5deb572e1baf4bdc2f4b5
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 23:15:28 -0700

    u

---
+++ userscript/youtube_plugin_raw/support/yt_what/as_nn.js
@@ -0,0 +1,12 @@
+/**
+ * @template T
+ * @param {T} x
+ * @returns {NonNullable<T>}
+ */
+export function as_nn(x) {
+	/** @arg {T} x @returns {x is NonNullable<T>} */
+	function assume_nonnull(x) {x; return true;}
+	if(!assume_nonnull(x))
+		throw 1;
+	return x;
+}
