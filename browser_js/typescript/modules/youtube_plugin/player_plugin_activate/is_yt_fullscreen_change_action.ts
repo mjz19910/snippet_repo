111806dfb7d5bea9e84f032d49e9df17723f4e67
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-27 00:13:48 -0700

    u

--- browser_js/typescript/modules/youtube_plugin/player_plugin_activate/is_yt_fullscreen_change_action.ts
+++
@@ -1,3 +0,0 @@
-export function is_yt_fullscreen_change_action(detail: any): detail is {actionName: "yt-fullscreen-change-action"; args: [boolean]} {
-	return detail.actionName==='yt-fullscreen-change-action'
-}
