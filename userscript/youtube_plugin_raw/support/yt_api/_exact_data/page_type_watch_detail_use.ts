eaa36792940ea8cd451b750d6c89662f27d18241
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 17:32:17 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_exact_data/page_type_watch_detail_use.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_exact_data/page_type_watch_detail_use.ts
@@ -3,34 +3,23 @@ import * as json from "./page_type_watch_detail.json";
 import {use_ecatcher} from "./use_ecatcher";
 import {use_gfeedback} from "./use_gfeedback";
 import {use_guided_help} from "./use_guided_help";
+import {use_page_load_response} from "./use_page_load_response";
 import {use_service_csi} from "./use_service_csi";
 
 type Json=typeof json;
 type json_d=keyof Json;
-type JEndpoint=Json['endpoint'];
-type JResponse=Json['response'];
+export type JResponse=Json['response'];
 type JResponseContent=Json['response']['response'];
-type json_d_2=keyof JResponse;
-const j_endpoint: JEndpoint=json.endpoint;
-const w_endpoint: WatchEndpoint<JEndpoint['watchEndpoint']['videoId']>=j_endpoint;
-const j_response=json.response.response;
-const {
-	responseContext,
-}=j_response;
-const {
-	serviceTrackingParams,
-}=responseContext;
-export {serviceTrackingParams};
-function use_page_load_response(response: JResponse) {
-	const endpoint=response.endpoint;
-	console.log('page ep',endpoint);
-	use_ui_load_response(response.response);
+type JResponseContext=JResponseContent['responseContext'];
+export function use_ui_load_response(response: JResponseContent) {
+	use_response_context(response.responseContext);
+	response.responseContext;
 }
-function use_ui_load_response(response: JResponseContent) {
-
-}
-export function page_type_watch_detail_use() {
-	use_page_load_response(json.response);
+function use_response_context(context: JResponseContext) {
+	const {
+		serviceTrackingParams,
+	}=context;
+	context.serviceTrackingParams;
 	for(let i=0;i<serviceTrackingParams.length;i++) {
 		const service_param=serviceTrackingParams[i];
 		switch(service_param.service) {
@@ -40,12 +29,12 @@ export function page_type_watch_detail_use() {
 			case "GUIDED_HELP": /*2*/use_guided_help(service_param); break;
 		}
 	}
+}
+export function page_type_watch_detail_use() {
+	use_page_load_response(json.response);
 	type XX={a: {[U in json_d]: Json[U]};};
-	type JR_x={a: {[U in json_d_2]: JResponse[U]};};
 	return [
-		w_endpoint,
 		class implements XX {a=json;},
-		class implements JR_x {a=json.response;},
 		json.response.response.contents,
 	] as {}[];
 }
