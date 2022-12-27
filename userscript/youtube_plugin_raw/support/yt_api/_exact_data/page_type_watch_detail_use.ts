82a8893ff9171ebf663478edeec50efc7d38f1dd
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 17:24:00 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_exact_data/page_type_watch_detail_use.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_exact_data/page_type_watch_detail_use.ts
@@ -1,7 +1,7 @@
 import {WatchEndpoint} from "../_abc/w/WatchEndpoint.js";
-import {ExtractParamKey} from "./ExtractParamKey";
-import {KVStore} from "./KVStore";
 import * as json from "./page_type_watch_detail.json";
+import {ServiceParams} from "./ServiceParams";
+import {use_service_csi} from "./use_service_csi";
 
 type Json=typeof json;
 type json_d=keyof Json;
@@ -19,70 +19,19 @@ const {
 const {
 	serviceTrackingParams,
 }=responseContext;
+export {serviceTrackingParams};
 
 export function page_type_watch_detail_use() {
 	const xx:JResponseContent=json.response.response;
 	for(let i=0;i<serviceTrackingParams.length;i++) {
 		const service_param=serviceTrackingParams[i];
 		switch(service_param.service) {
-			case "CSI": use_service_csi(service_param); break;
-			case "ECATCHER": break;
-			case "GFEEDBACK": break;
-			case "GUIDED_HELP": break;
+			case "CSI": /*0*/use_service_csi(service_param); break;
+			case "ECATCHER": /*3*/use_ecatcher(service_param); break;
+			case "GFEEDBACK": /*1*/use_gfeedback(service_param); break;
+			case "GUIDED_HELP": /*2*/use_guided_help(service_param); break;
 		}
 	}
-	type CsiServiceParams={
-		service: "CSI";
-		params: KVStore[];
-	};
-	function use_service_csi(service_param: typeof serviceTrackingParams[0]) {
-		const service_param_t: CsiServiceParams=service_param;
-		service_param_t;
-		service_param.service;
-		const sp_0_params=service_param.params;
-		type CsiPt=typeof sp_0_params;
-		type Pxg<T extends number>=PvSP<CsiPt[T],CsiPt[T]['key']>;
-		type CsiV={
-			c: Pxg<0>["c"];
-			cver: Pxg<1>["cver"];
-			yt_li: Pxg<2>["yt_li"];
-			GetWatchNext_rid: Pxg<3>["GetWatchNext_rid"];
-		};
-		class ActivateCsi implements CsiV {
-			c!: Pxg<0>["c"];
-			cver!: Pxg<1>["cver"];
-			yt_li!: Pxg<2>["yt_li"];
-			GetWatchNext_rid!: Pxg<3>["GetWatchNext_rid"];
-			constructor(sp_0_params: CsiPt) {
-				for(let i=0;i<sp_0_params.length;i++) {
-					let sp_x=sp_0_params[i];
-					switch(sp_x.key) {
-						case "c": {
-							this[sp_x.key]=sp_x.value;
-						} break;
-						case "cver": {
-							this[sp_x.key]=sp_x.value;
-						} break;
-						case "GetWatchNext_rid": this[sp_x.key]=sp_x.value; break;
-						case "yt_li": this[sp_x.key]=sp_x.value; break;
-						default: console.log("new params for",service_param.service);
-					}
-				}
-			}
-		}
-		const csi_params_obj: CsiV=new ActivateCsi(sp_0_params);
-		csi_params_obj;
-	}
-	type GFeedbackParams={
-		service: "GFEEDBACK";
-		params: KVStore[];
-	};
-	function use_gfeedback(service_param_1: GFeedbackParams) {
-		const service_param_1_t: GFeedbackParams=service_param_1;
-		service_param_1_t;
-
-	}
-	type PvSP<U extends KVStore,T extends string>=ExtractParamKey<[U],T>;
 	type XX={a: {[U in json_d]: Json[U]};};
 	type JR_x={a: {[U in json_d_2]: JResponse[U]};};
 	return [
@@ -93,4 +42,17 @@ export function page_type_watch_detail_use() {
 		class implements JR_x {a=json.response;},
 		json.response.response.contents,
 	] as {}[];
+}
+function use_ecatcher(service_param: typeof serviceTrackingParams[3]) {
+	const service_param_t: ServiceParams<"ECATCHER">=service_param;
+	throw new Error("Function not implemented.");
+}
+
+function use_guided_help(service_param: typeof serviceTrackingParams[2]) {
+	const service_param_t: ServiceParams<"GUIDED_HELP">=service_param;
+	throw new Error("Function not implemented.");
+}
+function use_gfeedback(service_param_1: typeof serviceTrackingParams[1]) {
+	const service_param_1_t: ServiceParams<"GFEEDBACK">=service_param_1;
+	service_param_1_t;
 }
