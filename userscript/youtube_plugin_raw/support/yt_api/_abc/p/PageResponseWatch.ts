dfa48139c1f8aaf21c289572e13679bd43492242
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-27 06:55:12 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_abc/p/PageResponseWatch.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_abc/p/PageResponseWatch.ts
@@ -3,4 +3,33 @@ import {WatchEndpoint} from "../w/WatchEndpoint.js";
 export type PageResponseWatch<T>={
 	page: "watch";
 	endpoint: WatchEndpoint<T>;
+	response: {
+		currentVideoEndpoint: never/*{}*/;
+		engagementPanels: never; //{}[]
+		frameworkUpdates: {entityBatchUpdate: never/*{}*/,elementUpdate: never/*{}*/;};
+		onResponseReceivedEndpoints: never;//{}[];
+		pageVisualEffects: never;//{}[];
+		playerOverlays: never;//{}
+		responseContext: never;//{}
+		topbar: never;//{}
+		trackingParams: string;
+	};
+	playerResponse: {
+		responseContext: {};
+		playabilityStatus: {};
+		streamingData: {};
+		playerAds: {};
+		playbackTracking: {};
+		captions: {};
+		videoDetails: {};
+		playerConfig: {};
+		storyboards: {};
+		microformat: {};
+		cards: {};
+		trackingParams: {};
+		attestation: {};
+		videoQualityPromoSupportedRenderers: {};
+		adPlacements: {};
+		frameworkUpdates: {};
+	};
 };
