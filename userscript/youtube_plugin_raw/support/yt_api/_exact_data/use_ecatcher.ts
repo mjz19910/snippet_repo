2a48bdb9543b274389e437c8f067f7806243d265
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 17:27:47 -0700

    u

---
+++ userscript/youtube_plugin_raw/support/yt_api/_exact_data/use_ecatcher.ts
@@ -0,0 +1,6 @@
+import {ServiceParams} from "./ServiceParams";
+
+export function use_ecatcher(service_param: typeof serviceTrackingParams[3]) {
+	const service_param_t: ServiceParams<"ECATCHER">=service_param;
+	service_param_t;
+}
---
+++ userscript/youtube_plugin_raw/support/yt_api/_exact_data/use_gfeedback.ts
@@ -0,0 +1,6 @@
+import {ServiceParams} from "./ServiceParams";
+
+export function use_gfeedback(service_param_1: typeof serviceTrackingParams[1]) {
+	const service_param_1_t: ServiceParams<"GFEEDBACK">=service_param_1;
+	service_param_1_t;
+}
