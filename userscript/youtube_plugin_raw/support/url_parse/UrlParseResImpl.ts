f02dcf1798e2b205b9efd453904881f9b736d0a0
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-27 10:17:59 -0700

    u

--- userscript/youtube_plugin_raw/support/url_parse/UrlParseResImpl.ts
+++ userscript/youtube_plugin_raw/support/url_parse/UrlParseResImpl.ts
@@ -5,14 +5,14 @@ export type UrlParseResImpl<
 	Search extends string,
 	Pathname extends string>={
 		hash: "";
-		host: Host;
-		hostname: Host;
-		href: `${Protocol}//${Host}${Pathname}?${Search}`;
+		host: `${Host}`;
+		hostname: `${Host}`;
+		href: `${Protocol}://${Host}/${Pathname}?${Search}`;
 		origin: `${Protocol}//${Host}`;
 		password: "";
-		pathname: Pathname;
+		pathname: `/${Pathname}`;
 		port: "";
-		protocol: Protocol;
+		protocol: `${Protocol}`;
 		search: `?${Search}`;
     /*grep-skip*/ searchParams: URLSearchParams;
 		toJSON(): T;
