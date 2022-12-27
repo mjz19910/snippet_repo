d7569e53358c81150edcd2cc8a7a6e46cb69fe09
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 17:32:17 -0700

    u

---
+++ userscript/youtube_plugin_raw/support/yt_api/_exact_data/use_page_load_response.ts
@@ -0,0 +1,16 @@
+import * as json from "./page_type_watch_detail.json";
+import {use_ui_load_response} from "./page_type_watch_detail_use";
+
+type Json=typeof json;
+type JResponse=Json['response'];
+export function use_page_load_response(response: JResponse) {
+	type json_d_2=keyof JResponse;
+	type JR_x={a: {[U in json_d_2]: JResponse[U]};};
+	class VV implements JR_x {
+		a=response;
+	}
+	VV;
+	const endpoint=response.endpoint;
+	console.log('page ep',endpoint);
+	use_ui_load_response(response.response);
+}
