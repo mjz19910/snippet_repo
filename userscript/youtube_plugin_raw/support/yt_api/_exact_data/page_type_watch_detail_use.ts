bee6b99b9802779c2abcbb8fd3bba664296b69e2
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 17:27:47 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_exact_data/page_type_watch_detail_use.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_exact_data/page_type_watch_detail_use.ts
@@ -1,6 +1,8 @@
 import {WatchEndpoint} from "../_abc/w/WatchEndpoint.js";
 import * as json from "./page_type_watch_detail.json";
-import {ServiceParams} from "./ServiceParams";
+import {use_ecatcher} from "./use_ecatcher";
+import {use_gfeedback} from "./use_gfeedback";
+import {use_guided_help} from "./use_guided_help";
 import {use_service_csi} from "./use_service_csi";
 
 type Json=typeof json;
@@ -10,7 +12,6 @@ type JResponse=Json['response'];
 type JResponseContent=Json['response']['response'];
 type json_d_2=keyof JResponse;
 const j_endpoint: JEndpoint=json.endpoint;
-const j_res_endpoint: JEndpoint=json.response.endpoint;
 const w_endpoint: WatchEndpoint<JEndpoint['watchEndpoint']['videoId']>=j_endpoint;
 const j_response=json.response.response;
 const {
@@ -20,9 +21,16 @@ const {
 	serviceTrackingParams,
 }=responseContext;
 export {serviceTrackingParams};
+function use_page_load_response(response: JResponse) {
+	const endpoint=response.endpoint;
+	console.log('page ep',endpoint);
+	use_ui_load_response(response.response);
+}
+function use_ui_load_response(response: JResponseContent) {
 
+}
 export function page_type_watch_detail_use() {
-	const xx:JResponseContent=json.response.response;
+	use_page_load_response(json.response);
 	for(let i=0;i<serviceTrackingParams.length;i++) {
 		const service_param=serviceTrackingParams[i];
 		switch(service_param.service) {
@@ -35,24 +43,9 @@ export function page_type_watch_detail_use() {
 	type XX={a: {[U in json_d]: Json[U]};};
 	type JR_x={a: {[U in json_d_2]: JResponse[U]};};
 	return [
-		xx,
 		w_endpoint,
-		j_res_endpoint,
 		class implements XX {a=json;},
 		class implements JR_x {a=json.response;},
 		json.response.response.contents,
 	] as {}[];
-}
-function use_ecatcher(service_param: typeof serviceTrackingParams[3]) {
-	const service_param_t: ServiceParams<"ECATCHER">=service_param;
-	service_param_t;
-}
-
-function use_guided_help(service_param: typeof serviceTrackingParams[2]) {
-	const service_param_t: ServiceParams<"GUIDED_HELP">=service_param;
-	service_param_t;
-}
-function use_gfeedback(service_param_1: typeof serviceTrackingParams[1]) {
-	const service_param_1_t: ServiceParams<"GFEEDBACK">=service_param_1;
-	service_param_1_t;
 }
