c2034dabb9ae52d83f25ed8b9876ed19d0d6ae76
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-26 17:33:45 -0700

    u

---
+++ userscript/youtube_plugin_raw/support/yt_api/_exact_data/use_response_context.ts
@@ -0,0 +1,21 @@
+import {use_ecatcher} from "./use_ecatcher";
+import {use_gfeedback} from "./use_gfeedback";
+import {use_guided_help} from "./use_guided_help";
+import {use_service_csi} from "./use_service_csi";
+import {JResponseContext} from "./page_type_watch_detail_use";
+
+export function use_response_context(context: JResponseContext) {
+	const {
+		serviceTrackingParams,
+	}=context;
+	context.serviceTrackingParams;
+	for(let i=0;i<serviceTrackingParams.length;i++) {
+		const service_param=serviceTrackingParams[i];
+		switch(service_param.service) {
+			case "CSI": /*0*//*0*/ use_service_csi(service_param); break;
+			case "ECATCHER": /*3*//*3*/ use_ecatcher(service_param); break;
+			case "GFEEDBACK": /*1*//*1*/ use_gfeedback(service_param); break;
+			case "GUIDED_HELP": /*2*//*2*/ use_guided_help(service_param); break;
+		}
+	}
+}
