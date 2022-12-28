3c3b2d98ae8e5054a804f84a223e0a7b7616e926
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-27 20:53:01 -0700

    u

--- userscript/youtube_plugin_raw/AnySavedData.ts
+++ userscript/youtube_plugin_raw/AnySavedData.ts
@@ -1,18 +1,18 @@
 
 export type AnySavedData={
-	browse?: {};
 	"att.get"?: {};
-	guide?: {};
-	"notification.get_unseen_count"?: {};
 	"notification.get_notification_menu"?: {};
-	reel_item_watch?: {};
-	reel_watch_sequence?: {};
-	player?: {};
-	next?: {};
+	"notification.get_unseen_count"?: {};
+	browse?: {};
 	getDatasyncIdsEndpoint?: {};
-	page_type_watch?: {};
+	guide?: {};
+	next?: {};
 	page_type_browse?: {};
-	page_type_shorts?: {};
 	page_type_channel?: {};
 	page_type_playlist?: {};
+	page_type_shorts?: {};
+	page_type_watch?: {};
+	player?: {};
+	reel_item_watch?: {};
+	reel_watch_sequence?: {};
 };
