802dc6ef23d298454c04b6cde0bde37897047a0e
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-28 11:17:26 -0700

    u

--- userscript/youtube_plugin_raw/parse_url_partial.js
+++ userscript/youtube_plugin_raw/parse_url_partial.js
@@ -9,7 +9,7 @@ if(u[0]==="/") {
 	const b=u;
 	/** @type {`/${string}?${string}`} */
 	const x=b;
-	/** @template {string} T @returns {import("./parse_url/index.js").UrlParse_ext<T>} @arg {T} x */
+	/** @template {string} T @returns {import("./parse_url/UrlParse_ext.js").UrlParse_ext<T>} @arg {T} x */
 	function create_from_parse_partial(x) {
 		/** @type {any} */
 		const a=x.split("?");
