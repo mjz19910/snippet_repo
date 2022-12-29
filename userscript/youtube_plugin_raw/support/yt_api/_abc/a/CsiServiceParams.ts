49599b2e1ea630fd2c07cfd5f471d5411df695f3
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-28 17:41:12 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_abc/a/CsiServiceParams.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_abc/a/CsiServiceParams.ts
@@ -1,11 +1,13 @@
 import {VerifyHex} from "./VerifyHex";
 
+type CsiVarMap={
+	c: {key: 'c',value: 'WEB';};
+	cver: {key: 'cver',value: '2.20221220.09.00';};
+	yt_li: {key: 'yt_li',value: '1';};
+	GetWatchNext_rid: {key: 'GetWatchNext_rid',value: VerifyHex<"0xda1e19901199623f">;};
+} 
+
 export type CsiServiceParams={
 	service: 'CSI',
-	params: [
-		{key: 'c',value: 'WEB';},
-		{key: 'cver',value: '2.20221220.09.00';},
-		{key: 'yt_li',value: '1';},
-		{key: 'GetWatchNext_rid',value: VerifyHex<"0xda1e19901199623f">;}
-	];
+	params: CsiVarMap[keyof CsiVarMap][];
 };
