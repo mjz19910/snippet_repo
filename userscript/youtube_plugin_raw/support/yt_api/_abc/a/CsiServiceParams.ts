0875dc712b596460e7c05fdd94c097429e68ee39
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-28 18:12:31 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_abc/a/CsiServiceParams.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_abc/a/CsiServiceParams.ts
@@ -1,14 +1,7 @@
-import {VerifyHex} from "./VerifyHex";
-
-type CsiVarMap={
-	c: {key: 'c',value: 'WEB';};
-	cver: {key: 'cver',value: '2.20221220.09.00';};
-	yt_li: {key: 'yt_li',value: '1';};
-	GetWatchNext_rid: {key: 'GetWatchNext_rid',value: VerifyHex<"0xda1e19901199623f">;};
-	GetAttestationChallenge_rid: {key: "GetAttestationChallenge_rid",value: VerifyHex<"0x841ae46b5aadd14c">;};
-};
+import {CsiVarMap} from "./CsiVarMap";
+import {ToServiceParams} from "./ToServiceParams";
 
 export type CsiServiceParams={
 	service: 'CSI',
-	params: CsiVarMap[keyof CsiVarMap][];
+	params: ToServiceParams<CsiVarMap>[keyof CsiVarMap][];
 };
---
+++ userscript/youtube_plugin_raw/support/yt_api/_abc/a/CsiVarMap.ts
@@ -0,0 +1,9 @@
+import {VerifyHex} from "./VerifyHex";
+
+export type CsiVarMap={
+	c: 'WEB';
+	cver: '2.20221220.09.00';
+	yt_li: '1';
+	GetWatchNext_rid: VerifyHex<"0xda1e19901199623f">;
+	GetAttestationChallenge_rid: VerifyHex<"0x841ae46b5aadd14c">;
+};
