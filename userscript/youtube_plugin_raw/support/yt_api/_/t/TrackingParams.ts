a57fdc7c5e9ae409af9db362de955cfbd68beac7
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-30 03:36:55 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_/t/TrackingParams.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_/t/TrackingParams.ts
@@ -1,5 +1,3 @@
-export const FindTrackingParamsRegexp=new RegExp("(?<!URLSearch|UrlSearch|/\*grep-skip\*/search|/\*grep-skip\*/"tracking|/\*grep-skip\*/"clickTracking|/\*grep-skip\*/tracking|"\./ClickTracking|/\*grep-skip\*/clickTracking|Tracking)Params");
-
 export type TrackingParams={
 	trackingParams: string;
 };
