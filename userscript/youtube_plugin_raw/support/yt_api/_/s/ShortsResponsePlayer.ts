be0f138ef1f9fdf9e34e1acae66190cd78a96973
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-29 21:48:37 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_/s/ShortsResponsePlayer.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_/s/ShortsResponsePlayer.ts
@@ -26,7 +26,7 @@ export type ShortsResponsePlayer={
 		videoQualityPromoRenderer: {};
 	};
 	frameworkUpdates: FrameworkUpdates;
-	cacheMetadata: {
+	cacheMetadata?: {
 		isCacheHit: boolean;
 	};
 };
