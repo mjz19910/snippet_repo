d5080b084de6338d84b7be363f18d803a6bbbe73
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-30 17:23:36 -0700

    u

--- userscript/DebugApi_raw/__global.ts
+++ userscript/DebugApi_raw/__global.ts
@@ -280,23 +280,11 @@ declare global {
 declare global {
 	interface WeakRef<T extends object> {
 		readonly [Symbol.toStringTag]: "WeakRef";
-
-		/**
-		 * Returns the WeakRef instance's target object, or undefined if the target object has been
-		 * reclaimed.
-		 */
 		deref(): T|undefined;
 	}
-
 	interface WeakRefConstructor {
 		readonly prototype: WeakRef<any>;
-
-		/**
-		 * Creates a WeakRef instance for the given target object.
-		 * @param target The target object for the WeakRef instance.
-		 */
 		new <T extends object>(target: T): WeakRef<T>;
 	}
-
 	var WeakRef: WeakRefConstructor;
 }
