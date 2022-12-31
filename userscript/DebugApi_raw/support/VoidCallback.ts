3b9713a4e09066580ea99bf20f4b858e39b8f863
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-30 17:23:17 -0700

    u

--- userscript/DebugApi_raw/support/VoidCallback.ts
+++ userscript/DebugApi_raw/support/VoidCallback.ts
@@ -1,13 +1,8 @@
-/** @template {any[]} U @template C */
-
 export class VoidCallback<U extends any[],C> {
-	/** @type {(...args: U)=>C} */
 	m_callback: ((...args: U) => C);
-	/** @arg {(...args: U)=>C} callback */
 	constructor(callback: (...args: U) => C) {
 		this.m_callback=callback;
 	}
-	/** @param {U} args */
 	execute(...args: U) {
 		return this.m_callback(...args);
 	}
