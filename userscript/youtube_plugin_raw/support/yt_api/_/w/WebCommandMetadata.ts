14a2675caf6f0bc22b1cd744e31d79928adf5ea6
Matthias Zimmerman <matthias291999@gmail.com>
2022-12-31 03:43:32 -0700

    u

--- userscript/youtube_plugin_raw/support/yt_api/_/w/WebCommandMetadata.ts
+++ userscript/youtube_plugin_raw/support/yt_api/_/w/WebCommandMetadata.ts
@@ -1,3 +1,5 @@
+import {WebCommandPageType} from "./WebCommandPageType";
+
 export type WebCommandMetadata={
 	sendPost?: boolean;
 	apiUrl?: string;
@@ -5,10 +7,3 @@ export type WebCommandMetadata={
 	url?: string;
 	webPageType?: WebCommandPageType;
 };
-type WebCommandPageType="WEB_PAGE_TYPE_BROWSE"|
-"WEB_PAGE_TYPE_CHANNEL"|
-"WEB_PAGE_TYPE_PLAYLIST"|
-"WEB_PAGE_TYPE_SHORTS"|
-"WEB_PAGE_TYPE_WATCH"|
-"WEB_PAGE_TYPE_SETTINGS"|
-"WEB_PAGE_TYPE_UNKNOWN";
