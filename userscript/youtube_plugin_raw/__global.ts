b736e2184c7f9ff80532af43200cb712c31800ed
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-01 11:53:26 -0700

    u

--- userscript/youtube_plugin_raw/__global.ts
+++ userscript/youtube_plugin_raw/__global.ts
@@ -1,7 +1,6 @@
 import {GlobalAttachWindow} from "./support/make/make_Window.js";
 import {GlobalAttach_yt} from "./support/make/make_yt.js";
 import {GlobalAttach_ytcfg} from "./support/make/make_ytcfg.js";
-import {__ia_excludeKeysS} from "./support/make/__ia_excludeKeysS";
 
 export type Attachments=[
 	GlobalAttach_yt,
