ac2aef35eac58f3a03608061b4a6dc3fbcfb77db
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-03 17:05:02 -0700

    u

--- userscript/youtube_plugin_raw/youtube_plugin.user.js
+++ userscript/youtube_plugin_raw/youtube_plugin.user.js
@@ -584,6 +584,9 @@ function to_url(url) {
 /** @arg {Error} rejection @returns {Promise<Response>} */
 function fetch_rejection_handler(rejection) {
 	if(rejection instanceof DOMException) {
+		if(rejection.name==="AbortError") {
+			throw rejection;
+		}
 		if(rejection.message!=="") {
 			console.log("fetch_rejection_handler",rejection.name);
 			console.log(rejection);
