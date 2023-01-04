0c87e4a75a776bb3dd0ed74ab21b4066ab4dc289
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-03 22:40:23 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/CommentsHeaderRendererData.ts
+++ userscript/youtube_plugin_raw/yt_json_types/CommentsHeaderRendererData.ts
@@ -1,12 +1,12 @@
 type CommentsHeaderRendererData={
 	countText: TextT;
 	createRenderer: CommentSimpleboxRenderer;
-	commentsCount: TextT;
-	customEmojis: CustomEmoji[];
-	loggingDirectives: LoggingDirectives;
-	showSeparator: true;
 	sortMenu: SortFilterSubMenuRenderer;
-	titleText: TextT;
 	trackingParams: string;
+	titleText: TextT;
+	commentsCount: TextT;
+	showSeparator: true;
+	customEmojis: CustomEmoji[];
 	unicodeEmojisUrl: string;
+	loggingDirectives: LoggingDirectives;
 };
