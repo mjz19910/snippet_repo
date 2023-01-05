421f887dc22e75be9f0281c6620c7b74465946aa
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-04 23:25:40 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/endpoint_data_handler_names.ts
+++ userscript/youtube_plugin_raw/yt_json_types/endpoint_data_handler_names.ts
@@ -5,12 +5,9 @@ type endpoint_data_handler_names={
 	setSettingEndpoint: "SetSettingEndpointData";
 	signalServiceEndpoint: "SignalServiceEndpointData";
 	urlEndpoint: "UrlEndpointRoot";
-	resolveUrlCommandMetadata: "ResolveUrlCommandMetadataData";
 	signalNavigationEndpoint: "SignalNavigationEndpointData";
 	signOutEndpoint: "SignOutEndpointData";
 	getAccountsListInnertubeEndpoint: "GetAccountsListInnertubeEndpointData";
-	clickTrackingParams: "clickTrackingParams";
-	commandMetadata: "CommandMetadata";
 	loadMarkersCommand: "LoadMarkersCommand";
 	changeKeyedMarkersVisibilityCommand: "ChangeKeyedMarkersVisibilityCommand";
 	createCommentEndpoint: "CreateCommentEndpointData";
