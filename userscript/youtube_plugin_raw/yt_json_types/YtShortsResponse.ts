51c65bf0c4b3ab0faefd4e2096d700de5040ce14
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-05 18:13:32 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/YtShortsResponse.ts
+++ userscript/youtube_plugin_raw/yt_json_types/YtShortsResponse.ts
@@ -1,3 +1,15 @@
+type ReelResponse={
+	responseContext: ResponseContext;
+	overlay: {};
+	status: "REEL_ITEM_WATCH_STATUS_SUCCEEDED";
+	trackingParams: string;
+	desktopTopbar: DesktopTopbarRenderer;
+};
 type YtShortsResponse={
 	page: "shorts";
+	endpoint: ReelWatchEndpoint;
+	response: ReelResponse;
+	playerResponse: ReelPlayerResponse;
+	reelWatchSequenceResponse: {};
 };
+type ReelPlayerResponse={};
