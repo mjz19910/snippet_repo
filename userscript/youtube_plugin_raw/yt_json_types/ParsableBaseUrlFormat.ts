c69c5625211de3d8ab2f5ed0d7c5efec7430ab60
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-07 00:38:39 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ParsableBaseUrlFormat.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ParsableBaseUrlFormat.ts
@@ -1,4 +1,5 @@
 type ParsableBaseUrlFormat=
-`https://www.youtube.com/api/stats/ads?${ApiStatsAdsArgs}`|
-`https://www.googleadservices.com/pagead/aclk?${string}`|
-`https://ad.doubleclick.net/ddm/trackclk/${string}`;
+	|`https://ad.doubleclick.net/ddm/trackclk/${string}`
+	|`https://www.google.com/get/videoqualityreport/`
+	|`https://www.googleadservices.com/pagead/aclk?${string}`
+	|`https://www.youtube.com/api/stats/ads?${ApiStatsAdsArgs}`;
