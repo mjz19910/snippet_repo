40d423d6ac8da10d9ae240c40daa5a9ed026d282
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-07 02:18:41 -0700

    u

--- userscript/youtube_plugin_raw/youtube_plugin.meta.js
+++ userscript/youtube_plugin_raw/youtube_plugin.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name	youtube plugin
 // @namespace	https://github.com/mjz19910/
-// @version	0.1.2.16
+// @version	0.1.2.17
 // @description	try to take over the world!
 // @author	@mjz19910
 // @copyright	@mjz19910 2020-2022
