738c774c87982d953344e0181aa7c63c4548de35
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-07 20:23:32 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/PlaylistLoopStateEntity.ts
+++ userscript/youtube_plugin_raw/yt_json_types/PlaylistLoopStateEntity.ts
@@ -1,6 +1,8 @@
+type PlaylistLoopStateEntityData={
+	key: string;
+	state: "PLAYLIST_LOOP_STATE_NONE";
+};
+
 type PlaylistLoopStateEntity={
-	playlistLoopStateEntity: {
-		key: string;
-		state: string;
-	};
+	playlistLoopStateEntity: PlaylistLoopStateEntityData;
 };
