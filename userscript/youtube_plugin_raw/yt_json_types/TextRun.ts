e6521b6423b0bd8d36ccdd210b31a1f21b1aecab
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-08 05:33:16 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/TextRun.ts
+++ userscript/youtube_plugin_raw/yt_json_types/TextRun.ts
@@ -1,10 +1,14 @@
+type BrowseData_BId_CBUrl={
+	browseId: `UC${string}`;
+	canonicalBaseUrl: `/@${string}`;
+};
+
+type BrowseEndpoint_BId_CBUrl={
+	browseEndpoint: BrowseData_BId_CBUrl;
+};
+
 type TextRun={
 	text: string;
-	navigationEndpoint?: NavigationEndpoint<{
-		browseEndpoint: {
-			browseId: `UC${string}`;
-			canonicalBaseUrl: `/@${string}`;
-		};
-	},never>;
+	navigationEndpoint?: NavigationEndpoint<BrowseEndpoint_BId_CBUrl,never>;
 	bold?: boolean;
 };
