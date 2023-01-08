a2410a6f3f6bfd4c585d703d6fb80f64c34b492a
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-07 23:43:16 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/YtUrlFormat.ts
+++ userscript/youtube_plugin_raw/yt_json_types/YtUrlFormat.ts
@@ -14,6 +14,7 @@ type YtUrlFormat=[
 	`/results?search_query=${string}`,
 	`/shorts/${string}`,
 	`/watch?${YtWatchUrlParamsFormat}`,
+	`/watch?v=${string}&list=RD${string}&start_radio=1&rv=${string}`,
 	`${YtDomain|""}/api/stats/ads?${ApiStatsAdsArgs}`,
 	YtExternalUrlFormat,
 	ParsableBaseUrlFormat,
