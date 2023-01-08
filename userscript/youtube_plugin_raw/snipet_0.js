20fa7115af8a5841f07d2b86dce7a034f89d2ed4
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-07 18:18:34 -0700

    u

--- userscript/youtube_plugin_raw/snipet_0.js
+++ userscript/youtube_plugin_raw/snipet_0.js
@@ -3,9 +3,17 @@ class Tmp {
 	generate_typedef(x,r_name=null) {
 		x; r_name;
 	}
+	/** @arg {{}} x @arg {string|null} r_name */
+	generate_renderer(x,r_name=null) {
+		x; r_name;
+	}
 	/** @arg {{}} x */
 	log_new_typedef(x) {
-		let td=this.generate_typedef(x,"InfoRowData");
+		let td,rn;
+		td=this.generate_typedef(x,"InfoRowData");
 		console.log(td);
+		td=this.generate_typedef(x,"CLA");
+		rn=this.generate_renderer(x,"VideoDescriptionMusicSectionData");
+		console.log(rn);
 	}
 }
