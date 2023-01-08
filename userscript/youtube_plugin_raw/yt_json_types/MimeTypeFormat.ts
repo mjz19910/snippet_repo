02a2b5ce9f4390b2cf1dc80486968d83dfc4ffe4
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-07 23:16:02 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/MimeTypeFormat.ts
+++ userscript/youtube_plugin_raw/yt_json_types/MimeTypeFormat.ts
@@ -1,5 +1,6 @@
 type MimeTypeFormat=[
-	`${"video/mp4"}; codecs="${acv1_codec}"`,
+	`video/mp4; codecs="${acv1_codec}"`,
+	`video/mp4; codecs="av01.0.08M.08"`,
 	`video/webm; codecs="vp9"`,
 	`audio/mp4; codecs="mp4a.40.2"`,
 	`audio/webm; codecs="opus"`,
@@ -15,6 +16,8 @@ type CodecType=[
 type GenericCodecType=[
 	acv1_codec,
 	"mp4a.40.2",
+	// av1 profile=0 level_id=08M bit_depth=8-bit
+	"av01.0.08M.08",
 ][number];
 type Mimetype2=`${string}; codecs="${CodecTypeStr|GenericCodecType}"`;
 function gv():MimeTypeFormat {
