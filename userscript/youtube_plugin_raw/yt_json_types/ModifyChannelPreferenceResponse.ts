ae862162888ecac42417773ece827ceba38b2682
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-08 20:44:39 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ModifyChannelPreferenceResponse.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ModifyChannelPreferenceResponse.ts
@@ -1,3 +1,8 @@
 type ModifyChannelPreferenceResponse={
 	responseContext: ResponseContext;
+	actions: OpenPopupAction[];
+	channelId: `UC${string}`;
+	newNotificationButton: SubscriptionNotificationToggleButtonRenderer;
+	trackingParams: string;
+	frameworkUpdates: EntityBatchUpdate;
 };
