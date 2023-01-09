cce7c7422acdd81b9dae600de5fba0d31c6139b8
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-09 14:40:08 -0700

    u

--- browser_js/typescript/modules/youtube_plugin/GApiType.ts
+++ browser_js/typescript/modules/youtube_plugin/GApiType.ts
@@ -12,16 +12,6 @@ import {PropertyHandler} from "./PropertyHandler.js";
 import {HTMLMediaElementGainController} from "./volume_range_plugin/HTMLMediaElementGainController.js";
 
 export abstract class GApiType {
-	Seen?: typeof Seen;
-	PropertyHandler?: typeof PropertyHandler;
-	dom_observer?: DomObserver;
-	yt_state_map?: Map<string,{}>;
-	blob_create_args_arr?: any[];
-	yt_handlers?: {};
-	port_state?: MessagePortState;
-	plugin_overlay_element?: PluginOverlayElement;
-	yt_watch_page_loaded_handler?: () => void;
-	gain_controller?: HTMLMediaElementGainController;
 	abstract create():ReturnType<this['real_create']>&{
 		parse_javascript_str?: (code_str:string)=>void;
 	}
@@ -29,6 +19,10 @@ export abstract class GApiType {
 		let g_api={
 			Seen,
 			PropertyHandler,
+			DomObserver,
+			MessagePortState,
+			PluginOverlayElement,
+			HTMLMediaElementGainController,
 			dom_observer,
 			port_state,
 			yt_state_map,
