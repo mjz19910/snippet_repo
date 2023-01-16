dfdd3c4f018fcf3cf72a0159aab9e6c47b487406
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-16 02:43:14 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/PlayerOverlay.ts
+++ userscript/youtube_plugin_raw/yt_json_types/PlayerOverlay.ts
@@ -1,7 +1,40 @@
+type SettingItemAutonavForDesktop<T extends boolean>={
+	settingItemId: "407";
+	boolValue: T;
+	settingItemIdForClient: "AUTONAV_FOR_DESKTOP";
+};
+
+type SetSettingCommandMetadata={
+	webCommandMetadata: {
+		sendPost: true;
+		apiUrl: "/youtubei/v1/account/set_setting";
+	};
+};
+
+type SetSettingEndpointAutonavForDesktop<T extends boolean>={
+	clickTrackingParams: string;
+	commandMetadata: SetSettingCommandMetadata;
+	setSettingEndpoint: SettingItemAutonavForDesktop<T>;
+}
+type SetSettingEndpoint=SetSettingEndpointAutonavForDesktop<boolean>;
+type AutoplaySwitchButton={
+	onEnabledCommand: SetSettingEndpointAutonavForDesktop<true>;
+	onDisabledCommand: SetSettingEndpointAutonavForDesktop<false>;
+	enabledAccessibilityData: Accessibility;
+	disabledAccessibilityData: Accessibility;
+	trackingParams: string;
+	enabled: boolean;
+};
+
+type AutoplaySwitchButtonRenderer={
+	autoplaySwitchButtonRenderer: AutoplaySwitchButton;
+};
+
 type PlayerOverlay={
 	endScreen: WatchNextEndScreenRenderer;
 	shareButton: ButtonRenderer;
 	addToMenu: MenuRenderer;
 	videoDetails: PlayerOverlayVideoDetailsRenderer;
+	autonavToggle: AutoplaySwitchButtonRenderer;
 	decoratedPlayerBarRenderer: DecoratedPlayerBarRenderer;
 };
