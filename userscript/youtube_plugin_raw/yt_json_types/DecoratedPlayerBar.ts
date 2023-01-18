cfc70ac7bee74c25a552a45ca4608d5d93d96c62
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-18 10:14:24 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/DecoratedPlayerBar.ts
+++ userscript/youtube_plugin_raw/yt_json_types/DecoratedPlayerBar.ts
@@ -1,1 +1,53 @@
-type DecoratedPlayerBar={};
+type KeyTemplate<T>={key: T;};
+type MapTemplate<T,U>={key: T; value: U;};
+
+type ChapterRenderer={
+	chapterRenderer: {
+		title: SimpleText;
+		timeRangeStartMillis: 0;
+		onActiveCommand: {
+			clickTrackingParams: string;
+			setActivePanelItemAction: {};
+		};
+		thumbnail: Thumbnail;
+	};
+};
+
+type HeatmapRenderer={
+	heatmapRenderer: {
+		maxHeightDp: 40;
+		minHeightDp: 4;
+		showHideAnimationDurationMillis: 200;
+		heatMarkers: {
+			heatMarkerRenderer: {};
+		}[];
+		heatMarkersDecorations: {
+			timedMarkerDecorationRenderer: {};
+		}[];
+	};
+};
+
+type DescriptionChaptersItem=MapTemplate<"DESCRIPTION_CHAPTERS",{
+	chapters: ChapterRenderer[];
+	trackingParams: string;
+	onChapterRepeat: OpenPopupAction;
+}>;
+
+type HeatSeekerItem=MapTemplate<"HEATSEEKER",{
+	trackingParams: string;
+	heatmap: HeatmapRenderer;
+}>;
+
+type MultiMarkersPlayerBar={
+	visibleOnLoad: KeyTemplate<"DESCRIPTION_CHAPTERS">;
+	markersMap: [DescriptionChaptersItem,HeatSeekerItem];
+};
+
+type MultiMarkersPlayerBarRenderer={
+	multiMarkersPlayerBarRenderer: MultiMarkersPlayerBar;
+};
+
+type DecoratedPlayerBar={
+	playerBar: MultiMarkersPlayerBarRenderer;
+	playerBarActionButton: ButtonRenderer;
+};
