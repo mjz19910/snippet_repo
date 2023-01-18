f2ca6c5a9ae0f08a0e24e3bdff5005dd35388f14
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-18 10:32:53 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/MacroMarkersListRenderer.ts
+++ userscript/youtube_plugin_raw/yt_json_types/MacroMarkersListRenderer.ts
@@ -1,5 +1,50 @@
+type RepeatChapterCommand={
+	clickTrackingParams: string;
+	repeatChapterCommand: {
+		repeat: "REPEAT_CHAPTER_TYPE_ENABLE_REPEAT";
+		startTimeMs: "0";
+		endTimeMs: "60000";
+		repeatStateEntityKey: string;
+	};
+};
+
+type StyleTypeObj={
+	styleType: "STYLE_DEFAULT_ACTIVE"|"STYLE_BLUE_TEXT";
+};
+
+type ToggleButton={
+	style: StyleTypeObj;
+	isToggled: false;
+	isDisabled: false;
+	defaultIcon: Icon<"LOOP">;
+	defaultServiceEndpoint: RepeatChapterCommand;
+	toggledServiceEndpoint: CommandExecutorCommand;
+	trackingParams: string;
+	toggledStyle: StyleTypeObj;
+	accessibilityData: Accessibility;
+	toggledAccessibilityData: Accessibility;
+};
+
 type ToggleButtonRenderer={
-	toggleButtonRenderer: Accessibility;
+	toggleButtonRenderer: ToggleButton;
+};
+
+type LightColorPalette={
+	section1Color: 4294966779;
+	section2Color: 4294505969;
+	section3Color: 4294110695;
+	primaryTitleColor: 4279833872;
+	secondaryTitleColor: 4285554760;
+	section4Color: 4293650141;
+};
+
+type DarkColorPalette={
+	section1Color: 4281871903;
+	section2Color: 4280819991;
+	section3Color: 4279833614;
+	primaryTitleColor: 4294964453;
+	secondaryTitleColor: 4291607459;
+	section4Color: 4278979079;
 };
 
 type MacroMarkersListItem={
@@ -14,23 +59,9 @@ type MacroMarkersListItem={
 	endRepeatCommand: CommandExecutorCommand;
 	playerStateEntityKey: string;
 	carouselType: "MACRO_MARKERS_LIST_ITEM_RENDERER_CAROUSEL_TYPE_DEFAULT";
-	lightColorPalette: {
-		section1Color: 4294966779;
-		section2Color: 4294505969;
-		section3Color: 4294110695;
-		primaryTitleColor: 4279833872;
-		secondaryTitleColor: 4285554760;
-		section4Color: 4293650141;
-	};
-	darkColorPalette: {
-		section1Color: 4281871903;
-		section2Color: 4280819991;
-		section3Color: 4279833614;
-		primaryTitleColor: 4294964453;
-		secondaryTitleColor: 4291607459;
-		section4Color: 4278979079;
-	};
-	timeDescriptionA11yLabel: "0 seconds";
+	lightColorPalette: LightColorPalette;
+	darkColorPalette: DarkColorPalette;
+	timeDescriptionA11yLabel: `${number} seconds`;
 };
 
 type MacroMarkersListItemRenderer={
