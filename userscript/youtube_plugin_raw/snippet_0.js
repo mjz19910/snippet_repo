39a88e1113f4e6821d6ad4299fb2262b803937aa
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-18 05:02:36 -0700

    u

--- userscript/youtube_plugin_raw/snippet_0.js
+++ userscript/youtube_plugin_raw/snippet_0.js
@@ -273,14 +273,3 @@ export class Snippet_0_tmp {
 		return true;
 	}
 }
-
-export class SearchAndReplaceTemplates {
-	items=[
-		{
-			query: `(\w+)\(x\) \{x;\}`,
-			replace:`$1(x) {
-				this.save_keys("[$1]",x);
-			}`
-		},
-	];
-}
