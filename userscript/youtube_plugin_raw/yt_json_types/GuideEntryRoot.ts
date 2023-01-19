1c00dea261e03ea53dba6524ac5795963d08eb8e
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-18 23:54:49 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/GuideEntryRoot.ts
+++ userscript/youtube_plugin_raw/yt_json_types/GuideEntryRoot.ts
@@ -8,7 +8,8 @@ type GuideEntryRoot={
 	entryData: GuideEntryData;
 	presentationStyle: "GUIDE_ENTRY_PRESENTATION_STYLE_NEW_CONTENT"|"GUIDE_ENTRY_PRESENTATION_STYLE_NONE";
 }|{
-	icon: Icon<"EXPAND">;
+	navigationEndpoint?: BrowseEndpoint;
+	icon: Icon<"EXPAND"|"ADD_CIRCLE">;
 	trackingParams: string;
 	formattedTitle: SimpleText;
 	accessibility: Accessibility;
