defc875fd4cbb6a834681744e4dd4921948dc251
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-18 22:25:19 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/PlaylistHeader.ts
+++ userscript/youtube_plugin_raw/yt_json_types/PlaylistHeader.ts
@@ -1,3 +1,38 @@
+type PlaylistBylineRenderer={
+	playlistBylineRenderer: {
+		text: TextWithRuns;
+	};
+};
+
+type YtShareData={
+	canShare: false;
+};
+
+type EditableDetails={
+	canDelete: false;
+};
+
+type PlaylistEditorArgs={
+	playlistId: string;
+};
+
+type PlaylistEditorEndpoint={
+	clickTrackingParams: string;
+	commandMetadata: CommandMetadata;
+	playlistEditorEndpoint: PlaylistEditorArgs;
+};
+type HeroPlaylistThumbnail={
+	thumbnail: Thumbnail;
+	maxRatio: 0.5625;
+	trackingParams: string;
+	onTap: WatchEndpoint;
+	thumbnailOverlays: ThumbnailOverlayHoverTextRenderer;
+};
+
+type HeroPlaylistThumbnailRenderer={
+	heroPlaylistThumbnailRenderer: HeroPlaylistThumbnail;
+};
+
 type PlaylistHeader={
 	playlistId: string;
 	title: TextWithRuns;
@@ -5,26 +40,22 @@ type PlaylistHeader={
 	descriptionText: {};
 	ownerText: TextWithRuns;
 	viewCountText: TextWithRuns;
-	shareData: {};
+	shareData: YtShareData;
 	isEditable: boolean;
 	privacy: string;
-	ownerEndpoint: {};
-	editableDetails: {};
+	ownerEndpoint: BrowseEndpoint;
+	editableDetails: EditableDetails;
 	trackingParams: string;
-	serviceEndpoints: {}[];
+	serviceEndpoints: PlaylistEditEndpoint[];
 	stats: TextWithRuns[];
 	briefStats: TextWithRuns[];
-	editorEndpoint: {};
-	playlistHeaderBanner: {};
+	editorEndpoint: PlaylistEditorEndpoint;
+	playlistHeaderBanner: HeroPlaylistThumbnailRenderer;
 	moreActionsMenu: MenuRenderer;
 	playButton: ButtonRenderer;
 	shufflePlayButton: ButtonRenderer;
-	onDescriptionTap: {};
+	onDescriptionTap: OpenPopupAction;
 	cinematicContainer: CinematicContainerRenderer;
-	byline: {
-		playlistBylineRenderer: {
-			text: TextWithRuns;
-		};
-	}[];
+	byline: PlaylistBylineRenderer[];
 	descriptionTapText: TextWithRuns;
 };
