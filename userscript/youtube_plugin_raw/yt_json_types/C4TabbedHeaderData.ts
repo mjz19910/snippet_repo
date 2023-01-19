3a99895da098ad14fc6abcf0d2c1e0cd14aacb85
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-19 00:34:25 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/C4TabbedHeaderData.ts
+++ userscript/youtube_plugin_raw/yt_json_types/C4TabbedHeaderData.ts
@@ -1,6 +1,30 @@
+type PrimaryLinkItem={
+	navigationEndpoint: UrlEndpoint;
+	icon: Thumbnail;
+	title: SimpleText;
+};
+
+type ChannelHeaderLinks={
+	primaryLinks: PrimaryLinkItem[];
+};
+
+type ChannelHeaderLinksRenderer={
+	channelHeaderLinksRenderer: ChannelHeaderLinks;
+};
+
 type C4TabbedHeaderData={
 	channelId: `UC${string}`;
 	title: string;
+	navigationEndpoint: BrowseEndpoint;
+	avatar: Thumbnail;
+	banner: Thumbnail;
+	badges: MetadataBadgeRenderer[];
+	headerLinks: ChannelHeaderLinksRenderer;
 	subscribeButton: SubscribeButtonRenderer;
+	subscriberCountText: SimpleText;
+	tvBanner: Thumbnail;
+	mobileBanner: Thumbnail;
 	trackingParams: string;
+	channelHandleText: TextWithRuns;
+	videosCountText: TextWithRuns;
 };
