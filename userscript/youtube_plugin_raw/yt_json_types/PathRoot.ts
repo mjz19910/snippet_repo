8158d8aafc793065674a4e6d682ceab78d62dbe3
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-19 21:41:04 -0700

    u

---
+++ userscript/youtube_plugin_raw/yt_json_types/PathRoot.ts
@@ -0,0 +1,30 @@
+type PathRoot=
+	|"watch_page_url.pp"
+	|"get_transcript.params"
+	|"reel.player_params"
+	|"reel.sequence_params"
+	|"playlist_edit.params"
+	|"watch.params"
+	|"watch.player_params"
+	|"ypc_get_offers.params"
+	|"create_playlist.params"
+	|"watch_playlist.params"
+	|"watch.params.f2"
+	|"watch.params.f3"
+	|"watch.params.f24"
+	|"watch.params.f27"
+	|"watch.params.f27.f1"
+	|"watch.params.f33"
+	|"watch.params.f33.f2"
+	|"watch.params.f33.f3"
+	|"watch.params.f33.f4"
+	|"watch.params.f33.f5"
+	|"watch.params.f56"
+	|"watch.player_params.f8"
+	|"watch.player_params.f9"
+	|`watch.player_params.f40`
+	|"watch.player_params.f40.f1"
+	|`record_notification_interactions`
+	;
+;
+
