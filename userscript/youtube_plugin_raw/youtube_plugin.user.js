4d422fcfa53fb3a55ef348e4a1f874f36a68df38
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-21 02:38:19 -0700

    u

--- userscript/youtube_plugin_raw/youtube_plugin.user.js
+++ userscript/youtube_plugin_raw/youtube_plugin.user.js
@@ -5832,7 +5832,17 @@ case "${path}": {
 			let off=1;
 			for(let val of tva) {
 				switch(path) {
-					default: console.log(`\ncase ${JSON.stringify(path)}: {};`); debugger; return;
+					default: {
+						console.log(`\ncase ${JSON.stringify(path)}: {};`);
+						console.log(`\n${JSON.stringify(`${path}[${off}]`)},\n`);
+					} debugger; return;
+					case "report.params.f28.f1[1].f1.f1": {
+						switch(off) {
+							case 1: break;
+							default: console.log(`\ncase ${JSON.stringify(off)}: break;`); debugger; return;
+						}
+						this.parse_param_next(root,`${path}[${off}]`,[val]);
+					}; return;
 					case "report.params.f28.f1": {
 						switch(off) {
 							case 1: break;
--- userscript/youtube_plugin_raw/yt_json_types/generated/P$PathRoot.ts
+++ userscript/youtube_plugin_raw/yt_json_types/generated/P$PathRoot.ts
@@ -1,5 +1,6 @@
 type P$LogItems=[
 	"[parse_value.gen_ns] [report.params.f28.f1[1].f1.f1]",
+	"[parse_value.gen_ns] [report.params.f28.f1[1].f1.f1[1]]",
 	"[parse_value.gen_ns] [report.params.f28.f1.f1.f1[1]]",
 ];
 type P$PathRoot=[
