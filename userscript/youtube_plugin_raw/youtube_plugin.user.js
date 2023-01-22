c108242cabcc11507aae2b86c53d59c41cc513a2
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-22 12:27:13 -0700

    u

--- userscript/youtube_plugin_raw/youtube_plugin.user.js
+++ userscript/youtube_plugin_raw/youtube_plugin.user.js
@@ -5226,7 +5226,7 @@ class ParserService extends BaseService {
 			case "${path}":
 				switch(ta) {case ${ta}: break; default: new_ns(); debugger; return;}
 				/** @type {P$PathRoot} */
-				return this.parse_param_next(root,\`\${path}.f\${ta}\`,tv);\n`.split("\n").map(e => e.slice(3)).join("\n"));
+				return this.parse_param_next(root,\`\${path}.f\${ta}\`,tv);\n`.split("\n").map(e => e.slice(0,3).trim()+e.slice(3)).join("\n"));
 		};
 		return {u,gen_next_part,new_path};
 	}
