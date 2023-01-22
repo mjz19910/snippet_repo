75a1ac9ff8be8df0d44135c2f775ffdfa21dc61e
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-22 11:32:22 -0700

    u

---
+++ userscript/youtube_plugin_raw/yt_json_types/generated/P$unknown$param.ts
@@ -0,0 +1,8 @@
+type P$unknown$param=[
+	"like.likeParams",
+	"like.remove_like_params",
+	"next.queue_context_params",
+	"playlist_edit.params",
+	"watch_page_url.pp",
+	"watch_playlist.params"
+][number];
