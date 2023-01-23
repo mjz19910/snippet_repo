18a1eaebb1103282f586d16dc2bf5f094ac95622
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-23 03:20:18 -0700

    u

--- userscript/youtube_plugin_raw/youtube_plugin.user.js
+++ userscript/youtube_plugin_raw/youtube_plugin.user.js
@@ -5899,6 +5899,7 @@ class ParserService extends BaseService {
 	}
 	/** @public @arg {YtTargetIdType} x */
 	parse_target_id(x) {
+		if(x===void 0) {debugger; return;}
 		if(this.str_starts_with("browse-feed",x)) {
 			console.log("[target_id.browse_feed","browse-feed",split_string_once("browse-feed")[1]);
 			return this.save_enum_with_sep("browse-feed",x,"");
