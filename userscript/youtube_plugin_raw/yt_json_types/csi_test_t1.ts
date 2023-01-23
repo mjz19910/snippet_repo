714c2bb609800902a943c61ad4b599bd4dea456a
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-23 16:29:02 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/csi_test_t1.ts
+++
@@ -1,9 +0,0 @@
-function csi_test_t1() {
-	type U0=HexNib<"d">;
-	type U1=`${HexNib<"d">}${HexNib<"a">}`;
-	type Cx=HexByte<"da">;
-	const u: Cx&U1="da"; u;
-	const [x]: Split<U1,"">=[...u] as Split<U1,"">;
-	const A: U0=x; A;
-	u;
-}
