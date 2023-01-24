5192c753efc8f50ef7e87d1b2cfba7bd8b9c3a10
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-24 01:42:08 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/D$ContinuationItem.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/D$ContinuationItem.ts
@@ -5,8 +5,8 @@ type D_ContinuationItem={
 	trigger: "CONTINUATION_TRIGGER_ON_ITEM_SHOWN";
 	continuationEndpoint: C_Continuation;
 	button: R_Button;
-};;
-type DD$D_ContinuationItem={
-	button?: R_Button;
-	ghostCards?: R_GhostGrid;
-}
+}|{
+	trigger: "CONTINUATION_TRIGGER_ON_ITEM_SHOWN";
+	continuationEndpoint: C_Continuation;
+	ghostCards: R_GhostGrid;
+};
