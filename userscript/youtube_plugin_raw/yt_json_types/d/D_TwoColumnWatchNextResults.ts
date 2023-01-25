29f1aca87f0341f5c07ced202c4d5765080ee05f
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-25 04:12:01 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/D_TwoColumnWatchNextResults.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/D_TwoColumnWatchNextResults.ts
@@ -1,5 +1,5 @@
 type D_TwoColumnWatchNextResults={
-	results: T_Results<Record<"contents",G_WatchResultItem[]>>;
+	results: T_Results<{trackingParams: string;}&Record<"contents",G_WatchResultItem[]>>;
 	secondaryResults: T_SecondaryResults<G_Watch_SecondaryResults>;
 	playlist?: T_Playlist<PlaylistContent>;
 	autoplay?: T_Autoplay<AutoplayContent>;
