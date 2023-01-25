54c60c0b07837b06ce10206c6482b76fec8bea2a
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-25 09:32:25 -0700

    u

--- userscript/youtube_plugin_raw/youtube_plugin.user.js
+++ userscript/youtube_plugin_raw/youtube_plugin.user.js
@@ -5852,7 +5852,17 @@ class ParserService extends BaseService {
 						if(path_parts.length===2) {
 							switch(map_entry_value) {default: debugger; return;}
 						}
-						switch(path_parts[2]) {default: u(idx); debugger; path_parts[2]===""; break; case "f1": u(idx); debugger; break;}
+						switch(path_parts[2]) {
+							default: u(idx); debugger; path_parts[2]===""; break;
+							case "f1": {
+								const idx=4;
+								if(path_parts.length===3) {
+									if(typeof map_entry_value==="number") return this.save_number(`[${path}]`,map_entry_value);
+									switch(map_entry_value) {default: debugger; return;}
+								}
+								switch(path_parts[3]) {default: u(idx); debugger; path_parts[3]===""; break;}
+							} break;
+						}
 					} break;
 					case "sequence_params": u(idx); debugger; break;
 				}
