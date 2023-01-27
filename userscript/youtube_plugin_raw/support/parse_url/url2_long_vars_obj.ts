8130abed5c3aad6136c66ec55ab9fda178cdaf97
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-26 23:02:37 -0700

    u

--- userscript/youtube_plugin_raw/support/parse_url/url2_long_vars_obj.ts
+++ userscript/youtube_plugin_raw/support/parse_url/url2_long_vars_obj.ts
@@ -1,4 +1,7 @@
 namespace Url2_long_vars_obj {
+	const url2="/youtubei/v1/browse?key=[--snip--]&prettyPrint=false";
+	const str=url2;
+	const vv=create_from_parse_partial(str);
 	const res=make_search_params(vv.search);
 	export type url2_long_vars_obj={
 		key: typeof res.key;
