3fe1eecd9866f0efd25c384a08487e1783d3e511
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-27 10:18:26 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/m/NumArrStrVerify.ts
+++ userscript/youtube_plugin_raw/yt_json_types/m/NumArrStrVerify.ts
@@ -1,8 +1,1 @@
-type T_NumArrStrVerify<T extends string,C extends string="">=
-	C extends ''?
-	T extends `${number},${number},${infer X}`?
-	T_NumArrStrVerify<T,X>:
-	C extends ''
-	? `${T}`:`${T},${C}`:
-	C extends `${number},${number},${infer X}`?
-	T_NumArrStrVerify<T,X>:T;
+type T_NumArrStrVerify<T extends string,C extends string="">=C extends ''?T extends `${number},${number},${infer X}`?T_NumArrStrVerify<T,X>:C extends ''? `${T}`:`${T},${C}`:C extends `${number},${number},${infer X}`?T_NumArrStrVerify<T,X>:T;
