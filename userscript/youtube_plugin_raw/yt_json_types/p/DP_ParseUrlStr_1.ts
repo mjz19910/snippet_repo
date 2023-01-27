3f97644826c36b8bd099828347b98ce257084b1f
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-27 09:42:29 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/p/DP_ParseUrlStr_1.ts
+++ userscript/youtube_plugin_raw/yt_json_types/p/DP_ParseUrlStr_1.ts
@@ -1,4 +1,5 @@
 type ParseUrlStr_1=T_SplitOnce<Exclude<YtUrlFormat,"/">,"/">[1];
 type ParseUrlStr_2=Extract<T_SplitOnce<ParseUrlStr_1,"/">,["youtubei",...any]>;
+type ParseUrlStr_3=Extract<T_SplitOnce<ParseUrlStr_1,"/">,[any,any]>;
 type ParseUrlStr_4=ParseUrlStr_2[1];
 type ParseUrlStr_5=Extract<T_SplitOnce<Extract<ParseUrlStr_2[1],`${string}/${string}`>,"/">[1],`${string}/${string}`>;
--- userscript/youtube_plugin_raw/yt_json_types/p/ParseUrlStr_3.ts
+++
@@ -1,1 +0,0 @@
-type ParseUrlStr_3=Extract<T_SplitOnce<ParseUrlStr_1,"/">,[any,any]>;
