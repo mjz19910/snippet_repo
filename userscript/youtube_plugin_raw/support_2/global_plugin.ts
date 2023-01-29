62646b28028cd4f40320d0ddbd09262e1e3f3f3b
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-29 05:51:35 -0700

    u

--- userscript/youtube_plugin_raw/support_2/global_plugin.ts
+++ userscript/youtube_plugin_raw/support_2/global_plugin.ts
@@ -1,2 +1,2 @@
 export {};
-declare global {interface Window {yt_plugin?: import("../z_child_modules/youtube_plugin_base.user").YtPlugin;}}
+declare global {interface Window {yt_plugin?: import("../zb_child_modules/youtube_plugin_base.user").YtPlugin;}}
