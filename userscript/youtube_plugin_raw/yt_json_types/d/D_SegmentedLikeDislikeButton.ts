2c20d35a04210a8976042a9816e54e548e95332d
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-29 12:22:42 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/D_SegmentedLikeDislikeButton.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/D_SegmentedLikeDislikeButton.ts
@@ -1,4 +1,14 @@
 type D_SegmentedLikeDislikeButton={
 	likeButton: R_ToggleButton;
 	dislikeButton: R_ToggleButton;
+}|{
+	style: "STYLE_DEFAULT";
+	size: "SIZE_DEFAULT";
+	isDisabled: false;
+	text: D_Text;
+	serviceEndpoint: SE_ShareEntity;
+	icon: T_Icon<"SHARE">;
+	tooltip: "Share";
+	trackingParams: string;
+	accessibilityData: D_Accessibility;
 };
