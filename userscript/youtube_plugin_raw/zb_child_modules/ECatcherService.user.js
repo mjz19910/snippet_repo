7954421be16f424523e32f86c8eb3904c8acda04
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-29 08:55:05 -0700

    u

--- userscript/youtube_plugin_raw/zb_child_modules/ECatcherService.user.js
+++ userscript/youtube_plugin_raw/zb_child_modules/ECatcherService.user.js
@@ -1,3 +1,16 @@
+// ==UserScript==
+// @name	youtube plugin ECatcherService
+// @namespace	https://github.com/mjz19910/
+// @version	0.1.0
+// @description	try to take over the world!
+// @author	@mjz19910
+// @copyright	@mjz19910 2020-2022
+// @match	https://www.youtube.com/*
+// @grant	none
+// @run-at	document-start
+// @updateURL	https://github.com/mjz19910/snippet_repo/raw/master/userscript/youtube_plugin_raw/youtube_plugin.meta.js
+// @downloadURL	https://github.com/mjz19910/snippet_repo/raw/master/userscript/youtube_plugin_raw/youtube_plugin.user.js
+// ==/UserScript==
 const __module_name__="mod$ECatcherService";
 const store=required(window.__plugin_modules__);
 const as=required(required(store["mod$YoutubePluginBase"]).as_);
--- userscript/youtube_plugin_raw/zb_child_modules/Plug_LoadServices.user.js
+++ userscript/youtube_plugin_raw/zb_child_modules/Plug_LoadServices.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name	youtube plugin Services
+// @name	youtube plugin LoadServices
 // @namespace	https://github.com/mjz19910/
 // @version	0.1.0
 // @description	try to take over the world!
