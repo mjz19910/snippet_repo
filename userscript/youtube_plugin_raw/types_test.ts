5ed5002c4ccd4843631049932322da723ab2c847
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-29 06:41:28 -0700

    u

--- userscript/youtube_plugin_raw/types_test.ts
+++ userscript/youtube_plugin_raw/types_test.ts
@@ -3,7 +3,6 @@ namespace D_NotificationTopbarButton$updateUnseenCountEndpoint {
 
 	export type D_NotificationTopbarButton$Icon={
 		icon: T_Icon<"NOTIFICATIONS">;
-
 		menuRequest: T_SE_Signal<{webCommandMetadata: {sendPost: true; apiUrl: "/youtubei/v1/notification/get_notification_menu";};},{signal: "GET_NOTIFICATIONS_MENU"; actions: TA_OpenPopup<{popup: TR_MP_Menu<{trackingParams: string; style: "MULTI_PAGE_MENU_STYLE_TYPE_NOTIFICATIONS"; showLoadingSpinner: true;}>; popupType: "DROPDOWN"; beReused: true;}>[];}>;
 		style: "NOTIFICATION_BUTTON_STYLE_TYPE_DEFAULT";
 		trackingParams: string;
