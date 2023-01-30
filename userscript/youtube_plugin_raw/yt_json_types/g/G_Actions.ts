22814d230ce11c4dde976552965fa877425e07f9
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-30 13:24:21 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/g/G_Actions.ts
+++ userscript/youtube_plugin_raw/yt_json_types/g/G_Actions.ts
@@ -1,12 +1,12 @@
 type G_Actions=
 	|A_AddToGuideSection
 	|A_GetMultiPageMenu
-	|TA_OpenPopup<{}>
-	|C_RefreshPlaylist
 	|A_RemoveFromGuideSection
-	|C_RunAttestation
 	|A_UpdateChannelSwitcherPage
 	|A_UpdateNotificationsUnseenCount
 	|A_UpdateSubscribeButton
+	|C_RefreshPlaylist
+	|C_RunAttestation
+	|TA_OpenPopup<{}>
 	;
 ;
