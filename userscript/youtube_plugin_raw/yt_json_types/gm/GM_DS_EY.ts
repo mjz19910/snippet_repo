c8397893e2d75a1301614f5021f0bf5d06145223
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-30 16:56:10 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/gm/GM_DS_EY.ts
+++ userscript/youtube_plugin_raw/yt_json_types/gm/GM_DS_EY.ts
@@ -1,7 +1,5 @@
-//#region DS_
 type DS_EY_MacroMarkersList={key: string;};
 type DS_EY_Subscription={key: string; subscribed: true;};
 type DS_EY_PlaylistLoop={key: string; state: T_EnumStr<"PLAYLIST_LOOP_STATE","NONE">;};
 type DS_EY_TranscriptTrackSelection={key: string; selectedTrackIndex: 0; serializedParams: string;};
 type DS_EY_TranscriptSearchBox={key: string; isHidden: false;};
-//#endregion
