62ec9d69b56d76df993ebf34e438700ae4b38b98
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-30 17:03:11 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/gr/GM_DC.ts
+++ userscript/youtube_plugin_raw/yt_json_types/gr/GM_DC.ts
@@ -1,4 +1,4 @@
-//#region CommonType
+//#region Common data
 type DC_Empty_WCM={webCommandMetadata: {};};
 type DC_Params={params: string;};
 type DC_Generic_CTP={continuation: string; clickTrackingParams: string;};
