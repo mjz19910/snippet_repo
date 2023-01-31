b2d1af089859f09f75eabe69fd9e5d630bfa6baa
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-31 01:09:20 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/gr/GR_CF.ts
+++ userscript/youtube_plugin_raw/yt_json_types/gr/GR_CF.ts
@@ -1,4 +1,4 @@
-type CF_mod=import("./CF_mod.js").CF_mod;
+type CF_mod=import("../../zb_plugin_types/CF_mod.js").CF_mod;
 type CF_M_s_priv=
 	|`D_Params:${CF_D_Params}`
 	|`T_Endpoint_Tracking:${"C_GetSurvey"}`
