988b4b78c4f0fdda72d7983bed9b8a32baf982f5
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-31 11:16:49 -0700

    u

--- userscript/youtube_plugin_raw/za_userscript_meta/YTPlugin_HandleTypes_Service.meta.js
+++ userscript/youtube_plugin_raw/za_userscript_meta/YTPlugin_HandleTypes_Service.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name	youtube plugin HandleTypes
+// @name	YTPlugin HandleTypes Service
 // @namespace	https://github.com/mjz19910/
 // @version	0.1.1
 // @description	try to take over the world!
 // @author	@mjz19910
 // @copyright	@mjz19910 2020-2023
-// @match	https://www.youtube.com/*
+// @match	https://*.youtube.com/*
 // @grant	none
 // @run-at	document-start
-// @updateURL	https://github.com/mjz19910/snippet_repo/raw/master/userscript/youtube_plugin_raw/youtube_plugin.meta.js
-// @downloadURL	https://github.com/mjz19910/snippet_repo/raw/master/userscript/youtube_plugin_raw/youtube_plugin.user.js
+// @updateURL	https://github.com/mjz19910/snippet_repo/raw/master/userscript/youtube_plugin_raw/za_userscript_meta/YTPlugin_HandleTypes_Service.meta.js
+// @downloadURL	https://github.com/mjz19910/snippet_repo/raw/master/userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes_Service.user.js
 // ==/UserScript==
