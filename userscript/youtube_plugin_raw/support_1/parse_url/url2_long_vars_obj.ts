3a03d0c8315654770ea1ca9d103f32d0b9505b88
Matthias Zimmerman <matthias291999@gmail.com>
2023-01-30 19:00:39 -0700

    u

--- userscript/youtube_plugin_raw/support_1/parse_url/url2_long_vars_obj.ts
+++ userscript/youtube_plugin_raw/support_1/parse_url/url2_long_vars_obj.ts
@@ -2,11 +2,25 @@ namespace Url2_long_vars_obj {
 	const url2="/youtubei/v1/browse?key=[--snip--]&prettyPrint=false";
 	const str=url2;
 	const vv=create_from_parse_partial(str);
-	let base=new ApiBase();
-	const url_key=get_url_params(vv.search,"key");
-	const url_prettyPrint=get_url_params(vv.search,"prettyPrint");
-	export type url2_long_vars_obj={
-		key: typeof url_key;
-		prettyPrint: typeof url_prettyPrint;
-	};
+	export async function do_test_async() {
+		const ApiBase=(await import("../../zc_child_modules/YtPlugin_Base_Plugin.user.js")).ApiBase;
+		class ApiUser extends ApiBase {
+			run_test() {
+				const url_parsed=this.parse_url_search_params(vv.search);
+				return url_parsed;
+			}
+		}
+		return (new ApiUser).run_test();
+	}
+	const res=do_test_async().then(x => {
+		type url2_long_vars_obj={
+			key: typeof x.key;
+			prettyPrint: typeof x.prettyPrint;
+		};
+		let tv: url2_long_vars_obj=x;
+		tv=x;
+		x=tv;
+		return x;
+	});
+	export type url2_long_vars_obj=Awaited<typeof res>;
 }
