2ccdb796d22e69cab20a4146a0a12d947463312e
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-02 01:21:58 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/done/lmn_opq/Popup.ts
+++ userscript/youtube_plugin_raw/yt_json_types/done/lmn_opq/Popup.ts
@@ -1,1 +1,9 @@
 type Popup_ConfirmDialog={popup: R_ConfirmDialog; popupType: "DIALOG";};
+type Popup_DismissibleDialog={popup: R_FancyDismissibleDialog; popupType: "DIALOG";};
+type D_FancyDismissibleDialog={
+	dialogMessage: G_Text;
+	confirmLabel: G_Text;
+	trackingParams: string;
+};
+
+type R_FancyDismissibleDialog={fancyDismissibleDialogRenderer: D_FancyDismissibleDialog;};
