ba2760296f263f5309ed31edb6f46646ab68dae6
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-01 21:56:06 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/C_tmp_sub.ts
+++ userscript/youtube_plugin_raw/yt_json_types/C_tmp_sub.ts
@@ -2,7 +2,8 @@ type CD_MusicLibraryStatusUpdate={
 	libraryStatus: "MUSIC_LIBRARY_STATUS_IN_LIBRARY";
 	addToLibraryFeedbackToken: string;
 };
-type CP_Tracking={clickTrackingParams: string;};export type CR_ContinuationItemsFor<T,U,V extends {continuationItems: any;}>={
+type CP_Tracking={clickTrackingParams: string;};
+type CR_ContinuationItemsFor<T,U,V extends {continuationItems: any;}>={
 	slot: U;
 	targetId: T;
 	continuationItems: V["continuationItems"];
