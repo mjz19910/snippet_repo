9f3a852d5600a20a74beaa7cc3fcb06c59da10a6
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-02 23:46:51 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/done/CF.ts
+++ userscript/youtube_plugin_raw/yt_json_types/done/CF.ts
@@ -72,7 +72,7 @@ type CF_T_Signal=
 ;
 type CF_M_w=
 	|T_ExtractImport<"CF_M_w">
-	// |`y:${CF_M_y}`
+	|`y:${CF_M_y}`
 	|`zy:${CF_M_zy}`
 	|`Unpack:T_WCM:${CF_T_WCM_Unpack}`
 	|`TA_Page:${CF_TA_Page}`
