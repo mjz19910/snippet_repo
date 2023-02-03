95fa0f4aa719c3a0571b1b5d986876cb058de794
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-02 22:22:49 -0700

    u

--- userscript/youtube_plugin_raw/zd_gen_CF/gen.pm
+++ userscript/youtube_plugin_raw/zd_gen_CF/gen.pm
@@ -1,13 +1,16 @@
 while (<>) {
 	$_ =~ s/.+?: \w+ TS\d+: .+?'(".+?")'.+type .(.+).\./\t\t|{n: Prelude.$2; t: Types.$2_; v: $1;}/g;
-	if($_ =~ s/.+?: \w+ TS\d+: .+?'(`.+?`)'.+type .(.+).\./\t\t/) {
+	if(s/.+?: \w+ TS\d+: .+?'(`.+?`)'.+type .(.+).\./\t\t/) {
+		$_ =~ s/.+?: \w+ TS\d+: .+?'(".+?")'.+type .(.+).\./\t\t/;
 		chop;
 		print "{n: Prelude.$2; t: Types.$2_; v: $1;}","\n";
-
+		my $n=<>;
+		print $n;
+		my $n=<>;
+		print $n;
 		next;
 	}
 	$_ =~ s/.+?: \w+ TS\d+: .+?'(`.+?`)'.+type .(.+).\./\t\t|{n: Prelude.$2; t: Types.$2_; v: $1;}/g;
 	chop;
 	print $_,"\n";
-	print "C1\n";
 }
