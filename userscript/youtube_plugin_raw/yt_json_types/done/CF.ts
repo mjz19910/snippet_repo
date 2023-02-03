5bd7884f153e00f405d83a54487634023f39d31f
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-02 17:29:43 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/done/CF.ts
+++ userscript/youtube_plugin_raw/yt_json_types/done/CF.ts
@@ -94,21 +94,11 @@ type CF_TE_Endpoint_3=T_ExtractImport<"CF_TE_Endpoint_3">|"CF_TE_Endpoint_3"|"a"
 type CF_TD_ItemSection_3_=T_ExtractImport<"CF_TD_ItemSection_3"> extends never? "CF_TD_ItemSection_3"|"a":T_ExtractImport<"CF_TD_ItemSection_3">;
 interface CF_TD_ItemSection_3_IF {v: CF_TD_ItemSection_3_;}
 interface CF_TD_ItemSection_3x extends CF_TD_ItemSection_3_IF {}
-type CF_TD_ItemSection_3=T_ExtractImport<"CF_TD_ItemSection_3">|"CF_TD_ItemSection_3"|"a";
-type CF_T_WCM_Unpack=T_ExtractImport<"CF_T_WCM_Unpack">|"CF_T_WCM_Unpack"|"a";
-type CF_T_Commands=T_ExtractImport<"CF_T_Commands">|"CF_T_Commands"|"a";
-type CF_T_Items=T_ExtractImport<"CF_T_Items">|"CF_T_Items"|"a";
-type CF_T_Items_TP=T_ExtractImport<"CF_T_Items_TP">|"CF_T_Items_TP"|"a";
 type CF_M_y=
 	|T_ExtractImport<"CF_M_y">|"CF_M_y"|"a"
 	|`T_WCM:${CF_T_WCM}`
 	;
 ;
-type CF_M_zy=T_ExtractImport<"CF_M_zy">|"CF_M_zy"|"a";
-type CF_TA_Page=T_ExtractImport<"CF_TA_Page">|"CF_TA_Page"|"a";
-type CF_T_Icon=T_ExtractImport<"CF_T_Icon">|"CF_T_Icon"|"a";
-type CF_D_Video_Handle=T_ExtractImport<"CF_D_Video_Handle">|"CF_D_Video_Handle"|"a";
-type CF_T_SE_Signal_P1=T_ExtractImport<"CF_T_SE_Signal_P1">|"CF_T_SE_Signal_P1"|"a";
 type CF_T_SE_Signal=
 	|`${CF_GE_ResponseReceived}.SE_Signal`
 	|`${CF_T_SE_Signal_P1}.SE_Signal`
@@ -138,10 +128,20 @@ type CF_D_ChipCloudChip_Omit=T_ExtractImport<"CF_D_ChipCloudChip_Omit">|"CF_D_Ch
 type CF_D_Link=T_ExtractImport<"CF_D_Link">|"CF_D_Link"|"a";
 type CF_D_Playlist_Omit=T_ExtractImport<"CF_D_Playlist_Omit">|"CF_D_Playlist_Omit"|"a";
 type CF_D_STR=T_ExtractImport<"CF_D_STR">|"a"|"";
+type CF_D_Video_Handle=T_ExtractImport<"CF_D_Video_Handle">|"CF_D_Video_Handle"|"a";
 type CF_DC_Generic_CTP=T_ExtractImport<"CF_DC_Generic_CTP">|"CF_DC_Generic_CTP"|"a";
 type CF_GE_ResponseReceived=T_ExtractImport<"CF_GE_ResponseReceived">|"CF_GE_ResponseReceived"|"a";
 type CF_M_VE=T_ExtractImport<"CF_M_VE">|"CF_M_VE"|"a";
+type CF_M_zy=T_ExtractImport<"CF_M_zy">|"CF_M_zy"|"a";
 type CF_onMissingIcon=T_ExtractImport<"CF_onMissingIcon">|"CF_onMissingIcon"|"a";
+type CF_T_Commands=T_ExtractImport<"CF_T_Commands">|"CF_T_Commands"|"a";
 type CF_T_Endpoint=T_ExtractImport<"CF_T_Endpoint">|"CF_T_Endpoint"|"a";
+type CF_T_Icon=T_ExtractImport<"CF_T_Icon">|"CF_T_Icon"|"a";
+type CF_T_Items_TP=T_ExtractImport<"CF_T_Items_TP">|"CF_T_Items_TP"|"a";
+type CF_T_Items=T_ExtractImport<"CF_T_Items">|"CF_T_Items"|"a";
+type CF_T_SE_Signal_P1=T_ExtractImport<"CF_T_SE_Signal_P1">|"CF_T_SE_Signal_P1"|"a";
+type CF_T_WCM_Unpack=T_ExtractImport<"CF_T_WCM_Unpack">|"CF_T_WCM_Unpack"|"a";
 type CF_T_WCM=T_ExtractImport<"CF_T_WCM">|"CF_T_WCM"|"a";
+type CF_TA_Page=T_ExtractImport<"CF_TA_Page">|"CF_TA_Page"|"a";
+type CF_TD_ItemSection_3=T_ExtractImport<"CF_TD_ItemSection_3">|"CF_TD_ItemSection_3"|"a";
 type CF_TE_Endpoint_Opt_3=T_ExtractImport<"CF_TE_Endpoint_Opt_3">|"CF_TE_Endpoint_Opt_3"|"a";
