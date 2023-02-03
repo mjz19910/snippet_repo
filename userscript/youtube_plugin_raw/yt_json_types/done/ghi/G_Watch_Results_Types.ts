4b07c9a3526dd1a4a97f45493a22ab858bf2f6ce
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-03 03:38:09 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/done/ghi/G_Watch_Results_Types.ts
+++ userscript/youtube_plugin_raw/yt_json_types/done/ghi/G_Watch_Results_Types.ts
@@ -45,7 +45,7 @@ type D_PlaylistContent={
 	titleText: G_Text;
 	isEditable: true;
 	menu: R_Menu;
-	localCurrentIndex: 0|25;
+	localCurrentIndex: 0|1|25;
 	playlistButtons: R_Menu;
 	isCourse: false;
 	nextVideoLabel: G_Text;
