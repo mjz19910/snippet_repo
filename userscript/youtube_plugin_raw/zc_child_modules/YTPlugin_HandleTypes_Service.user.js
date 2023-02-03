7db368c5b3abff0906c3fc323467a1fb36f5ad05
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-03 00:36:13 -0700

    u

--- userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes_Service.user.js
+++ userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes_Service.user.js
@@ -169,6 +169,7 @@ class HandleTypesEval extends ServiceMethods {
 	}
 	//#endregion
 
+	//#region parse binary params
 	/** @private @template {CF_L_TP_Params|CF_L_Params} T @arg {T} root @arg {P_ParamParse} path @arg {V_ParamMapType} map @arg {number[]} mk @arg {T_ParseCallbackFunction<T>} callback */
 	make_parse_key(root,path,map,mk,callback) {
 		/** @private @arg {number[]} ta */
@@ -486,6 +487,7 @@ class HandleTypesEval extends ServiceMethods {
 			off++;
 		}
 	}
+	//#endregion
 
 	//#endregion
 }
