df389ee861a38135069f36bf4cf634a833f33370
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-04 14:45:57 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/done/d/D.ts
+++ userscript/youtube_plugin_raw/yt_json_types/done/d/D.ts
@@ -3107,3 +3107,69 @@ type D_RichMetadataRow={
 };
 type D_RelatedChipCloud={content: R_ChipCloud;};
 type D_LoggingDirectives_Gestures=T_Types<4>;
+type DD_Streaming={
+	expiresInSeconds: `${number}`;
+	adaptiveFormats: D_AdaptiveFormatItem[];
+	formats: D_FormatItem[];
+	probeUrl?: D_Youtube_Streaming_ProbeUrl;
+};
+type DMD_AdSlot={
+	slotId: `${number}:${number}:${number}:${number}`;
+	slotType: "SLOT_TYPE_IN_FEED";
+	slotPhysicalPosition: 1;
+}|{
+	slotId: `${number}:${number}:${number}:${number}`;
+	slotType: "SLOT_TYPE_IN_FEED";
+	slotPhysicalPosition: 1;
+	adSlotLoggingData: D_SerializedSlotAdServingDataEntry;
+}|{
+	slotId: `${number}:${number}:${number}:${number}`;
+	slotType: "SLOT_TYPE_PAGE_TOP";
+	slotPhysicalPosition: 0;
+};
+type DMD_Badge={
+	style: "BADGE_STYLE_TYPE_SIMPLE";
+	label: "New";
+	trackingParams: string;
+}|{
+	icon: T_Icon<"CHECK_CIRCLE_THICK">;
+	style: "BADGE_STYLE_TYPE_VERIFIED";
+	tooltip: "Verified";
+	trackingParams: string;
+	accessibilityData: D_Label;
+}|{
+	icon: T_Icon<"LIVE">;
+	style: "BADGE_STYLE_TYPE_LIVE_NOW";
+	label: "LIVE";
+	trackingParams: string;
+}|{
+	icon: T_Icon<"LIVE">;
+	style: "BADGE_STYLE_TYPE_LIVE_NOW";
+	label: "PREMIERE";
+	trackingParams: string;
+}|{
+	style: "BADGE_STYLE_TYPE_COLLECTION";
+	label: string;
+	trackingParams: string;
+}|{
+	icon: T_Icon<"OFFICIAL_ARTIST_BADGE">;
+	style: "BADGE_STYLE_TYPE_VERIFIED_ARTIST";
+	tooltip: "Official Artist Channel";
+	trackingParams: string;
+	accessibilityData: TD_Label<"Official Artist Channel">;
+}|{
+	style: "BADGE_STYLE_TYPE_YPC";
+	label: "Fundraiser";
+	trackingParams: string;
+};
+type DMD_RowContainer={
+	rows?: R_RichMetadataRow[];
+	collapsedItemCount: number;
+	trackingParams: string;
+};
+type DRC_Csi_SPs=RC_To_SPs<RC_CsiVarMap>|(RC_CsiServiceC|RC_CsiServiceCVer)[];
+type DSS_Context={context: D_ContextTypeStr|null;};
+type DT_MenuFlexibleItem={
+	menuItem: R_MenuServiceItem;
+	topLevelButton: R_Button;
+};
