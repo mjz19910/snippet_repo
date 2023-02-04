4c901876d029966ca3f24fb7988c62e6053018b7
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-04 14:46:21 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/done/d/D.ts
+++ userscript/youtube_plugin_raw/yt_json_types/done/d/D.ts
@@ -1,3 +1,7 @@
+//#region Extract on Url
+type D_EX_YoutubeUrl=Extract<DU_Url['url'],`${string}www.youtube.com${string}`>;
+type D_EX_YoutubeKidsUrl=Extract<DU_Url['url']|"https://www.youtubekids.com/?source=youtube_web",`https://www.youtubekids.com${string}`>;
+//#endregion
 //#region String data, ie `D_${string}`
 type D_EndpointLikeEndings="Endpoint"|"Command"|"Action";
 //#endregion
--- userscript/youtube_plugin_raw/yt_json_types/done/d/D_EX.ts
+++
@@ -1,4 +0,0 @@
-//#region Extract on Url
-type D_EX_YoutubeUrl=Extract<DU_Url['url'],`${string}www.youtube.com${string}`>;
-type D_EX_YoutubeKidsUrl=Extract<DU_Url['url']|"https://www.youtubekids.com/?source=youtube_web",`https://www.youtubekids.com${string}`>;
-//#endregion
