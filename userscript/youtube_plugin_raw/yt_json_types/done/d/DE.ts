e74266cc638dccaf2b7064007735744122367cd6
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-04 14:43:04 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/done/d/DE.ts
+++ userscript/youtube_plugin_raw/yt_json_types/done/d/DE.ts
@@ -1,15 +1,8 @@
 type DE_UserFeedback={additionalDatas: G_AdditionalDataItem[];};
 type DE_YpcGetCart={transactionParams: string;};
 //#region String Enum
-type DE_AdPlacementKind=T_EnumStr<"AD_PLACEMENT_KIND",[
-	"END",
-	"SELF_START",
-	"START"
-][number]>;
-type DE_OpportunityType=T_EnumStr<"OPPORTUNITY_TYPE",T_EnumStr<"ORGANIC",T_EnumStr<[
-	"BROWSE",
-	"WATCH_NEXT",
-][number],"RESPONSE_RECEIVED">>>;
+type DE_AdPlacementKind=T_EnumStr<"AD_PLACEMENT_KIND","END"|"SELF_START"|"START">;
+type DE_OpportunityType=T_EnumStr<"OPPORTUNITY_TYPE",T_EnumStr<"ORGANIC",T_EnumStr<"BROWSE"|"WATCH_NEXT","RESPONSE_RECEIVED">>>;
 type DE_IconType_Button=
 	|"SETTINGS"|"DELETE"
 	|"NOTIFICATIONS_ACTIVE"|"NOTIFICATIONS_NONE"|"NOTIFICATIONS_OFF"
