3780fb222b2e3ef69640515321269bebe83dadb2
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-12 10:09:39 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/done/r/R.ts
+++ userscript/youtube_plugin_raw/yt_json_types/done/r/R.ts
@@ -1,1 +1,51 @@
-type RE_D_VE3832_PreconnectUrl=`https://rr3---sn-nx5s7nee.googlevideo.com/generate_204`;
+//#region {pageType:string}
+type R_PageTypeWatch={
+	pageType: "watch";
+	endpoint: E_Watch;
+	response: G_RS_WatchPage;
+	fromHistory: boolean;
+	navigationDoneMs: number;
+};
+type R_PageTypeBrowse={
+	pageType: "browse";
+	endpoint: GE_Browse;
+	response: RS_Page_Browse;
+	fromHistory: boolean;
+	navigationDoneMs: number;
+};
+type R_PageTypeChannel={
+	pageType: "channel";
+	endpoint: {};
+	response: RS_Page_Channel;
+	fromHistory: boolean;
+	navigationDoneMs: number;
+};
+type R_PageTypePlaylist={
+	pageType: "playlist";
+	endpoint: {};
+	response: G_RS_Page_Playlist;
+	fromHistory: boolean;
+	navigationDoneMs: number;
+};
+type R_PageTypeSearch={
+	pageType: "search";
+	endpoint: E_Search;
+	response: RS_Page_Search;
+	fromHistory: boolean;
+	navigationDoneMs: number;
+};
+type R_PageTypeSettings={
+	pageType: "settings";
+	endpoint: E_Settings;
+	response: G_RS_Page_Settings;
+	fromHistory: boolean;
+	navigationDoneMs: number;
+};
+type R_PageTypeShorts={
+	pageType: "shorts";
+	endpoint: E_ReelWatch;
+	response: G_RS_Page_Shorts;
+	fromHistory: boolean;
+	navigationDoneMs: number;
+};
+//#endregion
---
+++ userscript/youtube_plugin_raw/yt_json_types/done/r/r_sub/re/RE_D_VE3832_PreconnectUrl.ts
@@ -0,0 +1,5 @@
+type RE_D_VE3832_PreconnectUrl=
+	|"https://rr3---sn-nx5s7nee.googlevideo.com/generate_204"
+	|"https://rr4---sn-nx5s7nel.googlevideo.com/generate_204"
+	;
+;
