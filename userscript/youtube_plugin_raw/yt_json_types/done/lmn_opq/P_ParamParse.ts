b4cad6ecfd0a2b67c0c65df9b7572d0948f006a4
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-11 22:27:29 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/done/lmn_opq/P_ParamParse.ts
+++ userscript/youtube_plugin_raw/yt_json_types/done/lmn_opq/P_ParamParse.ts
@@ -1,14 +1,4 @@
 type P_LogItems=[
-	"[parse_value.gen_ns] [D_Browse.param.f5]",
-	"[parse_value.gen_ns] [D_Browse.param.f23]",
-	"[parse_value.gen_ns] [create_comment.params.f5.f1]",
-	"[parse_value.gen_ns] [create_comment.params.f2]",
-	"[parse_value.gen_ns] [create_comment.params.f5]",
-	"[parse_value.gen_ns] [create_comment.params.f10]",
-	"[parse_value.gen_ns] [aadc_guidelines_state_entity_key.f2]",
-	"[parse_value.gen_ns] [aadc_guidelines_state_entity_key.f4]",
-	"[parse_value.gen_ns] [aadc_guidelines_state_entity_key.f5]",
-	"[parse_value.gen_ns] [unsubscribe.params.f1.f1]",
 ];
 type P_ParamParse=
 	|P_PathRootStr
