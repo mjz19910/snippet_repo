629a9e46eb00cdd7b7a52986410b099642e16f36
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-12 11:08:46 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/done/r/r_sub/re/RE_D_VE3832_PreconnectUrl.ts
+++ userscript/youtube_plugin_raw/yt_json_types/done/r/r_sub/re/RE_D_VE3832_PreconnectUrl.ts
@@ -1,7 +1,8 @@
 type RE_D_VE3832_PreconnectUrl=
+	|"https://rr2---sn-nx57ynsz.googlevideo.com/generate_204"
 	|"https://rr3---sn-nx5s7nee.googlevideo.com/generate_204"
 	|"https://rr4---sn-nx5s7nel.googlevideo.com/generate_204"
-	|"https://rr2---sn-nx57ynsz.googlevideo.com/generate_204"
+	|"https://rr4---sn-nx57ynsd.googlevideo.com/generate_204"
 	;
 ;
 type RE_D_GoogleVideoUrl_Hostname=UrlParse<Extract<D_UrlFormat,`https://${string}.googlevideo.com/${string}`>>["host"];
