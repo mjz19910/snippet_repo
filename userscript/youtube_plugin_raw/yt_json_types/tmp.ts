49119f67d29c7dbdedd2aa86f915f3a1c5127097
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-13 07:38:32 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/tmp.ts
+++ userscript/youtube_plugin_raw/yt_json_types/tmp.ts
@@ -1,1 +1,66 @@
 // empty
+// cSpell:ignoreRegExp /sn-\w+?\.googlevideo\.com"/
+// cSpell:ignoreRegExp /D_googlevideo_host_\w+/
+type D_googlevideo_host=[
+	D_googlevideo_host_9gv7ln,
+	D_googlevideo_host_p5qs7n,
+	D_googlevideo_host_tt1e7n,
+	D_googlevideo_host_q4flrn,
+	D_googlevideo_host_nx57yn,
+	D_googlevideo_host_nx5s7n,
+];
+type D_googlevideo_host_9gv7ln=[
+	"sn-9gv7lnes.googlevideo.com",
+	"sn-9gv7lnez.googlevideo.com",
+	"sn-9gv7lnle.googlevideo.com",
+	"sn-9gv7lns7.googlevideo.com",
+];
+type D_googlevideo_host_p5qs7n=[
+	"sn-p5qs7n6d.googlevideo.com",
+	"sn-p5qs7nsk.googlevideo.com",
+	"sn-p5qs7nsr.googlevideo.com",
+	"sn-p5qs7nzk.googlevideo.com",
+	"sn-p5qs7nzr.googlevideo.com",
+	"sn-p5qs7nzy.googlevideo.com",
+];
+type D_googlevideo_host_tt1e7n=[
+	"sn-tt1e7n7k.googlevideo.com",
+	"sn-tt1e7nls.googlevideo.com",
+	"sn-tt1e7nlz.googlevideo.com",
+];
+type D_googlevideo_host_q4flrn=[
+	"sn-q4flrn7k.googlevideo.com",
+	"sn-q4flrn7r.googlevideo.com",
+	"sn-q4flrn7y.googlevideo.com",
+	"sn-q4flrne6.googlevideo.com",
+	"sn-q4flrne7.googlevideo.com",
+	"sn-q4flrnee.googlevideo.com",
+	"sn-q4flrnek.googlevideo.com",
+	"sn-q4flrnel.googlevideo.com",
+	"sn-q4flrner.googlevideo.com",
+	"sn-q4flrnes.googlevideo.com",
+	"sn-q4flrney.googlevideo.com",
+	"sn-q4flrnez.googlevideo.com",
+	"sn-q4flrnl6.googlevideo.com",
+	"sn-q4flrnl7.googlevideo.com",
+	"sn-q4flrnld.googlevideo.com",
+	"sn-q4flrnle.googlevideo.com",
+	"sn-q4flrnlz.googlevideo.com",
+	"sn-q4flrnsd.googlevideo.com",
+	"sn-q4flrnsk.googlevideo.com",
+	"sn-q4flrnsl.googlevideo.com",
+	"sn-q4flrnss.googlevideo.com",
+];
+type D_googlevideo_host_nx57yn=[
+	"sn-nx57ynlk.googlevideo.com",
+	"sn-nx57ynsd.googlevideo.com",
+	"sn-nx57ynsl.googlevideo.com",
+	"sn-nx57ynss.googlevideo.com",
+	"sn-nx57ynsz.googlevideo.com",
+];
+type D_googlevideo_host_nx5s7n=[
+	"sn-nx5s7n7d.googlevideo.com",
+	"sn-nx5s7n7s.googlevideo.com",
+	"sn-nx5s7n7z.googlevideo.com",
+	"sn-nx5s7nee.googlevideo.com",
+];
