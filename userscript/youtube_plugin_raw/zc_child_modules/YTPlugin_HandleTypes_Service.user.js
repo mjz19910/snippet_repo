affa3cce75ad2c20f0f8e3dc261983706c4309de
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-14 07:21:18 -0700

    u

--- userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes_Service.user.js
+++ userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes_Service.user.js
@@ -6464,6 +6464,7 @@ class HandleTypes extends HandleTypesEval {
 		let dec=reader.try_read_any();
 		if(!dec) {debugger; return;}
 		if(dec.length===0) debugger;
+		debugger;
 		let bin_obj=this.convert_arr_to_obj(dec);
 		if(!bin_obj) {debugger; return;}
 		/** @type {GR_RootBinaryObj} */
