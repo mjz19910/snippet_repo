00b57fa4a65a933f8d70077c8b0a2770db00129e
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-13 22:04:45 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/D.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/D.ts
@@ -1214,14 +1214,6 @@ type D_ActionRemoveVideoByVideoId={
 	action: "ACTION_REMOVE_VIDEO_BY_VIDEO_ID";
 	removedVideoId: string;
 };
-type D_ActionSetPlaylistVideoOrder={
-	title: string;
-	selected: boolean;
-	continuation?: CD_Reload;
-	serviceEndpoint?: C_Continuation;
-	accessibility?: D_Accessibility;
-	trackingParams: string;
-};
 type D_AdBreakService={
 	prefetchMilliseconds: "10000";
 	getAdBreakUrl: string;
@@ -2519,28 +2511,6 @@ type D_Shelf=Record<"content",G_ShelfItem>&{
 type D_SimpleMenuHeader={
 	title: G_Text;
 	buttons: R_Button[];
-};
-type D_SubMenuItem={
-	title: "Top chat replay";
-	selected: boolean;
-	continuation: D_ReloadContinuationData;
-	accessibility: TD_Accessibility<"Top chat replay">;
-	subtitle: "Some messages, such as potential spam, may not be visible";
-	trackingParams: string;
-};
-
-type D_SortFilterSubMenu={
-	subMenuItems: D_ActionSetPlaylistVideoOrder[];
-	title?: string;
-	icon?: T_Icon<"SORT">;
-	accessibility?: D_Accessibility;
-	tooltip?: string;
-	trackingParams: string;
-}|{
-	subMenuItems: D_SubMenuItem[];
-	accessibility: TD_Accessibility<"Live Chat mode selection">;
-	trackingParams: string;
-	targetId: "live-chat-view-selector-sub-menu";
 };
 type D_StreamSelectionConfig={maxBitrate: `${number};`;};
 type D_StructuredDescriptionContent={items: G_StructuredDescriptionContentItem[];};
