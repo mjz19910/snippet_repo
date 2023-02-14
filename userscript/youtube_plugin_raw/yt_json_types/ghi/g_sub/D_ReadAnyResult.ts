bc5abeae19f733fbf594a8cc870bef9c97dc7915
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-14 07:38:46 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_ReadAnyResult.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_ReadAnyResult.ts
@@ -1,6 +1,10 @@
+type D_ReadAny_CD_0x4c82a9c=[
+	["child",2,Uint8Array,null],
+	["child",3,Uint8Array,null],
+	["child",35,Uint8Array,null],
+];
+type D_ReadAny_CR_0x4c82a9c=["child",0x4c82a9c,Uint8Array,D_ReadAny_CD_0x4c82a9c];
 type D_ReadAnyResult=
-	|[
-		["child",0x4c82a9c,Uint8Array,[{},{},{}]]
-	]
+	|[D_ReadAny_CR_0x4c82a9c]
 	;
 ;
