f0d90bca0c95849e01037cb94cc1298758fee085
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-13 20:49:45 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/lmn_opq/p_sub/P_param.ts
+++ userscript/youtube_plugin_raw/yt_json_types/lmn_opq/p_sub/P_param.ts
@@ -1,5 +1,4 @@
 type P_param_category=P_LogItems extends []? never:P_LogItems[number] extends `[${string}] [${infer U}]`? U:never;
-type P_param_missing=Exclude<P_param_category,P_param_known>;
 type P_param=Extract<P_param_category,`${string}.params.${string}`>;
 type P_param_known_like=`${LP_LogItems_Str}.${P_param_known_like_paths}`;
 type P_param_known_like_paths=[
@@ -21,15 +20,6 @@ type P_param_known_like_paths=[
 type P_param_known_service=
 	|"service$create_playlist.f1"
 	|"service$create_playlist"
-	;
-;
-type P_param_other_known=
-	|`${"entity_key.subscribed"|"entity_key.normal"}.${"f2"|"f4"|"f5"}`
-	|`${"player_state"|"load_markers"|"change_markers_visibility"|"macro_marker_repeat_state"}.entity_key.${"f2"|"f4"|"f5"}`
-	|`D_Browse.param.f110.f1.f${keyof B_BinaryBrowseTab}`
-	|P_param_known_like
-	|P_param_known_service
-	|P_param_tracking
 	;
 ;
 type P_param_tracking<T extends string="tracking">=[
