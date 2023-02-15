446f3a02e2944cfa434a373de7c43d3ff1a569e5
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 15:43:12 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/R_ClickTrackingObj_t1.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/R_ClickTrackingObj_t1.ts
@@ -21,13 +21,13 @@ type R_ClickTrackingObj_t1=R_ClickTrackingObj_t1_v337|{
 	3: T_VW<0>;
 	4: T_VW<V_BinaryTimestamp>;
 	6: T_VW<"endscreen">;
-	9: T_VW<V_Bigint<bigint>>;
+	9: VW_Bigint<bigint>;
 	19: T_VW<V_VeDescObj>;
 }|{
 	1: T_VW<0>;
 	2: T_VW<13187>;
 	4: T_VW<V_BinaryTimestamp>;
 	6: T_VW<"related-auto">;
-	9: T_VW<V_Bigint<bigint>>;
+	9: VW_Bigint<bigint>;
 	19: T_VW<V_VeDescObj>;
 };
