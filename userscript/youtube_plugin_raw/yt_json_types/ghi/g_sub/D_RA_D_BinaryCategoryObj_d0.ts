ba674a629a72eb6dfbb0714a2b7cfb2749b292f8
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 04:16:00 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_D_BinaryCategoryObj_d0.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_D_BinaryCategoryObj_d0.ts
@@ -4,15 +4,20 @@ type D_RA_V_BinaryTimestamp_asFixed=[["data32",1,number],["data_fixed32",2,numbe
 
 type D_RA_D_BinaryCategoryObj_a6=["child",6,Uint8Array,null];
 type D_RA_D_BinaryCategoryObj_a9=["data64",9,number[],bigint];
+type D_RA_D_BinaryCategoryObj_a19=["child",19,Uint8Array,[{},{}]];
+
+type D_RA_D_BinaryObj_a2=["data32",2,13187];
+
 type D_RA_D_BinaryCategoryObj_item=
-	|["data32",2,13187]
+	|D_RA_D_BinaryObj_a2
 	|D_RA_D_BinaryCategoryObj_13_a1
+	|D_RA_D_BinaryCategoryObj_13_a1_ext
 	|D_RA_D_BinaryCategoryObj_23_a2
 	|D_RA_D_BinaryCategoryObj_a3
 	|D_RA_D_BinaryCategoryObj_a6
 	|D_RA_D_BinaryCategoryObj_a9
+	|D_RA_D_BinaryCategoryObj_a19
 	|D_RA_D_BinaryCategoryObj_d0
-	|D_RA_D_BinaryCategoryObj_13_a1_ext
 	;
 ;
 
