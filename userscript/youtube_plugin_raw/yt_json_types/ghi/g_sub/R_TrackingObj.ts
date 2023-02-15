7675d007aea8fca4cfc139c406f6f3be78ce4a40
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 14:55:23 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/R_TrackingObj.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/R_TrackingObj.ts
@@ -7,4 +7,10 @@ type R_TrackingObj={
 	2: T_VW<number>;
 	3: T_VW<0>;
 	4: T_VW<V_BinaryTimestamp>;
+}|{
+	1: T_VW<number>;
+	2: T_VW<number>;
+	3: T_VW<0>;
+	4: T_VW<V_BinaryTimestamp>;
+	8: VW_Bigint<bigint>;
 };
