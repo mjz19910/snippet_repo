4eb0e2c55665956f4cbaff42b99390d5c987d9e6
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-14 19:46:49 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_CD_0x19ac5ceb_f1.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_CD_0x19ac5ceb_f1.ts
@@ -2,6 +2,10 @@ type D_RA_CD_0x19ac5ceb_map_f1=["child",1,Uint8Array,D_RA_CD_0x19ac5ceb_f1_data]
 
 type D_RA_CD_0x19ac5ceb_map_f2=["child",2,Uint8Array,null];
 
-type D_RA_CD_0x19ac5ceb_map_f3=["child",3,[["data32",1,0],["data32",2,1]]];
+type D_RA_CD_0x19ac5ceb_map_f3_d1=["data32",1,0];
+
+type D_RA_CD_0x19ac5ceb_map_f3_d2=["data32",2,1];
+
+type D_RA_CD_0x19ac5ceb_map_f3=["child",3,[D_RA_CD_0x19ac5ceb_map_f3_d1,D_RA_CD_0x19ac5ceb_map_f3_d2]];
 
 type D_RA_CD_0x19ac5ceb_map=D_RA_CD_0x19ac5ceb_map_f1|D_RA_CD_0x19ac5ceb_map_f2|D_RA_CD_0x19ac5ceb_map_f3;
