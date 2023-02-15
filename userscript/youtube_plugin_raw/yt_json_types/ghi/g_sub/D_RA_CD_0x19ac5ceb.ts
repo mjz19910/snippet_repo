55933b250c28aa897c9d70328add676a1918a461
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-14 19:26:45 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_CD_0x19ac5ceb.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_CD_0x19ac5ceb.ts
@@ -1,1 +1,1 @@
-type D_RA_CD_0x19ac5ceb=[["child",1,Uint8Array,(["child",1,Uint8Array,[["child",1006,Uint8Array,[]]]])[]]];
+type D_RA_CD_0x19ac5ceb=[["child",1,Uint8Array,D_RA_CD_0x19ac5ceb_f1_arr]];
---
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_CD_0x19ac5ceb_f1_item.ts
@@ -0,0 +1,1 @@
+type D_RA_CD_0x19ac5ceb_f1_item=["child",1,Uint8Array,D_RA_CD_0x19ac5ceb_f1_item_arr];
