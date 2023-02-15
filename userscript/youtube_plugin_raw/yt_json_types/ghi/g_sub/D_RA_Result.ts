98f7d35cdee2cc78c710a98cedcd42aef3543018
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 09:42:40 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_Result.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_Result.ts
@@ -9,10 +9,3 @@ type D_RA_Result=
 	|D_RA_D_Binary_x1
 	;
 ;
-type R_GetPgdBuyFlow={
-	1: T_VW<{
-		1: ["tAuRQs_d9F8"];
-		2: ["UC_mYaQAE6-71rjSN6CeCA-g"];
-		3: T_VW<1>;
-	}>;
-};
---
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/R_GetPgdBuyFlow.ts
@@ -0,0 +1,7 @@
+type R_GetPgdBuyFlow={
+	1: T_VW<{
+		1: ["tAuRQs_d9F8"];
+		2: ["UC_mYaQAE6-71rjSN6CeCA-g"];
+		3: T_VW<1>;
+	}>;
+};
