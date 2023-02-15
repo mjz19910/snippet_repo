218516ac7c2ff9a83e5bcb8215ff49d4ed5ccebd
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 06:29:48 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/GR_RootBinaryObj.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/GR_RootBinaryObj.ts
@@ -3,6 +3,11 @@ type R_ClickTrackingObj={
 	4: T_VW<V_BinaryTimestamp>;
 	6: "external";
 };
+type R_TrackingObj={
+	1: T_VW<0>;
+	2: T_VW<13188>;
+	4: T_VW<V_BinaryTimestamp>;
+}
 type GR_RootBinaryObj=
 	|R_0x4c82a9c
 	|R_0x12f639cf
