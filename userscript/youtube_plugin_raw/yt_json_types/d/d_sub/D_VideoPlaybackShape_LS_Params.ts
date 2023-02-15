2cde0caedf0efa32ca99b8b626881e12f69df702
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 11:56:09 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/D_VideoPlaybackShape_LS_Params.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/D_VideoPlaybackShape_LS_Params.ts
@@ -1,6 +1,6 @@
 // cSpell:ignoreRegExp /"sn-(?:nx57yn(?:lk|sl)|n4v7sn(?:ls|ly))"/
 type G_VideoHostnames_Gen_0=T_Split<D_googlevideo_host_9gv7ln[number],".">[0];
-type G_VideoHostnames_Gen_1=T_Split<D_googlevideo_host_9gv7ln[number],".">[0];
+type G_VideoHostnames_Gen_1=T_Split<D_googlevideo_host_p5qs7n[number],".">[0];
 type G_VideoHostnames_Gen=G_VideoHostnames_Gen_0|G_VideoHostnames_Gen_1;
 type G_VideoHostnames=|G_VideoHostnames_U|G_VideoHostnames_Gen;
 type G_VideoHostnames_U=
