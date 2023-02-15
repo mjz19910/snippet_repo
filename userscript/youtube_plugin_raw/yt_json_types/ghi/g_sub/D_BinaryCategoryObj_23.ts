8f586675a4ee85cf8b6efb64cfa201007e96c803
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 07:25:35 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_BinaryCategoryObj_23.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_BinaryCategoryObj_23.ts
@@ -1,31 +1,13 @@
-type D_BinaryCategoryObj_23=
-	|{
-		2: TR_Binary_Num_f1<1>;
-		3: "Recently uploaded";
-	}
-	|{
-		2: TR_Binary_Num_f1<3>;
-		3: "New For You";
-	}
-	|{
-		2: TR_Binary_Num_f1<4>;
-		3: "Live";
-	}
-	|{
-		2: TR_Binary_Num_f1<14>;
-		3: "Mixes";
-	}
-	|{
-		2: TR_Binary_Num_f1<15>;
-		3: "Music";
-	}
-	|{
-		2: TR_Binary_Num_f1<31>;
-		3: "Watched";
-	}
-	|{
-		2: TR_Binary_Num_f1<53>;
-		3: "Gaming";
-	}
-	;
-;
+type G_CategoryMap={
+	1: "Recently uploaded";
+	3: "New For You";
+	4: "Live";
+	14: "Mixes";
+	15: "Music";
+	31: "Watched";
+	53: "Gaming";
+};
+type D_BinaryCategoryObj_23={
+	2: TR_Binary_Num_f1<keyof G_CategoryMap>;
+	3: G_CategoryMap[keyof G_CategoryMap];
+};
