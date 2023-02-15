ef65f98a98b3dc5c20f0bcac96916bcb1bec6eed
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 08:53:28 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_Result_CTP.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_Result_CTP.ts
@@ -4,7 +4,7 @@ type D_RA_Result_CTP=
 		["child",6,Uint8Array,null],
 	]
 	|[
-		["data32",1,301],
+		["data32",1,number],
 		["data32",2,number],
 		["child",4,Uint8Array,[]],
 		["child",6,Uint8Array,[]],
