d8f7593597e17c01135edf00d9a05bc05694ff2b
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 12:02:03 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/D_VideoPlaybackShape_LS_Params.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/D_VideoPlaybackShape_LS_Params.ts
@@ -1,7 +1,23 @@
-// cSpell:ignoreRegExp /"sn-(?:nx57yn(?:lk|sl)|n4v7sn(?:ls|ly))"/
-type G_VideoHostnames_Gen_0=T_Split<D_googlevideo_host_9gv7ln[number],".">[0];
-type G_VideoHostnames_Gen_1=T_Split<D_googlevideo_host_p5qs7n[number],".">[0];
-type G_VideoHostnames_Gen=G_VideoHostnames_Gen_0|G_VideoHostnames_Gen_1;
+// cSpell:ignoreRegExp /"sn-(?:9gv7ln.{2})"/
+// cSpell:ignoreRegExp /"sn-(?:n4v7sn.{2})"/
+// cSpell:ignoreRegExp /"sn-(?:nx57yn.{2})"/
+// cSpell:ignoreRegExp /"sn-(?:q4flrn.{2})"/
+// cSpell:ignore googlevideo_host_q4flrn
+type G_VideoHostnames_Gen_0=T_Split<(D_googlevideo_host_9gv7ln|D_googlevideo_host_tt1e7n)[number],".">[0];
+type G_VideoHostnames_Gen_1=T_Split<(D_googlevideo_host_p5qs7n)[number],".">[0];
+type G_VideoHostnames_Gen_2=T_Split<(D_googlevideo_host_nx57yn)[number],".">[0];
+type G_VideoHostnames_Gen_3=T_Split<(D_googlevideo_host_q4flrn)[number],".">[0];
+type G_VideoHostnames_Gen=
+	|"sn-9gv7lnes"|"sn-9gv7lnez"|"sn-9gv7lnle"|"sn-9gv7lns7"
+	|"sn-tt1e7n7k"|"sn-tt1e7nls"|"sn-tt1e7nlz"
+	|"sn-p5qs7n6d"|"sn-p5qs7nsk"|"sn-p5qs7nsr"|"sn-p5qs7nzk"|"sn-p5qs7nzr"|"sn-p5qs7nzy"
+	|"sn-nx57ynlk"|"sn-nx57ynsd"|"sn-nx57ynse"|"sn-nx57ynsk"|"sn-nx57ynsl"|"sn-nx57ynss"|"sn-nx57ynsz"
+	|"sn-q4flrn7k"|"sn-q4flrn7r"|"sn-q4flrn7y"
+	|"sn-q4flrne6"|"sn-q4flrne7"|"sn-q4flrnee"|"sn-q4flrnek"|"sn-q4flrnel"|"sn-q4flrner"|"sn-q4flrnes"|"sn-q4flrney"|"sn-q4flrnez"
+	|"sn-q4flrnl6"|"sn-q4flrnl7"|"sn-q4flrnld"|"sn-q4flrnle"|"sn-q4flrnlz"
+	|"sn-q4flrnsd"|"sn-q4flrnsk"|"sn-q4flrnsl"|"sn-q4flrnss"
+	;
+;
 type G_VideoHostnames=|G_VideoHostnames_U|G_VideoHostnames_Gen;
 type G_VideoHostnames_U=
 	|"sn-n4v7snls"
