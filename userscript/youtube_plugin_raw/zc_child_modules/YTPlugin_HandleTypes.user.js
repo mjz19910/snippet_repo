561c4e2622d4616bd27befc4509b7f034cdc89da
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 05:26:16 -0700

    u

--- userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes.user.js
+++ userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes.user.js
@@ -2371,7 +2371,8 @@ class HandleTypes extends HandleTypesEval {
 		if(typeof obj!=="object") return obj;
 		if(obj instanceof Array) {
 			if(obj.length===1) {
-				return `TYPE::VW<${obj[0]}>`;
+				let inner_str=JSON.stringify(obj[0],this.typedef_json_replace_bin.bind(this,state),"\t");
+				return `TYPE::VW<${inner_str}>`;
 			}
 		}
 		state;
