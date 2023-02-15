9738229a3a84b816adcdd96e065f1345a96082bf
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 06:57:19 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_D_BinaryCategoryObj_item.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_D_BinaryCategoryObj_item.ts
@@ -7,6 +7,7 @@ type D_RD_ObjArr=
 	|D_RD_Obj_a6
 	|["data32",7,4]
 	|D_RD_Obj_a9
+	|["child",10,Uint8Array,[["data32",1,5],["data32",6,1],["data32",11,2]]]
 	|D_RD_Obj_a19
 	|D_RA_D_BinaryCategoryObj_d0
 	|D_RD_Obj_a5
