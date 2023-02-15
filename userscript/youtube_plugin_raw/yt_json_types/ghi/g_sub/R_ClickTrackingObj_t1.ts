985e2b4e816f83a858beefa66e25159588f0f513
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 15:40:48 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/R_ClickTrackingObj_t1.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/R_ClickTrackingObj_t1.ts
@@ -1,20 +1,4 @@
 type R_ClickTrackingObj_t1=R_ClickTrackingObj_t1_v337|{
-	1: T_VW<0>;
-	2: T_VW<0>;
-	4: T_VW<V_BinaryTimestamp>;
-}|{
-	1: T_VW<0>;
-	2: T_VW<0>;
-	3: T_VW<8>;
-	4: T_VW<V_BinaryTimestamp>;
-}|{
-	1: T_VW<0>;
-	2: T_VW<0>;
-	4: T_VW<V_BinaryTimestamp>;
-	6: T_VW<"related-auto">;
-	9: VW_Bigint<bigint>;
-	19: T_VW<V_VeDescObj>;
-}|{
 	1: T_VW<354>;
 	2: T_VW<21965>;
 	3: T_VW<1>;
@@ -27,8 +11,19 @@ type R_ClickTrackingObj_t1=R_ClickTrackingObj_t1_v337|{
 	4: T_VW<V_BinaryTimestamp>;
 	6: T_VW<"rellist">;
 }|{
-	1: T_VW<355>,
-	2: T_VW<5531>,
+	1: T_VW<355>;
+	2: T_VW<5531>;
 	4: T_VW<V_BinaryTimestamp>;
 	6: T_VW<"watch">;
+}|{
+	1: T_VW<77>;
+	2: T_VW<10045>;
+	3: T_VW<0>;
+	4: T_VW<V_BinaryTimestamp>;
+	6: T_VW<"endscreen">;
+	9: T_VW<V_Bigint<bigint>>;
+	19: T_VW<{
+		1: T_VW<2>;
+		2: T_VW<3832>;
+	}>;
 };
