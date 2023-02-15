bbde055f1a3042fbca041fdc44e0832e89d8967e
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 04:05:56 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_D_BinaryCategoryObj_d0.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/D_RA_D_BinaryCategoryObj_d0.ts
@@ -4,6 +4,14 @@ type D_RA_V_BinaryTimestamp_2=[["data32",1,number],["data_fixed32",2,number],["d
 
 type D_RA_D_BinaryCategoryObj_a6=["child",6,Uint8Array,null];
 
-type D_RA_D_BinaryCategoryObj_item=D_RA_D_BinaryCategoryObj_d0|D_RA_D_BinaryCategoryObj_a6|D_RA_D_BinaryCategoryObj_23[0]|D_RA_D_BinaryCategoryObj_13[0]|D_RA_D_BinaryCategoryObj_13[1];
+type D_RA_D_BinaryCategoryObj_item=
+	|D_RA_D_BinaryCategoryObj_13_a1
+	|D_RA_D_BinaryCategoryObj_23_a2
+	|D_RA_D_BinaryCategoryObj_a3
+	|D_RA_D_BinaryCategoryObj_a6
+	|D_RA_D_BinaryCategoryObj_d0
+	;
+;
+
 
 type D_RA_D_BinaryCategoryObj_r=D_RA_D_BinaryCategoryObj_item[];
