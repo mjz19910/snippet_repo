54effb5d4f41c0b959ace168ecd678e4855e43b1
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 17:13:44 -0700

    u

---
+++ browser_js/javascript/final/userscripts/hellrage_fractory_rebalancing.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://hellrage.github.io/fractory-rebalancing/fractory.html
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+// @ts-nocheck
+
+(function() {
+	'use strict';
+	window.Analytics=function(){return "ga"}
+	// Your code here...
+})();
