32a3d6c198d7412d0700d8823fc783cd5cfd57a6
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 20:55:46 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/P.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/P.ts
@@ -42,6 +42,16 @@ type P_tracking_params={
 	1: T_D32<340>;
 	2: T_D32<7099>;
 	4: T_VW_2<V_BinaryTimestamp>;
+}|{
+	1: T_D32<409>;
+	2: T_D32<62457>;
+	3: T_D32<3>;
+	4: T_VW_2<V_BinaryTimestamp>;
+}|{
+	1: T_D32<395>;
+	2: T_D32<62457>;
+	3: T_D32<3>;
+	4: T_VW_2<V_BinaryTimestamp>;
 };
 type P_ad_layout_ad_serving_data_entry={
 	4: T_D32<3>;
@@ -112,60 +122,89 @@ type P_playability_status_context_params={
 type P_entity_key={
 	2: {
 		1: T_VSR<D_VideoId>;
-	},
-	4: T_D32<246>,
+	};
+	4: T_D32<246>;
 	5: T_D32<1>;
 };
 type P_remove_like_params={
 	1: {
 		1: T_VSR<D_VideoId>;
-	},
-	3: T_D32<0>,
+	};
+	3: T_D32<0>;
 	5: {
-		1: T_D32<1676518936>,
+		1: T_D32<1676518936>;
 		2: T_D32<441847181>;
 	};
 };
 type P_dislike_params={
 	1: {
 		1: T_VSR<D_VideoId>;
-	},
-	2: T_D32<0>,
+	};
+	2: T_D32<0>;
 	4: {
-		1: T_D32<1676518936>,
+		1: T_D32<1676518936>;
 		2: T_D32<441858121>;
 	};
 };
 type P_subscribe_button_entity_key={
-	2: T_VSR<`UC${string}`>,
-	4: T_D32<51>,
+	2: T_VSR<`UC${string}`>;
+	4: T_D32<51>;
 	5: T_D32<1>;
 };
 type P_subscribe_params={
 	2: {
 		1: T_D32<3>;
-	},
-	3: T_D32<0>,
+	};
+	3: T_D32<0>;
 	4: T_VSR<D_VideoId>;
 };
 type P_unsubscribe_params={
 	1: {
-		1: T_D32<3>
-	},
-	2: T_VSR<D_VideoId>,
-	3: T_D32<0>
+		1: T_D32<3>;
+	};
+	2: T_VSR<D_VideoId>;
+	3: T_D32<0>;
 };
 type P_continuation_request_watch_next_token={
 	2: {
-		2: T_VSR<"z6EQlZaB7v8">
-	},
-	3: T_D32<6>,
+		2: T_VSR<"z6EQlZaB7v8">;
+	};
+	3: T_D32<6>;
 	6: {
 		4: {
-			4: T_VSR<"z6EQlZaB7v8">,
-			6: T_D32<0>,
-			15: T_D32<2>
-		},
-		8: T_VSR<"comments-section">
-	}
+			4: T_VSR<"z6EQlZaB7v8">;
+			6: T_D32<0>;
+			15: T_D32<2>;
+		};
+		8: T_VSR<"comments-section">;
+	};
+};
+type P_entity_key_normal={
+	2: T_VSR<"RDz6EQlZaB7v8">;
+	4: T_D32<354>;
+	5: T_D32<1>;
+};
+type P_playlist_loop_state_entity_key={
+	2: T_VSR<"RDz6EQlZaB7v8">;
+	4: T_D32<354>;
+	5: T_D32<1>;
+};
+type P_watch_playlist_params={
+	2: T_D32<1>;
+	3: T_D32<1>;
+	7: T_D32<1>;
+	12: T_VSR<"z6EQlZaB7v8">;
+	13: T_D32<0>;
+	27: {
+		1: T_D32<1>;
+	};
+};
+type P_load_markers_entity_key={
+	2: T_VSR<"HEATSEEKER">;
+	4: T_D32<274>;
+	5: T_D32<1>;
+};
+type P_create_backstage_post_params={
+	1: T_VSR<`UC${string}`>;
+	2: T_D32<1>;
 };
