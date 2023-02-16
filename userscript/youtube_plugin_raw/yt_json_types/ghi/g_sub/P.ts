e868afdd5104b45655666b9608d61a99ff97ff2d
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 18:13:48 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/P.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/P.ts
@@ -29,6 +29,11 @@ type P_LikeParams={
 	}>;
 	7: T_VW<1>;
 };
+type P_tracking_params={
+	1: T_VW_3<"data32",0>,
+	2: T_VW_3<"data32",13188>,
+	4: T_VW_2<V_BinaryTimestamp>
+}
 type P_dislike_params={
 	1: T_VW<{
 		1: T_VW<"gKyQXCkdZl0">;
--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_VW.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_VW.ts
@@ -1,2 +1,3 @@
 type T_VW<T>=[T];
 type T_VW_2<T>=["child",Uint8Array,T];
+type T_VW_3<Type,T>=[Type,T];
