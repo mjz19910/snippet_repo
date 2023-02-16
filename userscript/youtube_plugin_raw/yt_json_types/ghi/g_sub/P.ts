aeb0b808bcc4a79abeae24664aa2922afe76b98c
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 19:27:57 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/P.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/P.ts
@@ -1,7 +1,7 @@
 type V_BinaryTimestamp={
 	1: T_D32<number>;
-	2: T_VW_3<"data_fixed32",number>;
-	3: T_VW_3<"data_fixed32",number>;
+	2: T_FD32<number>;
+	3: T_FD32<number>;
 };
 type R_ClickTrackingObj=
 	|R_ClickTrackingObj_t1
@@ -24,16 +24,20 @@ type P_tracking_params={
 	3: T_D32<2>;
 	4: T_VW_2<V_BinaryTimestamp>;
 }|{
-	1: T_D32<521>,
-	2: T_D32<6236>,
-	4: T_VW_2<V_BinaryTimestamp>,
+	1: T_D32<521>;
+	2: T_D32<6236>;
+	4: T_VW_2<V_BinaryTimestamp>;
 	8: T_VW_Bigint<12391810279397228368n>;
 }|{
-	1: T_D32<395>,
-	2: T_D32<6236>,
-	4: T_VW_2<V_BinaryTimestamp>,
-	8: T_VW_Bigint<12391810279397228368n>
-};;
+	1: T_D32<395>;
+	2: T_D32<6236>;
+	4: T_VW_2<V_BinaryTimestamp>;
+	8: T_VW_Bigint<12391810279397228368n>;
+}|{
+	1: T_D32<340>;
+	2: T_D32<7099>;
+	4: T_VW_2<V_BinaryTimestamp>;
+};
 type P_ad_layout_ad_serving_data_entry={
 	4: T_D32<3>;
 	5: T_D32<2>;
@@ -44,7 +48,7 @@ type P_ad_layout_ad_serving_data_entry={
 		1: T_D32<5>;
 		6: T_D32<1>;
 		11: T_D32<2>;
-	},
+	};
 	13: T_D32<1>;
 	14: T_D32<37>;
 };
@@ -55,7 +59,7 @@ type P_ad_slot_logging_data_serialized_slot_ad_serving_data_entry={
 		1: T_D32<5>;
 		6: T_D32<1>;
 		11: T_D32<2>;
-	},
+	};
 	4: T_D32<2>;
 };
 type P_service$create_playlist={1: T_D32<4>;};
