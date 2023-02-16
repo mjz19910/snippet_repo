217347f0345430c68b3c951a58f1522cdd8dc229
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-15 18:38:25 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/P.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/P.ts
@@ -24,60 +24,60 @@ type P_LikeParams={
 	4: T_VW<0>;
 	5: T_VW<1>;
 	6: T_VW<{
-		1: T_VW<number>,
+		1: T_VW<number>;
 		2: T_VW<number>;
 	}>;
 	7: T_VW<1>;
 };
 type P_tracking_params={
-	1: T_VW_3<"data32",0>,
-	2: T_VW_3<"data32",13188>,
-	4: T_VW_2<V_BinaryTimestamp>
-}
+	1: T_VW_3<"data32",0>;
+	2: T_VW_3<"data32",13188>;
+	4: T_VW_2<V_BinaryTimestamp>;
+};
 type P_dislike_params={
 	1: T_VW<{
 		1: T_VW<"gKyQXCkdZl0">;
-	}>,
-	2: T_VW<0>,
-	3: T_VW<1>,
+	}>;
+	2: T_VW<0>;
+	3: T_VW<1>;
 	4: T_VW<{
-		1: T_VW<number>,
+		1: T_VW<number>;
 		2: T_VW<number>;
-	}>,
+	}>;
 	5: T_VW<1>;
 };
 type P_remove_like_params={
 	1: T_VW<{
 		1: T_VW<"gKyQXCkdZl0">;
-	}>,
-	3: T_VW<0>,
-	4: T_VW<1>,
+	}>;
+	3: T_VW<0>;
+	4: T_VW<1>;
 	5: T_VW<{
-		1: T_VW<number>,
+		1: T_VW<number>;
 		2: T_VW<number>;
-	}>,
+	}>;
 	6: T_VW<1>;
 };
 type P_subscribe_params={
 	2: T_VW<{
 		1: T_VW<12>;
-	}>,
+	}>;
 	3: T_VW<0>;
 };
 type P_unsubscribe_params={
 	1: T_VW<{
 		1: T_VW<12>;
-	}>,
+	}>;
 	3: T_VW<0>;
 };
 type P_continuation_request_browse_token={
 	80226972: T_VW<{
-		2: T_VW<"FEcomment_shorts_web_top_level">,
+		2: T_VW<"FEcomment_shorts_web_top_level">;
 		3: T_VW<string>;
 	}>;
 };
 type P_playability_status_context_params={
-	1: T_VW<1>,
+	1: T_VW<1>;
 	2: T_VW<{
 		1: T_VW<2>;
 	}>;
@@ -85,15 +85,15 @@ type P_playability_status_context_params={
 type P_entity_key_normal={
 	2: T_VW<{
 		1: T_VW<"gKyQXCkdZl0">;
-	}>,
-	4: T_VW<246>,
+	}>;
+	4: T_VW<246>;
 	5: T_VW<1>;
 };
 type P_entity_key={
 	2: T_VW<{
 		1: T_VW<"gKyQXCkdZl0">;
-	}>,
-	4: T_VW<246>,
+	}>;
+	4: T_VW<246>;
 	5: T_VW<1>;
 };
 type reel_watch_sequence_a15_a6={
@@ -158,27 +158,23 @@ type P_reel_watch_sequence_a3={
 type P_reel_watch_sequence_a5={3: T_VW<12>;};
 
 type P_continuation_request_reel_watch_sequence_token={
-	1: T_VW<"gKyQXCkdZl0">,
-	3: T_VW<P_reel_watch_sequence_a3>,
-	5: T_VW<P_reel_watch_sequence_a5>,
+	1: T_VW<"gKyQXCkdZl0">;
+	3: T_VW<P_reel_watch_sequence_a3>;
+	5: T_VW<P_reel_watch_sequence_a5>;
 	15: T_VW<P_reel_watch_sequence_a15>;
 };
 
 type P_ad_layout_ad_serving_data_entry={
-	4: T_VW<3>,
-	5: T_VW<2>,
-	6: T_VW<8>,
-	7: T_VW<4>,
-	9: T_VW<{
-		1: T_VW<1676502518345713>,
-		2: T_VW<982901>,
-		3: T_VW<3893336635>;
-	}>,
-	10: T_VW<{
-		1: T_VW<5>,
-		6: T_VW<1>,
-		11: T_VW<2>;
-	}>,
-	13: T_VW<1>,
-	14: T_VW<37>;
+	4: T_VW_3<"data32",3>;
+	5: T_VW_3<"data32",2>;
+	6: T_VW_3<"data32",8>;
+	7: T_VW_3<"data32",4>;
+	9: T_VW_2<V_BinaryTimestamp>;
+	10: {
+		1: T_VW_3<"data32",5>;
+		6: T_VW_3<"data32",1>;
+		11: T_VW_3<"data32",2>;
+	},
+	13: T_VW_3<"data32",1>;
+	14: T_VW_3<"data32",37>;
 };
