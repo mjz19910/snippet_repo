19d03beb0acd659bd851ebf12e8542c7d84015e2
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 13:08:24 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/Ret_get_gv_parts.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/Ret_get_gv_parts.ts
@@ -10,7 +10,6 @@ type G_Gv_0=
 	|"p5qs7"
 	;
 ;
-`9gv7l,a5mek,hp57k,n4v7s,nx5s7,nx57y,o097z,p5qls,p5qs7,`;
 type PT_TypeArr=[
 	["p5qs7",["zk","zy","sk","sr","6d"]],
 	["9gv7l",["es","ez","le","s7"]],
@@ -22,6 +21,18 @@ type PT_TypeArr=[
 	["o097z",["sd","se","sk","sl","sr","ss","sz","z7","zd","ze","zk","zr"]],
 	["p5qls",["6l","7d","7l","7s","76","d6","dd","dk","dr","dz","rl","rr","y6"]],
 	["p5qs7",["6d","sk","sr","zk","zr","zy"]],
+	// 
+];
+type G_Gv_1_List=[
+	"6d","6k","6l","6r","6s","6y","6z",
+	"7d","7l","7s","7s","7y","7z","76",
+	"d6","dd","de","dk","dl","dr","ds","ds","dy","dz",
+	"ee","el","es","ey","ez",
+	"k7","l7","le","lk","ll","lr","ls","ly",
+	"rl","rr",
+	"s7","sd","se","sk","sl","sr","ss","sy","sz",
+	"y6","z7",
+	"zd","ze","zk","zl","zr","zs","zy",
 ];
 // end ["p5qs7","zy"],
 type G_Gv_1="lk"|`s${"d"|"e"|"k"|"l"|"s"|"z"}`|`7${"6"|"d"|"s"|"y"|"z"}`|`e${"l"|"e"}`;
