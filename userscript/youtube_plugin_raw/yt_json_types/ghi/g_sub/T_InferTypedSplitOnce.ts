9208068e7e86f269e454ea640787a3d0b623b89c
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 08:17:54 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_InferTypedSplitOnce.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_InferTypedSplitOnce.ts
@@ -18,8 +18,9 @@ type TI_SplitOnce_3<WA extends string,S extends string,D extends string,Split ex
 	Split[0] extends ""? TI_SplitOnce_NB_2<WA,S,D>:TI_SplitOnce_2<WA,Split>;
 ;
 // S extends `${infer Begin}${D}`? TI_SplitOnce_NB_1<WA,Begin>:S extends `${D}${WA}${infer Rest}`?["",`${WA}${Rest}`]:[S]
-type TI_SplitOnce_NE<WA extends string,S extends string,D extends string>=S extends `${infer Begin}${D}`? TI_SplitOnce_NB_1<WA,Begin>:S extends `${D}${WA}${infer Rest}`? ["",`${WA}${Rest}`]:[S];
+type TI_SplitOnce_NE<WA extends string,S extends string,D extends string>=S extends `${infer Begin}${D}`? TI_SplitOnce_NB_1<WA,Begin>:TI_SplitOnce_NE_1<WA,S,D>;
+type TI_SplitOnce_NE_1<WA extends string,S extends string,D extends string>=S extends `${D}${WA}${infer Rest}`? ["",`${WA}${Rest}`]:[S];
 type TI_SplitOnce_2<WA extends string,Split extends [string,string]>=Split[1] extends WA? [Split[0],WA]:never;
 type TI_SplitOnce_NB_1<WA extends string,Begin extends string>=Begin extends WA? [WA,""]:never;
-type TI_SplitOnce_NB_2<WA extends string,S extends string,D extends string>=S extends `${D}${infer Rest}`? TI_SplitOnce_NR_2<WA,Rest>:[S];
+type TI_SplitOnce_NB_2<WA extends string,S extends string,D extends string>=S extends `${D}${infer Rest}`? Rest extends `${WA}${infer Rest2}`? ["",`${WA}${Rest2}`]:never:[S];
 type TI_SplitOnce_NR_2<WA extends string,Rest extends string>=Rest extends `${WA}${infer Rest2}`? ["",`${WA}${Rest2}`]:never;
