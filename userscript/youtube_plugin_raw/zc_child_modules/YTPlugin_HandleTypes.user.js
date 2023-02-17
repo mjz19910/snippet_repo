31fe2fa28331b00e9943e5e8c2c09b9607e43492
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-16 20:24:40 -0700

    u

--- userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes.user.js
+++ userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes.user.js
@@ -2211,6 +2211,14 @@ class HandleTypes extends ServiceMethods {
 			}
 			return;
 		}
+		if(3 in x) {
+			const {1: [,f1],2: [,f2],3: [,f3],4: [,,f4],9: f9,...y}=this.s(cf,x);
+			this.save_number(`${cf}.w3.f1`,f1);
+			this.save_number(`${cf}.w3.f3`,f3);
+			this.a_primitive_num(f2);
+			this.V_BinaryTimestamp(f4);
+			return;
+		}
 		x;
 		debugger;
 	}
