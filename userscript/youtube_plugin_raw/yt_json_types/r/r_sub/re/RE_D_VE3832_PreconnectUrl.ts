a3797b59f47dc704f3fbb24aaaa76c6b65b2cd4c
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 05:51:57 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/r/r_sub/re/RE_D_VE3832_PreconnectUrl.ts
+++ userscript/youtube_plugin_raw/yt_json_types/r/r_sub/re/RE_D_VE3832_PreconnectUrl.ts
@@ -1,18 +1,4 @@
-type RE_D_VE3832_PreconnectUrl=
-	|"https://rr1---sn-nx5s7n7s.googlevideo.com/generate_204"
-	|"https://rr1---sn-nx57ynsl.googlevideo.com/generate_204"
-	|"https://rr2---sn-nx5s7n7z.googlevideo.com/generate_204"
-	|"https://rr2---sn-nx5s7n76.googlevideo.com/generate_204"
-	|"https://rr2---sn-nx57ynsz.googlevideo.com/generate_204"
-	|"https://rr3---sn-nx5s7n7d.googlevideo.com/generate_204"
-	|"https://rr3---sn-nx5s7nee.googlevideo.com/generate_204"
-	|"https://rr3---sn-nx57ynlk.googlevideo.com/generate_204"
-	|"https://rr3---sn-nx57ynss.googlevideo.com/generate_204"
-	|"https://rr4---sn-nx5s7nel.googlevideo.com/generate_204"
-	|"https://rr4---sn-nx57ynsd.googlevideo.com/generate_204"
-	|"https://rr4---sn-nx57ynse.googlevideo.com/generate_204"
-	|"https://rr5---sn-nx5s7n7y.googlevideo.com/generate_204"
-	|"https://rr5---sn-nx57ynsk.googlevideo.com/generate_204"
+type RE_D_VE3832_PreconnectUrl=`https://rr${number}---sn-${string}n${string}.googlevideo.com/generate_204`;
 	;
 ;
-type RE_D_GoogleVideoUrl_Hostname=UrlParse<RE_D_VE3832_PreconnectUrl|Extract<D_UrlFormat,`https://${string}.googlevideo.com/${string}`>>["host"];
+type RE_D_GoogleVideoUrl_Hostname=UrlParse<RE_D_VE3832_PreconnectUrl|Extract<D_UrlFormat,`https://rr${number}---sn-${string}n${string}.googlevideo.com/${string}`>>["host"];
