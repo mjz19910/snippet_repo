e323f522163f3d1a4cc2496f0c417e83339b612a
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 11:26:35 -0700

    u

--- userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_IndexedDB.user.js
+++ userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_IndexedDB.user.js
@@ -126,6 +126,9 @@ class IndexedDBService extends BaseService {
 		}
 		this.database_opening=true;
 		let db=await this.get_async_result(indexedDB.open("yt_plugin",version));
+		db.onclose=() => {
+			this.database_open=false;
+		};
 		this.database_opening=false;
 		this.database_open=true;
 		const tx=this.transaction(db,key,"readwrite");
