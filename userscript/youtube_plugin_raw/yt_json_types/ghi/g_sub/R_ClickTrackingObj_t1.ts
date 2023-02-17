8337e83f5e28c713a33f838db4f160e45133df74
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-16 21:09:00 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/R_ClickTrackingObj_t1.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/R_ClickTrackingObj_t1.ts
@@ -26,13 +26,29 @@ type RB_ClickTrackingObj_w9={
 	4: T_VW_2<V_BinaryTimestamp>;
 	9: T_VW_Bigint<bigint>;
 };
-type RB_ClickTrackingObj_w19={
+type RB_ClickTrackingObj_w19w11={
 	1: T_D32<416>;
 	2: T_D32<6236>;
 	4: T_VW_2<V_BinaryTimestamp>;
-	6: T_VW_2<{5: T_FD32<number>;}>;
+	6: T_VW_2<{
+		5: T_FD32<number>;
+	}>;
 	11: T_VSR<"FEwhat_to_watch">;
-	19: T_VW_2<{2: T_D32<3854>; 3: T_D32<158>;}>;
+	19: T_VW_2<{
+		2: T_D32<3854>;
+		3: T_D32<158>;
+	}>;
+};
+type RB_ClickTrackingObj_w19={
+	1: T_D32<0>,
+	2: T_D32<13187>,
+	4: T_VW_2<V_BinaryTimestamp>,
+	6: T_VSR<"related-auto">,
+	9: T_VW_Bigint<2846068225792461286n>,
+	19: {
+		1: T_D32<3>,
+		2: T_D32<3832>
+	}
 };
 type RB_ClickTrackingObj=
 	|RB_ClickTrackingObj_w1w2w4
@@ -41,6 +57,7 @@ type RB_ClickTrackingObj=
 	|RB_ClickTrackingObj_w1w4
 	|RB_ClickTrackingObj_w4w6
 	|RB_ClickTrackingObj_w9
+	|RB_ClickTrackingObj_w19w11
 	|RB_ClickTrackingObj_w19
 	;
 ;
