607810561fc5f3137a90d4bfd203817253f5b971
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 15:26:13 -0700

    u

--- ze_zsh_scripts/dig.zsh
+++ ze_zsh_scripts/dig.zsh
@@ -1,6 +1,6 @@
 function run {
 	echo /tmp/dig_res.* | xargs -n 1 bash -c 'echo -n > $1' v
-	echo rr1.sn-{a,h,n,o,p}{g,p}{v,m}{e,s,l}{l,y,z}n{es,6d,dd,ee,lk}.googlevideo.com | stdbuf -i0 -o0 -e0 xargs -n 100 -P 25 zsh -c '. ./dig.zsh child "$@" >> $TF'
+	echo rr1.sn-{a,h,n,o,p}{g,p}{v,m}{e,s,l}{l,y,z}n{es,6d,dd,ee,lk}.googlevideo.com | stdbuf -i0 -o0 -e0 xargs -n 100 -P 25 zsh -c '. ./dig.zsh child "$@"'
 	eval 'cat /tmp/dig_res.*'
 }
 function run_child {
