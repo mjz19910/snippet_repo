6c60c6b170a8b2a2be1bf99eb6dcaed91cda0d54
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 05:19:15 -0700

    u

--- userscript/youtube_plugin_raw/zc_child_modules/YtPlugin_ServiceLoader_Plugin.user.js
+++ userscript/youtube_plugin_raw/zc_child_modules/YtPlugin_ServiceLoader_Plugin.user.js
@@ -28,7 +28,7 @@ class ServiceLoader {
 	constructor(x) {
 		class HT_Caller extends HandleTypes {
 			/** @public @arg {YTNavigateFinishDetail} detail */
-			run(detail) {this.x.get("handle_types").x_EventInput.YTNavigateFinishDetail(detail);}
+			run(detail) {this.x.get("x_EventInput").YTNavigateFinishDetail(detail);}
 		}
 		class RT_Caller extends HandleTypes {
 			/** @public @arg {Response} response @arg {G_ResponseTypes} x */
