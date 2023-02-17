994d4c5aca686c207d6f7ef37b4aa5ca1ee4164e
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 07:54:03 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_InferTypedSplitOnce.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_InferTypedSplitOnce.ts
@@ -1,6 +1,9 @@
 // TI_SplitOnce<T,D>
 // @template {string} WA @template {string} S @template {string} D 
-type TI_SplitOnce<WA extends string,S extends string,D extends string>=S extends `${infer Begin}${D}${infer Rest}`? TI_SplitOnce_1<WA,S,D,Begin,Rest>:[S];
+type TI_SplitOnce<WA extends string,S extends string,D extends string>=
+	S extends `${infer Begin}${D}${infer Rest}`? TI_SplitOnce_1<WA,S,D,Begin,Rest>:[S]
+	;
+;
 type TI_SplitOnce_1<WA extends string,S extends string,D extends string,Begin extends string,Rest extends string>=
 	Rest extends ""
 	? TI_SplitOnce_NB_1<WA,Begin>
