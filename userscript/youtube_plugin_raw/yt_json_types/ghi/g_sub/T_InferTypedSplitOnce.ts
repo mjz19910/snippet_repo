b6c087b7298cc2add6882df022b11436a8603551
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 07:15:37 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_InferTypedSplitOnce.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_InferTypedSplitOnce.ts
@@ -7,14 +7,14 @@ type T_InferTypedSplitOnce<WA extends string,S extends string,D extends string>=
 	:Begin extends `${WA}`
 	? [WA,Rest]
 	:Begin extends ""
-	? T_InferTypedSplitOnce_NoBegin<WA,S,D>
+	? T_InferredSplitOnce_NoBegin<WA,S,D>
 	:Rest extends `${WA}`
 	? [Begin,WA]
 	:never
 	:[S]
 	;
 ;
-type T_InferTypedSplitOnce_NoBegin<WA extends string,S extends string,D extends string>=
+type T_InferredSplitOnce_NoBegin<WA extends string,S extends string,D extends string>=
 	S extends `${D}${infer Rest}`
 	? T_InferTypedSplitOnce_NB<WA,Rest>
 	:[S]
