4b716d9cc5d89ea95a82038fb12c018a398877e9
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 07:52:43 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_InferTypedSplitOnce.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_InferTypedSplitOnce.ts
@@ -3,11 +3,11 @@
 type T_InferTypedSplitOnce<WA extends string,S extends string,D extends string>=S extends `${infer Begin}${D}${infer Rest}`? T_InferTypedSplitOnce_1<WA,S,D,Begin,Rest>:[S];
 type T_InferTypedSplitOnce_1<WA extends string,S extends string,D extends string,Begin extends string,Rest extends string>=Rest extends ""
 	? T_SplitOnce_NB_1<WA,Begin>
-	:Begin extends `${WA}`
+	:Begin extends WA
 	? [WA,Rest]
 	:Begin extends ""
 	? T_SplitOnce_NB_2<WA,S,D>
-	:Rest extends `${WA}`
+	:Rest extends WA
 	? [Begin,WA]
 	:never;
 function never_return(): never {throw new Error();}
