8aefcaa78dde41084b9bae09eba0eafd3682cf03
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 00:34:58 -0700

    u

--- userscript/youtube_plugin_raw/support_1/AsyncPluginEventDetail.ts
+++ userscript/youtube_plugin_raw/support_1/AsyncPluginEventDetail.ts
@@ -1,1 +1,1 @@
-type AsyncPluginEventDetail={handle_types: typeof import("../zc_child_modules/YTPlugin_ServiceLoader.user.js").ServiceLoader["prototype"]["handle_types"];};
+type AsyncPluginEventDetail={handle_types: typeof import("../zc_child_modules/YtPlugin_ServiceLoader_Plugin.user.js").ServiceLoader["prototype"]["handle_types"];};
--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/GA_ResponseReceived.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/GA_ResponseReceived.ts
@@ -2,6 +2,6 @@ type GA_ResponseReceived=
 	|A_AppendContinuationItems
 	|C_AdsControlFlowOpportunityReceived
 	|C_ReloadContinuationItems
-	|RS_Channel["onResponseReceivedActions"][number]
+	|C_ResetChannelUnreadCount
 	;
 ;
