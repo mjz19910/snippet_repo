4f401f0434681f7a8c10dca4d0748f4ddacf3ac4
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 07:14:48 -0700

    i

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_InferTypedSplitOnce.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_InferTypedSplitOnce.ts
@@ -2,7 +2,9 @@
 // @template {string} WA @template {string} S @template {string} D 
 type T_InferTypedSplitOnce<WA extends string,S extends string,D extends string>=
 	S extends `${infer Begin}${D}${infer Rest}`
-	? Begin extends `${WA}`
+	? Rest extends ""
+	? T_InferTypedSplitOnceLast<WA,Begin>
+	:Begin extends `${WA}`
 	? [WA,Rest]
 	:Begin extends ""
 	? T_InferTypedSplitOnce_NoBegin<WA,S,D>
@@ -13,15 +15,14 @@ type T_InferTypedSplitOnce<WA extends string,S extends string,D extends string>=
 	;
 ;
 type T_InferTypedSplitOnce_NoBegin<WA extends string,S extends string,D extends string>=
-	S extends `${D}${infer U}`
-	? U extends `${WA}${infer A}`
-	? ["",`${WA}${A}`]
-	:never
+	S extends `${D}${infer Rest}`
+	? T_InferTypedSplitOnce_NB<WA,Rest>
 	:[S]
 	;
 ;
-type T_InferTypedSplitOnceLast<WA extends string,S extends string,D extends string>=
-	S extends `${infer U}${D}`
-	? U extends WA? [WA,""]
-	:never
-	:T_InferTypedSplitOnce_NoBegin<WA,S,D>;
+type T_InferTypedSplitOnce_NB_1<WA extends string,Begin extends string,Rest extends string>=
+	Rest extends `${WA}`
+	? [Begin,WA]
+	:never;
+type T_InferTypedSplitOnce_NB<WA extends string,Rest extends string>=Rest extends `${WA}${infer A}`? ["",`${WA}${A}`]:never;
+type T_InferTypedSplitOnceLast<WA extends string,Begin extends string>=Begin extends WA? [WA,""]:never;
