fe679405be797ca7565ccd417ac52b388b29d3a2
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-16 20:52:08 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/R_ClickTrackingObj_t1.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/R_ClickTrackingObj_t1.ts
@@ -1,24 +1,31 @@
-type RB_Obj_f19={
+type RB_Obj_f19_w1={
 	1: T_D32<16>;
 	2: T_D32<3832>;
-}|{
+};
+
+type RB_Obj_f19_w3={
 	2: T_D32<3854>;
 	3: T_D32<158>;
 };
 
-type R_ClickTrackingObj_t1_v0_v13187={
+type RB_Obj_f19=RB_Obj_f19_w1|RB_Obj_f19_w3;
+type RB_ClickTrackingObj_w_all={
 	1: T_D32<0>;
-	2: T_D32<13187>;
+	2: T_D32<13188>;
 	4: T_VW_2<V_BinaryTimestamp>;
-}|{
-	1: T_D32<0>;
-	2: T_D32<13187>;
-	4: T_VW_2<V_BinaryTimestamp>;
-	6: T_VSR<"autoplay">;
-	9: T_VW_Bigint<14961257671762767615n>;
-	19: T_VW_2<RB_Obj_f19>;
 };
-
+type R_ClickTrackingObj_t3={
+	1: T_D32<242>;
+	2: T_D32<119920>;
+	3: T_D32<0>;
+	4: T_VW_2<V_BinaryTimestamp>;
+};
+type R_ClickTrackingObj_t6={
+	1: T_D32<411>;
+	2: T_D32<5531>;
+	4: T_VW_2<V_BinaryTimestamp>;
+	6: T_VSR<"watch">;
+};
 type R_ClickTrackingObj_t9={
 	1: T_D32<370>;
 	2: T_D32<21965>;
@@ -26,55 +33,21 @@ type R_ClickTrackingObj_t9={
 	4: T_VW_2<V_BinaryTimestamp>;
 	9: T_VW_Bigint<bigint>;
 };
-
-type R_ClickTrackingObj_t4={
-	1: T_D32<242>;
-	2: T_D32<119920>;
-	3: T_D32<0>;
-	4: T_VW_2<V_BinaryTimestamp>;
-};
-
-type R_ClickTrackingObj_t6={
-	1: T_D32<411>;
-	2: T_D32<5531>;
-	4: T_VW_2<V_BinaryTimestamp>;
-	6: T_VSR<"watch">;
-};
-
-// R_ClickTrackingObj_t1_v337
-type RB_ClickTrackingObj_t1={
-	1: T_D32<0>;
-	2: T_D32<13188>;
-	4: T_VW_2<V_BinaryTimestamp>;
-}|{
-	1: T_D32<512>;
-	2: T_D32<62457>;
-	3: T_D32<1>;
-	4: T_VW_2<V_BinaryTimestamp>;
-}|{
-	1: T_D32<501>;
-	2: T_D32<79431>;
-	3: T_D32<2>;
-	4: T_VW_2<V_BinaryTimestamp>;
-}|{
+type RB_ClickTrackingObj_t3={
 	1: T_D32<416>;
 	2: T_D32<76764>;
 	3: T_D32<26>;
 	4: T_VW_2<V_BinaryTimestamp>;
-}|{
+};
+type RB_ClickTrackingObj_t19={
 	1: T_D32<416>;
 	2: T_D32<6236>;
 	4: T_VW_2<V_BinaryTimestamp>;
-	6: T_VW_2<{
-		5: T_FD32<1516463474>;
-	}>;
+	6: T_VW_2<{5: T_FD32<number>;}>;
 	11: T_VSR<"FEwhat_to_watch">;
 	19: T_VW_2<{
 		2: T_D32<3854>;
 		3: T_D32<158>;
 	}>;
-}|R_ClickTrackingObj_t4|{
-	1: T_D32<238>;
-	2: T_D32<119921>;
-	4: T_VW_2<V_BinaryTimestamp>;
-}|R_ClickTrackingObj_t6|R_ClickTrackingObj_t9|R_ClickTrackingObj_t1_v0_v13187;
+};
+type RB_ClickTrackingObj_t1=RB_ClickTrackingObj_w_all|RB_ClickTrackingObj_t3|RB_ClickTrackingObj_t19|R_ClickTrackingObj_t3|R_ClickTracking_v238|R_ClickTrackingObj_t6|R_ClickTrackingObj_t9|R_ClickTrackingObj_t1_u1_v0_u2_v13187;
