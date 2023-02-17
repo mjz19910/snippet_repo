64c38fb9bee17e7deb5a46d7ffc569007776ea41
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 13:02:29 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/Ret_get_gv_parts.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/Ret_get_gv_parts.ts
@@ -12,8 +12,16 @@ type G_Gv_0=
 ;
 `9gv7l,a5mek,hp57k,n4v7s,nx5s7,nx57y,o097z,p5qls,p5qs7,`;
 type PT_TypeArr=[
-	"sn-p5qs7nzk",["p5qs7",["zk","zy","sk","sr","6d"]],
-	["9gv7l","es"],["9gv7l",["es","ez","le","s7"]],
+	["p5qs7",["zk","zy","sk","sr","6d"]],
+	["9gv7l",["es","ez","le","s7"]],
+	["a5mek",["6d","6k","6l","6r","6s","6z","d6","de","dl","ds","dz","sd","sy","zk","zl","zr","zs"]],
+	["hp57k",["6r","6y","d6","dd","dk","dr","ds","dy","dz","k7"]],
+	["n4v7s",["ee","ey","l7","ll","lr","ls","ly","s7","se"]],
+	["nx5s7",["7d","7s","7y","7z","76","ee","el"]],
+	["nx57y",["lk","sd","se","sk","sl","ss","sz"]],
+	["o097z",["sd","se","sk","sl","sr","ss","sz","z7","zd","ze","zk","zr"]],
+	["p5qls",["6l","7d","7l","7s","76","d6","dd","dk","dr","dz","rl","rr","y6"]],
+	["p5qs7",["6d","sk","sr","zk","zr","zy"]],
 ];
 // end ["p5qs7","zy"],
 type G_Gv_1="lk"|`s${"d"|"e"|"k"|"l"|"s"|"z"}`|`7${"6"|"d"|"s"|"y"|"z"}`|`e${"l"|"e"}`;
