ee88b8794243c4b656aa270a620a2862bc52a13d
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 12:48:22 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/Ret_get_gv_parts.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/Ret_get_gv_parts.ts
@@ -10,9 +10,12 @@ type G_Gv_0=
 	|"p5qs7"
 	;
 ;
+`9gv7l,a5mek,hp57k,n4v7s,nx5s7,nx57y,o097z,p5qls,p5qs7,`;
 type PT_TypeArr=[
 	"sn-p5qs7nzk",["p5qs7",["zk","zy","sk","sr","6d"]],
+	["9gv7l","es"],["9gv7l",["es","ez","le","s7"]],
 ];
+// end ["p5qs7","zy"],
 type G_Gv_1="lk"|`s${"d"|"e"|"k"|"l"|"s"|"z"}`|`7${"6"|"d"|"s"|"y"|"z"}`|`e${"l"|"e"}`;
 type D_GoogleVideoHostPartition={
 	partition: G_Gv_0,
