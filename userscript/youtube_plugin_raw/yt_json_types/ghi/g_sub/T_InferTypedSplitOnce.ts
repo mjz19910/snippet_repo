d31dc77ff9fe1d890087208106f82b4487f3352f
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 07:53:44 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_InferTypedSplitOnce.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/T_InferTypedSplitOnce.ts
@@ -1,12 +1,12 @@
-// T_SplitOnce<T,D>
+// TI_SplitOnce<T,D>
 // @template {string} WA @template {string} S @template {string} D 
-type T_InferTypedSplitOnce<WA extends string,S extends string,D extends string>=S extends `${infer Begin}${D}${infer Rest}`? T_InferTypedSplitOnce_1<WA,S,D,Begin,Rest>:[S];
-type T_InferTypedSplitOnce_1<WA extends string,S extends string,D extends string,Begin extends string,Rest extends string>=Rest extends ""
-	? T_SplitOnce_NB_1<WA,Begin>
+type TI_SplitOnce<WA extends string,S extends string,D extends string>=S extends `${infer Begin}${D}${infer Rest}`? TI_SplitOnce_1<WA,S,D,Begin,Rest>:[S];
+type TI_SplitOnce_1<WA extends string,S extends string,D extends string,Begin extends string,Rest extends string>=Rest extends ""
+	? TI_SplitOnce_NB_1<WA,Begin>
 	:Begin extends WA
 	? [WA,Rest]
 	:Begin extends ""
-	? T_SplitOnce_NB_2<WA,S,D>
+	? TI_SplitOnce_NB_2<WA,S,D>
 	:Rest extends WA
 	? [Begin,WA]
 	:never;
@@ -60,6 +60,6 @@ function TF_InferTypedSplitOnce<WA extends string,S extends string,D extends str
 function T_SplitOnce_NB_1(WA: string,Begin: string) {WA; Begin;}
 function TF_InferredSplitOnce_NB_2(WA: string,S: string,D: string) {WA; S; D;}
 type T_SplitOnce_NR_1<WA extends string,Begin extends string,Rest extends string>=Rest extends WA? [Begin,WA]:never;
-type T_SplitOnce_NB_1<WA extends string,Begin extends string>=Begin extends WA? [WA,""]:never;
-type T_SplitOnce_NB_2<WA extends string,S extends string,D extends string>=S extends `${D}${infer Rest}`? T_SplitOnce_NR_2<WA,Rest>:[S];
+type TI_SplitOnce_NB_1<WA extends string,Begin extends string>=Begin extends WA? [WA,""]:never;
+type TI_SplitOnce_NB_2<WA extends string,S extends string,D extends string>=S extends `${D}${infer Rest}`? T_SplitOnce_NR_2<WA,Rest>:[S];
 type T_SplitOnce_NR_2<WA extends string,Rest extends string>=Rest extends `${WA}${infer Rest2}`? ["",`${WA}${Rest2}`]:never;
--- userscript/youtube_plugin_raw/zc_child_modules/YtPlugin_Base.user.js
+++ userscript/youtube_plugin_raw/zc_child_modules/YtPlugin_Base.user.js
@@ -1873,7 +1873,7 @@ function split_string(x,s=as(",")) {
 	let r=x.split(s);
 	return as(r);
 }
-/** @private @arg {WA|null} _wa @template {string} WA @template {string} S @arg {S} s @template {string} D @arg {D} d @returns {T_InferTypedSplitOnce<WA,S,D>} */
+/** @private @arg {WA|null} _wa @template {string} WA @template {string} S @arg {S} s @template {string} D @arg {D} d @returns {TI_SplitOnce<WA,S,D>} */
 function split_string_once_ex(s,d=as(","),_wa) {
 	if(s==="") {
 		/** @private @type {[]} */
