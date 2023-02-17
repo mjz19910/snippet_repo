5b01f4b919339b2a8847ba2a907501e6c0c233a2
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 01:30:59 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/D_VideoPlaybackShape_S_Params.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/D_VideoPlaybackShape_S_Params.ts
@@ -3,6 +3,7 @@ type D_VideoPlaybackShape_S_Params={
 	ei: string;
 	ip: `${number}.${number}.${number}.${number}`;
 	id: string;
+	itag: "140";
 	aitags?: "133,134,135,136,137,160,242,243,244,247,248,278,394,395,396,397,398,399";
 	source: "youtube";
 	requiressl: "yes";
