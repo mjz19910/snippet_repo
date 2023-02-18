cda50002240d6d1cdb39d17cae93ae5ef934b27b
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-17 19:13:06 -0700

    u

--- ze_zsh_scripts/dig.zsh
+++ ze_zsh_scripts/dig.zsh
@@ -2,9 +2,11 @@ function do_dig() {
 	printf "."
 	echo $$ >/tmp/dig_res.$a2.pid
 	echo /tmp/dig_res.$a2.* | xargs -n 1 bash -c 'echo -n >"$1"' v
+	echo $$ >/tmp/dig_res.$a2.pid
 	echo "rr1.sn-"$a2"n"{{0..9},{a..z}}{{0..9},{a..z}}".googlevideo.com" | stdbuf -i0 -o0 -e0 xargs -n 25 -P 30 zsh -c '. ./dig.zsh child '$a2' "$@"'
+	list=(/tmp/dig_res.$a2.*)
 	TF_2=$(mktemp /tmp/dig_res.$1.out.XXX)
-	cat /tmp/dig_res.$a2.* >> $TF_2
+	cat $list >> $TF_2
 	if ((`wc -l <$TF_2` != 0)); then
 		foo=$(<$TF_2)
 		printf "[$a2]\n%s\n" $foo
@@ -44,15 +46,15 @@ else
 fi
 case $MODE in
 "dig")
-	run $1
+	run $1;
 	;;
 "dig_batch")
-	dig_batch $1
+	dig_batch $1;
 	;;
 "child")
-	run_child "$@"
+	run_child "$@";
 	;;
 "failure")
-	echo "$0 dig [section]"
+	echo "$0 dig [section]";
 	;;
-esac
+esac;
