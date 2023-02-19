b1041a5726fcb42d065cc1180352b307b80d4841
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-18 19:20:09 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/G_PR.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/G_PR.ts
@@ -1,14 +1,13 @@
-type PR_TrackingObj_3g=PR_TrackingObj_3w8n|PR_TrackingObj_3w8w;
-
 type G_PR_TrackingObj=
-	|PR_TrackingObj_3g
+	|G_PR_TrackingObj_3
 	|PR_TrackingObj_4w
-	|PR_TrackingObj_6g
+	|G_PR_TrackingObj_6
 	|PR_TrackingObj_9w
-	|PR_TrackingObj_16g
-	|PR_TrackingObj_19g
+	|G_PR_TrackingObj_16
+	|G_PR_TrackingObj_19
 	;
 ;
-type PR_TrackingObj_6g=PR_TrackingObj_6w1n|PR_TrackingObj_6w2w|PR_TrackingObj_6w3w;
-type PR_TrackingObj_16g=PR_TrackingObj_16w3n|PR_TrackingObj_16n3n;
-type PR_TrackingObj_19g=PR_TrackingObj_19w3n|PR_TrackingObj_19w3w|PR_TrackingObj_19w11w;
+type G_PR_TrackingObj_3=PR_TrackingObj_3w8n|PR_TrackingObj_3w8w;
+type G_PR_TrackingObj_6=PR_TrackingObj_6w1n|PR_TrackingObj_6w2w|PR_TrackingObj_6w3w;
+type G_PR_TrackingObj_16=PR_TrackingObj_16w3n|PR_TrackingObj_16n3n;
+type G_PR_TrackingObj_19=PR_TrackingObj_19w3n|PR_TrackingObj_19w3w|PR_TrackingObj_19w11w;
