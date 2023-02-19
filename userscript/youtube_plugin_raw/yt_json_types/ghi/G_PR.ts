490cef11e1ab83294482331592b7d31a1bc271c8
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-18 19:08:55 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/G_PR.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/G_PR.ts
@@ -1,14 +1,11 @@
 type G_PR_TrackingObj=
 	|PR_TrackingObj_3w
 	|PR_TrackingObj_4w
-	|PR_TrackingObj_6w1n
-	|PR_TrackingObj_6w2w
-	|PR_TrackingObj_6w3w
 	|PR_TrackingObj_9w
-	|PR_TrackingObj_16n3n
-	|PR_TrackingObj_16w3n
-	|PR_TrackingObj_19w3n
-	|PR_TrackingObj_19w3w
-	|PR_TrackingObj_19w11w
+	|PR_TrackingObj_16g
+	|PR_TrackingObj_19g
 	;
 ;
+type PR_TrackingObj_6g=PR_TrackingObj_6w1n|PR_TrackingObj_6w2w|PR_TrackingObj_6w3w;
+type PR_TrackingObj_16g=PR_TrackingObj_16w3n|PR_TrackingObj_16n3n;
+type PR_TrackingObj_19g=PR_TrackingObj_19w3n|PR_TrackingObj_19w3w|PR_TrackingObj_19w11w;
