7c2f7b5677925400d732ba712ff411116523bf0b
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-20 04:20:13 -0700

    u

--- ze_zsh_scripts/dig.zsh
+++ ze_zsh_scripts/dig.zsh
@@ -116,7 +116,7 @@ function dig_user_child {
 	((n = n + arg_num_1 + 9))
 	lock_printf "\e7""\e[H\e[500C\e[0K\e[${n}D [run]:$1""\e8"
 	lock_printf "."
-	dig @8.8.4.4 +time=3 +https +noall +answer "$@" >$TF
+	dig @1.1.1.2 +time=3 +https +noall +answer "$@" >$TF
 	if (($(wc -l <$TF) != 0)); then
 		lock_printf "!"
 	fi
