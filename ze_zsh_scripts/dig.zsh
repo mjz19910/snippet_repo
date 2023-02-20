fb4ca579ff43576834a9561f03f782d75d31a225
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-20 02:57:22 -0700

    u

--- ze_zsh_scripts/dig.zsh
+++ ze_zsh_scripts/dig.zsh
@@ -1,12 +1,13 @@
+TMP_DIR="/dev/shm/";
 function do_dig() {
 	printf "\r.\e[2C"
-	echo $$ >"/tmp/out.dig_batch.$a2.pid"
-	printf "%s\0" "/tmp/out.dig_batch.$a2."* | xargs -0r truncate -s 0
-	echo $$ >/tmp/pid.dig_batch.$a2
+	echo $$ >"$TMP_DIR/out.dig_batch.$a2.pid"
+	printf "%s\0" "$TMP_DIR/out.dig_batch.$a2."* | xargs -0r truncate -s 0
+	echo $$ >$TMP_DIR/pid.dig_batch.$a2
 	printf "%s\0" "rr1.sn-"$a2"n"{{0..9},{a..z}}{{0..9},{a..z}}".googlevideo.com" |
 		stdbuf -i0 -o0 -e0 xargs -0rn35 -P100 zsh -c '. ./dig.zsh run_child '$a2' "$@"'
-	list=("/tmp/out.dig_batch.$a2."*)
-	TF_2="/tmp/result.dig_batch.$a2"
+	list=("$TMP_DIR/out.dig_batch.$a2."*)
+	TF_2="$TMP_DIR/result.dig_batch.$a2"
 	cat $list >>"$TF_2"
 	if (($(wc -l <"$TF_2") != 0)); then
 		foo=$(<"$TF_2")
@@ -29,7 +30,7 @@ function dig_batch() {
 }
 function run_child() {
 	a1=$1
-	TF=$(mktemp /tmp/dig_res.$1.XXX)
+	TF=$(mktemp $TMP_DIR/dig_res.$1.XXX)
 	shift
 	if ((${#@} == 0)); then
 		return 0
@@ -60,11 +61,11 @@ function gen_z_get {
 	echo "${z1[*]}"
 }
 function dig_user {
-	echo /tmp/dig_res.t.*(N) | xargs -r rm
+	echo $TMP_DIR/dig_res.t.*(N) | xargs -r rm
 	z1=({{0..9},{a..z}})
 	z=$(gen_z_get)
 	echo "\eD\eD"
-	eval 'printf "%s\0" rr1.sn-'$1{$z}{$z}nr{$z}'.googlevideo.com' | stdbuf -i0 -o0 -e0 xargs -0rn32 -P60 zsh -c 'printf "\e7""\e[H\e[2K\r$1 \r""\e8";TF=`mktemp /tmp/dig_res.t.XXX`;sleep $(shuf -i0-2 -n1).$(shuf -i0-9 -n1);printf ".";dig @1.1.1.1 +time=3 +https +noall +answer "$@" > $TF;if ((`wc -l <$TF` != 0)); then printf "\n";cat $TF;fi' ''
+	eval 'printf "%s\0" rr1.sn-'$1{$z}{$z}nr{$z}'.googlevideo.com' | stdbuf -i0 -o0 -e0 xargs -0rn32 -P60 zsh -c 'printf "\e7""\e[H\e[2K\r$1 \r""\e8";TF=`mktemp $TMP_DIR/dig_res.t.XXX`;sleep $(shuf -i0-2 -n1).$(shuf -i0-9 -n1);printf ".";dig @1.1.1.1 +time=3 +https +noall +answer "$@" > $TF;if ((`wc -l <$TF` != 0)); then printf "\n";cat $TF;fi' ''
 }
 case $MODE in
 "dig")
