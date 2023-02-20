271ca09a117281dddaca43d6126ff5560da72ead
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-20 07:43:56 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/V_ParamItem.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/g_sub/V_ParamItem.ts
@@ -4,16 +4,16 @@ type V_ParamItem_RawChild=[type: "raw_child",binary_arr: Uint8Array,obj: V_Param
 
 type V_ParamItem=
 	|[type: "child",binary_arr: Uint8Array,obj: V_ParamObj|null]
-	|[type: "raw",obj: V_RawValue]
-	|V_ParamItem_RawChild
-	|[type: "data32",value: number]
-	|[type: "data64",raw_number: number[],value: bigint]
 	|[type: "data_fixed32",value: number]
 	|[type: "data_fixed64",value: bigint]
+	|[type: "data32",value: number]
+	|[type: "data64",raw_number: number[],value: bigint]
 	|[type: "error",location: number]
 	|[type: "group",value: V_ParamObj]
 	|[type: "info",value: number]
+	|[type: "raw",obj: V_RawValue]
 	|[type: "struct",value: V_ParamObj]
+	|V_ParamItem_RawChild
 	;
 ;
 type V_ParamItemFiltered=
