c73307fd7280b22b286214852ed8d32287234df6
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-21 03:08:35 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DT_DatabaseStoreTypes.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DT_DatabaseStoreTypes.ts
@@ -105,5 +105,5 @@ type T_IdBox<SV extends G_IdSrc,T_IdType extends string,T extends SV["key_type"]
 	base: "boxed_id";
 	type: T;
 	id: T_IdType;
-	value: [`many_${T}`,make_item_group<V>];
+	value: make_item_group<V>;
 };
