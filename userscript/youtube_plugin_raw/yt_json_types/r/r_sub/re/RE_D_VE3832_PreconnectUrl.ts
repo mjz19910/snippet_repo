56990acae601da17fe9192be75e72fc9ae1b6cf9
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-23 04:07:17 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/r/r_sub/re/RE_D_VE3832_PreconnectUrl.ts
+++ userscript/youtube_plugin_raw/yt_json_types/r/r_sub/re/RE_D_VE3832_PreconnectUrl.ts
@@ -1,4 +1,4 @@
-type RE_D_VE3832_PreconnectUrl=`https://rr${number}---sn-${string}n${string}.googlevideo.com/generate_204`;
+type RE_D_VE3832_PreconnectUrl=`https://${GV_SubDomain}.googlevideo.com/generate_204`;
 type RE_D_GoogleVideoUrl_Hostname=UrlParse<RE_D_GoogleVideoUrl>["host"];
 type RE_D_GoogleVideoUrl=
 	|RE_D_VE3832_PreconnectUrl
