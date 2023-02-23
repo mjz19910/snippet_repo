e8fab178c139ea3cc69ca75579ad80798ae4292d
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-23 07:10:28 -0700

    u

--- browser_js/javascript/final/userscripts/itch.io_user_tools.js
+++ browser_js/javascript/final/userscripts/itch.io_user_tools.js
@@ -13,9 +13,9 @@
 (function() {
 	'use strict';
 	document.addEventListener("DOMContentLoaded",function() {
-		var e=document.getElementById("user_tools");
-		if(!e) return;
-		var sl=e.children.length;
+		var user_tools_element=document.getElementById("user_tools");
+		if(!user_tools_element) return;
+		var number_of_tools=user_tools_element.children.length;
 		var list_item_element=document.createElement("li");
 		list_item_element.innerHTML='<div data-label="custom_btn" class="action_btn"><span class="full_label">Hide</span></div>';
 		if(!list_item_element.firstChild||!(list_item_element.firstChild instanceof HTMLElement)) return;
@@ -26,16 +26,16 @@
 			if(!tools) return;
 			if(!child_element.firstChild||!(child_element.firstChild instanceof HTMLElement)) return;
 			if(child_element.getAttribute("mode")=='h') {
-				Array.prototype.slice.call(tools.children,0,sl).forEach(function(e) {e.style.display='none';});
+				Array.prototype.slice.call(tools.children,0,number_of_tools).forEach(function(e) {e.style.display='none';});
 				child_element.firstChild.innerText="Show";
 				child_element.setAttribute("mode","s");
 			} else {
-				Array.prototype.slice.call(tools.children,0,sl).forEach(function(e) {e.style.display='';});
+				Array.prototype.slice.call(tools.children,0,number_of_tools).forEach(function(e) {e.style.display='';});
 				child_element.firstChild.innerText="Hide";
 				child_element.setAttribute("mode","h");
 			}
 		};
-		e.appendChild(list_item_element);
+		user_tools_element.appendChild(list_item_element);
 	});
 	// Your code here...
 })();
