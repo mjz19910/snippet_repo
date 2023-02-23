1f748c9ed92e7058616b86a057f77d1d8ddecfc9
Matthias Zimmerman <matthias291999@gmail.com>
2023-02-23 05:31:17 -0700

    u

---
+++ browser_js/javascript/final/userscripts/itch.io_user_tools.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         itch.io user_tools hide show
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.itch.io/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+/* eslint-disable no-undef */
+
+(function() {
+	'use strict';
+	document.addEventListener("DOMContentLoaded",function(){
+		var e=document.getElementById("user_tools")
+		if(!e)return;
+		var sl=e.children.length;
+		var ne=document.createElement("li")
+		ne.innerHTML='<div data-label="custom_btn" class="action_btn"><span class="full_label">Hide</span></div>'
+		if(!ne.firstChild||!(ne.firstChild instanceof HTMLElement)) return;
+		let child_element=ne.firstChild;
+		child_element.onclick=function(){
+			let tools=document.getElementById('user_tools');
+			if(!tools) return;
+			if(!child_element.firstChild||!(child_element.firstChild instanceof HTMLElement)) return;
+			if(child_element.getAttribute("mode") == 'h'){
+				Array.prototype.slice.call(tools.children,0,sl).forEach(function(e){e.style.display='none'})
+				child_element.firstChild.innerText="Show"
+				child_element.setAttribute("mode","s");
+			}else{
+				Array.prototype.slice.call(tools.children,0,sl).forEach(function(e){e.style.display=''})
+				child_element.firstChild.innerText="Hide"
+				child_element.setAttribute("mode","h");
+			}
+		}
+		child_element.setAttribute("mode","h");
+		e.appendChild(ne)
+	})
+	// Your code here...
+})();
---
+++ browser_js/javascript/final/userscripts/revert_no_left_click.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         revert no left_click(games that use that use the right mouse button for something)
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://game296248.konggames.com/*
+// @match        http://game296248.konggames.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+// @info         Barbasu/orbit-idle-redux = game296248
+// @ts-nocheck
+/* eslint-disable no-undef */
+
+(function() {
+	'use strict';
+setTimeout(e=>window.revert(),300)
+	// Your code here...
+})();
