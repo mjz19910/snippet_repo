63facf83049b68cc992516ce1720dcf431953493
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-02 08:00:58 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/R_SimpleCardContent.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/R_SimpleCardContent.ts
@@ -1,12 +1,11 @@
-type R_SimpleCardContent={
-	simpleCardContentRenderer: {
-		image: D_Thumbnail;
-		title: G_Text;
-		actionButton: R_SimpleCardButton;
-		trackingParams: string;
-		displayDomain: G_Text;
-		showLinkIcon: true;
-		callToAction: G_Text;
-		command: E_Url;
-	};
+type D_SimpleCardContent={
+	image: D_Thumbnail;
+	title: G_Text;
+	actionButton: R_SimpleCardButton;
+	trackingParams: string;
+	displayDomain: G_Text;
+	showLinkIcon: true;
+	callToAction: G_Text;
+	command: E_Url;
 };
+type R_SimpleCardContent={simpleCardContentRenderer: D_SimpleCardContent;};
--- userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_Support_Service.user.js
+++ userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_Support_Service.user.js
@@ -1094,7 +1094,7 @@ class Support_RS_Player extends ServiceMethods {
 		const cf="D_Card_Content"; this.k(cf,x);
 		if("collaboratorInfoCardContentRenderer" in x) return this.R_CollaboratorInfoCardContent(x);
 		if("playlistInfoCardContentRenderer" in x) return this.R_PlaylistInfoCardContent(x);
-		this.codegen_typedef(cf,x);
+		x===""; this.codegen_typedef(cf,x);
 	}
 	/** @private @arg {D_Card} x */
 	D_Card(x) {
