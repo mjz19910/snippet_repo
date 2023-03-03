7716aa37822cb711eb56d9be5d1cd8098976d4d7
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-03 15:58:00 -0700

    u

--- userscript/youtube_plugin_raw/support_0_mod/T_Split.ts
+++ userscript/youtube_plugin_raw/support_0_mod/T_Split.ts
@@ -12,10 +12,10 @@ type T_Split_Helper<S extends string,D extends string=",">=
 type T_Split_Helper_t<S extends string,D extends string=",">=
 	string extends S
 	? string[]:S extends ''
-	? []:S extends `${infer T}${D}${infer U}${D}${infer X}`
+	? [0.1]:S extends `${infer T}${D}${infer U}${D}${infer X}`
 	? X extends ''
-	? [1,T,U,""]:[2,T,U,...T_Split_Helper<X,D>]:S extends `${infer T}${D}${infer U}`
-	? [3,T,...T_Split_Helper<U,D>]:[4,S]
+	? [1.1,T,U,""]:[1.2,T,U,...T_Split_Helper<X,D>]:S extends `${infer T}${D}${infer U}`
+	? [2.1,T,...T_Split_Helper<U,D>]:[2.2,S]
 	;
 ;
 type TS_Test2=T_Split_Helper<`
