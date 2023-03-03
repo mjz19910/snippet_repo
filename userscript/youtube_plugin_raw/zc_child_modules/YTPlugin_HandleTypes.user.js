151ca9a5fcaef5cab4d4be0c14740a046a559234
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-03 10:40:13 -0700

    u

--- userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes.user.js
+++ userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes.user.js
@@ -2599,8 +2599,8 @@ class HandleTypes extends ServiceMethods {
 	PX_watch_next_token_item(x) {
 		const cf="PX_watch_next_token_item";
 		const {1: v1,3: v3,...y}=this.s(cf,x); this.h_gen_keys(cf,x,y);
-		console.log(v1);
-		v3&&console.log(v3);
+		console.log(`${cf}.f1`,v1[1][0]);
+		v3&&this.T_D32(v3,x => console.log(`${cf}.f3`,x));
 	}
 	/** @private @arg {PX_watch_next_token_3_f1} x */
 	PX_watch_next_token_3_f1(x) {
