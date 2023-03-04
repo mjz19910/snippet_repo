7b5a66bba05327abfd60529c432a198feb8acaed
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-03 17:40:36 -0700

    u

--- userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes.user.js
+++ userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes.user.js
@@ -767,7 +767,7 @@ class HandleTypes extends ServiceMethods {
 			} break;
 			case "6 ": case "6 ": case "6d": case "6 ": case "6 ": case "6l": case "6 ": case "6 ": case "6 ": case "6z":
 			case "76": case "7 ": case "7d": case "7 ": case "7k": case "7l": case "7r": case "7s": case "7y": case "7z":
-			case "d6": case "d ": case "d ": case "d ": case "d ": case "d ": case "d ": case "ds": case "dy": case "dz":
+			case "d6": case "d ": case "d ": case "d ": case "d ": case "d ": case "dr": case "ds": case "dy": case "dz":
 			case "e6": case "e7": case "e ": case "ee": case "ek": case "el": case "e ": case "es": case "ey": case "ez":
 			case "k ": case "k ": case "kd": case "k ": case "k ": case "k ": case "k ": case "k ": case "k ": case "k ":
 			case "l6": case "l7": case "ld": case "le": case "lk": case "ll": case "lr": case "ls": case "ly": case "l ":
