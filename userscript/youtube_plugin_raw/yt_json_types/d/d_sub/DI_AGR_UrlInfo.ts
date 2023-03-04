168797de03ece3e3ee2f35f8f9584f41b84b3146
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-04 10:30:12 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DI_AGR_UrlInfo.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DI_AGR_UrlInfo.ts
@@ -3,8 +3,8 @@ type DI_AGR_UrlInfo=
 	|T_UrlInfoPartial<"raw",["channel_id"],DU_ChannelId>
 	|T_UrlInfoPartial<"raw",["guide_entry_id"],D_GuideEntryData["guideEntryId"]>
 	|T_UrlInfoPartial<"raw",["playlist_id","RD"],Extract<GU_PlaylistId,`RD${string}`>>
-	|DI_R_PlaylistId
-	|DIR_VideoId
+	|T_UrlInfoPartial<"raw",["playlist_id"],GU_PlaylistId>
+	|T_UrlInfoPartial<"raw",["video_id"],D_VideoIdStr>
 	|{type: "raw_key",key: "start_radio",info_arr: [{start_radio: `${0|1}`;}];}
 	;
 ;
