5ccf8af8eba82a1de65cac16b80e3ab284bf05a9
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-05 08:58:41 -0700

    u

---
+++ userscript/youtube_plugin_raw/yt_json_types/d/DU/DU.ts
@@ -0,0 +1,27 @@
+//#region UrlType
+type DU_UrlTypeWithPageType=`page_type_${S_PageTypeStr}`|DU_UrlType;
+type DU_UrlType=NonNullable<Ret_ParserService_GetUrlType>;
+//#endregion
+//#region TargetId
+type DU_TargetId_ShoppingPanel_EntryPoint=`shopping_panel_for_entry_point_${"5"|"22"}`;
+type DU_TargetId=
+	|Extract<D_Menu,{targetId: any;}>["targetId"]
+	|A_WatchNextContinuation['targetId']
+	|AD_AppendContinuationItems['targetId']
+	|AD_UpdateEngagementPanel['targetId']
+	|D_Button_targetId
+	|D_Button_With_TargetId["targetId"]
+	|D_ChipCloudChip_tid['targetId']
+	|D_EngagementPanelSectionTargetId
+	|D_Menu_TargetId
+	|DU_TargetId_ShoppingPanel_EntryPoint
+	|D_TranscriptSearchPanel['targetId']
+	|DC_ReloadContinuationItems["targetId"]
+	|DC_ScrollToEngagementPanel['targetId']
+	|DC_SectionList_TargetId
+	|G_SI_DB_EngagementPanel['targetId']
+	|RS_Search['targetId']
+	|TA_Continuation<"browse-feedFEwhat_to_watch",G_BrowseFeed>['targetId']
+	;
+;
+//#endregion
