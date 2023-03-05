449b9a3e67051fff6f6e54d8697c1e59502577be
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-05 05:55:02 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/G_IDBBoxedType.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/G_IDBBoxedType.ts
@@ -1,7 +1,10 @@
-type G_IDBBoxedType=G_BoxedIdObj|
-	G_BoxedVideoId|
-	G_PlaylistIdObj|
-	I_BoxedBrowseId|
-	I_BoxedChannelId|
-	I_BoxedHashtagId|
-	I_BoxedUserId;
+type G_IDBBoxedType=
+	|G_BoxedIdObj
+	|G_BoxedVideoId
+	|G_PlaylistIdObj
+	|I_BoxedBrowseId
+	|I_BoxedChannelId
+	|I_BoxedHashtagId
+	|I_BoxedUserId
+	;
+;
