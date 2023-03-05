c79665a3acec2a632d5915a9e0fd2b004549e34d
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-05 08:57:26 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/DU/DU_TemplateString.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/DU/DU_TemplateString.ts
@@ -3,10 +3,11 @@ type DU_Browse_Playlist_Id=`VL${DU_Playlist_Id}`;
 type DU_Channel_Id=T_ChannelIdStr<D_UserIdStr>;
 type DU_Playlist_Id=`PL${string}`;
 type DU_Playlist_UserUploads_Id=`UU${string}`;
-type DU_Radio_ChannelMode_Id=`CM${DU_Channel_Id}`;
 type DU_Radio_ChannelMix_Id=`RD${DU_Radio_ChannelMode_Id}`;
-type DU_Radio_GeneralMode_Id=`GM${string}`;
+type DU_Radio_ChannelMode_Id=`CM${DU_Channel_Id}`;
 type DU_Radio_GeneralMix_Id=`RD${DU_Radio_GeneralMode_Id}`;
-type DU_Radio_MyMix_Id=`RDMM${string}`;
+type DU_Radio_GeneralMode_Id=`GM${string}`;
 type DU_Radio_Id=`RD${string}`;
+type DU_Radio_MyMix_Id=`RD${DU_Radio_MyMixMode_Id}`;
+type DU_Radio_MyMixMode_Id=`MM${string}`;
 //#endregion
