c9401251cf3dc3c45ebac6ea871987faef75870b
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-05 10:42:13 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DT_DatabaseStoreTypes.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DT_DatabaseStoreTypes.ts
@@ -1,9 +1,3 @@
 type DT_DatabaseStoreTypes={
-	video_id: D_BoxedVideoId;
-	hashtag_id: D_BoxedHashtagId;
 	boxed_id: G_BoxedIdObj;
-	channel_id: D_Boxed_Channel_UC;
-	playlist_id: never;
-	browse_id: G_Boxed_BrowseId;
-	user_id: D_BoxedUserId;
 };
