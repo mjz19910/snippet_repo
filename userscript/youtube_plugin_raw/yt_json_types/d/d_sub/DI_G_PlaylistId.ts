cc71b574df27aac72d7795de23e06b223e6331a9
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-05 04:22:47 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DI_G_PlaylistId.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DI_G_PlaylistId.ts
@@ -1,10 +1,8 @@
 type DI_G_PlaylistId=
 	|DI_A_Playlist_LL
-	|DI_A_Playlist_WL
 	|DI_A_Playlist_PL
-	|DI_A_Playlist_RD_CM_UC
-	|DI_A_Playlist_RD_MM
-	|DI_A_Playlist_RD
 	|DI_A_Playlist_UU
+	|DI_A_Playlist_WL
+	|DI_G_Playlist_RD
 	;
 ;
