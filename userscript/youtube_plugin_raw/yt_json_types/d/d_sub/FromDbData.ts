bddf875c5e3bef9b8f3c24a475175d074f14bacd
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-06 18:36:50 -0700

    u

---
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/FromDbData.ts
@@ -0,0 +1,9 @@
+type FromDbData={
+	key: G_BoxedIdObj["key"];
+	type: "boxed_id";
+	tag: J_DU_TagStr;
+	value: {
+		type: string;
+		info_arr: [FromDbData_InfoItem_L1];
+	};
+};
---
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/FromDbData_InfoItem_L1.ts
@@ -0,0 +1,1 @@
+type FromDbData_InfoItem_L1={raw_id: string;}|["one",bigint]|["arr",bigint[]];
