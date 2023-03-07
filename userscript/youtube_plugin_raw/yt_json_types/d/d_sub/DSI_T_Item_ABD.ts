c6dfea9d54e9e5efc2b46d423971414810c8292b
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-07 11:12:44 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DSI_T_Item_ABD.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DSI_T_Item_ABD.ts
@@ -2,8 +2,8 @@ type DSI_T_Item_ABD<T_Tag1 extends keyof J_StoreTypeMap,T>={
 	key: `boxed_id:${T_Tag1}:${string}`;
 	a: "SI:T:D"; b: "boxed_id"; d: T_Tag1; w: "/key/a/b/d/w/z"; z: [T];
 	descriptive_name?: "boxed_store";
-	renames_arr?: [
+	renames_arr?: DEX_Renames<[
 		DRN_KeyIs<"SI:T:D">,
 		DRN_KeyType<T_Tag1>,
-	];
+	]>;
 };
