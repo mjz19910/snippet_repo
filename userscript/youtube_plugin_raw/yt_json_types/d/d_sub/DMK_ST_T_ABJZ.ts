c749220edd69afa114a02ab6637e8b860d2be4e0
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-07 10:53:05 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DMK_ST_T_ABJZ.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DMK_ST_T_ABJZ.ts
@@ -1,9 +1,9 @@
-type T_DI_A_WithId<K>={b: string; c: string; z: [any,T_DI_FromObj<{id: K;}>];};
-type DST_MK_ST_D<DI extends T_DI_A_WithId<any>,K extends string=DI extends T_DI_A_WithId<infer J>? J:never,J1 extends string=DI["b"],J2 extends string=DI['c']>={
+type T_DI_A_WithId<K,B=string,C=string,Z1=DIT_Item_AB<string,any>>={b: B; c: C; z: [Z1,T_DI_FromObj<{id: K;}>];};
+type DMK_DST_ABJWZ<DI extends T_DI_A_WithId<string>,K extends string=DI extends T_DI_A_WithId<infer J>? J:never,J1 extends string=DI["b"],J2 extends string=DI['c']>={
 	key: `boxed_id:${J1}:${J2}:${K}`;
 	a: "ST:D"; b: "boxed_id"; j: `${DI['b']}:${DI['c']}`; w: "/key/a/b/j/w/z"; z: [DI];
 };
-type DST_ST_D_Test1=DST_MK_ST_D<DI_A_ChannelId_UC>;
+type DST_ST_D_Test1=DMK_DST_ABJWZ<DI_A_ChannelId_UC>;
 
 namespace __TestNamespace {
 	const DST_Src: DST_ST_D_Test1={} as DST_ST_D_Test1;
