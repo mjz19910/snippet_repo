e2aaaf7819b7eca1de5976cb97fdbd134f51947f
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-07 08:14:41 -0700

    u

---
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DST_Playlist_RD_CM_UC.ts
@@ -0,0 +1,7 @@
+type DST_Playlist_RD_CM_UC={
+	a: "ST:D";
+	b: "boxed_id";
+	j: "playlist_id:RD:CM:UC";
+	key: `boxed_id:playlist_id:RD:CM:UC:${string}`;
+	z: [DI_A_Playlist_RD_CM_UC];
+};
--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DST_Playlist_RD_MM.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DST_Playlist_RD_MM.ts
@@ -5,10 +5,3 @@ type DST_Playlist_RD_MM={
 	key: `boxed_id:playlist_id:RD:MM:${string}`;
 	z: [DI_A_Playlist_RD_MM];
 };
-type DST_Playlist_RD_CM_UC={
-	a: "ST:D";
-	b: "boxed_id";
-	j: "playlist_id:RD:CM:UC";
-	key: `boxed_id:playlist_id:RD:CM:UC:${string}`;
-	z: [DI_A_Playlist_RD_CM_UC];
-};
