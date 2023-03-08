b8d5a0684d7a96339fc702deda1bda7ff386a6a4
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-08 12:31:57 -0700

    u

--- browser_js/javascript/final/userscripts/reddit.js
+++ browser_js/javascript/final/userscripts/reddit.js
@@ -11,34 +11,41 @@
 
 (function() {
 	'use strict';
-	if (localStorage.getItem("reddit_custom_context") === null){
-			localStorage.reddit_custom_context=7//7 extra items by default
+	if(localStorage.getItem("reddit_custom_context")===null) {
+		localStorage.reddit_custom_context=7;
 	}
-	if (document.querySelector(".sr-bar") === null){
-		// Nothing if there is no header bar, we probably got injected into a reddit related iframe
-		// We might be on new reddit, there is no parent UX on each comment, so i cant do anything
-	}else{
-	var a,b,c,e //these are most of the vars we use, make them private to the function we are currently in.
-	a=document.createElement("li");"      "//preparing the required dom elements to prepend the configure link @create @dom
-	var link=b=document.createElement("a");//@create @dom
-	b.classList.add("choice");"           "//@create @dom
-	b.innerHTML="configure context";"     "//@create @dom
-	c=document.querySelector(".sr-bar");" "//@get @header
-	e=document.createElement("span");"    "//@create @dom
-	e.classList.add("separator");"        "//@create @dom
-	e.innerHTML="-";"                     "//@create @dom
-	c.firstChild.prepend(e);//add items to the reddit header bar @insert @header @dom
-	a.append(b);0//@insert @header @dom
-	c.prepend(a);//@insert @header @dom
-	link.onclick=function(){//add the click handler to the context_configure button
-		var a=prompt("Enter a number");//get a number from the user
-		if (a == null){return};// bail out if we got a negative response
-		a=Number.parseInt(a);//try to turn the response into a number
-		if (Number.isNaN(a)){return};//bail out if we parsed NaN
-		if (a < 0){return};//bail out if the number is negative
-		localStorage.reddit_custom_context=a//save the users choice so it persists
-	}
-	document.querySelector(".sitetable.nestedlisting").querySelector("ul :nth-child(4)>*").href+="?context="+localStorage.reddit_custom_context
+	let separator_renderer_bar=document.querySelector(".sr-bar");
+	// Do nothing if there is no header bar; We probably got injected into a reddit related iframe.
+	// We might be on new reddit, there is no parent UX on each comment, so i cant do anything
+	if(separator_renderer_bar===null) return;
+	// These are most of the vars we use, make them private to the function we are currently in.
+	var list_item_element,context_configure_anchor_item,separator_renderer_bar,sr_bar_span;
+	// Create the dom elements for the configure link
+	list_item_element=document.createElement("li");
+	var context_configure_anchor_item=document.createElement("a");
+	context_configure_anchor_item.classList.add("choice");
+	context_configure_anchor_item.innerHTML="configure context";
+	sr_bar_span=document.createElement("span");
+	sr_bar_span.classList.add("separator");
+	sr_bar_span.innerHTML="-";
+	// Add items to the reddit header bar
+	separator_renderer_bar.firstChild.prepend(sr_bar_span);
+	list_item_element.append(context_configure_anchor_item); 0;
+	separator_renderer_bar.prepend(list_item_element);
+	context_configure_anchor_item.onclick=function() {
+		var custom_context_str=prompt("Enter a number");
+		// if the user canceled, bail
+		if(custom_context_str==null) return;
+		// Parse the response into a number
+		let custom_context_len=Number.parseInt(custom_context_str);
+		if(Number.isNaN(custom_context_len)) return;
+		if(custom_context_len<0) return;
+		// Save the choice
+		localStorage.reddit_custom_context=custom_context_len;
+	};
+	let nested_listing=document.querySelector(".sitetable.nestedlisting");
+	let context_dom=nested_listing.querySelector("ul :nth-child(4)>*");
 	// append the context option to the first "parent" link
-	}
+	context_dom.href+="?context="+localStorage.reddit_custom_context;
+
 })();
