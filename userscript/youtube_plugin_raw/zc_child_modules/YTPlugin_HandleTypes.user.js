d5231340ba4af997c8a3808b2d1161ca5d4bc1f0
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-09 07:08:30 -0700

    u

--- userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes.user.js
+++ userscript/youtube_plugin_raw/zc_child_modules/YTPlugin_HandleTypes.user.js
@@ -28,10 +28,7 @@ function init_module() {
 	const {ECatcherService}=require("./YTPlugin_ECatcherService_Plugin.user");
 	// [new_fexp_expected]
 	ECatcherService.known_experiments.push(...[
-		[24412856,24447748,24447992,24451320,24456736,24465486,24483504,24483766,24491863,24492030],
-		[24447748,24465486],
-		[24486981],
-		[24494197],
+		[24412856,24447748,24447992,24451320,24456736,24465486,24483504,24483766,24491863,24492030,24447748,24465486,24486981,24494197],
 	].flat());
 }
 export_((exports) => {exports.init_module=init_module;});
