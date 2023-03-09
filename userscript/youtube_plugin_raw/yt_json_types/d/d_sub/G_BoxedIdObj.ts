487777b267a87d9ee41971a2af37cab53536959d
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-08 17:21:55 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/G_BoxedIdObj.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/G_BoxedIdObj.ts
@@ -1,3 +1,4 @@
+//#region G_Boxed
 type G_BoxedIdObj=
 	|DST_Browse_MP
 	|DST_Browse_SP
@@ -31,6 +32,7 @@ type G_BoxedIdObj=
 	|GST_DSS
 	;
 ;
+//#endregion
 type G_BoxedPrintable=|G_BoxedIdObj;
 type GB_A1=G_BoxedIdObj["z"][0];
 type GB_A1_J_Shape=
@@ -51,7 +53,7 @@ type GB_A1_J_Shape=
 	|"guide_entry_id:VL:LL"
 	;
 ;
-type TP1=G_BoxedIdObj['key'] extends infer I?
+type G_Boxed_StrExtract=G_BoxedIdObj['key'] extends infer I?
 	I extends `${infer f0}:${infer f1}:${infer f2}:${infer f3}:${infer f4}:${infer f5}`? [f0,f1,f2,f3,f4,f5]:
 	I extends `${infer f0}:${infer f1}:${infer f2}:${infer f3}:${infer f4}`? [f0,f1,f2,f3,f4]:
 	I extends `${infer f0}:${infer f1}:${infer f2}:${infer f3}`? [f0,f1,f2,f3]:
@@ -59,17 +61,17 @@ type TP1=G_BoxedIdObj['key'] extends infer I?
 	I extends `${infer f0}:${infer f1}`? [f0,f1]:[I]:never
 	;
 ;
-type TP3=[
-	Join<Extract<TP1,[any,any]>,":">,
+type G_Boxed_StrArr=[
+	Join<Extract<G_Boxed_StrExtract,[any,any]>,":">,
 	// 3
-	Join<Extract<TP1,[any,any,any]>,":">,
+	Join<Extract<G_Boxed_StrExtract,[any,any,any]>,":">,
 	// 4
-	Join<Exclude<Extract<TP1,[any,any,any,any]>,[any,any,"FE",any]>,":">,
-	Join<Extract<Extract<TP1,[any,any,any,any]>,[any,any,"FE",any]>,":">,
+	Join<Exclude<Extract<G_Boxed_StrExtract,[any,any,any,any]>,[any,any,"FE",any]>,":">,
+	Join<Extract<Extract<G_Boxed_StrExtract,[any,any,any,any]>,[any,any,"FE",any]>,":">,
 	// 5
-	Join<Extract<TP1,[any,any,any,any,any]>,":">,
+	Join<Extract<G_Boxed_StrExtract,[any,any,any,any,any]>,":">,
 	// 6
-	Join<Extract<TP1,[any,any,any,any,any,any]>,":">,
+	Join<Extract<G_Boxed_StrExtract,[any,any,any,any,any,any]>,":">,
 ];
 type GB_A1_Keys=G_BoxedIdObj extends infer V? V extends infer I? T_DistributedKeyof<Omit<I,"key"|"a"|"b"|"j"|"w"|"z">>:[]:[];
 type GB_A1_KS1=Extract<G_BoxedIdObj,{w: any;}>;
@@ -99,3 +101,27 @@ type G_BoxedInner_Tmp=[
 	Exclude<G_BoxedPrintable,{a: "SI:T:D";}>|null
 ];
 type G_BoxedInner_Tmp_l=Exclude<G_BoxedPrintable,{a: "SI:T:D";}>;
+type DST_Browse_MP=DST_T_abz_j<"browse_id:MP",DI_BrowseId_MP>;
+//#region DST_T
+type DST_T_abz_j<J,X>={
+	key: `boxed_id:browse_id:MP:${string}`;
+	a: "ST:D"; b: "boxed_id"; j: J; w: "/db/key/a/b/j/w/z"; z: [X];
+};
+type DST_T_abz<T>=DST_T_abz_j<T,any>;
+type OneKey<K extends string,V=any>={
+	[P in K]: (Record<P,V>&Partial<Record<Exclude<K,P>,never>>) extends infer O? Decay<O>:never;
+}[K];
+type GW2<T>=T extends {z: [T_DI_FromObj<infer J extends {[U in string]: any}>];}? keyof J:never;
+type GW1<T,K extends string>=T extends {z: [T_DI_FromObj<{[U in K]: infer W;}>];}? W:never;
+type UB_Prim<T extends {z: [any];} extends {z: [T_DI_FromObj<{[U in string]: any}>];}? {}:never>=T extends {z: [T_DI_FromObj<infer A>];}? A[keyof A]:never;
+type ip=UB_Prim<DI_Key_StartRadio>;
+type UB_Obj<T>=T extends {z: [infer A];}? A:never;
+type UB_obj2<T extends T_DI_FromObj<any>>=T extends T_DI_FromObj<infer A extends {}>? A:never;
+type Ub1=UB_Obj<DI_Key_StartRadio>;
+type ub_obj=UB_obj2<Ub1>;
+type G1_test1=GW2<DI_Key_StartRadio>;
+type CQ=T_DI_FromObj<{
+	start_radio: 0|1;
+	id: "test";
+}>;
+//#endregion
