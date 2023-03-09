8760ca8155ee47d1b20a267936392e5fe426d1f1
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-09 10:32:14 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/G_BoxedInner.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/G_BoxedInner.ts
@@ -21,4 +21,3 @@ type G_BoxedInner=
 	|[2,6,...Ret_W_DSS_Impl<DSS_String,"string",string>]
 	;
 ;
-type Ret_w_dst<T extends G_Boxed_DST>=[true,number,T["j"],string,T["z"][0]['z'][0],T["z"][0],T]|[false,number,T["j"],T["z"][0],T];
---
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/Ret_w_dst.ts
@@ -0,0 +1,1 @@
+type Ret_w_dst<T extends G_Boxed_DST>=[true,number,T["j"],string,T["z"][0]['z'][0],T["z"][0],T]|[false,number,T["j"],T["z"][0],T];
