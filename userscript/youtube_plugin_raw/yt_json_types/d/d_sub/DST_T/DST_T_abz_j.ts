c7c6afbf45bc5e700710febc40152c27c583b01f
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-09 11:08:49 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DST_T/DST_T_abz_j.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/d_sub/DST_T/DST_T_abz_j.ts
@@ -1,4 +1,4 @@
-type DST_T_abz_j<J,X=any>={
-	key: `boxed_id:browse_id:MP:${string}`;
+type DST_T_abz_j<J=string,K extends string="browse_id:MP",X=any>={
+	key: `boxed_id:${K}:${string}`;
 	a: "ST:D"; b: "boxed_id"; j: J; w: "/db/key/a/b/j/w/z"; z: [X];
 };
