2b85c9055e7a432e2fc5f94a3d50294b9da7e595
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-09 18:24:51 -0700

    u

--- userscript/rebuild_the_universe_raw/support/ExtractKey.ts
+++ userscript/rebuild_the_universe_raw/support/ExtractKey.ts
@@ -1,10 +1,12 @@
-import {Box} from "./box/mod/Box.js"
-import {VoidBox} from "./VoidBox.js"
-import {Primitives} from "./Primitives.js"
+import {Box} from "./box/mod/Box.js";
+import {VoidBox_ForUndefined} from "./box/mod/VoidBox_ForUndefined.js";
+import {Primitives} from "./Primitives.js";
 
 export type ExtractKey<T extends Box,U>=
 	T extends Exclude<Box,Primitives|null>?
-	U extends keyof Exclude<T,VoidBox>?
-	Exclude<T,VoidBox>[U]:
+	U extends keyof Exclude<T,VoidBox_ForUndefined>?
+	Exclude<T,VoidBox_ForUndefined>[U]:
 	never:
 	never
+	;
+;
