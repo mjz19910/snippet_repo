dea47efc656d848e8d977b6233bba745a7c53d9b
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-11 10:30:03 -0700

    u

--- userscript/youtube_plugin_raw/zd_gen_CF/gen_out_support.sh
+++ userscript/youtube_plugin_raw/zd_gen_CF/gen_out_support.sh
@@ -1,4 +1,8 @@
-set -x
+export BACKUP_DATE=$(date '+%F_%H/%M')
+export PROJ_DIR="$PWD"
+export DEST_DIR="userscript/youtube_plugin_raw/zd_gen_CF"
+export TMP_DIR="/dev/shm/snippet_repo_tmp"
+
 function generate_ts_after_tmp_git_repo {
 	git apply --allow-empty "../snippet_repo.diff"
 }
@@ -63,4 +67,52 @@ function generate_ts_filter_errors {
 function generate_ts_with_perl {
 	# |{n: Prelude.CF_M_s; t: Types.CF_M_s_; v: "AD_AddToGuideSection";}
 	perl gen.pm
+}
+
+function gen_code_v1 {
+	for ((i = 0; ; ++i)); do
+		generate_ts_filter_errors "$TMP_DIR/errors.out" | generate_ts_with_perl | sort -u >"$TMP_DIR/tmp_out.txt"
+		echo "--- [tmp_out.txt] $i ---"
+		tail "$TMP_DIR/tmp_out.txt"
+		if grep -q "n:" "$TMP_DIR/tmp_out.txt"; then
+			generate_typescript_code_unique >"$TMP_DIR/tmp.ts"
+			cp "$TMP_DIR/tmp.ts" "generated/tmp.ts"
+			tsc -p "$TMP_DIR/userscript" >"$TMP_DIR/errors.out"
+			mv "$TMP_DIR/tmp_acc.ts" "$TMP_DIR/tmp.ts"
+			grep "|{n:" "$TMP_DIR/tmp.ts" | sort -u >"$TMP_DIR/tmp_partial.ts"
+		else
+			generate_typescript_code_unique >"$TMP_DIR/tmp.ts"
+			cp "$TMP_DIR/tmp.ts" "generated/tmp.ts"
+			cp "$TMP_DIR/tmp.ts" "generated/out.ts"
+			grep "|{n:" "$TMP_DIR/tmp.ts" | sort -u >"$TMP_DIR/tmp_partial_end.ts"
+			break
+		fi
+	done
+}
+generate_typescript_code_unique() {
+	echo "export namespace Gen {\n\texport type CF_Generated="
+	cat "$TMP_DIR/tmp_partial.ts" "$TMP_DIR/tmp_out.txt" | sort -u
+	src_file=`realpath "/proc/self/fd/0" <&0`
+	if [ -f $src_file ]; then
+		# if stdin is a normal file, save the partial result
+		cp "$src_file" "$TMP_DIR/tmp_acc.ts"
+	fi
+	echo "\t\t;"
+	echo "}"
+}
+generate_typescript_code_force_valid() {
+	echo "export namespace Gen {\n\texport type CF_Generated="
+	echo "\n\t\t|never"
+	cat "$TMP_DIR/tmp_partial.ts"
+	echo "\t\t;"
+	echo "}"
+}
+
+function generate_ts_output_v1 {
+	generate_ts_setup
+	grep "|{n:" "generated/out.ts.bak" >"$TMP_DIR/tmp_partial.ts"
+	generate_typescript_code_force_valid >"$TMP_DIR/tmp.ts"
+	tsc -p "$TMP_DIR/userscript" >"$TMP_DIR/errors.out"
+	gen_code_v1
+	generate_ts_restore
 }
