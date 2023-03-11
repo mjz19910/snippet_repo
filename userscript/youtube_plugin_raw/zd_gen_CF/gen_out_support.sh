b124ffffe9d4c56428b21b7290b983255e480255
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-11 10:53:37 -0700

    u

--- userscript/youtube_plugin_raw/zd_gen_CF/gen_out_support.sh
+++ userscript/youtube_plugin_raw/zd_gen_CF/gen_out_support.sh
@@ -38,13 +38,13 @@ function generate_ts-init_cwd {
 
 function generate_ts-backup_output_at_project {
 	cp -ru "$DEST_DIR/generated" "$TMP_DIR/$DEST_DIR"
-	pushd "$DEST_DIR"
-	mkdir -p "$PROJ_DIR/$DEST_DIR/bak/$BACKUP_DATE"
-	cp "generated/out.ts" "$PROJ_DIR/$DEST_DIR/bak/"${BACKUP_DATE}"/out.ts.bak" || exit 1
-	pushd "$TMP_DIR/$DEST_DIR"
-	cp "generated/out.ts" "generated/out.ts.bak" || exit 1
-	popd
-	popd
+	{
+		pushd "$DEST_DIR"
+		mkdir -p "$PROJ_DIR/$DEST_DIR/bak/$BACKUP_DATE"
+		cp "generated/out.ts" "$PROJ_DIR/$DEST_DIR/bak/"${BACKUP_DATE}"/out.ts.bak" || exit 1
+		cp "$TMP_DIR/$DEST_DIR/generated/out.ts" "$TMP_DIR/$DEST_DIR/generated/out.ts.bak" || exit 1
+		popd
+	}
 }
 
 function generate_ts-backup_output_at_tmp() {
@@ -97,6 +97,13 @@ generate_typescript_code_unique() {
 	echo "}" >>"$TMP_DIR/tmp.ts"
 }
 
+generate_typescript_code_unique_final() {
+	echo "export namespace Gen {\n\texport type CF_Generated="
+	cat "$TMP_DIR/tmp_partial.ts" "$TMP_DIR/tmp_out.txt" | sort -u
+	echo "\t\t;"
+	echo "}"
+}
+
 generate_typescript_code_force_valid() {
 	echo "export namespace Gen {\n\texport type CF_Generated="
 	echo "\n\t\t|never"
@@ -108,16 +115,19 @@ generate_typescript_code_force_valid() {
 function generate_ts-output_v1 {
 	generate_ts-make_tmp_git_repo
 	generate_ts-backup_output_at_project
-	pushd "$TMP_DIR/$DEST_DIR"
-	generate_ts-init_cwd
+	{
+		pushd "$TMP_DIR/$DEST_DIR"
+		generate_ts-init_cwd
 
-	cp "$PROJ_DIR/$DEST_DIR/generated/out.ts" "generated/tmp.ts"
+		cp "$PROJ_DIR/$DEST_DIR/generated/out.ts" "generated/tmp.ts"
 
-	grep "|{n:" "generated/out.ts.bak" >"$TMP_DIR/tmp_partial.ts"
-	generate_typescript_code_force_valid >"$TMP_DIR/tmp.ts"
-	tsc -p "$TMP_DIR/userscript" >"$TMP_DIR/errors.out"
-	gen_code_v1
-	generate_ts-backup_output_at_tmp
-	generate_ts-restore
-	popd
+		grep "|{n:" "generated/out.ts.bak" >"$TMP_DIR/tmp_partial.ts"
+		generate_typescript_code_force_valid >"$TMP_DIR/tmp.ts"
+		tsc -p "$TMP_DIR/userscript" >"$TMP_DIR/errors.out"
+		gen_code_v1
+		generate_ts-backup_output_at_tmp
+		generate_ts-restore
+		popd
+	}
+	generate_typescript_code_unique_final >$DEST_DIR/generated/out.ts
 }
