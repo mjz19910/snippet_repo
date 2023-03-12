c7bab6f71f88e5a04047fa8e107d418984833bd6
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-12 14:20:47 -0700

    u

--- browser_js/javascript/group2/item_02.js
+++ browser_js/javascript/group2/item_02.js
@@ -4,8 +4,8 @@ v1 (old): snippet_repo_v2/javascript/group2/item_02.js
 v2 (cur): snippet_repo_v2/javascript/group2/item_02.js
 */
 x: {
-	if(!('temp1' in window)) {
-		if(queryObjects) {
+	if (!('temp1'in window)) {
+		if (queryObjects) {
 			console.log("please export the queryObjects result as temp1");
 			queryObjects(Function.prototype);
 		} else {
@@ -14,12 +14,15 @@ x: {
 		break x;
 	}
 	/** @arg {any} v */
-	function any(v) {return v;}
+	function any(v) {
+		return v;
+	}
 	/** @type {{}[]} */
-	let tmp=any(window.temp1);
-	let GeneratorFunction=(function*() {}).prototype.constructor;
-	let not_proto=tmp.filter(e => e!==Function.prototype&&e!==GeneratorFunction&&e!==GeneratorFunction.constructor);
-	let fn_map_str=[...new Set(not_proto.map(e => {
+	let tmp = any(window.temp1);
+	let GeneratorFunction = (function*() {}
+	).prototype.constructor;
+	let not_proto = tmp.filter(e=>e !== Function.prototype && e !== GeneratorFunction && e !== GeneratorFunction.constructor);
+	let fn_map_str = [...new Set(not_proto.map(e=>{
 		try {
 			return e.toString();
 		} catch {
@@ -27,16 +30,17 @@ x: {
 		}
 	}
 	))];
-	let ids=not_proto.map(e => {
+	let ids = not_proto.map(e=>{
 		try {
-			return [e,fn_map_str.indexOf(e.toString())];
-		} catch(x) {
-			throw new AggregateError([e,x],"reasons");
+			return [e, fn_map_str.indexOf(e.toString())];
+		} catch (x) {
+			throw new AggregateError([e, x],"reasons");
 		}
 	}
 	);
-	for(let x of ids) {
-		if(fn_map_str[any(x[1])]&&!fn_map_str[any(x[1])]?.includes("gold")) continue;
-		console.log(x[0],fn_map_str[any(x[1])]);
+	for (let x of ids) {
+		if (fn_map_str[any(x[1])] && !fn_map_str[any(x[1])]?.includes("gold"))
+			continue;
+		console.log(x[0], fn_map_str[any(x[1])]);
 	}
 }
