9aa5e87175293a2b077ab2e64c508064d6894756
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-13 09:01:30 -0700

    u

---
+++ userscript/youtube_plugin_raw/zb_plugin_types/ServiceLoader.ts
@@ -0,0 +1,1 @@
+type ServiceLoader=InstanceType<PluginStore["mod$ServiceLoaderPlugin"]["ServiceLoader"]>;
--- userscript/youtube_plugin_raw/zc_child_modules/a_exports.ts
+++ userscript/youtube_plugin_raw/zc_child_modules/a_exports.ts
@@ -1,6 +1,4 @@
 type ServiceResolver<T,U>=import("./YtPlugin_Base.user.js").ServiceResolver<T,U>;
-// circular import when using instance type
-type ServiceLoader=import("./YtPlugin_ServiceLoader_Plugin.user.js").ServiceLoader;
 interface PluginStore {
 	mod$ServiceLoaderPlugin: typeof import("./YtPlugin_ServiceLoader_Plugin.user.js");
 	mod$YoutubePluginBase: typeof import("./YtPlugin_Base.user.js");
