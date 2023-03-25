090fd48a2ca4290c41f908e7c831f80b54243fb9
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-24 21:37:25 -0700

    u

--- bitburner_js/code-contract/modules/solver.compression3/mod.js
+++ bitburner_js/code-contract/modules/solver.compression3/mod.js
@@ -17,35 +17,31 @@ function to_buffer_obj(arr) {
 	let not_parts=arr.filter(exclude_part_arr);
 	return {part: parts[0],_: not_parts};
 }
-
-/** @arg {string} input */
-function solve(input) {
+function process_not_len6_arr(r_not_len6) {
 	let show_l2_rest=false;
 	let show_l2_before=true;
+	if(r_not_len6.length===0) return;
+	let sel2=min_len_sel(r_not_len6); sel2;
+	let sel_before=sel2[0];
+	if(sel_before[1]!=="before") throw new Error("Unhandled");
+	let before_compress=sel_before[4];
+	if(!before_compress) throw new Error("Unhandled");
+	if(show_l2_before) {
+		log_sel(sel2);
+		console.log("sel_before: ",[before_compress[0]]);
+		console.log("sel_before.item: ",[[before_compress[0][2]]]);
+		console.log("sel_mode_after: ",before_compress[0][2]);
+	}
+	let sel2_rest=sel2[4];
+	if(sel2_rest[1]!=="rest") throw new Error("Unhandled");
+	if(show_l2_rest) console.log("i2:",sel2_rest);
+}
+/** @arg {string} input */
+function solve(input) {
 
 	let r1=calc_compression_step1(input);
 	let [r_not_len6,r2_len6]=calc_compression_step2(r1);
-	if(r_not_len6.length>0) {
-		let sel2=min_len_sel(r_not_len6); sel2;
-		let sel_before=sel2[0];
-		if(sel_before[1]==="before") {
-			let before_compress=sel_before[4];
-			if(!before_compress) throw new Error("TODO");
-			if(show_l2_before) {
-				log_sel(sel2);
-				console.log("sel_before: ",[before_compress[0]]);
-				console.log("sel_before.item: ",[[before_compress[0][2]]]);
-				console.log("sel_mode_after: ",before_compress[0][2]);
-			}
-			else throw new Error("TODO");
-		}
-		else throw new Error("TODO");
-		let sel2_rest=sel2[4];
-		if(sel2_rest[1]==="rest") {
-			if(show_l2_rest) console.log("i2:",sel2_rest);
-		}
-		else throw new Error("TODO");
-	}
+	process_not_len6_arr(r_not_len6);
 	let part_uniq=calc_compression_step3(r2_len6);
 	return `${part_uniq[0]}:${part_uniq[1]}`;
 }
