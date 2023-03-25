09ca17bac0fbdbd2811025a69bf622d175f7d1cd
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-24 21:15:16 -0700

    u

--- bitburner_js/code-contract/modules/solver.compression3/lz_compress1.js
+++ bitburner_js/code-contract/modules/solver.compression3/lz_compress1.js
@@ -8,6 +8,8 @@ export function lz_compress1(input,part) {
 	let first_idx=input.indexOf(part);
 	let next_idx=input.indexOf(part,first_idx+1);
 	if(next_idx===-1) return [["1","data",input.length,input]];
+	/** @type {LZBufferItem[]} */
+	let out=[];
 	if(first_idx===0) {
 		let cp_1=lz_compress_mode1(input,first_idx,part.length,next_idx);
 		let p3=input.slice(next_idx,next_idx+part.length);
@@ -17,17 +19,15 @@ export function lz_compress1(input,part) {
 		out.push(...cp_1);
 		out.push(['1',"part",p3.length,p3]);
 		add_opts_lz_item(out,"rest",p4);
-		return out;
+	} else {
+		let p1=input.slice(0,first_idx);
+		let cp_1=lz_compress_mode1(input,first_idx,part.length,next_idx);
+		let p3=input.slice(next_idx,next_idx+part.length);
+		let p4=input.slice(next_idx+part.length+1);
+		add_opts_lz_item(out,"before",p1);
+		out.push(...cp_1);
+		out.push(['1',"part",p3.length,p3]);
+		add_opts_lz_item(out,"rest",p4);
 	}
-	let p1=input.slice(0,first_idx);
-	let cp_1=lz_compress_mode1(input,first_idx,part.length,next_idx);
-	let p3=input.slice(next_idx,next_idx+part.length);
-	let p4=input.slice(next_idx+part.length+1);
-	/** @type {LZBufferItem[]} */
-	let out=[];
-	add_opts_lz_item(out,"before",p1);
-	out.push(...cp_1);
-	out.push(['1',"part",p3.length,p3]);
-	add_opts_lz_item(out,"rest",p4);
 	return out;
 }
