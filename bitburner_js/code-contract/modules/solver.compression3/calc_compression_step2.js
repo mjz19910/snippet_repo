bc66fad4f78b8e4db2c9ad42b45e5287dda62800
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-24 21:00:27 -0700

    u

---
+++ bitburner_js/code-contract/modules/solver.compression3/calc_compression_step2.js
@@ -0,0 +1,22 @@
+import {calc_compression_step1} from "./calc_compression_step1.js";
+import {compress_set_exclude_len} from "./compress_set_exclude_len.js";
+import {compress_set_extract_len} from "./compress_set_extract_len.js";
+import {log_sel} from "./log_sel.js";
+import {min_len_sel} from "./min_len_sel.js";
+
+/** @arg {string} input */
+export function calc_compression_step2(input) {
+	let r1=calc_compression_step1(input);
+	let r2=compress_set_exclude_len(r1,6);
+	let r2_n=compress_set_extract_len(r1,6);
+	if(r2.length>0) {
+		let sel2=min_len_sel(r2); sel2;
+		let sel_before=sel2[0];
+		let before_compress=sel_before[4];
+		if(before_compress) {
+			log_sel(sel2,"sel_before: ",before_compress[0]);
+		}
+		else {throw new Error("TODO");}
+	}
+	return r2_n;
+}
--- bitburner_js/code-contract/modules/solver.compression3/mod.js
+++ bitburner_js/code-contract/modules/solver.compression3/mod.js
@@ -1,30 +1,11 @@
-import {calc_compression_step1} from "./calc_compression_step1.js";
-import {compress_set_exclude_len} from "./compress_set_exclude_len.js";
-import {compress_set_extract_len} from "./compress_set_extract_len.js";
+import {calc_compression_step2} from "./calc_compression_step2.js";
 import {exclude_null} from "./exclude_null.js";
 import {find_part_buf_arr} from "./find_part_buf_arr.js";
-import {log_sel} from "./log_sel.js";
-import {min_len_sel} from "./min_len_sel.js";
-/** @arg {string} input */
-function calc_compression_step2(input) {
-
-}
 
 /** @arg {string} input */
 function solve(input) {
-	let r1=calc_compression_step1(input);
-	let r2=compress_set_exclude_len(r1,6);
-	let r2_n=compress_set_extract_len(r1,6);
+	let r2_n=calc_compression_step2(input);
 	let r2_part=r2_n.map(v => find_part_buf_arr(v));
-	if(r2.length>0) {
-		let sel2=min_len_sel(r2); sel2;
-		let sel_before=sel2[0];
-		let before_compress=sel_before[4];
-		if(before_compress) {
-			log_sel(sel2,"sel_before: ",before_compress[0]);
-		}
-		else {throw new Error("TODO");}
-	}
 	let r2_2=r2_part.filter(exclude_null);
 	let part_sel_arr=r2_2.map(v => v[3]);
 	let part_uniq=[];
