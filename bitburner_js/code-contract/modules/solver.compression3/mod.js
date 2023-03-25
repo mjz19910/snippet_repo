fa2c734e00ba1882f05d6774abf3eb0737eb6c49
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-24 21:21:45 -0700

    u

--- bitburner_js/code-contract/modules/solver.compression3/mod.js
+++ bitburner_js/code-contract/modules/solver.compression3/mod.js
@@ -5,6 +5,8 @@ import {log_sel} from "./log_sel.js";
 import {min_len_sel} from "./min_len_sel.js";
 /** @arg {string} input */
 function solve(input) {
+	let show_l2_rest=false;
+
 	let r1=calc_compression_step1(input);
 	let [r_not_len6,r2_len6]=calc_compression_step2(r1);
 	if(r_not_len6.length>0) {
@@ -18,8 +20,11 @@ function solve(input) {
 			else throw new Error("TODO");
 		}
 		else throw new Error("TODO");
-		let sel2_i1=sel2[4];
-		console.log(sel2_i1);
+		let sel2_rest=sel2[4];
+		if(sel2_rest[1]==="rest") {
+			if(show_l2_rest) console.log("i2:",sel2_rest);
+		}
+		else throw new Error("TODO");
 	}
 	let part_uniq=calc_compression_step3(r2_len6);
 	return `${part_uniq[0]}:${part_uniq[1]}`;
