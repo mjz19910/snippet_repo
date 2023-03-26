c32aa058eca2994ea9b6898597fee776b1c96772
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-26 06:30:17 -0700

    u

--- bitburner_js/net-fs/tix-auto.js
+++ bitburner_js/net-fs/tix-auto.js
@@ -1,25 +1,32 @@
 /** @param {NS} ns */
 export async function main(ns) {
+	ns.tail();
 	let symbols=ns.stock.getSymbols();
 	let stock_ask_price=new Map;
 	for(let cur_sym of symbols) {
 		let ask_price=ns.stock.getAskPrice(cur_sym);
 		stock_ask_price.set(cur_sym,ask_price);
 	}
-	let chk_sym=symbols[0];
-	let cur_ask_price=ns.stock.getAskPrice(chk_sym);
-	let prev_ask_price=cur_ask_price;
-	const wait_seconds=4;
-	while(cur_ask_price===prev_ask_price) {
-		cur_ask_price=ns.stock.getAskPrice(chk_sym);
-		ns.tprintf(`sleep: ${wait_seconds}s`);
-		await ns.sleep(wait_seconds*1000);
-	}
+	const wait_seconds=5;
+	await ns.sleep(wait_seconds*1000);
+	/** @type {Map<string,number[]>} */
 	let stock_ask_price_change_rate=new Map;
 	for(let cur_sym of symbols) {
 		let ask_price=ns.stock.getAskPrice(cur_sym);
 		let prev_price=stock_ask_price.get(cur_sym);
 		stock_ask_price_change_rate.set(cur_sym,[prev_price-ask_price]);
-		ns.tprintf("%s: %s",cur_sym,ns.formatNumber(prev_price-ask_price));
+		stock_ask_price.set(cur_sym,ask_price);
+	}
+	await ns.sleep(wait_seconds*1000);
+	for(let cur_sym of symbols) {
+		let ask_price=ns.stock.getAskPrice(cur_sym);
+		let prev_price=stock_ask_price.get(cur_sym);
+		const price_arr=stock_ask_price_change_rate.get(cur_sym);
+		if(!price_arr) continue;
+		price_arr.push(prev_price-ask_price);
+		ns.tprintf("%s:\t%s",cur_sym,price_arr.map(v => ns.formatNumber(v,0)).map(s => {
+			ns.print(s.length);
+			return s;
+		}).join(",\t"));
 	}
 }
