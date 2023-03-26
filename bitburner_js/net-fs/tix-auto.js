df2a1d97153a9390fb892fc6d0921148249e6a6f
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-26 06:45:47 -0700

    u

--- bitburner_js/net-fs/tix-auto.js
+++ bitburner_js/net-fs/tix-auto.js
@@ -17,26 +17,29 @@ export async function main(ns) {
 		stock_ask_price_change_rate.set(cur_sym,[prev_price-ask_price]);
 		stock_ask_price.set(cur_sym,ask_price);
 	}
-	await ns.sleep(wait_seconds*1000);
-	ns.tprintf("--- TIX Start ---");
-	for(let cur_sym of symbols) {
-		let ask_price=ns.stock.getAskPrice(cur_sym);
-		let prev_price=stock_ask_price.get(cur_sym);
-		const price_arr=stock_ask_price_change_rate.get(cur_sym);
-		if(!price_arr) continue;
-		price_arr.push(prev_price-ask_price);
-		ns.tprintf("%s:\t%s",cur_sym,price_arr.map(v => ns.formatNumber(v,0)).map(s => {
-			if(s.startsWith("-")) {
-				let w=s.split("-")[1];
-				if(w.length===1) return "-  "+w;
-				if(w.length===2) return "- "+w;
-				if(w.length===3) return "-"+w;
-			}
-			if(s.length===1) return "   "+s;
-			if(s.length===2) return "  "+s;
-			if(s.length===3) return " "+s;
-			ns.print("len: ",s.length," ",s);
-			return s;
-		}).join(",\t"));
+	for(;;) {
+		await ns.sleep(wait_seconds*1000);
+		ns.tprintf("--- TIX Start ---");
+		for(let cur_sym of symbols) {
+			let ask_price=ns.stock.getAskPrice(cur_sym);
+			let prev_price=stock_ask_price.get(cur_sym);
+			stock_ask_price.set(cur_sym,ask_price);
+			const price_arr=stock_ask_price_change_rate.get(cur_sym);
+			if(!price_arr) continue;
+			price_arr.push(prev_price-ask_price);
+			ns.tprintf("%s:\t%s",cur_sym,price_arr.map(v => ns.formatNumber(v,0)).map(s => {
+				if(s.startsWith("-")) {
+					let w=s.split("-")[1];
+					if(w.length===1) return "-  "+w;
+					if(w.length===2) return "- "+w;
+					if(w.length===3) return "-"+w;
+				}
+				if(s.length===1) return "   "+s;
+				if(s.length===2) return "  "+s;
+				if(s.length===3) return " "+s;
+				ns.print("len: ",s.length," ",s);
+				return s;
+			}).join(",\t"));
+		}
 	}
 }
