7d5c33b667c09b525ad6412d84ad32982968f2f5
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-26 02:33:27 -0700

    u

--- bitburner_js/net-fs/empty-port.js
+++ bitburner_js/net-fs/empty-port.js
@@ -2,8 +2,6 @@ import {read_port1_msg,read_port2_msg} from "/hack-support-v3.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
-	ns.tail();
-	ns.clearLog();
 	let msg=read_port1_msg(ns);
 	ns.tprint(msg);
 	while(msg!==null) {
