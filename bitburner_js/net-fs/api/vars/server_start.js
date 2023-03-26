a7d58ee24ed5134fe2828f05d8f1285375ad1af4
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-26 05:07:51 -0700

    u

--- bitburner_js/net-fs/api/vars/server_start.js
+++
@@ -1,2 +0,0 @@
-export const hack_template_v2="/_auto/early-hack-template-v2.js";
-export const hack_template_v3="hack-template-v3.js";
---
+++ bitburner_js/net-fs/run/script_paths.js
@@ -0,0 +1,1 @@
+export const hack_template_v3="/run/hack-template-v3.js";
