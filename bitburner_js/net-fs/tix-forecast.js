0af07944b4bd3756aa126cbb149dc98e06166eee
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-26 07:21:48 -0700

    u

--- bitburner_js/net-fs/tix-forecast.js
+++ bitburner_js/net-fs/tix-forecast.js
@@ -10,8 +10,11 @@ export async function main(ns) {
 	const num_map=new Map;
 
 	const symbols=tix.getSymbols();
-	for(;;) {
-		ns.printf("--- TIX Forecast ---");
+	for(let print_header=true;;) {
+		if(print_header) {
+			ns.printf("--- TIX Forecast ---");
+			print_header=false;
+		}
 		for(let sym of symbols.slice(0,18)) {
 			const cur=tix.getForecast(sym);
 			let prev=num_map.get(sym);
@@ -19,6 +22,7 @@ export async function main(ns) {
 			if(prev===cur) continue;
 			num_map.set(sym,cur);
 			ns.printf("%s:\t%s%%",sym,ns.formatNumber(tix.getForecast(sym)*100,3));
+			print_header=true;
 		}
 		await ns.sleep(1000);
 	}
