abd55444425a974774e2aa779ad969e9185256a0
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-26 05:04:27 -0700

    u

--- bitburner_js/net-fs/api/search/show-nearby.js
+++ bitburner_js/net-fs/api/search/show-nearby.js
@@ -1,24 +1,30 @@
+import {as} from "../helper/as.js";
+
 /** @param {NS} ns */
 export async function main(ns) {
 	// All servers that are one hop from the current server.
 	ns.tprint("All servers.");
-	let scanned_set = new Set;
-	let server_paths = new Map;
-	const start_server = ns.args[0];
-	let n_stack = [start_server];
-	server_paths.set(start_server, start_server);
-	let cur_pos = null;
+	let scanned_set=new Set;
+	let server_paths=new Map;
+	/** @type {ScriptFlags} */
+	let f_=as(ns.flags([]));
+	const start_server=f_._[0];
+	let n_stack=[start_server];
+	server_paths.set(start_server,start_server);
+	let cur_pos=null;
 	do {
-		cur_pos = n_stack.shift();
-		if (cur_pos === void 0) break;
-		if (scanned_set.has(cur_pos)) continue;
-		let neighbors = ns.scan(cur_pos);
+		cur_pos=n_stack.shift();
+		if(cur_pos===void 0) break;
+		if(scanned_set.has(cur_pos)) continue;
+		let neighbors=ns.scan(cur_pos);
 		scanned_set.add(cur_pos);
-		for (let i = 0; i < neighbors.length; i++) {
-			const neighbor = neighbors[i];
-			if (scanned_set.has(neighbor)) continue;
+		for(let i=0;i<neighbors.length;i++) {
+			const neighbor=neighbors[i];
+			if(scanned_set.has(neighbor)) continue;
 			n_stack.push(neighbor);
 			ns.tprint(neighbor,",");
 		}
-	} while (cur_pos !== void 0);
+	} while(cur_pos!==void 0);
 }
+
+/** @typedef {{_:[string]}} ScriptFlags */
