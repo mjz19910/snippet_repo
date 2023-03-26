08fa623fb312227082570d8925568e479431f8fc
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-26 00:17:48 -0700

    u

--- bitburner_js/net-fs/home/old/early-hack-template.js
+++ bitburner_js/net-fs/home/old/early-hack-template.js
@@ -1,35 +1,36 @@
+// @ts-nocheck
 /** @param {NS} ns */
 export async function main(ns) {
     // Defines the "target server", which is the server
     // that we're going to hack. In this case, it's "n00dles"
-    let target = ns.args[1] > 10 ? "joesguns" : "n00dles";
-    if (ns.args[1] > 80) target = "harakiri-sushi";
-    if (ns.args[1] > 95) target = "neo-net";
+    let target=ns.args[1]>10? "joesguns":"n00dles";
+    if(ns.args[1]>80) target="harakiri-sushi";
+    if(ns.args[1]>95) target="neo-net";
 
     // Defines how much money a server should have before we hack it
     // In this case, it is set to 75% of the server's max money
-    const moneyThresh = ns.getServerMaxMoney(target) * 0.85;
+    const moneyThresh=ns.getServerMaxMoney(target)*0.85;
 
     // Defines the maximum security level the target server can
     // have. If the target's security level is higher than this,
     // we'll weaken it before doing anything else
-    const securityThresh = ns.getServerMinSecurityLevel(target) + .5;
+    const securityThresh=ns.getServerMinSecurityLevel(target)+.5;
 
     // If we have the BruteSSH.exe program, use it to open the SSH Port
     // on the target server
-    if (ns.fileExists("BruteSSH.exe", "home")) {
+    if(ns.fileExists("BruteSSH.exe","home")) {
         ns.brutessh(target);
     }
 
     // Get root access to target server
     ns.nuke(target);
 
-    // Infinite loop that continously hacks/grows/weakens the target server
-    while (true) {
-        if (ns.getServerSecurityLevel(target) > securityThresh) {
+    // Infinite loop that continuously hacks/grows/weakens the target server
+    while(true) {
+        if(ns.getServerSecurityLevel(target)>securityThresh) {
             // If the server's security level is above our threshold, weaken it
             await ns.weaken(target);
-        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+        } else if(ns.getServerMoneyAvailable(target)<moneyThresh) {
             // If the server's money is less than our threshold, grow it
             await ns.grow(target);
         } else {
