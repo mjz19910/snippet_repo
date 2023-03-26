93d1f77fa0c3dca89a41b0f763d3afb11b571383
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-26 01:51:05 -0700

    u

---
+++ bitburner_js/net-fs/port-dbg.js
@@ -0,0 +1,19 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	ns.tail();
+	ns.clearLog();
+	let delayed_messages=[];
+	let handle=ns.getPortHandle(3);
+	if(handle.empty()) {
+		ns.print("empty port");
+		return;
+	}
+	while(!handle.empty()) {
+		delayed_messages.push(handle.read());
+	}
+	ns.print(delayed_messages);
+	for(let msg of delayed_messages) {
+		let removed_msg=handle.write(msg);
+		if(removed_msg!==null) ns.print("dropped: ",removed_msg);
+	}
+}
