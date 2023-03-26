5d6a67daab550a568b551cd3ed6f5a76ad726449
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-26 02:06:12 -0700

    u

--- bitburner_js/net-fs/port-dbg.js
+++ bitburner_js/net-fs/port-dbg.js
@@ -2,21 +2,27 @@
 export async function main(ns) {
 	ns.tail();
 	ns.clearLog();
-	let delayed_messages=[];
-	let handle=ns.getPortHandle(3);
-	if(handle.empty()) {
-		ns.print("empty port");
-		return;
+	/** @param {number} port */
+	function debug_port_handle(port) {
+		let delayed_messages=[];
+		let handle=ns.getPortHandle(port);
+		if(handle.empty()) {
+			ns.print("empty port");
+			return;
+		}
+		while(!handle.empty()) {
+			delayed_messages.push(handle.read());
+		}
+		ns.print(port,": ",delayed_messages.map(v => {
+			if(typeof v==="string") return JSON.parse(v);
+			return v;
+		}));
+		for(let msg of delayed_messages) {
+			let removed_msg=handle.write(msg);
+			if(removed_msg!==null) ns.print("dropped: ",removed_msg);
+		}
 	}
-	while(!handle.empty()) {
-		delayed_messages.push(handle.read());
-	}
-	ns.print("3: ",delayed_messages.map(v => {
-		if(typeof v==="string") return JSON.parse(v);
-		return v;
-	}));
-	for(let msg of delayed_messages) {
-		let removed_msg=handle.write(msg);
-		if(removed_msg!==null) ns.print("dropped: ",removed_msg);
-	}
+	debug_port_handle(1);
+	debug_port_handle(2);
+	debug_port_handle(3);
 }
