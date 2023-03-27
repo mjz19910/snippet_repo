e406e2e4a8318c5916c537cfee90f64e60550160
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-26 19:07:47 -0700

    u

--- bitburner_js/net-fs/backdoor-auto.js
+++ bitburner_js/net-fs/backdoor-auto.js
@@ -3,16 +3,30 @@ import {as_any} from "./run/as.js";
 /** @param {NS} ns */
 export async function main(ns) {
 	ns.tail();
+	ns.disableLog("disableLog");
+	ns.disableLog("sleep");
 	const terminalInput_nt=globalThis["document"].getElementById("terminal-input");
 	if(!terminalInput_nt) {
 		ns.tprint("not at terminal");
 		return;
 	}
 	if(!(terminalInput_nt instanceof HTMLInputElement)) ns.exit();
-	let scan_res=ns.scan();
-	for(let hostname of scan_res) {
+	let seen_hosts=new Set(["home"]);
+	/** @type {{[x:string]:Server}} */
+	let server_map={};
+	/** @type {([{final:string},{path:string[]}])[]} */
+	let scan_res=ns.scan().map(v => [{final: v},{path: []}]);
+	for(let host_desc of scan_res) {
+		const hostname=host_desc[0].final;
 		if(hostname.startsWith("big-")) continue;
+		let next=ns.scan(hostname);
+		for(let res of next) {
+			if(seen_hosts.has(res)) continue;
+			seen_hosts.add(res);
+			scan_res.push([{final: res},{path: [...host_desc[1].path,hostname]}]);
+		}
 		let srv=ns.getServer(hostname);
+		server_map[hostname]=srv;
 		if(srv.purchasedByPlayer) continue;
 		if(!srv.hasAdminRights) continue;
 		if(srv.backdoorInstalled) continue;
@@ -25,10 +39,21 @@ export async function main(ns) {
 			terminalInput[handler].onChange({target: terminalInput});
 			terminalInput[handler].onKeyDown({key: 'Enter',preventDefault: () => null});
 		}
-		start_terminal_command(terminalInput,`connect ${hostname};backdoor`);
+		let full_path=[...host_desc[1].path,hostname].filter((v,idx,arr) => {
+			if(idx+1>=arr.length) return !server_map[v].backdoorInstalled;
+			let v2=arr[idx+1];
+			return !server_map[v2].backdoorInstalled;
+		});
+		start_terminal_command(terminalInput,`connect ${full_path.reduce((p,c) => `${p};connect ${c}`)};backdoor`);
 		let delay=ns.getHackTime(hostname); delay;
-		await ns.sleep(5000);
-		console.log(terminalInput);
+		let acc_delay=0;
+		for(;;) {
+			await ns.sleep(80);
+			acc_delay+=80;
+			let has_disabled=terminalInput.classList.contains("Mui-disabled");
+			if(!has_disabled) break;
+		}
+		console.log(delay/acc_delay,delay,acc_delay);
 		start_terminal_command(terminalInput,"home");
 	}
 }
