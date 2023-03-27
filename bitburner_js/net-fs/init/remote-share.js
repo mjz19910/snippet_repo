f9733d5e845e951bf85905c6d3a252b298569df8
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-27 09:16:01 -0700

    u

--- bitburner_js/net-fs/init/remote-share.js
+++ bitburner_js/net-fs/init/remote-share.js
@@ -9,30 +9,35 @@ export async function main(ns) {
 	ns.disableLog("exec");
 	ns.disableLog("scan");
 	const share_script="/api/share.js";
-	let seen_srv=new Set;
-	let servers_arr=["home"];
-	for(let cur_srv;cur_srv=servers_arr.shift();) {
-		seen_srv.add(cur_srv);
-		/** @type {number|null} */
-		let pid=null;
-		let srv=cur_srv;
-		x: {
-			let hosts=ns.scan(srv);
-			for(let host of hosts) {
-				if(seen_srv.has(host)) continue;
-				servers_arr.push(host);
-			}
-			if(!ns.ls(srv).includes(share_script)) {
-				ns.scp([share_script],srv);
-			}
-			ns.killall(srv);
-			const server_ram=ns.getServerMaxRam(srv);
-			if(server_ram===0) break x;
-			let thread_n=server_ram/4|0;
-			pid=ns.exec(share_script,srv,thread_n,"auto",srv);
+	const seen_srv=new Set;
+	const servers_arr=[];
+	let hostname="home";
+	for(;;) {
+		let next=servers_arr.shift();
+		if(next===void 0) break;
+		hostname=next;
+		seen_srv.add(hostname);
+		const hosts=ns.scan(hostname);
+		for(const host of hosts) {
+			if(seen_srv.has(host)) continue;
+			servers_arr.push(host);
 		}
-		if(!pid) continue;
-		await ns.sleep(33);
-		ns.closeTail(pid);
+		if(hostname==="home") continue;
+		if(!ns.ls(hostname).includes(share_script)) {
+			ns.scp([share_script],hostname);
+		}
+		ns.killall(hostname);
+		const server_ram=ns.getServerMaxRam(hostname);
+		if(server_ram===0) continue;
+		const thread_n=server_ram/4|0;
+		const pid=ns.exec(share_script,hostname,thread_n,"auto",hostname);
+		if(pid===0) {
+			ns.print("failed to start ",share_script," on ",hostname);
+			ns.exit();
+		}
 	}
+	hostname="home";
+	const server_ram=ns.getServerMaxRam(hostname);
+	const thread_n=server_ram/4|0;
+	ns.spawn(share_script,thread_n,"auto",hostname);
 }
