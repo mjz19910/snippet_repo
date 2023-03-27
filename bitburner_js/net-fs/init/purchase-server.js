2cc975d44cec6871a6b2400c508d3b6a44cb8a25
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-27 14:48:00 -0700

    u

--- bitburner_js/net-fs/init/purchase-server.js
+++ bitburner_js/net-fs/init/purchase-server.js
@@ -74,7 +74,7 @@ export async function main(ns) {
 	ns.print("upg_ram: ",ns.formatRam(ram));
 	const buy_cost1=ns.getPurchasedServerCost(ram)-prev_ram;
 	let cur_server_money=ns.getServerMoneyAvailable("home");
-	while((buy_cost1*10)>cur_server_money) {
+	while((buy_cost1*25*10)>cur_server_money) {
 		await upgrade_purchased_server_list(prev_ram,ram,server_hostname_list);
 		prev_ram=ram; ram*=2;
 		ns.print("upg_ram: ",ns.formatRam(ram));
