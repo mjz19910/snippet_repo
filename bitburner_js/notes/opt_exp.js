a49a37933aeebd174bf47361378b98d368d372c4
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-27 03:59:30 -0700

    u

--- bitburner_js/notes/opt_exp.js
+++ bitburner_js/notes/opt_exp.js
@@ -30,7 +30,30 @@ function log_university_min(key,exp_rate,cost_rate,calc_base) {
 	if(calc_base!==void 0) console.log("ratio:",(val/calc_base).toFixed(0));
 	console.log(key+":",val,"exp/sec",cost_rate,"cost/sec");
 }
+/**
+ * @param {string} key
+ * @param {number} exp_rate
+ * @param {number} cost_rate
+ * @param {number} [calc_base]
+ */
+function log_gym_min(key,exp_rate,cost_rate,calc_base) {
+	if(cost_rate>0) throw new Error("Invalid cost rate: "+cost_rate.toFixed(0));
+	const val=round_float(exp_rate);
+	if(calc_base!==void 0) console.log("ratio:",(val/calc_base).toFixed(0));
+	let cost_str;
+	if(cost_rate<-1000) {
+		cost_str=`${(cost_rate/1000).toFixed(3)}k`;
+	} else cost_str=""+cost_rate;
+	if(typeof document==="undefined") {
+		console.log(`${key}: %o exp/sec \x1b[33m%s\x1b[0m cost/sec`,val,cost_str);
+	}
+	else {
+		console.log(`${key}: %o exp/sec %c%s%c cost/sec`,val,"color: yellow;",cost_str,"");
+	}
+}
 function main() {
+	const json_mults=JSON.parse('{"hacking_chance":1.8598182764745166,"hacking_speed":1.8505297937672616,"hacking_money":3.1076812888716248,"hacking_grow":1.861109886735911,"hacking":2.9804916217637816,"strength":2.1358245948972256,"defense":2.0429626559886507,"dexterity":2.6911389895705042,"agility":2.0476057529340794,"charisma":2.0635986424127784,"hacking_exp":4.122273968381145,"strength_exp":2.0717875259144165,"defense_exp":2.0717875259144165,"dexterity_exp":2.0717875259144165,"agility_exp":2.0717875259144165,"charisma_exp":2.0717875259144165,"company_rep":2.4861450310973,"faction_rep":1.883443205376742,"crime_money":2.0472208754095025,"crime_success":1.4888879093887288,"hacknet_node_money":3.7551148907198795,"hacknet_node_purchase_cost":0.5138063081686742,"hacknet_node_ram_cost":0.6716422329002277,"hacknet_node_core_cost":0.6716422329002277,"hacknet_node_level_cost":0.5708958979651936,"work_money":1.6377767003276018,"bladeburner_max_stamina":1,"bladeburner_stamina_gain":1,"bladeburner_analysis":1,"bladeburner_success_chance":1}');
+	json_mults;
 	const mults={
 		hacking_exp: 4.122273968381145,
 		charisma_exp: 2.0717875259144165,
@@ -54,10 +77,10 @@ function main() {
 	// log_crime_min("rob_store.hack",112.426,1,0);
 	// log_crime_min("rob_store.dex",84.755,1,0);
 	// log_crime_min("shoplift",3.767,0,2);
-	{
+	let is_enabled_rothman_uni=false;
+	if(is_enabled_rothman_uni) {
 		const cur="rothman_uni";
-		let log_hack=false;
-		if(log_hack) {
+		{
 			const base=mults.hacking_exp;
 			// Computer Science
 			log_university_min(`${cur}.hack1`,base,0);
@@ -71,6 +94,35 @@ function main() {
 			log_university_min(`${cur}.cha1`,base,-432);
 			log_university_min(`${cur}.cha2`,base*2,-864);
 		}
+	}
+	{
+		const cur="summit_uni";
+		/** @type {()=>`hack${1|2|3}`|`cha${1|2}`} */
+		function get_sel() {return "hack1";}
+		const sel=get_sel();
+		{
+			const base=mults.hacking_exp;
+			// Computer Science
+			if(sel==="hack1") log_university_min(`${cur}.hack1`,base,0);
+			// Data Structures
+			if(sel==="hack2") log_university_min(`${cur}.hack2`,base*2,0);
+			// Networks
+			if(sel==="hack3") log_university_min(`${cur}.hack3`,base*4,0);
+		}
+		{
+			const base=mults.charisma_exp;
+			if(sel==="cha1") log_university_min(`${cur}.cha1`,base,-432);
+			if(sel==="cha2") log_university_min(`${cur}.cha2`,base*2,-864);
+		}
+	}
+	let is_gym=false;
+	if(is_gym) {
+		const cur="snap_fitness_gym";
+		log_gym_min(`${cur}.str`,10.359,-1080);
+	}
+	if(is_gym) {
+		const cur="crush_fitness_gym";
+		log_gym_min(`${cur}.str`,4.144,-324);
 	}
 }
 main();
