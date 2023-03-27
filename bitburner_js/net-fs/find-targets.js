8794e5a022633355f8f47a146457f62cc4678b4f
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-27 16:19:24 -0700

    u

---
+++ bitburner_js/net-fs/find-targets.js
@@ -0,0 +1,63 @@
+import {as,canHack,getNetworkNodes,get_keys_of} from "/utils.js";
+
+/** @param {NS} ns */
+export async function main(ns) {
+	/** @type {{compareField:"maxMoney"|"hackChance"}} */
+	let f_=as(ns.flags([
+		["compareField","maxMoney"],
+	]));
+	var compareField=f_.compareField;
+	var player=ns.getPlayer();
+	var filename="network-report.txt";
+
+	/** @param {string} node @returns {NodeInfoType} */
+	function getNodeInfo(node) {
+		var server=ns.getServer(node);
+		var maxMoney=server.moneyMax;
+		var chance=ns.formulas.hacking.hackChance(server,player);
+		var reqHackLevel=server.requiredHackingSkill;
+		return {
+			node,
+			maxMoney,
+			hackChance: chance,
+			reqHackLevel,
+		};
+	}
+
+	/** @param {NodeInfoType[]} nodes */
+	async function writeNodesToFile(nodes) {
+		let lines=[];
+		for(let node of nodes) {
+			for(var field of get_keys_of(node)) {
+				var value=node[field];
+				lines.push(field+": "+value);
+			}
+			lines.push("");
+		}
+		var fileContent=lines.join("\n");
+		ns.write(filename,fileContent,"w");
+		ns.alert(fileContent);
+		ns.toast("Wrote targets to "+filename,"info");
+	}
+
+	/** @returns {(a:NodeInfoType,b:NodeInfoType)} */
+	function getComparator() {
+		return (a,b) => {
+			if(a[compareField]>b[compareField]) {
+				return -1;
+			} else if(a[compareField]<b[compareField]) {
+				return 1;
+			} else {
+				return 0;
+			}
+		};
+	}
+
+	var networkNodes=getNetworkNodes(ns);
+	var hackableNodes=networkNodes.filter(node => canHack(ns,node)&&!node.includes("big-"));
+	var nodeDetails=hackableNodes.map(node => getNodeInfo(node));
+	var nodeAsc=nodeDetails.sort(getComparator());
+	writeNodesToFile(nodeAsc);
+}
+
+/** @typedef {{node:string;maxMoney:number;hackChance:number;reqHackLevel:number}} NodeInfoType */
---
+++ bitburner_js/net-fs/utils.js
@@ -0,0 +1,53 @@
+/** @private @template U @template {U} T @arg {U} e @arg {any} [x] @returns {T} */
+export function as(e,x=e) {return x;}
+
+/** @private @template U @template T @arg {U} e @arg {any} [x] @returns {T} */
+export function as_any(e,x=e) {return x;}
+
+/** @template {{}} T @typedef {T extends infer A ? keyof A : never} T_DistributedKeyof */
+/** @template {{}} T @typedef {T_DistributedKeyof<T> extends never?[]:T_DistributedKeyof<T>[]} T_DistributedKeysOf */
+/** @protected @template {{}} T @arg {T} obj @returns {T_DistributedKeysOf<T>} */
+export function get_keys_of(obj) {
+	if(!obj) {
+		debugger;
+	}
+	let rq=Object.keys(obj);
+	/** @private @type {any} */
+	let ra=rq;
+	return ra;
+}
+
+/** @param {NS} ns */
+export function getNetworkNodes(ns) {
+	// Depth first search traversal
+	ns.print("Retrieving all nodes in network");
+	/** @type {Map<string,boolean>} */
+	var visited=new Map;
+	var stack=[];
+	var origin=ns.getHostname();
+	stack.push(origin);
+
+	while(stack.length>0) {
+		var node=stack.pop();
+		if(!node) break;
+		if(!visited.get(node)) {
+			visited.set(node,true);
+			var neighbours=ns.scan(node);
+			for(var i=0;i<neighbours.length;i++) {
+				var child=neighbours[i];
+				if(visited.get(child)) {
+					continue;
+				}
+				stack.push(child);
+			}
+		}
+	}
+	return [...visited.keys()];
+}
+
+/** @param {NS} ns @arg {string} server */
+export function canHack(ns,server) {
+	var pHackLvl=ns.getHackingLevel();
+	var sHackLvl=ns.getServerRequiredHackingLevel(server);
+	return pHackLvl>=sHackLvl;
+}
