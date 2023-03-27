8460e6a817b4d55b06c794e05b7b750e111465d9
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-27 14:02:19 -0700

    u

--- bitburner_js/net-fs/dom/dom-support.js
+++ bitburner_js/net-fs/dom/dom-support.js
@@ -9,6 +9,13 @@ export function query_element(root,selector) {
 	return element;
 }
 /** @param {Element} node */
+export function as_svg_element(node) {
+	if(!(node instanceof SVGElement)) {
+		throw new Error("Wrong type");
+	}
+	return node;
+}
+/** @param {Element} node */
 export function as_html_element(node) {
 	if(!(node instanceof HTMLElement)) {
 		throw new Error("Wrong type");
--- bitburner_js/net-fs/dom/purchase-tor-router.js
+++ bitburner_js/net-fs/dom/purchase-tor-router.js
@@ -1,4 +1,4 @@
-import {as_div_element, as_html_element,query_element} from "/dom/dom-support.js";
+import {as_div_element,as_svg_element,query_element} from "/dom/dom-support.js";
 import {as_any} from "/run/as.js";
 
 /** @param {NS} ns */
@@ -11,6 +11,13 @@ export async function main(ns) {
 	/** @type {HTMLDivElement} */
 	const root_element=as_any(window.root);
 	class DomList {
+		/** @arg {HTMLDivElement} header_element */
+		expand_collapsed_section(header_element) {
+			if(as_svg_element(query_element(header_element,"svg:nth-child(3)")).dataset.testid==="ExpandMoreIcon") {
+				let r=Object.values(header_element)[1];
+				r.onClick();
+			}
+		}
 		/** @arg {HTMLDivElement} root_element */
 		constructor(root_element) {
 			/** @type {HTMLDivElement} */
@@ -20,21 +27,43 @@ export async function main(ns) {
 			/** @type {HTMLUListElement} */
 			const MuiList_root=query_element(MuiPaper_root,"ul.MuiList-root");
 			this.hacking_section_header=as_div_element(MuiList_root.children[0]);
-			if(as_html_element(query_element(this.hacking_section_header,"svg:nth-child(3)")).dataset.testid!=="ExpandMoreIcon") {
-				let r=Object.values(this.hacking_section_header)[1];
-				console.log(r);
-			}
-			this.hacking_section=MuiList_root.children[1];
-			// 2 is a divider
-			MuiList_root.children[2];
-			this.character_section_header=MuiList_root.children[3];
-			this.character_section=MuiList_root.children[4];
-			MuiList_root.children[5];
-			this.world_section_header=MuiList_root.children[6];
-			this.world_section=MuiList_root.children[7];
-			MuiList_root.children[8];
-			this.help_section_header=MuiList_root.children[9];
-			this.help_section=MuiList_root.children[10];
+			this.expand_collapsed_section(this.hacking_section_header);
+			// 2,5,8 are dividers
+			this.character_section_header=as_div_element(MuiList_root.children[3]);
+			this.expand_collapsed_section(this.character_section_header);
+			this.help_section_header=as_div_element(MuiList_root.children[9]);
+			this.expand_collapsed_section(this.help_section_header);
+			this.world_section_header=as_div_element(MuiList_root.children[6]);
+			this.expand_collapsed_section(this.world_section_header);
+
+			const hacking_section=MuiList_root.children[1];
+			const character_section=MuiList_root.children[4];
+			const world_section=MuiList_root.children[7];
+			const help_section=MuiList_root.children[10];
+
+			this.terminal_button=hacking_section.children[0].children[0].children[0];
+			this.script_editor_button=hacking_section.children[0].children[0].children[1];
+			this.active_scripts_button=hacking_section.children[0].children[0].children[2];
+			this.create_program_button=hacking_section.children[0].children[0].children[3];
+
+			this.stats_button=character_section.children[0].children[0].children[0];
+			this.factions_editor_button=character_section.children[0].children[0].children[1];
+			this.augmentations_button=character_section.children[0].children[0].children[2];
+			this.hacknet_button=character_section.children[0].children[0].children[3];
+
+			this.city_button=world_section.children[0].children[0].children[0];
+			this.travel_button=world_section.children[0].children[0].children[1];
+			this.stock_market_button=world_section.children[0].children[0].children[2];
+
+			this.milestones_button=help_section.children[0].children[0].children[0];
+			this.tutorial_button=help_section.children[0].children[0].children[1];
+			this.achievements_button=help_section.children[0].children[0].children[2];
+			this.options_button=help_section.children[0].children[0].children[3];
+
+			this.hacking_section=hacking_section;
+			this.character_section=character_section;
+			this.world_section=world_section;
+			this.help_section=help_section;
 		}
 		async use() {
 			/** @type {any} */
