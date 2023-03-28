2fcbb66cdaf6ef967e839e9e7b886df1a588d2a6
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-27 18:16:40 -0700

    u

--- bitburner_js/net-fs/run/hack-server.js
+++ bitburner_js/net-fs/run/hack-server.js
@@ -22,8 +22,8 @@ export async function main(ns) {
 
 	await ns.sleep(100);
 
-	const width=250;
-	ns.resizeTail(width,0);
+	const width=250+120;
+	ns.resizeTail(width,120);
 	ns.moveTail(window_width-width-4,1);
 
 	const trace=false;
@@ -60,68 +60,71 @@ export async function main(ns) {
 				if(first===void 0) break;
 				await send_reply_msg(ns,write_handle,first);
 			}
-			let msg=await read_call_msg(ns,read_handle);
-			const {call,args}=msg;
-			switch(call) {
-				case "getServerMaxMoney": {
-					let reply=ns.getServerMaxMoney(...args);
-					await send_reply_msg(ns,write_handle,{call,id: args[0],reply});
-				} break;
-				case "getServerMinSecurityLevel": {
-					let reply=ns.getServerMinSecurityLevel(...args);
-					await send_reply_msg(ns,write_handle,{call,id: args[0],reply});
-				} break;
-				case "getServerMoneyAvailable": {
-					let reply=ns.getServerMoneyAvailable(...args);
-					await send_reply_msg(ns,write_handle,{call,id: args[0],reply});
-				} break;
-				case "getServerSecurityLevel": {
-					let reply=ns.getServerSecurityLevel(...args);
-					await send_reply_msg(ns,write_handle,{call,id: args[0],reply});
-				} break;
-				case "get_server": {
-					let reply=get_server(args[0]);
-					await send_reply_msg(ns,write_handle,{call,id: args[0],reply});
-				} break;
-				case "get_hack_target": {
-					if(randomize_hack) {
-						let reply=null;
-						for(;;) {
-							let hostname=hostname_list[rand_num(0,(hostname_list.length-1))];
-							await ns.sleep(100);
-							if(hostname==="home") continue;
-							if(hostname.startsWith("big-")) continue;
-							const scan_results=ns.scan(hostname).filter(v => !hostname_list.includes(v));
-							if(scan_results.length>0) ns.printf("scan: %s %s",hostname,JSON.stringify(scan_results));
-							for(let item of scan_results) get_server(item);
-							let srv=get_server(hostname);
-							if(srv.purchasedByPlayer) continue;
-							if(srv.moneyMax===0) continue;
-							if(srv.maxRam===0) continue;
-							if(srv.hasAdminRights) {
-								reply=srv;
-								break;
-							}
-						}
+			while(!read_handle.empty()) {
+				let msg=await read_call_msg(ns,read_handle);
+				const {call,args}=msg;
+				switch(call) {
+					case "getServerMaxMoney": {
+						let reply=ns.getServerMaxMoney(...args);
 						await send_reply_msg(ns,write_handle,{call,id: args[0],reply});
-					} else {
-						let srv;
-						for(let name of ["ecorp","foodnstuff","n00dles"]) {
-							srv=get_server(name);
-							if(srv.hasAdminRights) break;
-							if(srv.openPortCount<srv.numOpenPortsRequired) continue;
-							ns.nuke(name);
+					} break;
+					case "getServerMinSecurityLevel": {
+						let reply=ns.getServerMinSecurityLevel(...args);
+						await send_reply_msg(ns,write_handle,{call,id: args[0],reply});
+					} break;
+					case "getServerMoneyAvailable": {
+						let reply=ns.getServerMoneyAvailable(...args);
+						await send_reply_msg(ns,write_handle,{call,id: args[0],reply});
+					} break;
+					case "getServerSecurityLevel": {
+						let reply=ns.getServerSecurityLevel(...args);
+						await send_reply_msg(ns,write_handle,{call,id: args[0],reply});
+					} break;
+					case "get_server": {
+						let reply=get_server(args[0]);
+						await send_reply_msg(ns,write_handle,{call,id: args[0],reply});
+					} break;
+					case "get_hack_target": {
+						if(randomize_hack) {
+							let reply=null;
+							for(;;) {
+								let hostname=hostname_list[rand_num(0,(hostname_list.length-1))];
+								await ns.sleep(100);
+								if(hostname==="home") continue;
+								if(hostname.startsWith("big-")) continue;
+								const scan_results=ns.scan(hostname).filter(v => !hostname_list.includes(v));
+								if(scan_results.length>0) ns.printf("scan: %s %s",hostname,JSON.stringify(scan_results));
+								for(let item of scan_results) get_server(item);
+								let srv=get_server(hostname);
+								if(srv.purchasedByPlayer) continue;
+								if(srv.moneyMax===0) continue;
+								if(srv.maxRam===0) continue;
+								if(srv.hasAdminRights) {
+									reply=srv;
+									break;
+								}
+							}
+							await send_reply_msg(ns,write_handle,{call,id: args[0],reply});
+						} else {
+							let srv;
+							for(let name of ["ecorp","foodnstuff","n00dles"]) {
+								srv=get_server(name);
+								if(srv.hasAdminRights) break;
+								if(srv.openPortCount<srv.numOpenPortsRequired) continue;
+								ns.nuke(name);
+							}
+							if(!srv) srv=get_server("n00dles");
+							await send_reply_msg(ns,write_handle,{call,id: args[0],reply: srv});
 						}
-						if(!srv) srv=get_server("n00dles");
-						await send_reply_msg(ns,write_handle,{call,id: args[0],reply: srv});
-					}
-				} break;
+					} break;
+				}
+				while(!log_handle.empty()) {
+					let res=log_handle.read();
+					ns.printf("%s",res);
+				}
+				if(trace) ns.print(msg);
+				console.log(msg);
 			}
-			while(!log_handle.empty()) {
-				let res=log_handle.read();
-				ns.printf("%s",res);
-			}
-			if(trace) ns.print(msg);
 		}
 	}
 	await process_messages();
