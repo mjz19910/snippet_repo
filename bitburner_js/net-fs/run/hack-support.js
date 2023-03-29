a337e12c7a5ce587a0c38c052800f01b750b82d8
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-29 02:31:41 -0700

    u

--- bitburner_js/net-fs/run/hack-support.js
+++ bitburner_js/net-fs/run/hack-support.js
@@ -89,6 +89,7 @@ function should_accept(reply,call_,arg0) {
 /** @template {CallMsg["call"]} CallId @arg {HackState} this_ @arg {string} id @arg {CallId} call_id */
 export async function generic_get_call_with_id(this_,id,call_id) {
 	const wait_start_perf=performance.now();
+	console.log("start:generic_get_call_with_id");
 	function perf_diff() {return performance.now()-wait_start_perf;}
 	const request_port=this_.ns.getPortHandle(request_port_id);
 	const reply_port=this_.ns.getPortHandle(reply_port_id);
@@ -113,8 +114,7 @@ export async function generic_get_call_with_id(this_,id,call_id) {
 			if(reply_port.empty()) throw new Error("reply already removed");
 			let msg=await peek_reply_msg(reply_port);
 			if(!should_accept(msg,call_id,id)) {
-				const {reply,...l_msg}=msg;
-				tprint_log(`reject_reply ${i} ${j} ${JSON.stringify(l_msg)}`);
+				tprint_log(`reject_reply ${i} ${j}`);
 				await notify_port4.nextWrite();
 				continue;
 			}
