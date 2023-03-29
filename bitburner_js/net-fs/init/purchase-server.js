543cf007e0a07c70a7840cb14407229c01c46c09
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-29 03:50:26 -0700

    u

--- bitburner_js/net-fs/init/purchase-server.js
+++ bitburner_js/net-fs/init/purchase-server.js
@@ -16,24 +16,12 @@ export async function main(ns) {
 	const purchased_server_limit=ns.getPurchasedServerLimit();
 	const template_changed=false;
 	const s=new InitHackScript(ns,{trace: false,template_changed});
-	/** @arg {(s:string,d:string)=>void} rename_fn @arg {Server} srv @arg {string} new_str */
-	function rename_purchased_server(rename_fn,srv,new_str) {
-		if(srv.hostname===new_str) return;
-		rename_fn(srv.hostname,new_str);
-		let res=ns.renamePurchasedServer(srv.hostname,new_str);
-		if(!res) {
-			ns.printf("%s -> %s",srv.hostname,new_str);
-			ns.exit();
-		}
-		s.rename_server(srv,new_str);
-	}
 	/** @arg {number} prev_ram @arg {number} ram @arg {string[]} hostname_list */
 	async function upgrade_purchased_server_list(prev_ram,ram,hostname_list) {
 		const buy_cost1=ns.getPurchasedServerCost(ram)-prev_ram;
 		for(let hostname of hostname_list) {
 			let cur_server_money=ns.getServerMoneyAvailable("home");
 			if(cur_server_money<buy_cost1) return;
-			let host_parts=hostname.split("-");
 			let srv;
 			if(purchased_server_list.includes(hostname)) {
 				srv=ns.getServer(hostname);
@@ -50,22 +38,16 @@ export async function main(ns) {
 				ns.scp(s.scripts,new_host);
 				srv=ns.getServer(new_host);
 			}
-			rename_purchased_server((a1,a2) => {
-				let idx=hostname_list.indexOf(a1);
-				hostname_list[idx]=a2;
-				idx=purchased_server_list.indexOf(a1);
-				purchased_server_list[idx]=a2;
-			},srv,`big-${ram}-${host_parts[2]}`);
 			await s.start_script_template(srv);
 		}
 	}
 	server_hostname_list=ns.getPurchasedServers();
 	/** @param {string[]} a @param {number} limit */
 	function add_new_purchased_servers(a,limit) {
-		if(a.length!==limit) for(let i=0;i<limit;i++) a.push(`big-${ram}-${i}`);
+		if(a.length!==limit) for(let i=0;i<limit;i++) a.push(`pserv-${i}`);
 	}
 	add_new_purchased_servers(server_hostname_list,purchased_server_limit);
-	if(server_hostname_list.length!==purchased_server_limit) for(let i=0;i<purchased_server_limit;i++) server_hostname_list.push(`big-${ram}-${i}`);
+	if(server_hostname_list.length!==purchased_server_limit) for(let i=0;i<purchased_server_limit;i++) server_hostname_list.push(`pserv-${i}`);
 	await upgrade_purchased_server_list(0,ram,server_hostname_list);
 	/** @arg {string} hostname */
 	function get_ram(hostname) {return ns.getServerMaxRam(hostname);}
