12d6f3873fe74b4efd2d6dcbd8d96dab5621b2c4
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-30 12:35:00 -0700

    u

---
+++ bitburner_js/NetscriptExtra.d.ts
@@ -0,0 +1,5 @@
+interface NS {
+	heart: {
+		break(): number;
+	};
+}
---
+++ bitburner_js/net-fs/get-karma.js
@@ -0,0 +1,6 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	let karma=ns.heart.break();
+	ns.tprint("karma: ",karma);
+	
+}
