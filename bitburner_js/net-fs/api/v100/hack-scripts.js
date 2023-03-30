5a7b783005b79ac6aadf3c6c3699231a8842ae4e
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-30 15:31:11 -0700

    u

--- bitburner_js/net-fs/api/v100/hack-scripts.js
+++ bitburner_js/net-fs/api/v100/hack-scripts.js
@@ -1,7 +1,7 @@
-export const hack_template="/api/hack-template.js";
-export const hack_support="/api/v1.00/hack-support.js";
-export const hack_server="/api/hack-server.js";
-export const remote_share="/init/remote-share.js";
-export const share_script="/api/share.js";
+export const hack_template="/api/v100/hack-template.js";
+export const hack_support="/api/v100/hack-support.js";
+export const hack_server="/api/v100/hack-server.js";
+export const remote_share="/api/v100/remote-share.js";
+export const share_script="/api/v100/share.js";
 export const host_scan_list_file="/data/host_scan.list.txt";
 export const backdoor_list_file="/data/backdoor_list.txt";
