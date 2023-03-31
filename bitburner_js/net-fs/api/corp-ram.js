add43f3519f478f4052dcb8de02eb1e132ed0404
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-31 13:47:52 -0700

    u

--- bitburner_js/net-fs/api/corp-ram.js
+++ bitburner_js/net-fs/api/corp-ram.js
@@ -1,10 +1,10 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	ns.tail();
-	let corporation_=ns.corporation;
-	let my_corp=corporation_["getCorporation"]();
+	let corporation=ns.corporation;
+	let my_corp=corporation.getCorporation();
 	let div_name=my_corp.divisions[0];
-	let division=corporation_["getDivision"](my_corp.divisions[0]);
-	let product=corporation_["getProduct"](div_name,division.products[0]);
+	let division=corporation.getDivision(my_corp.divisions[0]);
+	let product=corporation.getProduct(div_name,division.products[0]);
 	ns.print(product);
 }
