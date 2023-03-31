ed5c1e3bf4bc9ccb15617928c8b7001a6fb31198
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-31 02:00:24 -0700

    u

--- bitburner_js/net-fs/api/v100/hack-support.js
+++ bitburner_js/net-fs/api/v100/hack-support.js
@@ -229,7 +229,6 @@ export function generic_get_call(this_,call_id) {
 }
 /** @arg {HackState} this_ @arg {Extract<ReplyMsg,{reply:number}>["call"]} call_id */
 async function memoed_get_call_ret_number(this_,call_id) {
-	if(!netscript_lock.locked) await netscript_lock.lock();
 	let prev_ret=memoized_number.get(call_id);
 	if(prev_ret!==void 0) {
 		fill_port_handle_cache(this_.ns);
@@ -239,10 +238,8 @@ async function memoed_get_call_ret_number(this_,call_id) {
 		})();
 		return prev_ret;
 	}
-	if(netscript_lock.locked) netscript_lock.unlock();
 	let memoized_ret=await generic_get_call(this_,call_id);
 	memoized_number.set(call_id,memoized_ret);
-	if(!netscript_lock.locked) await netscript_lock.lock();
 	return memoized_ret;
 
 }
