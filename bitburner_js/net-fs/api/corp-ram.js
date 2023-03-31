1c6d731d18f783c622b67268ca100e0cf0297d73
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-31 14:05:46 -0700

    u

--- bitburner_js/net-fs/api/corp-ram.js
+++ bitburner_js/net-fs/api/corp-ram.js
@@ -7,5 +7,25 @@ export async function main(ns) {
 	let div_name=my_corp.divisions[0];
 	let division=corporation.getDivision(my_corp.divisions[0]);
 	let product=corporation.getProduct(div_name,division.products[0]);
-	ns.print(product);
+	log_product(product);
+	/** @arg {Product} product */
+	function log_product(product) {
+		ns.print(product.name,".cityData.sec_12.stock: ",product.cityData["Sector-12"][0]);
+		ns.print(product.name,".cityData.sec_12.prod_rate: ",product.cityData["Sector-12"][1]);
+		ns.print(product.name,".cityData.sec_12.sell_rate: ",product.cityData["Sector-12"][2]);
+	}
+	product=corporation.getProduct(div_name,division.products[1]);
+	let prod1=product.cityData["Sector-12"][1];
+	let sell1=product.cityData["Sector-12"][2];
+	let m={
+		1.275: -0.019310254038757524,
+		1.276: 0.006892609109019565,
+	};
+	m;
+	let diff1_1=0.01; diff1_1;
+	let diff1_2=0.001; diff1_2;
+	let diff2=(prod1-sell1)+0.78;
+	ns.print("d2 ",diff2);
+	ns.print("pd ",prod1-sell1);
+	log_product(product);
 }
