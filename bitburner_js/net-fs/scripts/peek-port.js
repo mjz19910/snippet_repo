9d72be53c1e7c5922bd4067080d6266a27c03463
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-30 19:40:16 -0700

    u

--- bitburner_js/net-fs/scripts/peek-port.js
+++ bitburner_js/net-fs/scripts/peek-port.js
@@ -1,22 +1,36 @@
+import {ObjectPort} from "/api/v100/hack-support";
+
 /** @param {NS} ns */
 export async function main(ns) {
 	ns.tail();
 	ns.clearLog();
+	/** @typedef {{id:"link",data:number,next:LinkType|null}} LinkType */
 	/** @param {number} port */
 	function debug_port_handle(port) {
 		let delayed_messages=[];
-		let handle=ns.getPortHandle(port);
+		/** @type {ObjectPort<LinkType>} */
+		let handle=ObjectPort.getPortHandle(ns,port);
 		if(handle.empty()) {
 			ns.print(port,": empty");
 			return;
 		}
 		while(!handle.empty()) {
-			delayed_messages.push(handle.read());
+			let res=handle.read();
+			if(res===null) break;
+			delayed_messages.push(res);
 		}
-		ns.print(port,": ",delayed_messages.map(v => {
-			if(typeof v==="string") return JSON.parse(v);
-			return v;
-		}));
+		let delayed_messages_all=[];
+		let stack=[delayed_messages];
+		while(stack.length>0) {
+			let arr=stack.pop();
+			if(arr===void 0) break;
+			for(let item of arr) {
+				delayed_messages_all.push(item.data);
+				if(item.next===null) continue;
+				stack.push([item.next]);
+			}
+		}
+		ns.print(port,": ",delayed_messages_all);
 		for(let msg of delayed_messages) {
 			let removed_msg=handle.write(msg);
 			if(removed_msg!==null) ns.print("dropped: ",removed_msg);
