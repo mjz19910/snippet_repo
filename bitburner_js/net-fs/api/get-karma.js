2a49f7027c5eac7677f9e274e3e22829a38b78e6
Matthias Zimmerman <matthias291999@gmail.com>
2023-03-31 13:00:19 -0700

    u

--- bitburner_js/net-fs/api/get-karma.js
+++ bitburner_js/net-fs/api/get-karma.js
@@ -3,13 +3,10 @@ export async function main(ns) {
 	let karma=ns.heart.break();
 	ns.tail();
 	ns.tprintf("karma: %s",karma);
-	// let bypass_=get_func("bypass")(make_state("bypass"));
 	// ns.alterReality();
-	// bypass_(globalThis["window"]["document"]);
-	// let exploit_=get_func2("exploit");
-	// exploit_();
-	// let rainbow_=get_func2("rainbow");
-	// rainbow_("noodles");
+	// ns.bypass(globalThis["window"]["document"]);
+	// ns.exploit();
+	// ns.rainbow("noodles");
 	// ns.iKnowWhatImDoing();
 	// if(ns.tprintRaw) ns.tprintRaw("test");
 	// ns.openDevMenu();
