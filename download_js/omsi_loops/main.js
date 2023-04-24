5fa078149b3cd4ad2d5b24f587453408f48a6fec
Matthias Zimmerman <matthias291999@gmail.com>
2023-04-24 10:02:03 -0700

    u

--- download_js/omsi_loops/main.js
+++ download_js/omsi_loops/main.js
@@ -2,6 +2,24 @@
 	let res=null;
 	class OutputResult {
 		result=res;
+		use(val_any) {
+			val_any;
+		}
 	}
-	return new OutputResult;
+	let ret=new OutputResult;
+	ret.use({
+		setup: [{
+			pot: 50
+		}],
+		used: {
+			mana: 2_436,
+			time: "9.75s",
+		},
+		result: {
+			mana: "13.7k",
+			gold: 0,
+			reputation: 10,
+		},
+	});
+	return ret;
 })();
