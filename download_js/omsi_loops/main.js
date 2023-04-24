0b0f51ba834f34f1d7909849a210c6f30fadb2f0
Matthias Zimmerman <matthias291999@gmail.com>
2023-04-24 10:06:27 -0700

    u

--- download_js/omsi_loops/main.js
+++ download_js/omsi_loops/main.js
@@ -3,7 +3,7 @@
 	class OutputResult {
 		result=res;
 		/**
-		 * @param {{ setup: { pot: number; }[]; used: { mana: number; time: string; }; result: { mana: number; gold: number; time: string; }; }} val_any
+		 * @param {{ setup: ({ pot: number; }|{pick_locks:number}|{buy_mana:number})[]; used: { mana: number; time: string; }; result: { mana: number; gold: number; time: string; }; }} val_any
 		 */
 		use(val_any) {
 			val_any;
@@ -14,6 +14,22 @@
 		setup: [{
 			pot: 50
 		}],
+		used: {
+			mana: 2_436,
+			time: "9.75s",
+		},
+		result: {
+			mana: 2_813,
+			gold: 0,
+			time: "11.3s",
+		},
+	});
+	ret.use({
+		setup: [
+			{pot: 50},
+			{pick_locks: 7},
+			{buy_mana: 1},
+		],
 		used: {
 			mana: 2_436,
 			time: "9.75s",
