054a516732b963194ee4dde9e18b3e40d127be63
Matthias Zimmerman <matthias291999@gmail.com>
2023-09-10 20:29:22 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/ghi/group_G.ts
+++ userscript/youtube_plugin_raw/yt_json_types/ghi/group_G.ts
@@ -2,26 +2,24 @@ import {T_Split} from "../../support_0_mod/T_Split.mod.js";
 import {Join} from "../../support_1/Join.js";
 import {AU_ChannelSwitcherPage,AU_NotificationsUnseenCount,AU_SubscribeButton,A_AccountItem,A_AddChatItem,A_AddToGuideSection,A_ChangeEngagementPanelVisibility,A_GetMultiPageMenu,A_RemoveFromGuideSection,A_ReplayChatItem,A_SendFeedback,A_ShowEngagementPanelScrim,A_Signal} from "../abc/A.js";
 import {B_GenericResponseType} from "../abc/B.js";
-import {CD_Invalidation, CD_LiveChatReplay, CD_PlayerSeek, C_AddToPlaylist,C_RefreshPlaylist,C_RunAttestation,C_ScrollToEngagementPanel} from "../abc/C.js";
+import {CD_Invalidation,CD_LiveChatReplay,CD_PlayerSeek,C_AddToPlaylist,C_RefreshPlaylist,C_RunAttestation,C_ScrollToEngagementPanel} from "../abc/C.js";
 import {D_Accessibility,D_EY_Offlineability,D_NotificationTopbarButton,D_TextRun,D_WatchPlaylistUrlFormat} from "../d/group_D.js";
 import {DE_AdditionalDatas,DE_BucketIdentifier,DE_MutationDelete,DE_MutationReplace} from "../d/group_DE.js";
 import {DU_Playlist_Id,D_PlayerParamsUrl,S_acv1_codec} from "../d/mod_D/DU_T/DU.js";
 import {R_Button} from "../d/mod_D/D_T/D_Button.js";
 import {E_AddToPlaylistService,E_Feedback,E_GetReportForm,E_PlaylistEdit,E_ShareEntityService,E_ShowEngagementPanel,E_SignalService_SendPost} from "../e/E.js";
+import {make_item_group} from "../m/make_item_group.js";
 import {A_ClientSignal,TA_OpenPopup_Empty,TR_MultiPageMenu_Empty} from "../nop_q/Popup.js";
-import {NavFinishDetail_Watch,RA_Notification,R_AccountItem,R_AdBreakService,R_AdSlot,R_AutomixPreviewVideo,R_BrowseFeedActions,R_C4TabbedHeader,R_ChannelOptions,R_Channel_MD,R_CheckboxSurveyOption,R_ClientForecastingAd,R_CommentThread,R_CommentsHeader,R_CompactLink,R_CompactPlaylist,R_CompactRadio,R_CompactVideo,R_ConfirmDialog,R_ContinuationItem,R_CopyLink,R_EndScreenPlaylist,R_EndScreenVideo,R_ExpandableVideoDescriptionBody,R_FeedFilterChipBar,R_FeedNudge,R_FeedTabbedHeader,R_GuideCollapsibleEntry,R_GuideCollapsibleSectionEntry,R_GuideDownloadsEntry,R_GuideEntry,R_GuideSection,R_GuideSubscriptionsSection,R_HorizontalCardList,R_InlineSurvey,R_InstreamVideoAd,R_LinearAdSequence,R_LiveChatPlaceholderItem,R_LiveChatTextMessage,R_LiveChatViewerEngagementMessage,R_Menu,R_MenuNavigationItem,R_MenuServiceItem,R_NotificationTopbarButton,R_PdgBuyFlow,R_PlaylistPanelVideo,R_PlaylistSidebar,R_PlaylistSidebarPrimaryInfo,R_PlaylistSidebarSecondaryInfo,R_Playlist_MD,R_ProfileColumn,R_ProfileColumnStats,R_ProfileColumnUserInfo,R_Radio,R_RadioButtonSurveyOption,R_RichItem,R_RichSection,R_RichShelf,R_SearchBox,R_SettingsCheckbox,R_SettingsRadioOption,R_SettingsSidebar,R_SettingsSwitch,R_SingleColumnMusicWatchNextResults,R_SortFilterSubMenu,R_SourcePivotHeader,R_SubFeedSelector,R_ThumbnailOverlayBottomPanel,R_ThumbnailOverlayEndorsement,R_ThumbnailOverlayHoverText,R_ThumbnailOverlayLoadingPreview,R_ThumbnailOverlayNowPlaying,R_ThumbnailOverlayResumePlayback,R_ThumbnailOverlaySidePanel,R_ThumbnailOverlayTimeStatus,R_ThumbnailOverlayToggleButton,R_ToggleMenuServiceItem,R_TopbarMenuButton,R_TwoColumnBrowseResults,R_UnifiedSharePanel,R_Video,R_VideoDescriptionHeader,R_VideoDescriptionMusicSection,R_VoiceSearchDialog} from "../r/group_R.js";
-import {RS_PlaylistPage,RS_VE5754_Page_Playlist} from "../r/RS.js";
+import {NavFinishDetail_Watch,RA_Notification,RS_PlaylistPage,RS_VE5754_Page_Playlist,R_AccountItem,R_AdBreakService,R_AdSlot,R_AutomixPreviewVideo,R_BrowseFeedActions,R_C4TabbedHeader,R_ChannelOptions,R_Channel_MD,R_CheckboxSurveyOption,R_ClientForecastingAd,R_CommentThread,R_CommentsHeader,R_CompactLink,R_CompactPlaylist,R_CompactRadio,R_CompactVideo,R_ConfirmDialog,R_ContinuationItem,R_CopyLink,R_EndScreenPlaylist,R_EndScreenVideo,R_ExpandableVideoDescriptionBody,R_FeedFilterChipBar,R_FeedNudge,R_FeedTabbedHeader,R_GuideCollapsibleEntry,R_GuideCollapsibleSectionEntry,R_GuideDownloadsEntry,R_GuideEntry,R_GuideSection,R_GuideSubscriptionsSection,R_HorizontalCardList,R_InlineSurvey,R_InstreamVideoAd,R_LinearAdSequence,R_LiveChatPlaceholderItem,R_LiveChatTextMessage,R_LiveChatViewerEngagementMessage,R_Menu,R_MenuNavigationItem,R_MenuServiceItem,R_NotificationTopbarButton,R_PdgBuyFlow,R_PlaylistPanelVideo,R_PlaylistSidebar,R_PlaylistSidebarPrimaryInfo,R_PlaylistSidebarSecondaryInfo,R_Playlist_MD,R_ProfileColumn,R_ProfileColumnStats,R_ProfileColumnUserInfo,R_Radio,R_RadioButtonSurveyOption,R_RichItem,R_RichSection,R_RichShelf,R_SearchBox,R_SettingsCheckbox,R_SettingsRadioOption,R_SettingsSidebar,R_SettingsSwitch,R_SingleColumnMusicWatchNextResults,R_SortFilterSubMenu,R_SourcePivotHeader,R_SubFeedSelector,R_ThumbnailOverlayBottomPanel,R_ThumbnailOverlayEndorsement,R_ThumbnailOverlayHoverText,R_ThumbnailOverlayLoadingPreview,R_ThumbnailOverlayNowPlaying,R_ThumbnailOverlayResumePlayback,R_ThumbnailOverlaySidePanel,R_ThumbnailOverlayTimeStatus,R_ThumbnailOverlayToggleButton,R_ToggleMenuServiceItem,R_TopbarMenuButton,R_TwoColumnBrowseResults,R_UnifiedSharePanel,R_Video,R_VideoDescriptionHeader,R_VideoDescriptionMusicSection,R_VoiceSearchDialog} from "../r/group_R.js";
 import {NavFinishDetail_Browse} from "../r/r_sub/n/NavFinishDetail_Browse.js";
 import {NavFinishDetail_Channel} from "../r/r_sub/n/NavFinishDetail_Channel.js";
 import {NavFinishDetail_Playlist} from "../r/r_sub/n/NavFinishDetail_Playlist.js";
 import {NavFinishDetail_Search} from "../r/r_sub/n/NavFinishDetail_Search.js";
 import {NavFinishDetail_Settings} from "../r/r_sub/n/NavFinishDetail_Settings.js";
 import {NavFinishDetail_Shorts} from "../r/r_sub/n/NavFinishDetail_Shorts.js";
-import {SI_DB_EngagementPanel_Ads,SI_DB_EngagementPanel_ClipCreate,SI_DB_EngagementPanel_MacroMarkers_AutoChapters,SI_DB_EngagementPanel_MacroMarkers_DescriptionChapters} from "../stu/mod/group_SI.js";
+import {SI_DB_EngagementPanel_Ads,SI_DB_EngagementPanel_ClipCreate,SI_DB_EngagementPanel_MacroMarkers_AutoChapters,SI_DB_EngagementPanel_MacroMarkers_DescriptionChapters,Signal_GetNotificationsMenu} from "../stu/group_S.js";
 import {T_SplitOnce,T_UserFeedbackEndpointProductSpecificValueData} from "../stu/group_T.js";
-import {make_item_group} from "../m/make_item_group.js";
-import {Signal_GetNotificationsMenu} from "../stu/group_S.js";
-import {WD_account_account_menu,WD_account_set_setting,WD_accounts_list,WD_att_get,WD_att_log,WD_browse,WD_browse_edit_playlist,WD_feedback,WD_get_notification_menu,WD_get_survey,WD_get_transcript,WD_GetAccountSwitcherEndpoint,WD_getDatasyncIdsEndpoint,WD_guide,WD_like_dislike,WD_like_like,WD_like_removelike,WD_live_chat_get_live_chat,WD_live_chat_get_live_chat_replay,WD_music_get_search_suggestions,WD_next,WD_notification_get_unseen_count,WD_notification_modify_channel_preference,WD_notification_record_interactions,WD_pdg_get_pdg_buy_flow,WD_player,WD_playlist_get_add_to_playlist,WD_reel_reel_item_watch,WD_reel_reel_watch_sequence,WD_search,WD_share_get_share_panel,WD_subscription_subscribe,WD_subscription_unsubscribe,WD_update_metadata} from "../vw/group_WD.js";
+import {WD_GetAccountSwitcherEndpoint,WD_account_account_menu,WD_account_set_setting,WD_accounts_list,WD_att_get,WD_att_log,WD_browse,WD_browse_edit_playlist,WD_feedback,WD_getDatasyncIdsEndpoint,WD_get_notification_menu,WD_get_survey,WD_get_transcript,WD_guide,WD_like_dislike,WD_like_like,WD_like_removelike,WD_live_chat_get_live_chat,WD_live_chat_get_live_chat_replay,WD_music_get_search_suggestions,WD_next,WD_notification_get_unseen_count,WD_notification_modify_channel_preference,WD_notification_record_interactions,WD_pdg_get_pdg_buy_flow,WD_player,WD_playlist_get_add_to_playlist,WD_reel_reel_item_watch,WD_reel_reel_watch_sequence,WD_search,WD_share_get_share_panel,WD_subscription_subscribe,WD_subscription_unsubscribe,WD_update_metadata} from "../vw/group_WD.js";
 
 //#region G_RS
 export type G_RS_Page_Playlist=RS_PlaylistPage|RS_VE5754_Page_Playlist;
@@ -426,7 +424,7 @@ export type DST_LoadId={
 export type G_BoxedDatabaseData=DST_SaveId|DST_LoadId|DST_Group;
 //#endregion
 export type G_BoxedInner=[1,any];
-	;
+;
 ;
 export type Ret_w_dst=
 	|[
