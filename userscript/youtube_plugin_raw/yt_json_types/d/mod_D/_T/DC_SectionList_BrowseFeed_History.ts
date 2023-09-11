13ff517dcd0cfdf6daeb4831c7f1b061945700d8
Matthias Zimmerman <matthias291999@gmail.com>
2023-09-10 21:10:51 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/d/mod_D/_T/DC_SectionList_BrowseFeed_History.ts
+++ userscript/youtube_plugin_raw/yt_json_types/d/mod_D/_T/DC_SectionList_BrowseFeed_History.ts
@@ -1,3 +1,4 @@
+import {R_TextHeader} from "../../../r/r_sub/r/R_TextHeader.js";
 import {TR_SectionListItem_3_Empty} from "../../../stu/group_T.js";
 
 export type DC_SectionList_BrowseFeed_History={
