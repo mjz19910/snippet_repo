9e928149fa825b01336fa1732513721fea62bcd0
Matthias Zimmerman <matthias291999@gmail.com>
2023-09-10 21:33:43 -0700

    u

--- userscript/youtube_plugin_raw/support_1/url_parse/UrlParse.ts
+++ userscript/youtube_plugin_raw/support_1/url_parse/UrlParse.ts
@@ -1,5 +1,4 @@
+import {UrlParseErr} from "./UrlParseErr.js";
 import {UrlParseImpl} from "./UrlParseImpl.js";
 
-export type UrlParse<T extends (`https://${string}`|`http://${string}`)>=
-	UrlParseImpl<T> extends never? UrlParseErr<T>:UrlParseImpl<T>;
-;
+export type UrlParse<T extends (`https://${string}`|`http://${string}`)>=UrlParseImpl<T> extends never? UrlParseErr<T>:UrlParseImpl<T>;
