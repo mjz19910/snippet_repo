bf878b31473c8e4ece693df4885cbff943a1aebc
Matthias Zimmerman <matthias291999@gmail.com>
2023-09-10 20:27:27 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/vw/group_V.ts
+++ userscript/youtube_plugin_raw/yt_json_types/vw/group_V.ts
@@ -1,3 +1,5 @@
+import {D_ProtobufObj} from "../d/group_D.js";
+
 export type V_ParamMapValue=number|string|Uint8Array|['bigint',number[],bigint]|['group',D_ProtobufObj[]]|["failed",D_ProtobufObj[]|null]|V_ParamMapType;
 export type V_ParamMapType=Map<number,V_ParamMapValue[]>;
 export type V_Bigint<T extends bigint>=T;
