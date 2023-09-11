25634095795758f4fd4314e6403aa859b7209d9e
Matthias Zimmerman <matthias291999@gmail.com>
2023-09-10 21:09:02 -0700

    u

--- userscript/youtube_plugin_raw/yt_json_types/abc/C.ts
+++ userscript/youtube_plugin_raw/yt_json_types/abc/C.ts
@@ -1,13 +1,9 @@
 //#region group_C
-
 import {D_RunAttestation,D_TimestampWithNanos,D_UiTargetId} from "../d/group_D.js";
 import {DC_AddToPlaylist,DC_AdsControlFlowOpportunityReceived,DC_ChangeKeyedMarkersVisibility,DC_EngagementPanelHeaderShowNavigationButton,DC_Generic_CTP,DC_Invalidation,DC_Params,DC_ReloadContinuationItems,DC_RepeatChapter} from "../d/group_DC.js";
-import {D_UserIdStr,T_IdTemplate} from "../d/mod_D/DU_T/DU.js";
-import {G_DC_CommandExecutor_CommandItem,G_DC_Innertube} from "../ghi/_group.mod/G_DC.js";
 import {G_DE_MutationItem,G_FollowUpOption,G_Text} from "../ghi/group_G.js";
 import {M_Browse,M_GetPdgBuyFlow,M_GetSurvey,M_Next} from "../m/M.js";
 import {R_ContinuationItem} from "../r/group_R.js";
-import {SI_VE76278_EngagementPanel} from "../stu/mod/group_SI.js";
 import {TE_Endpoint_2,TE_Endpoint_3,TE_Endpoint_Opt_3,TR_ItemSection_3,TR_SectionListItem_3_Empty,T_DC_Content,T_DC_Content_2} from "../stu/group_T.js";
 import {A_GetSurvey} from "./A.js";
 
@@ -95,10 +91,10 @@ export type CD_Reload={reloadContinuationData: DC_Reload;};
 export type CD_TimedContinuation={timedContinuationData: DC_Timed;};
 //#endregion CD
 //#region CF
-import {CF_GetAutoTypename as CF_GetAutoTypename,CF_ShortTypeName} from "../_rtv_wrong/Ret_get_auto_type_name.js";
+import {CF_GetAutoTypename,CF_ShortTypeName} from "../_rtv_wrong/Ret_get_auto_type_name.js";
 import {Ret_json_auto_replace_1} from "../_rtv_wrong/Ret_json_auto_replace_1.js";
-import {T_ExtractImport,T_Split} from "../stu/group_T.js";
 import {P_LogItems} from "../nop_q/P_LogItems.js";
+import {T_ExtractImport,T_Split} from "../stu/group_T.js";
 
 export type CF_add_string_to_map=T_ExtractImport<"CF_add_string_to_map">|"CF_add_string_to_map";
 export type CF_D_CaseGen=T_ExtractImport<"CF_D_CaseGen">|"CF_D_CaseGen";
