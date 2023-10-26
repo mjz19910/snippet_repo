586cfa7715826b69df324a1f866d05c696a21203
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-26 16:46:56 -0700

    u

--- browser_js/modules/rebuild_the_universe/rebuild_the_universe.ts
+++ browser_js/modules/rebuild_the_universe/rebuild_the_universe.ts
@@ -1463,12 +1463,12 @@ class DocumentWriteList {
 	}
 }
 class NamedIdGenerator {
-	state_map: Map<any,any>;
+	state_map: Map<string,number>;
 	constructor() {
 		this.state_map=new Map;
 	}
 	current_named(name: string) {
-		let val=this.state_map.get(name);
+		const val=this.state_map.get(name);
 		if(val) {
 			return val;
 		} else {
@@ -1477,14 +1477,14 @@ class NamedIdGenerator {
 	}
 
 	next_named(name: string) {
-		if(this.state_map.has(name)) {
-			let state_item=this.state_map.get(name)+1;
-			this.state_map.set(name,state_item);
-			return state_item;
+		let state_item=this.state_map.get(name);
+		if(state_item!==void 0) {
+			state_item=state_item+1;
 		} else {
-			this.state_map.set(name,1);
-			return 1;
+			state_item=1;
 		}
+		this.state_map.set(name,state_item);
+		return state_item;
 	}
 }
 class EventHandlerDispatch {
