d06601d07b420d3d107e139448ce6f717fe9608d
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-26 01:25:01 -0700

    u

---
+++ download_js/iwgh/main.js
@@ -0,0 +1,47 @@
+function parse_sentence() {
+}
+function split_at(str,needle) {
+	let idx=str.indexOf(needle);
+	if(idx===-1) return [str];
+	let n_len=needle.length;
+	let arr=[];
+	let sp=str.slice(0,idx+n_len);
+	arr.push(sp);
+	console.log(sp);
+	return arr;
+}
+async function run() {
+	let dict=new Set;
+	let description_set=new Set;
+	let arr=[];
+	for(let j=0;j<1;j++) {
+		const request_count=1;
+		for(let i=0;i<request_count;i++) {
+			async function one_page() {
+				let res=await fetch("https://louigiverona.com/iwgh/?page=dictionary");
+				let rt=await res.text();
+				let start_pos=rt.indexOf("table ",rt.indexOf("table ")+43)+57;
+				let end_pos=rt.indexOf("</table>");
+				rt=rt.slice(start_pos+26,end_pos-10);
+				let page_arr=split_at(rt,"</p>");
+				page_arr=page_arr.map(v => v.slice(3,-4));
+				page_arr.forEach(v => {
+					let [word,description]=v.split(" - ");
+					word=word.slice(3,-4);
+					dict.add(word);
+					description_set.add(description);
+				});
+			}
+			arr.push(one_page());
+		}
+		let before_wait=dict.size;
+		await Promise.all(arr);
+		console.log("dict word num",dict.size-before_wait);
+		arr.length=0;
+	}
+	let description_arr=[...description_set.values()].sort();
+	console.log(description_arr);
+}
+await run();
+
+export {};
