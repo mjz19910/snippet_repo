1610c453141cb8652ade4e261263956d22f9fbf1
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-26 15:48:51 -0700

    u

--- browser_js/group2/item_types.ts
+++ browser_js/group2/item_types.ts
@@ -72,8 +72,7 @@ class AddEventListenerExtension {
 		real_value[key]=`weak_id:${val[this.namespace_key]}:${index}`;
 		return;
 	}
-	/** @private @arg {{}} val */
-	add_object_id(val: {}) {
+	add_object_id(val: Record<never,never>) {
 		if(!(this.namespace_key in val)) throw new Error("Invalid");
 		return this.object_ids.push(new WeakRef(val))-1;
 	}
@@ -166,8 +165,8 @@ class AddEventListenerExtension {
 			ids=[];
 		}
 		list.push(new WeakRef(val));
-		let node_id=this.node_id_max++;
-		let id_holder={value: node_id};
+		const node_id=this.node_id_max++;
+		const id_holder={value: node_id};
 		define_property_as_value(val,"__id_holder",id_holder);
 		if("__id_holder" in val) {
 			val.__id_holder=id_holder;
@@ -176,30 +175,47 @@ class AddEventListenerExtension {
 		this.node_list=new WeakRef(list);
 		return node_id;
 	}
+	capture_this<CallbackType extends ((this: This,...args: TParameters) => TReturnType),TParameters extends unknown[]=Parameters<CallbackType>,This=ThisParameterType<CallbackType>,TReturnType=ReturnType<CallbackType>>(cb: (cl: {
+		this: This;
+	},...args: Parameters<CallbackType>) => TReturnType): CallbackType {
+		return function(this: This,...args: Parameters<CallbackType>) {
+			return cb({this: this},...args);
+		} as CallbackType;
+	}
+	capture_this_2<CallbackType,TParameters extends unknown[],This,TReturnType>(cb: (cl: {
+		this: This;
+	},...args: TParameters) => TReturnType): CallbackType {
+		return function(this: This,...args: TParameters) {
+			return cb({this: this},...args);
+		} as CallbackType;
+	}
 	/** @private @arg {Extract<keyof EventTarget,string>} target */
 	init_overwrite(target: Extract<keyof EventTarget,string>) {
-		let t=this;
 		switch(target) {
 			case "addEventListener":
 				/** @arg {[string,EventListenerOrEventListenerObject,any?]} args */
-				t.target_prototype[target]=function(...args: [string,EventListenerOrEventListenerObject,any?]) {
-					if(api_debug_enabled) t.add_to_call_list([target,this,args]);
-					let original_function=args[1];
-					if(!t.elevated_event_handlers.includes(original_function)) {
-						/** @arg {[evt: Event]} args */
-						args[1]=function(...args: [evt: Event]) {
-							t.eventFireInterceptor(original_function,this,args);
-						};
+				this.target_prototype[target]=(...args: [string,EventListenerOrEventListenerObject,(boolean|AddEventListenerOptions)?]) => {
+					if(api_debug_enabled) this.add_to_call_list([target,this,args]);
+					const original_function=args[1];
+					if(!this.elevated_event_handlers.includes(original_function)) {
+						args[1]=this.capture_this_2<typeof original_function,[evt: Event],InterceptThis,void>((closure,...args: [evt: Event]) => {
+							this.eventFireInterceptor(original_function,closure.this,args);
+						});
 					}
-					return t.original_prototype.addEventListener.call(this,...args);
+					return this.original_prototype.addEventListener.call(this,...args);
 				}; break;
-			case "removeEventListener": t.target_prototype[target]=function(...args) {
-				if(api_debug_enabled) t.add_to_call_list([target,this,args]);
-				return t.original_prototype[target].call(this,...args);
-			}; break;
-			case "dispatchEvent": t.target_prototype[target]=function(...args) {
-				if(api_debug_enabled) t.add_to_call_list([target,this,args]);
-				return t.original_prototype[target].call(this,...args);
+			case "removeEventListener": this.target_prototype[target]=this.capture_this<EventTarget[typeof target]>((closure,...args) => {
+				if(api_debug_enabled) this.add_to_call_list([target,closure.this,args]);
+				return this.original_prototype[target].call(this,...args);
+			}); {
+					const original_prototype=this.original_prototype;
+					this.target_prototype[target]=function(...args) {
+						return original_prototype[target].call(this,...args);
+					};
+				} break;
+			case "dispatchEvent": this.target_prototype[target]=(...args) => {
+				if(api_debug_enabled) this.add_to_call_list([target,this,args]);
+				return this.original_prototype[target].call(this,...args);
 			}; return;
 			default: throw new Error("1");
 		}
@@ -207,11 +223,12 @@ class AddEventListenerExtension {
 	eventFireInterceptor(arg_function: InterceptThis[1],arg_this: InterceptThis,args: [evt: Event]) {
 		if(args[0] instanceof MessageEvent) {
 			/** @type {MessageEvent<unknown>} */
-			let msg_event: MessageEvent<unknown>=args[0];
-			let d=msg_event.data;
+			const msg_event: MessageEvent<unknown>=args[0];
+			const d=msg_event.data;
 			if(typeof d==="object"&&d!==null&&"type" in d) {
-				if(d.type===post_message_connect_message_type) {
-					if(api_debug_enabled) console.log("skip page event handler for "+d.type);
+				const d_type=Reflect.get(d,"type");
+				if(d_type===post_message_connect_message_type) {
+					if(api_debug_enabled) console.log("skip page event handler for "+d_type);
 					return;
 				}
 			}
