69f4f3e59610eabeb7a4a1cb3e8ab56b038f9aab
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-26 03:11:05 -0700

    u

--- download_js/iwgh/main.js
+++ download_js/iwgh/main.js
@@ -125,7 +125,10 @@ function parse_sentence(str) {
 				if(parsed_src.length==0) break x;
 				let word4=next_word(parsed_src);
 				switch(word4) {
-					case ",":
+					case ",": {
+						let word5=next_word(parsed_src);
+						console.log(["w5",word5]);
+					} break x;
 					case "...":
 					case "art":
 					case "but":
