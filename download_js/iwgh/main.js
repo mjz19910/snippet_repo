3ce2362e3c1f611d19e96e5ebdbac1f6b328aa87
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-26 06:48:55 -0700

    u

--- download_js/iwgh/main.js
+++ download_js/iwgh/main.js
@@ -289,6 +289,7 @@ function strip_word_part(word) {
   if (word.startsWith("b")) return strip_vowel("b", word);
   if (word.startsWith("c")) return strip_vowel("c", word);
   if (word.startsWith("ch")) return strip_vowel("ch", word);
+	return word;
 }
 /** @type {Map<string,number>} */
 const rng_word_num_map = new Map();
