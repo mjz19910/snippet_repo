fc9b94b283effd566aa95b5bcbd6942ac801e6cc
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-26 13:41:10 -0700

    u

--- userscript/DebugApi_raw/support/dbg/ConnectFlag.ts
+++ userscript/DebugApi_raw/support/dbg/ConnectFlag.ts
@@ -5,6 +5,22 @@ export enum ConnectFlag_base {
 	Syn=1<<0,
 	Ack=1<<1
 }
-export type ConnectFlag=("syn"|"ack")[];
+export type ConnectFlag={
+	none: true;
+	ack: false;
+	syn: false;
+}|{
+	none: false;
+	ack: false;
+	syn: true;
+}|{
+	none: false;
+	ack: true;
+	syn: false;
+}|{
+	none: false;
+	ack: true;
+	syn: true;
+};
 export type ConnectFlagT=typeof ConnectFlag_base;
 export type RecordKey<T>=Constructor&{key: T;};
