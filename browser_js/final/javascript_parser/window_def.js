ab2bce9a2a831938fcb4806ddaeb8e4a377d41e9
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-27 04:38:51 -0700

    u

--- browser_js/final/javascript_parser/window_def.js
+++ browser_js/final/javascript_parser/window_def.js
@@ -1,7 +1,5 @@
-/** @arg {unknown} v */
-function unknown(v) {return v;}
-let window=cast_as(globalThis);
+import {cast_as} from "./cast_as.js";
 /** @type {Window&typeof globalThis} */
 // deno-lint-ignore prefer-const
-let window=unknown(globalThis);
+let window=cast_as(globalThis);
 export default window;
