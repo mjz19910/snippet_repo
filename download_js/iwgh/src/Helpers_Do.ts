e85f02099a1666fcbe1b05b7f26314917d890b59
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-28 14:17:21 -0700

    u

--- download_js/iwgh/src/Helpers_Do.ts
+++ download_js/iwgh/src/Helpers_Do.ts
@@ -2,10 +2,11 @@ import {H} from "../Helpers.ts";
 
 type PageTypeShape={id: string;};
 export type RunTakeAct<T extends H.ItemShape>={take: T;};
-type ExtractItemTakeAction<I extends number,T extends H.ItemShape>={
-	id: string,
-	action: H.ActionArrExt<{[Z in I]: H.TakeAction<T>;}>;
+type PageWithActShape<T>={
+	id: string;
+	action: T;
 };
+type ExtractItemTakeAction<I extends number,T extends H.ItemShape>=PageWithActShape<H.ActionArrExt<Record<I,H.TakeAction<T>>>>;
 export type RunTakeAct2<U extends ExtractItemTakeAction<I,T>,I extends number,T extends H.ItemShape>={
 	target: U;
 	index: I;
@@ -13,16 +14,23 @@ export type RunTakeAct2<U extends ExtractItemTakeAction<I,T>,I extends number,T 
 };
 export type ActivateRoom<Room extends PageTypeShape,PosAfter=never>={room: Room; pos_after: PosAfter;};
 export type UseInventory<T extends H.ItemShape>={target: T;};
-export type UseAction<U extends {
+export type UseAction<
+	U extends PageWithActShape<H.UseAction<T>>,
+	T extends H.ItemShape
+>={
+	pos: U["id"],target: U["action"];
+};
+type ExtractItemUseAction<
+	I extends number,T extends H.ItemShape
+>={
 	id: string,
-	action: H.UseAction<T>,
-},T extends H.ItemShape>={pos: U["id"],target: U["action"];};
-type ExtractItemUseAction<I extends number,T extends H.ItemShape>={
-	id: string,
-	action: H.ActionArrExt<{[Z in I]: H.UseAction<T>;}>;
+	action: H.ActionArrExt<Record<I,H.UseAction<T>>>;
 };
 
-export type UseAction2<U extends ExtractItemUseAction<I,T>,I extends number,T extends H.ItemShape>={
+export type UseAction2<
+	U extends ExtractItemUseAction<I,T>,
+	I extends number,T extends H.ItemShape
+>={
 	target: U;
 	index: I;
 	use: T;
