e1d9f03bf6bca42a23b2a79c67abec12a7b0874a
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-28 05:15:43 -0700

    u

--- userscript/youtube_plugin_raw/zc_child_modules/a_exports.ts
+++
@@ -1,2 +0,0 @@
-export type ServiceResolver<T,U>=import("./YTPlugin_Base.user.js").ServiceResolver<T,U>;
-
--- userscript/youtube_plugin_raw/zc_child_modules/test_require.ts
+++
@@ -1,6 +0,0 @@
-import {ProcessImport} from "../../base_require_raw/ProcessImport.ts";
-
-export async function imp_test() {
-	const t3: ProcessImport<"./YTPlugin_HandleTypes.user.js">=await import("./YTPlugin_HandleTypes.user.js");
-	t3;
-}
