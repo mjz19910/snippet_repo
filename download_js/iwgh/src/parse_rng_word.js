1e6a8d63e79fde08581805e436b0c0d2c5aa3f80
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-28 17:22:36 -0700

    u

--- download_js/iwgh/src/parse_rng_word.js
+++ download_js/iwgh/src/parse_rng_word.js
@@ -58,6 +58,7 @@ export function save_dictionary(dictionary_file, dictionary_words_arr) {
 }
 export const new_words_set = new Set();
 export const partial_words = new Set();
+const length_limit = 3;
 /**
  * @param {string} word
  * @param {{add_new_words:boolean;destructure_word:boolean}} opts
@@ -73,10 +74,12 @@ export function parse_rng_word(word, opts) {
 		if (r2.rest === "") break;
 		v = r2.rest;
 	}
-	if (word_arr.length >= 4) {
+	const ll = length_limit + 1;
+	if (word_arr.length >= ll) {
 		if (word_arr.at(-2).type === "consonant") {
+			partial_words.add(word_arr.slice(0, -1).map((v) => v.v).join(""));
 			const truncated_word = word_arr.slice(0, -2).map((v) => v.v).join("");
-			if (word_arr.length > 4) {
+			if (word_arr.length > ll) {
 				parse_rng_word(truncated_word, opts);
 			} else {
 				partial_words.add(truncated_word);
