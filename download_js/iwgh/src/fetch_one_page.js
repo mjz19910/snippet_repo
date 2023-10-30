586294dbbabf65771d8594fa39d0543062def2ed
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-29 20:25:07 -0700

    u

--- download_js/iwgh/src/fetch_one_page.js
+++ download_js/iwgh/src/fetch_one_page.js
@@ -49,7 +49,7 @@ function on_poems_page_text(v) {
 	for (const poem_line of poem_lines) {
 		const poem_words = on_poem_line(poem_line);
 		for (const word of poem_words) {
-			parse_rng_word(new ParseRngOpts(word));
+			parse_rng_word(new ParseRngOpts(word, { length_limit: 3 }));
 		}
 	}
 }
