0a2dde4344b672f3c6dc17ca480c5c86bec12186
Matthias Zimmerman <matthias291999@gmail.com>
2023-12-03 11:13:54 -0700

    u

--- browser_js/final/named/idle-mine-remix/idle-mine-remix.js
+++ browser_js/final/named/idle-mine-remix/idle-mine-remix.js
@@ -1,20 +1,46 @@
-if (window.__cint) {
-	window.__message_channel_timers.clear(window.__cint);
-	window.__cint = void 0;
+function start_slow_upgrade_auto() {
+	window.__cint2_arr.push(setInterval(function () {
+		functions.craftPick(functions.getUsedGems());
+		game.upgrades.blacksmith.buy();
+		game.upgrades.blacksmithSkill.buy();
+		game.upgrades.activePower.buy();
+		game.upgrades.idlePower.buy();
+		game.gemUpgrades.blacksmith.buy();
+		game.gemUpgrades.gemMultiply.buy();
+	}, 500));
 }
-if (window.__cint2) {
-	clearInterval(window.__cint2);
-	window.__cint2 = void 0;
-}
-window.__timer_mode = 4;
+(function () {
+	function init() {
+		if (window.__cint2_arr === void 0) {
+			window.__cint2_arr = [];
+		}
+	}
+
+	function reset() {
+		if (window.__cint) {
+			__message_channel_timers.clear(window.__cint);
+			window.__cint = void 0;
+		}
+		if (window.__cint2) {
+			clearInterval(window.__cint2);
+			window.__cint2 = void 0;
+		}
+		for (const v of window.__cint2_arr) {
+			clearInterval(v);
+		}
+	}
+	init();
+	reset();
+})();
+window.__timer_mode = 6;
 switch (window.__timer_mode) {
 	case 1:
-		window.__cint = window.__message_channel_timers.set(function () {
+		window.__cint = __message_channel_timers.set(function () {
 			functions.clickMineObject();
 		});
 		break;
 	case 2:
-		window.__cint = window.__message_channel_timers.set(function () {
+		window.__cint = __message_channel_timers.set(function () {
 			functions.clickMineObject();
 			functions.craftPick(functions.getUsedGems());
 		});
@@ -26,6 +52,16 @@ switch (window.__timer_mode) {
 		break;
 	case 4:
 		break;
-	default:
+	case 5:
+		window.__cint = __message_channel_timers.set(function () {
+			functions.clickMineObject();
+		});
+		start_slow_upgrade_auto();
+		break;
+	case 6:
+		window.__cint2_arr.push(setInterval(function () {
+			functions.clickMineObject();
+		}, 33));
+		start_slow_upgrade_auto();
 		break;
 }
