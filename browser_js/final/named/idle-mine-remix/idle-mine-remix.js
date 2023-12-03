adaeb0b08e6592ca7f494dbb34f53146130bb9e6
Matthias Zimmerman <matthias291999@gmail.com>
2023-12-03 10:06:52 -0700

    u

--- browser_js/final/named/idle-mine-remix/idle-mine-remix.js
+++ browser_js/final/named/idle-mine-remix/idle-mine-remix.js
@@ -1,6 +1,29 @@
 if (window.__cint) {
 	__message_channel_timers.clear(window.__cint);
+	window.__cint = void 0;
 }
-window.__cint = __message_channel_timers.set(function () {
-	functions.clickMineObject();
-});
+if (window.__cint2) {
+	clearInterval(window.__cint2);
+	window.__cint2 = void 0;
+}
+window.__timer_mode = 4;
+switch (window.__timer_mode) {
+	case 1:
+		window.__cint = __message_channel_timers.set(function () {
+			functions.clickMineObject();
+		});
+		break;
+	case 2:
+		window.__cint = __message_channel_timers.set(function () {
+			functions.clickMineObject();
+			functions.craftPick(functions.getUsedGems());
+		});
+		break;
+	case 3:
+		window.__cint2 = setInterval(function () {
+			functions.clickMineObject();
+		}, 100 / 3);
+		break;
+	case 4:
+		break;
+}
