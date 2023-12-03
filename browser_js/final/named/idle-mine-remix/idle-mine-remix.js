d3db74a7a77bbc1932c40f054f2cae988f82e9ab
Matthias Zimmerman <matthias291999@gmail.com>
2023-12-03 12:09:56 -0700

    u

--- browser_js/final/named/idle-mine-remix/idle-mine-remix.js
+++ browser_js/final/named/idle-mine-remix/idle-mine-remix.js
@@ -1,40 +1,41 @@
 // https://veprogames.github.io/idle-mine-remix/
+window.__timer_mode = 6;
 
 function start_slow_upgrade_auto() {
 	window.__cint2_arr.push(setInterval(function () {
 		functions.craftPick(functions.getUsedGems());
+	}, 750));
+	window.__cint2_arr.push(setInterval(function () {
 		game.upgrades.blacksmith.buy();
 		game.upgrades.blacksmithSkill.buy();
 		game.upgrades.activePower.buy();
 		game.upgrades.idlePower.buy();
 		game.gemUpgrades.blacksmith.buy();
 		game.gemUpgrades.gemMultiply.buy();
-	}, 500));
+	}, 250));
 }
-(function () {
-	function init() {
-		if (window.__cint2_arr === void 0) {
-			window.__cint2_arr = [];
-		}
-	}
 
-	function reset() {
-		if (window.__cint) {
-			__message_channel_timers.clear(window.__cint);
-			window.__cint = void 0;
-		}
-		if (window.__cint2) {
-			clearInterval(window.__cint2);
-			window.__cint2 = void 0;
-		}
-		for (const v of window.__cint2_arr) {
-			clearInterval(v);
-		}
+function im_init() {
+	if (window.__cint2_arr === void 0) {
+		window.__cint2_arr = [];
 	}
-	init();
-	reset();
-})();
-window.__timer_mode = 6;
+}
+
+function im_reset() {
+	if (window.__cint) {
+		__message_channel_timers.clear(window.__cint);
+		window.__cint = void 0;
+	}
+	if (window.__cint2) {
+		clearInterval(window.__cint2);
+		window.__cint2 = void 0;
+	}
+	for (const v of window.__cint2_arr) {
+		clearInterval(v);
+	}
+}
+im_init();
+im_reset();
 switch (window.__timer_mode) {
 	case 1:
 		window.__cint = __message_channel_timers.set(function () {
@@ -63,7 +64,7 @@ switch (window.__timer_mode) {
 	case 6:
 		window.__cint2_arr.push(setInterval(function () {
 			functions.clickMineObject();
-		}, 33));
+		}, 0));
 		start_slow_upgrade_auto();
 		break;
 }
