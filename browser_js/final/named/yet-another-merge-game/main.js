72020100aba5e46e13428c5e468aff6741027440
Matthias Zimmerman <matthias291999@gmail.com>
2023-12-04 14:01:59 -0700

    u

--- browser_js/final/named/yet-another-merge-game/main.js
+++ browser_js/final/named/yet-another-merge-game/main.js
@@ -1,6 +1,14 @@
-if (window.__matter_max_upgrades_interval) {
-	clearInterval(window.__matter_max_upgrades_interval);
+function mg_reset() {
+	if (window.__matter_max_upgrades_interval) {
+		clearInterval(window.__matter_max_upgrades_interval);
+	}
 }
-window.__matter_max_upgrades_interval = setInterval(function () {
-	functions.maxUpgrades(game.matter.amount, game.matter.upgrades);
-}, 5 * 1000);
+setTimeout(function () {
+	window.__matter_max_upgrades_interval = setInterval(function () {
+		functions.maxUpgrades(game.matter.amount, game.matter.upgrades);
+	}, 30 * 1000);
+}, 300);
+const automation_obj = {
+	reset: mg_reset,
+};
+export { automation_obj as yet_another_merge_game_automation };
